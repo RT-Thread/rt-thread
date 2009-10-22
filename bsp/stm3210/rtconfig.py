2f55ad6e8925ece3c7534d105f26974fc7f8c7c2
bernard.xiong <bernard.xiong@gmail.com>
2009-10-22 09:44:08 +0000

    move more options to rtconfig.py
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@120 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/stm3210/rtconfig.py
+++ bsp/stm3210/rtconfig.py
@@ -1,12 +1,83 @@
+# component options
+RT_USING_FINSH = True
+RT_USING_DFS = True
+RT_USING_DFS_YAFFS2 = False
+RT_USING_DFS_EFSL = True
+RT_USING_LWIP = True
+
+# toolchains options
 ARCH='arm'
 CPU='stm32'
-CC='armcc'
-TextBase='0x08000000'
+PLATFORM = 'gcc'
+EXEC_PATH = 'd:/SourceryGCC/bin'
+#PLATFORM = 'armcc'
+#EXEC_PATH = 'C:/Keil'
+BUILD = 'debug'
 
-# component configuration
-RT_USING_FINSH = True
-RT_USING_DFS = False
-RT_USING_DFS_YAFFS2 = False
-RT_USING_DFS_EFSL = False
-RT_USING_LWIP = False
-RT_USING_MINILIBC = False
+if PLATFORM == 'gcc':
+    # toolchains
+    PREFIX = 'arm-none-eabi-'
+    CC = PREFIX + 'gcc'
+    AS = PREFIX + 'gcc'
+    AR = PREFIX + 'ar'
+    LINK = PREFIX + 'gcc'
+    TARGET_EXT = 'elf'
+    SIZE = PREFIX + 'size'
+    OBJDUMP = PREFIX + 'objdump'
+    OBJCPY = PREFIX + 'objcopy'
+
+    DEVICE = ' -mcpu=cortex-m3 -mthumb'
+    CFLAGS = DEVICE + ' -DRT_USING_MINILIBC'
+    AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp'
+    LFLAGS = DEVICE + ' -Wl,--gc-sections,-Map=main.elf.map,-cref,-u,Reset_Handler -T stm32_rom.ld'
+
+    CPATH = ''
+    LPATH = ''
+
+    if BUILD == 'debug':
+        CFLAGS += ' -O0 -gdwarf-2'
+        AFLAGS += ' -gdwarf-2'
+    else:
+        CFLAGS += ' -O2'
+
+    RT_USING_MINILIBC = True
+    POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
+
+elif PLATFORM == 'armcc':
+    # toolchains
+    CC = 'armcc'
+    AS = 'armasm'
+    AR = 'armar'
+    LINK = 'armlink'
+    TARGET_EXT = 'axf'
+
+    DEVICE = ' --device DARMSTM'
+    CFLAGS = DEVICE + ' --apcs=interwork'
+    AFLAGS = DEVICE
+    LFLAGS = DEVICE + ' --info sizes --info totals --info unused --info veneers --list rtthread-stm32.map --scatter stm32_rom.sct'
+
+    CFLAGS += ' -I' + EXEC_PATH + '/ARM/RV31/INC'
+    LFLAGS += ' --libpath ' + EXEC_PATH + '/ARM/RV31/LIB'
+
+    EXEC_PATH += '/arm/bin40/'
+
+    if BUILD == 'debug':
+        CFLAGS += ' --dwarf2'
+    else:
+        CFLAGS += ' -O2'
+
+    RT_USING_MINILIBC = False
+    if RT_USING_FINSH:
+        LFLAGS += ' --keep __fsym_* --keep __vsym_*'
+    POST_ACTION = 'fromelf --bin $TARGET --output rtthread.bin \nfromelf -z $TARGET'
+
+elif PLATFORM == 'iar':
+    # toolchains
+    CC = 'armcc'
+    AS = 'armasm'
+    AR = 'armar'
+    LINK = 'armlink'
+
+    CFLAGS = ''
+    AFLAGS = ''
+    LFLAGS = ''
