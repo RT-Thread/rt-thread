7474b055140a3673289e345458fdc19d2203bc7f
bernard.xiong <bernard.xiong@gmail.com>
2010-01-04 00:05:47 +0000

    add example directory.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@279 bbd45198-f89e-11dd-88c7-29a3b14d5316

---
+++ examples/gui/demo_panel.c
@@ -0,0 +1,22 @@
+#include <rtgui/rtgui.h>
+#include <rtgui/rtgui_server.h>
+
+void panel_init(void)
+{
+    rtgui_rect_t rect;
+
+    /* register dock panel */
+    rect.x1 = 0;
+    rect.y1 = 0;
+    rect.x2 = 240;
+    rect.y2 = 25;
+    rtgui_panel_register("info", &rect);
+
+    /* register main panel */
+    rect.x1 = 0;
+    rect.y1 = 25;
+    rect.x2 = 320;
+    rect.y2 = 320;
+    rtgui_panel_register("main", &rect);
+    rtgui_panel_set_default_focused("main");
+}
