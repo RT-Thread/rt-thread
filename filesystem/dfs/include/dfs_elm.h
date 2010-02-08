527b55c138f3ba0ba9d645dee3ae2368c6b20357
bernard.xiong <bernard.xiong@gmail.com>
2010-02-08 14:15:39 +0000

    add elm_init function declaration.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@394 bbd45198-f89e-11dd-88c7-29a3b14d5316

---
+++ filesystem/dfs/include/dfs_elm.h
@@ -0,0 +1,29 @@
+/*
++------------------------------------------------------------------------------
+| Project   : Device Filesystem
++------------------------------------------------------------------------------
+| Copyright 2004, 2005  www.fayfayspace.org.
+| All rights reserved.
+|------------------------------------------------------------------------------
+| File      : dfs_efs.h
+|------------------------------------------------------------------------------
+| Chang Logs:
+| Date           Author       Notes
+| 2010-02-06     Bernard      Add elm_init function declaration
++------------------------------------------------------------------------------
+*/
+
+#ifndef __DFS_ELM_H__
+#define __DFS_ELM_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int elm_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
