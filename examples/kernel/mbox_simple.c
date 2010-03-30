34e894f8ac60506b7edbb8890686fd00ce073245
bernard.xiong <bernard.xiong@gmail.com>
2010-03-30 02:40:59 +0000

    fix compiling issue.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@558 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- examples/kernel/mbox_simple.c
+++ examples/kernel/mbox_simple.c
@@ -70,7 +70,7 @@ int mbox_simple_init()
 	rt_mb_init(&mb,
 		"mbt",             /* 名称是mbt */
 		&mb_pool[0],       /* 邮箱用到的内存池是mb_pool */
-		size(mb_pool)/4,   /* 大小是mb_pool大小除以4，因为一封邮件的大小是4字节 */
+		sizeof(mb_pool)/4,   /* 大小是mb_pool大小除以4，因为一封邮件的大小是4字节 */
 		RT_IPC_FLAG_FIFO); /* 采用FIFO方式进行线程等待 */
 
 	/* 创建线程1 */
