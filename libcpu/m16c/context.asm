005d4141d992c845c9fbec791f131000c46d725f
dzzxzz <dzzxzz@gmail.com>
2010-04-20 01:00:45 +0000

    rewrite 3 functions in context.asm for M16C port rt_hw_context_switch() rt_hw_interrupt_disable() rt_hw_interrupt_enable()
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@654 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- libcpu/m16c/context.asm
+++ libcpu/m16c/context.asm
@@ -10,14 +10,11 @@
  * Change Logs:
  * Date           Author       Notes
  * 2010-04-09     fify         the first version
+ * 2010-04-19     fify         rewrite rt_hw_interrupt_disable/enable fuction
  *
  * For       : Renesas M16C
  * Toolchain : IAR's EW for M16C v3.401
 */
-
-;********************************************************************************************************
-;                                           PUBLIC FUNCTIONS
-;********************************************************************************************************
 
     RSEG        CSTACK
 
@@ -40,34 +37,51 @@
     PUBLIC    rt_hw_context_switch_interrupt
     PUBLIC    rt_hw_timer_handler
     PUBLIC    rt_hw_uart0_receive_handler
+    PUBLIC    os_context_switch
 
 rt_hw_interrupt_disable
-    FCLR    I
+    STC      FLG, R0    ;fify 20100419
+    FCLR     I
     RTS
 
 rt_hw_interrupt_enable
-    FSET    I
+    LDC      R0, FLG    ;fify 20100419
     RTS
     
-;/*
-; * void rt_hw_context_switch_to(rt_uint32 to);
-; * r0 --> to
-; * this fucntion is used to perform the first thread switch
-; */
+    .EVEN
+os_context_switch:
+    PUSHM    R0,R1,R2,R3,A0,A1,SB,FB
+    CMP.W    #0,rt_thread_switch_interrput_flag
+    JEQ      exit
+    MOV.W    #0, rt_thread_switch_interrput_flag
+    MOV.W    rt_interrupt_from_thread, A0
+    STC      ISP, [A0]    
+    MOV.W    rt_interrupt_to_thread, A0
+    LDC      [A0], ISP
+exit
+    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore registers from the new task's stack
+    REIT                                         ; Return from interrup    
+    
+/*
+ * void rt_hw_context_switch_to(rt_uint32 to);
+ * r0 --> to
+ * this fucntion is used to perform the first thread switch
+ */
 rt_hw_context_switch_to
-    MOV.W   R0, A0
-    LDC     [A0], ISP
-    POPM    R0,R1,R2,R3,A0,A1,SB,FB
+    MOV.W    R0, A0
+    LDC      [A0], ISP
+    POPM     R0,R1,R2,R3,A0,A1,SB,FB
     REIT
 
 rt_hw_context_switch
-    PUSHM    R0,R1,R2,R3,A0,A1,SB,FB 
-    MOV.W    R0, A0
-    STC      ISP, [A0]
-    MOV.W    R1, A0
-    LDC      [A0], ISP
-    POPM     R0,R1,R2,R3,A0,A1,SB,FB         ; Restore all processor registers from the new task's stack
-    REIT
+    CMP.W    #1,rt_thread_switch_interrput_flag
+    JEQ      jump1
+    MOV.W    #1,rt_thread_switch_interrput_flag
+    MOV.W    R0, rt_interrupt_from_thread
+jump1
+    MOV.W    R1, rt_interrupt_to_thread
+    INT      #0
+    RTS
   
 rt_hw_context_switch_interrupt
     CMP.W    #1,rt_thread_switch_interrput_flag
@@ -85,8 +99,8 @@ rt_hw_context_switch_interrupt_do
     
     MOV.W    rt_interrupt_to_thread, A0
     LDC      [A0], ISP
-    POPM     R0,R1,R2,R3,A0,A1,SB,FB         ; Restore all processor registers from the new task's stack
-    RTS                                      ; Normal return
+    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore all processor registers from the new task's stack
+    REIT
     
     .EVEN
 rt_hw_timer_handler:
@@ -98,7 +112,7 @@ rt_hw_timer_handler:
     CMP.W    #1,rt_thread_switch_interrput_flag
     JEQ      rt_hw_context_switch_interrupt_do
     
-    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore registers from the new task's stack
+    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore current task's registers
     REIT                                         ; Return from interrup
 
     .EVEN
@@ -111,7 +125,7 @@ rt_hw_uart0_receive_handler:
     CMP.W    #1, rt_thread_switch_interrput_flag
     JEQ      rt_hw_context_switch_interrupt_do
     
-    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore registers from the new task's stack
+    POPM     R0,R1,R2,R3,A0,A1,SB,FB             ; Restore current task's registers
     REIT                                         ; Return from interrup
 
     END
