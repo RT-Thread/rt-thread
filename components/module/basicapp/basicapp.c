5b0e4dc40a2fada0d8c0a5858723b3df80efc5d0
qiuyiuestc <qiuyiuestc@gmail.com>
2010-07-21 00:19:13 +0000

    reconstruct module
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@809 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- components/module/basicapp/basicapp.c
+++ components/module/basicapp/basicapp.c
@@ -1,11 +1,10 @@
 #include <rtthread.h>
-#include <interface_help.h>
 
 int a = 0;
 int b = 1000000;
 int c = 0;
 
-void function(int count1, int count2, int count3)
+static void function(int count1, int count2, int count3)
 {
 	rt_kprintf("Hello RT-Thread %d %d\n", count1, count2, count3);
 }
@@ -13,14 +12,15 @@ void function(int count1, int count2, int count3)
 int rt_application_entry(void)
 {
 	int i;
-	for(i=0; i<1000; i++) 
+	rt_kprintf("application entry\n");
+	for(i=0; i<100; i++)  
 	{	
 		a++;
 		b--;
 		c++;
-		function(a, b, c);
+		function(a, c, b );
 	}
-
+ 
 	return 0;
 }
 
