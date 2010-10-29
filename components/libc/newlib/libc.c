048618235365d05b53524679ac04b88192f5b517
bernard.xiong <bernard.xiong@gmail.com>
2010-10-29 05:57:19 +0000

    add stdin, stdout, stderr implementation in newlib.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1031 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- components/libc/newlib/libc.c
+++ components/libc/newlib/libc.c
@@ -12,9 +12,11 @@ void libc_system_init(const char* tty_name)
 
 	/* open console as stdin/stdout/stderr */
 	fd = open("/dev/console", O_RDONLY, 0);	/* for stdin */
-	rt_kprintf("stdin: %d\n", fd);
 	fd = open("/dev/console", O_WRONLY, 0);	/* for stdout */
-	rt_kprintf("stdout: %d\n", fd);
 	fd = open("/dev/console", O_WRONLY, 0);	/* for stderr */
-	rt_kprintf("stderr: %d\n", fd);
+
+	/* set PATH and HOME */
+	putenv("PATH=/");
+	putenv("HOME=/");
 }
+
