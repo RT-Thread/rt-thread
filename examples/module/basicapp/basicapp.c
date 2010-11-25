9555bf29756dc3bc6cf77b61999f7bca6f7f9f87
qiuyiuestc <qiuyiuestc@gmail.com>
2010-11-25 00:35:06 +0000

    clean up module config file and SConscript
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1113 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- examples/module/basicapp/basicapp.c
+++ examples/module/basicapp/basicapp.c
@@ -9,7 +9,7 @@ static void function(int count1, int count2, int count3)
 	rt_kprintf("Hello RT-Thread %d %d\n", count1, count2, count3);
 }
 
-int rt_application_entry(void)
+int rt_application_init(void)
 {
 	int i;
 	rt_kprintf("application entry\n");
