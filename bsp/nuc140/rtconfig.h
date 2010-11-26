6237d0538c06cb2935f55a3df9845a946f94799a
bernard.xiong <bernard.xiong@gmail.com>
2010-11-26 03:20:36 +0000

    update nuc140 project files.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1122 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/nuc140/rtconfig.h
+++ bsp/nuc140/rtconfig.h
@@ -66,7 +66,7 @@
 
 /* SECTION: finsh, a C-Express shell */
 /* Using FinSH as Shell*/
-#define RT_USING_FINSH
+/* #define RT_USING_FINSH */
 /* Using symbol table */
 #define FINSH_USING_SYMTAB
 #define FINSH_USING_DESCRIPTION
