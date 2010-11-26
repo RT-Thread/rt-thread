bad87a2c4b14d061ce1b0f7c8c688b3f3f14166d
qiuyiuestc <qiuyiuestc@gmail.com>
2010-11-26 01:18:59 +0000

    update lm3s sconscript and project file
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1119 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/lm3s/rtconfig.py
+++ bsp/lm3s/rtconfig.py
@@ -1,56 +1,16 @@
-import SCons.cpp
 
-# component options
-
-# make all component false 
-RT_USING_DFS 		= False
-RT_USING_FINSH 		= False
-RT_USING_DFS_ELMFAT= False
-RT_USING_DFS_YAFFS2= False
-RT_USING_LWIP 		= False
-RT_USING_RTGUI 		= False
-
-# parse rtconfig.h to get used component
-PreProcessor = SCons.cpp.PreProcessor()
-f = file('rtconfig.h', 'r')
-contents = f.read()
-f.close()
-PreProcessor.process_contents(contents)
-rtconfig_ns = PreProcessor.cpp_namespace
-
-# finsh shell options
-if rtconfig_ns.has_key('RT_USING_FINSH'):
-	RT_USING_FINSH = True
-
-# device virtual filesystem options 
-if rtconfig_ns.has_key('RT_USING_DFS'):
-    RT_USING_DFS = True
-    
-    if rtconfig_ns.has_key('RT_USING_DFS_EFSL'):
-        RT_USING_DFS_EFSL = True
-    if rtconfig_ns.has_key('RT_USING_DFS_ELMFAT'):
-        RT_USING_DFS_ELMFAT = True
-    if rtconfig_ns.has_key('RT_USING_DFS_YAFFS2'):
-        RT_USING_DFS_YAFFS2 = True
-
-# lwip options
-if rtconfig_ns.has_key('RT_USING_LWIP'):
-    RT_USING_LWIP = True
-    if rtconfig_ns.has_key('RT_USING_WEBSERVER'):
-        RT_USING_WEBSERVER = True
-
-# rtgui options
-if rtconfig_ns.has_key('RT_USING_RTGUI'):
-    RT_USING_RTGUI = True
 
 # toolchains options
 ARCH='arm'
 CPU='lm3s'
-#PLATFORM = 'gcc'
-#EXEC_PATH = 'E:/Program Files/CodeSourcery/Sourcery G++ Lite/bin'
-PLATFORM = 'armcc'
-EXEC_PATH = 'E:/Keil'
-BUILD = 'release'
+CROSS_TOOL 	= 'keil'
+if  CROSS_TOOL == 'gcc':
+	PLATFORM 	= 'gcc'
+	EXEC_PATH 	= 'E:/Program Files/CodeSourcery/Sourcery G++ Lite/bin'
+elif CROSS_TOOL == 'keil':
+	PLATFORM 	= 'armcc'
+	EXEC_PATH 	= 'E:/Keil'
+BUILD = 'debug'
 
 if PLATFORM == 'gcc':
     # toolchains
@@ -65,7 +25,7 @@ if PLATFORM == 'gcc':
     OBJCPY = PREFIX + 'objcopy'
 
     DEVICE = ' -mcpu=cortex-m3 -mthumb'
-    CFLAGS = DEVICE + ' -Dsourcerygxx' + ' -DRT_USING_MINILIBC' 
+    CFLAGS = DEVICE + ' -Dsourcerygxx' 
     AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp'
     LFLAGS = DEVICE + ' -Wl,--gc-sections,-Map=rtthread-lm3s.map,-cref,-u,Reset_Handler -T lm3s_rom.ld'
 
@@ -78,7 +38,6 @@ if PLATFORM == 'gcc':
     else:
         CFLAGS += ' -O2'
 
-    RT_USING_MINILIBC = True
     POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
 
 elif PLATFORM == 'armcc':
@@ -106,6 +65,4 @@ elif PLATFORM == 'armcc':
         CFLAGS += ' -O2'
 
     RT_USING_MINILIBC = False
-    if RT_USING_FINSH:
-        LFLAGS += ' --keep __fsym_* --keep __vsym_*'
     POST_ACTION = 'fromelf --bin $TARGET --output rtthread.bin \nfromelf -z $TARGET'
