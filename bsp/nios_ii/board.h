fb9a0952c8b33b73f711fabb9e2367e70fb84eab
wuyangyong <wuyangyong@gmail.com>
2011-03-04 14:08:36 +0000

    add Nios II app
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1307 bbd45198-f89e-11dd-88c7-29a3b14d5316

---
+++ bsp/nios_ii/board.h
@@ -0,0 +1,22 @@
+/*
+ * File      : board.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006-2011, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2011-02-14     aozima       first implementation for Nios II.
+ */
+
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
+#include <rthw.h>
+
+extern void rt_hw_board_init(void);
+
+#endif // BOARD_H_INCLUDED
---
+++ bsp/nios_ii/uart.h
@@ -0,0 +1,6 @@
+#ifndef UART_H_INCLUDED
+#define UART_H_INCLUDED
+
+extern void rt_hw_uart_init(void);
+
+#endif // UART_H_INCLUDED
