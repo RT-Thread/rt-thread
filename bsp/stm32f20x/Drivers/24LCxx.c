6f6903688b56909df0253893a43e832e4dcadf08
yungchi@cs.nctu.edu.tw <yungchi@cs.nctu.edu.tw>
2011-09-21 09:48:45 +0000

    add header notes
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1717 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/stm32f20x/Drivers/24LCxx.c
+++ bsp/stm32f20x/Drivers/24LCxx.c
@@ -1,3 +1,17 @@
+/*
+ * File      : 24LCxx.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2011-09-21     JoyChen      First version, support 24LC024H eeprom device
+ */
+
 #include <rtthread.h>
 #include "i2c.h"
 
--- bsp/stm32f20x/Drivers/i2c.c
+++ bsp/stm32f20x/Drivers/i2c.c
@@ -1,3 +1,16 @@
+/*
+ * File      : i2c.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2011-09-21     JoyChen      First version, support I2C1
+ */
 
 #include <rtthread.h>
 #include "i2c.h"
