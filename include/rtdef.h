86c5646fc0254ae86803c03ce67c64bc41537f9c
dzzxzz <dzzxzz@gmail.com>
2011-12-31 06:22:46 +0000

    change the version from 0.4.0 to 1.0.0
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@1893 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- include/rtdef.h
+++ include/rtdef.h
@@ -33,8 +33,9 @@ extern "C" {
 /**
  * RT-Thread version information
  */
-#define RT_VERSION                      4L      /**< major version number */
+#define RT_VERSION                      1L      /**< major version number */
 #define RT_SUBVERSION                   0L      /**< minor version number */
+#define RT_REVISION                   	0L      /**< revise version number */
 
 /**
  * RT-Thread basic data type definitions
--- src/kservice.c
+++ src/kservice.c
@@ -465,7 +465,7 @@ void rt_show_version(void)
 {
 	rt_kprintf("\n \\ | /\n");
 	rt_kprintf("- RT -     Thread Operating System\n");
-	rt_kprintf(" / | \\ 0.%d.%d build %s\n", RT_VERSION, RT_SUBVERSION, __DATE__);
+	rt_kprintf(" / | \\ %d.%d.%d build %s\n", RT_VERSION, RT_SUBVERSION, RT_REVISION, __DATE__);
 	rt_kprintf(" 2006 - 2011 Copyright by rt-thread team\n");
 }
 
