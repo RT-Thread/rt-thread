056228cce6b8b238ee6ee326995c65d6b3423f26
wuyangyong <wuyangyong@gmail.com>
2012-08-22 15:53:54 +0000

    fixed bug: store r8 - r11.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2258 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- libcpu/arm/cortex-m0/cpuport.c
+++ libcpu/arm/cortex-m0/cpuport.c
@@ -11,11 +11,12 @@
  * Date           Author       Notes
  * 2010-01-25     Bernard      first version
  * 2012-05-31     aozima       Merge all of the C source code into cpuport.c
+ * 2012-08-17     aozima       fixed bug: store r8 - r11.
  */
 
 #include <rtthread.h>
 
-struct stack_contex
+struct exception_stack_frame
 {
     rt_uint32_t r0;
     rt_uint32_t r1;
@@ -25,6 +26,23 @@ struct stack_contex
     rt_uint32_t lr;
     rt_uint32_t pc;
     rt_uint32_t psr;
+};
+
+struct stack_frame
+{
+    /* r4 ~ r7 low register */
+    rt_uint32_t r4;
+    rt_uint32_t r5;
+    rt_uint32_t r6;
+    rt_uint32_t r7;
+
+    /* r8 ~ r11 high register */
+    rt_uint32_t r8;
+    rt_uint32_t r9;
+    rt_uint32_t r10;
+    rt_uint32_t r11;
+	
+    struct exception_stack_frame exception_stack_frame;
 };
 
 /* flag in interrupt handling */
@@ -44,29 +62,37 @@ rt_uint32_t rt_thread_switch_interrupt_flag;
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter,
                              rt_uint8_t *stack_addr, void *texit)
 {
-    unsigned long *stk;
+    struct stack_frame * stack_frame;
+    rt_uint8_t * stk;
+    unsigned long i;
 
-    stk 	 = (unsigned long *)stack_addr;
-    *(stk)   = 0x01000000L;					/* PSR */
-    *(--stk) = (unsigned long)tentry;		/* entry point, pc */
-    *(--stk) = (unsigned long)texit;		/* lr */
-    *(--stk) = 0;							/* r12 */
-    *(--stk) = 0;							/* r3 */
-    *(--stk) = 0;							/* r2 */
-    *(--stk) = 0;							/* r1 */
-    *(--stk) = (unsigned long)parameter;	/* r0 : argument */
-    *(--stk) = 0;							/* r7 */
-    *(--stk) = 0;							/* r6 */
-    *(--stk) = 0;							/* r5 */
-    *(--stk) = 0;							/* r4 */
+    stk = stack_addr + sizeof(rt_uint32_t);
+
+    stk -= sizeof(struct stack_frame);
+    stack_frame = (struct stack_frame *)stk;
+	
+    /* init all register */
+    for(i=0; i<sizeof(struct stack_frame)/sizeof(rt_uint32_t); i++)
+    {
+        ((rt_uint32_t*)stack_frame)[i] = 0xdeadbeef;
+    }
+
+    stack_frame->exception_stack_frame.r0  = (unsigned long)parameter; /* r0 : argument */
+    stack_frame->exception_stack_frame.r1  = 0;                        /* r1 */
+    stack_frame->exception_stack_frame.r2  = 0;                        /* r2 */
+    stack_frame->exception_stack_frame.r3  = 0;                        /* r3 */
+    stack_frame->exception_stack_frame.r12 = 0;                        /* r12 */
+    stack_frame->exception_stack_frame.lr  = (unsigned long)texit;     /* lr */
+    stack_frame->exception_stack_frame.pc  = (unsigned long)tentry;    /* entry point, pc */
+    stack_frame->exception_stack_frame.psr = 0x01000000L;              /* PSR */
 
     /* return task's current stack address */
-    return (rt_uint8_t *)stk;
+    return stk;
 }
 
 extern long list_thread(void);
 extern rt_thread_t rt_current_thread;
-void rt_hw_hard_fault_exception(struct stack_contex* contex)
+void rt_hw_hard_fault_exception(struct exception_stack_frame* contex)
 {
     rt_kprintf("psr: 0x%08x\n", contex->psr);
     rt_kprintf(" pc: 0x%08x\n", contex->pc);
