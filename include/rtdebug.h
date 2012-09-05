cd053e7737341438f2fcf7ded2dff055444bc7a7
dzzxzz@gmail.com <dzzxzz@gmail.com>
2012-09-05 06:52:35 +0000

    re-format the coding style, convert the tab to 4 spaces and make sure the line length is not longer than 80 in rtdebug.h
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2271 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- include/rtdebug.h
+++ include/rtdebug.h
@@ -18,61 +18,77 @@
 
 /* Turn on some of these (set to non-zero) to debug kernel */
 #ifndef RT_DEBUG_MEM
-#define RT_DEBUG_MEM        0
+#define RT_DEBUG_MEM                   0
 #endif
 
 #ifndef RT_DEBUG_MEMHEAP
-#define RT_DEBUG_MEMHEAP	0
+#define RT_DEBUG_MEMHEAP               0
 #endif
 
 #ifndef RT_DEBUG_MODULE
-#define RT_DEBUG_MODULE     0
+#define RT_DEBUG_MODULE                0
 #endif
 
 #ifndef RT_DEBUG_SCHEDULER
-#define RT_DEBUG_SCHEDULER  0
+#define RT_DEBUG_SCHEDULER             0
 #endif
 
 #ifndef RT_DEBUG_SLAB
-#define RT_DEBUG_SLAB       0
+#define RT_DEBUG_SLAB                  0
 #endif
 
 #ifndef RT_DEBUG_THREAD
-#define RT_DEBUG_THREAD     0
+#define RT_DEBUG_THREAD                0
 #endif
 
 #ifndef RT_DEBUG_TIMER
-#define RT_DEBUG_TIMER      0
+#define RT_DEBUG_TIMER                 0
 #endif
 
 #ifndef RT_DEBUG_IRQ
-#define RT_DEBUG_IRQ        0
+#define RT_DEBUG_IRQ                   0
 #endif
 
 #ifndef RT_DEBUG_IPC
-#define RT_DEBUG_IPC        0
+#define RT_DEBUG_IPC                   0
 #endif
 
 /* Turn on this to enable context check */
 #ifndef RT_DEBUG_CONTEXT_CHECK
-#define RT_DEBUG_CONTEXT_CHECK 1
+#define RT_DEBUG_CONTEXT_CHECK         1
 #endif
 
-#define RT_DEBUG_LOG(type,message)  do { if (type) rt_kprintf message;} while (0)
+#define RT_DEBUG_LOG(type, message)                                           \
+do                                                                            \
+{                                                                             \
+    if (type)                                                                 \
+        rt_kprintf message;                                                   \
+}                                                                             \
+while (0)
 
-#define RT_ASSERT(EX)   if (!(EX)) {volatile char dummy = 0;\
-                            rt_kprintf("(%s) assert failed at %s:%d \n", \
-                            #EX, __FUNCTION__, __LINE__); while (dummy == 0);}
+#define RT_ASSERT(EX)                                                         \
+if (!(EX))                                                                    \
+{                                                                             \
+    volatile char dummy = 0;                                                  \
+    rt_kprintf("(%s) assert failed at %s:%d \n", #EX, __FUNCTION__, __LINE__);\
+    while (dummy == 0);                                                       \
+}
 
 /* Macro to check current context */
 #if RT_DEBUG_CONTEXT_CHECK
-#define RT_DEBUG_NOT_IN_INTERRUPT do {\
-        rt_base_t level;\
-        level = rt_hw_interrupt_disable();\
-        if (rt_interrupt_get_nest() != 0){\
-            rt_kprintf("Function[%s] shall not used in ISR\n", __FUNCTION__);\
-            RT_ASSERT(0)}\
-        rt_hw_interrupt_enable(level);} while (0)
+#define RT_DEBUG_NOT_IN_INTERRUPT                                             \
+do                                                                            \
+{                                                                             \
+    rt_base_t level;                                                          \
+    level = rt_hw_interrupt_disable();                                        \
+    if (rt_interrupt_get_nest() != 0)                                         \
+    {                                                                         \
+        rt_kprintf("Function[%s] shall not used in ISR\n", __FUNCTION__);     \
+        RT_ASSERT(0)                                                          \
+    }                                                                         \
+    rt_hw_interrupt_enable(level);                                            \
+}                                                                             \
+while (0)
 #else
 #define RT_DEBUG_NOT_IN_INTERRUPT
 #endif
@@ -80,7 +96,7 @@
 #else /* RT_DEBUG */
 
 #define RT_ASSERT(EX)
-#define RT_DEBUG_LOG(type,message)
+#define RT_DEBUG_LOG(type, message)
 #define RT_DEBUG_NOT_IN_INTERRUPT
 
 #endif /* RT_DEBUG */
