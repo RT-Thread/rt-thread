01aa24f2c5b11e3a3ad99ebbf9f99d471690c7c9
wuyangyong <wuyangyong@gmail.com>
2012-09-09 05:50:58 +0000

    update elmfat: add RT_DFS_ELM_CODE_PAGE define.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2285 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- components/dfs/filesystems/elmfat/ffconf.h
+++ components/dfs/filesystems/elmfat/ffconf.h
@@ -57,7 +57,11 @@
 / Locale and Namespace Configurations
 /----------------------------------------------------------------------------*/
 
-#define _CODE_PAGE	936
+#ifdef RT_DFS_ELM_CODE_PAGE
+#    define _CODE_PAGE	RT_DFS_ELM_CODE_PAGE
+#else
+#    define _CODE_PAGE	936
+#endif
 /* The _CODE_PAGE specifies the OEM code page to be used on the target system.
 /  Incorrect setting of the code page can cause a file open failure.
 /
