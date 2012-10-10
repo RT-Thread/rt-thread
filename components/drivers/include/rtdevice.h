66c2e8856632e34893502c55628e8c36752badb2
dzzxzz@gmail.com <dzzxzz@gmail.com>
2012-10-10 13:21:06 +0000

    change the coding style of rtdevice.h
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2337 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- components/drivers/include/rtdevice.h
+++ components/drivers/include/rtdevice.h
@@ -1,9 +1,23 @@
+/*
+ * File      : serial.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006 - 2012, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2012-01-08     bernard      first version.
+ */
+
 #ifndef __RT_DEVICE_H__
 #define __RT_DEVICE_H__
 
 #include <rtthread.h>
 
-#define RT_DEVICE(device)	((rt_device_t)device)
+#define RT_DEVICE(device)            ((rt_device_t)device)
 
 /* completion flag */
 struct rt_completion
@@ -14,8 +28,8 @@ struct rt_completion
     rt_list_t suspended_list;
 };
 
-#define RT_RINGBUFFER_SIZE(rb)		((rb)->write_index - (rb)->read_index)
-#define RT_RINGBUFFER_EMPTY(rb) 	((rb)->buffer_size - RT_RINGBUFFER_SIZE(rb))
+#define RT_RINGBUFFER_SIZE(rb)       ((rb)->write_index - (rb)->read_index)
+#define RT_RINGBUFFER_EMPTY(rb)      ((rb)->buffer_size - RT_RINGBUFFER_SIZE(rb))
 /* ring buffer */
 struct rt_ringbuffer
 {
@@ -25,17 +39,17 @@ struct rt_ringbuffer
 };
 
 /* pipe device */
-#define PIPE_DEVICE(device)	((struct rt_pipe_device*)(device))
+#define PIPE_DEVICE(device)          ((struct rt_pipe_device*)(device))
 struct rt_pipe_device
 {
-	struct rt_device parent;
+    struct rt_device parent;
 
-	/* ring buffer in pipe device */
-	struct rt_ringbuffer ringbuffer;
+    /* ring buffer in pipe device */
+    struct rt_ringbuffer ringbuffer;
 
-	/* suspended list */
-	rt_list_t suspended_read_list;
-	rt_list_t suspended_write_list;
+    /* suspended list */
+    rt_list_t suspended_read_list;
+    rt_list_t suspended_write_list;
 };
 
 #define RT_DATAQUEUE_EVENT_UNKNOWN   0x00
@@ -44,14 +58,14 @@ struct rt_pipe_device
 #define RT_DATAQUEUE_EVENT_LWM       0x03
 
 struct rt_data_item;
-#define RT_DATAQUEUE_SIZE(dq)		((dq)->put_index - (dq)->get_index)
-#define RT_DATAQUEUE_EMPTY(dq)		((dq)->size - RT_DATAQUEUE_SIZE(dq))
+#define RT_DATAQUEUE_SIZE(dq)        ((dq)->put_index - (dq)->get_index)
+#define RT_DATAQUEUE_EMPTY(dq)       ((dq)->size - RT_DATAQUEUE_SIZE(dq))
 /* data queue implementation */
 struct rt_data_queue
 {
     rt_uint16_t size;
     rt_uint16_t lwm;
-	rt_bool_t   waiting_lwm;
+    rt_bool_t   waiting_lwm;
 
     rt_uint16_t get_index;
     rt_uint16_t put_index;
@@ -62,16 +76,16 @@ struct rt_data_queue
     rt_list_t suspended_pop_list;
 
     /* event notify */
-	void (*evt_notify)(struct rt_data_queue *queue, rt_uint32_t event);
+    void (*evt_notify)(struct rt_data_queue *queue, rt_uint32_t event);
 };
 
 /**
  * Completion
  */
-void rt_completion_init(struct rt_completion* completion);
-rt_err_t rt_completion_wait(struct rt_completion* completion,
-                            rt_int32_t timeout);
-void rt_completion_done(struct rt_completion* completion);
+void rt_completion_init(struct rt_completion *completion);
+rt_err_t rt_completion_wait(struct rt_completion *completion,
+                            rt_int32_t            timeout);
+void rt_completion_done(struct rt_completion *completion);
 
 /**
  * RingBuffer for DeviceDriver
@@ -79,41 +93,49 @@ void rt_completion_done(struct rt_completion* completion);
  * Please note that the ring buffer implementation of RT-Thread
  * has no thread wait or resume feature.
  */
-void rt_ringbuffer_init(struct rt_ringbuffer* rb,
-                        rt_uint8_t *pool,
-                        rt_uint16_t size);
-rt_size_t rt_ringbuffer_put(struct rt_ringbuffer* rb,
-                            const rt_uint8_t *ptr,
-                            rt_uint16_t length);
-rt_size_t rt_ringbuffer_putchar(struct rt_ringbuffer* rb,
-                                const rt_uint8_t ch);
-rt_size_t rt_ringbuffer_get(struct rt_ringbuffer* rb,
-                            rt_uint8_t *ptr,
-                            rt_uint16_t length);
-rt_size_t rt_ringbuffer_getchar(struct rt_ringbuffer* rb, rt_uint8_t *ch);
-rt_inline rt_uint16_t rt_ringbuffer_get_size(struct rt_ringbuffer* rb)
+void rt_ringbuffer_init(struct rt_ringbuffer *rb,
+                        rt_uint8_t           *pool,
+                        rt_uint16_t           size);
+rt_size_t rt_ringbuffer_put(struct rt_ringbuffer *rb,
+                            const rt_uint8_t     *ptr,
+                            rt_uint16_t           length);
+rt_size_t rt_ringbuffer_putchar(struct rt_ringbuffer *rb,
+                                const rt_uint8_t      ch);
+rt_size_t rt_ringbuffer_get(struct rt_ringbuffer *rb,
+                            rt_uint8_t           *ptr,
+                            rt_uint16_t           length);
+rt_size_t rt_ringbuffer_getchar(struct rt_ringbuffer *rb, rt_uint8_t *ch);
+rt_inline rt_uint16_t rt_ringbuffer_get_size(struct rt_ringbuffer *rb)
 {
-	RT_ASSERT(rb != RT_NULL);
-	return rb->buffer_size;
+    RT_ASSERT(rb != RT_NULL);
+    return rb->buffer_size;
 }
 
 /**
  * Pipe Device
  */
-rt_err_t rt_pipe_create(const char* name, rt_size_t size);
-void rt_pipe_destroy(struct rt_pipe_device* pipe);
+rt_err_t rt_pipe_create(const char *name, rt_size_t size);
+void rt_pipe_destroy(struct rt_pipe_device *pipe);
 
 /**
  * DataQueue for DeviceDriver
  */
-rt_err_t rt_data_queue_init(struct rt_data_queue* queue, rt_uint16_t size, rt_uint16_t lwm,
-	void (*evt_notify)(struct rt_data_queue* queue, rt_uint32_t event));
-rt_err_t rt_data_queue_push(struct rt_data_queue* queue, void* data_ptr, rt_size_t data_size,
-	rt_int32_t timeout);
-rt_err_t rt_data_queue_pop(struct rt_data_queue* queue, void** data_ptr, rt_size_t *size,
-	rt_int32_t timeout);
-rt_err_t rt_data_queue_peak(struct rt_data_queue* queue, void** data_ptr, rt_size_t *size);
-void rt_data_queue_reset(struct rt_data_queue* queue);
+rt_err_t rt_data_queue_init(struct rt_data_queue *queue,
+                            rt_uint16_t           size,
+                            rt_uint16_t           lwm,
+                            void (*evt_notify)(struct rt_data_queue *queue, rt_uint32_t event));
+rt_err_t rt_data_queue_push(struct rt_data_queue *queue,
+                            void                 *data_ptr,
+                            rt_size_t             data_size,
+                            rt_int32_t            timeout);
+rt_err_t rt_data_queue_pop(struct rt_data_queue *queue,
+                           void                **data_ptr,
+                           rt_size_t            *size,
+                           rt_int32_t            timeout);
+rt_err_t rt_data_queue_peak(struct rt_data_queue *queue,
+                            void                **data_ptr,
+                            rt_size_t            *size);
+void rt_data_queue_reset(struct rt_data_queue *queue);
 
 #ifdef RT_USING_RTC
 #include "drivers/rtc.h"
@@ -150,7 +172,6 @@ void rt_data_queue_reset(struct rt_data_queue* queue);
 #ifdef RT_USING_I2C_BITOPS
 #include "drivers/i2c-bit-ops.h"
 #endif /* RT_USING_I2C_BITOPS */
-
 #endif /* RT_USING_I2C */
 
 #ifdef RT_USING_SDIO
