67f60b770c780700ca969a60ebabf87edd16e58b
bernard.xiong@gmail.com <bernard.xiong@gmail.com>
2012-11-04 16:29:36 +0000

    Add pcap driver(for win32) in simulator porting.
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2391 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/simulator/rtconfig.h
+++ bsp/simulator/rtconfig.h
@@ -109,7 +109,7 @@
 #define DFS_FD_MAX					4
 
 /* SECTION: lwip, a lighwight TCP/IP protocol stack */
-/* #define RT_USING_LWIP */
+#define RT_USING_LWIP
 /* LwIP uses RT-Thread Memory Management */
 #define RT_LWIP_USING_RT_MEM
 /* Enable ICMP protocol*/
