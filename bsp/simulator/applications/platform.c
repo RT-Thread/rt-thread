7c82d6e3044d6bd4787e3b094a863d30afa7e5f5
goprife@gmail.com <goprife@gmail.com>
2012-12-01 07:08:48 +0000

    revert RTGUI path in SConstruct, update applications/platform.c
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2462 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/simulator/applications/platform.c
+++ bsp/simulator/applications/platform.c
@@ -5,25 +5,21 @@ void rt_platform_init(void)
 {
 #ifdef RT_USING_DFS
     /* initilize sd card */
-#ifdef RT_USING_DFS_ELMFAT
     rt_hw_sdcard_init();
-#endif 
 
-#if defined(RT_USING_MTD_NAND) && defined(RT_USING_DFS_UFFS)
+#if defined(RT_USING_MTD_NAND)
     rt_hw_mtd_nand_init();
 #endif 
 
-#if defined(RT_USING_MTD_NOR) && defined(RT_USING_DFS_JFFS2)
+#if defined(RT_USING_MTD_NOR)
     sst25vfxx_mtd_init("nor", 0, RT_UINT32_MAX);
 #endif 
 
 #endif /* RT_USING_DFS */
 
 #ifdef RT_USING_RTGUI
-
     /* initilize sdl */
-	 sdl_start();
-
+    sdl_start();
 #endif /* RT_USING_RTGUI */
 
     rt_thread_delay(50);
