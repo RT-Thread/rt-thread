f29cfc5e79b11acb28c00d870764bc0afe586a39
dzzxzz@gmail.com <dzzxzz@gmail.com>
2012-12-26 00:27:14 +0000

    fixed the coding style in components/init/components.c
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2528 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- components/init/components.c
+++ components/init/components.c
@@ -13,6 +13,7 @@
  *                             And all components related header files.
  * 2012-12-23     Bernard      fix the pthread initialization issue.
  */
+
 #include "components.h"
 
 /**
@@ -21,77 +22,76 @@
 void rt_components_init(void)
 {
 #ifdef RT_USING_MODULE
-	rt_system_module_init();
+    rt_system_module_init();
 #endif
 
 #ifdef RT_USING_FINSH
-	/* initialize finsh */
-	finsh_system_init();
-	finsh_set_device(RT_CONSOLE_DEVICE_NAME);
+    /* initialize finsh */
+    finsh_system_init();
+    finsh_set_device(RT_CONSOLE_DEVICE_NAME);
 #endif
 
 #ifdef RT_USING_LWIP
-	/* initialize lwip stack */
-	/* register ethernetif device */
-	eth_system_device_init();
+    /* initialize lwip stack */
+    /* register ethernetif device */
+    eth_system_device_init();
 
-	/* initialize lwip system */
-	lwip_system_init();
-	rt_kprintf("TCP/IP initialized!\n");
+    /* initialize lwip system */
+    lwip_system_init();
+    rt_kprintf("TCP/IP initialized!\n");
 #endif
 
 #ifdef RT_USING_DFS
-	/* initialize the device file system */
-	dfs_init();
+    /* initialize the device file system */
+    dfs_init();
 
-#ifdef RT_USING_DFS_ELMFAT
-	/* initialize the elm chan FatFS file system*/
-	elm_init();
-#endif
+    #ifdef RT_USING_DFS_ELMFAT
+    /* initialize the elm chan FatFS file system*/
+    elm_init();
+    #endif
 
-#if defined(RT_USING_DFS_NFS) && defined(RT_USING_LWIP)
-	/* initialize NFSv3 client file system */
-	nfs_init();
-#endif
+    #if defined(RT_USING_DFS_NFS) && defined(RT_USING_LWIP)
+    /* initialize NFSv3 client file system */
+    nfs_init();
+    #endif
 
-#ifdef RT_USING_DFS_YAFFS2
-	dfs_yaffs2_init();
-#endif
+    #ifdef RT_USING_DFS_YAFFS2
+    dfs_yaffs2_init();
+    #endif
 
-#ifdef RT_USING_DFS_UFFS
-	dfs_uffs_init();
-#endif
+    #ifdef RT_USING_DFS_UFFS
+    dfs_uffs_init();
+    #endif
 
-#ifdef RT_USING_DFS_JFFS2
-	dfs_jffs2_init();
-#endif
+    #ifdef RT_USING_DFS_JFFS2
+    dfs_jffs2_init();
+    #endif
 
-#ifdef RT_USING_DFS_ROMFS
-	dfs_romfs_init();
-#endif
+    #ifdef RT_USING_DFS_ROMFS
+    dfs_romfs_init();
+    #endif
 
-#ifdef RT_USING_DFS_DEVFS
-	devfs_init();
-#endif
+    #ifdef RT_USING_DFS_DEVFS
+    devfs_init();
+    #endif
 #endif /* end of RT_USING_DFS */
 
 #ifdef RT_USING_NEWLIB
-	libc_system_init(RT_CONSOLE_DEVICE_NAME);
+    libc_system_init(RT_CONSOLE_DEVICE_NAME);
 #else
-
-	/* the pthread system initialization will be initiallized in libc */
-#ifdef RT_USING_PTHREADS 
-	pthread_system_init();
-#endif
+    /* the pthread system initialization will be initiallized in libc */
+    #ifdef RT_USING_PTHREADS 
+    pthread_system_init();
+    #endif
 #endif
 
 #ifdef RT_USING_RTGUI
-	rtgui_system_server_init();
+    rtgui_system_server_init();
 #endif
 
 #ifdef RT_USING_USB_HOST
-	rt_usb_host_init();
+    rt_usb_host_init();
 #endif
 
-	return;
+    return;
 }
