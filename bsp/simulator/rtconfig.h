e56e9a2674f13644e208265762720b704f66ba01
goprife@gmail.com <goprife@gmail.com>
2012-12-31 06:02:18 +0000

    remove one comment line in simulator/rtconfig.h
    git-svn-id: https://rt-thread.googlecode.com/svn/trunk@2574 bbd45198-f89e-11dd-88c7-29a3b14d5316

--- bsp/simulator/rtconfig.h
+++ bsp/simulator/rtconfig.h
@@ -123,7 +123,6 @@
 /* DFS: uffs nand file system options */
 #define RT_USING_DFS_UFFS
 /* configuration for uffs, more to see dfs_uffs.h and uffs_config.h */
-/* use ecc soft, the uffs will do all the things about ecc */
 #define RT_CONFIG_UFFS_ECC_MODE  UFFS_ECC_HW_AUTO
 /* enable this ,you need provide a mark_badblock/check_block function */
 //#define RT_UFFS_USE_CHECK_MARK_FUNCITON
