0961cc5e36154b1eb2c474cb9472fdf5201c1cf5
Grissiom <chaos.proton@gmail.com>
2013-01-24 13:06:12 +0800

    simulator: no need to dump the bin file

--- bsp/simulator/rtconfig.py
+++ bsp/simulator/rtconfig.py
@@ -52,7 +52,7 @@ if PLATFORM == 'gcc':
     else:
         CFLAGS += ' -O2'
 
-    POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
+    POST_ACTION = ''
 
 elif PLATFORM == 'cl':
     # toolchains
