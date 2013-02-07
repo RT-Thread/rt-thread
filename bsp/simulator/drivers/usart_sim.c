38f1c4ef5526ddd53a11c3ad5ad3caaab72bf2f2
prife <goprife@gmail.com>
2013-02-07 23:32:26 +0800

    add declaration in simulator/drivers/usart_sim.c

--- bsp/simulator/drivers/usart_sim.c
+++ bsp/simulator/drivers/usart_sim.c
@@ -1,3 +1,18 @@
+/*
+ * File      : serial.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2013 RT-Thread Develop Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2012-09-25     prife        first implementation
+ * 2013-01-15     prife        support linux
+ * 2013-02-6      prife        rewrite to fit the new serial.c
+ */
 #include  <rthw.h>
 #include  <rtthread.h>
 
