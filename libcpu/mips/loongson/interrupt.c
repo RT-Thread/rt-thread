93e04a13667608cb20d098f56a5f1cce75fd8f07
aozima <wuyangyong@gmail.com>
2013-03-23 17:48:36 +0800

    update loongson dev3210: Modify the interrupt interface implementations.

--- libcpu/mips/loongson/interrupt.c
+++ libcpu/mips/loongson/interrupt.c
@@ -10,8 +10,11 @@
  * Change Logs:
  * Date           Author       Notes
  * 2010-10-15     Bernard      first version
+ * 2013-03-29     aozima       Modify the interrupt interface implementations.
  */
+
 #include <rtthread.h>
+#include <rthw.h>
 #include "soc3210.h"
 
 #define MAX_INTR 32
@@ -20,17 +23,17 @@ extern rt_uint32_t rt_interrupt_nest;
 rt_uint32_t rt_interrupt_from_thread, rt_interrupt_to_thread;
 rt_uint32_t rt_thread_switch_interrupt_flag;
 
-static rt_isr_handler_t irq_handle_table[MAX_INTR];
+static struct rt_irq_desc irq_handle_table[MAX_INTR];
 void rt_interrupt_dispatch(void *ptreg);
 void rt_hw_timer_handler();
 
 /**
  * @addtogroup Loongson SoC3210
  */
- 
+
 /*@{*/
 
-void rt_hw_interrupt_handler(int vector)
+static void rt_hw_interrupt_handler(int vector, void *param)
 {
 	rt_kprintf("Unhandled interrupt %d occured!!!\n", vector);
 }
@@ -40,11 +43,12 @@ void rt_hw_interrupt_handler(int vector)
  */
 void rt_hw_interrupt_init(void)
 {
-	rt_int32_t index;
+	rt_int32_t idx;
 
-	for (index = 0; index < MAX_INTR; index ++)
+    rt_memset(irq_handle_table, 0x00, sizeof(irq_handle_table));
+	for (idx = 0; idx < MAX_INTR; idx ++)
 	{
-		irq_handle_table[index] = (rt_isr_handler_t)rt_hw_interrupt_handler;
+		irq_handle_table[idx].handler = rt_hw_interrupt_handler;
 	}
 
 	/* init interrupt nest, and context in thread sp */
@@ -79,52 +83,70 @@ void rt_hw_interrupt_umask(int vector)
  * @param new_handler the interrupt service routine to be installed
  * @param old_handler the old interrupt service routine
  */
-void rt_hw_interrupt_install(int vector, rt_isr_handler_t new_handler, rt_isr_handler_t *old_handler)
+rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler,
+        void *param, char *name)
 {
-	if (vector >= 0 && vector < MAX_INTR)
-	{
-		if (old_handler != RT_NULL)
-			*old_handler = irq_handle_table[vector];
-		if (new_handler != RT_NULL)
-			irq_handle_table[vector] = (rt_isr_handler_t)new_handler;
-	}
+    rt_isr_handler_t old_handler = RT_NULL;
+
+    if(vector < MAX_INTR)
+    {
+        old_handler = irq_handle_table[vector].handler;
+
+        if (handler != RT_NULL)
+        {
+#ifdef RT_USING_INTERRUPT_INFO
+		    rt_strncpy(irq_handle_table[vector].name, name, RT_NAME_MAX);
+#endif /* RT_USING_INTERRUPT_INFO */
+		    irq_handle_table[vector].handler = handler;
+            irq_handle_table[vector].param = param;
+        }
+    }
+
+    return old_handler;
 }
 
 void rt_interrupt_dispatch(void *ptreg)
 {
-    int i;
-	rt_isr_handler_t irq_func;
-	static rt_uint32_t status = 0;
-	rt_uint32_t c0_status;
+    int irq;
+    void *param;
+    rt_isr_handler_t irq_func;
+    static rt_uint32_t status = 0;
+    rt_uint32_t c0_status;
 
-	/* check os timer */
-	c0_status = read_c0_status();
-	if (c0_status & 0x8000)
-	{
-		rt_hw_timer_handler();
-	}
+    /* check os timer */
+    c0_status = read_c0_status();
+    if (c0_status & 0x8000)
+    {
+        rt_hw_timer_handler();
+    }
 
-	if (c0_status & 0x0400)
-	{
-		/* the hardware interrupt */
-		status |= INT_ISR;
-		if (!status) return;
+    if (c0_status & 0x0400)
+    {
+        /* the hardware interrupt */
+        status |= INT_ISR;
+        if (!status) return;
 
-		for (i = MAX_INTR; i > 0; --i)
-		{
-			if ((status & (1<<i)))
-			{
-				status &= ~(1<<i);
-				irq_func = irq_handle_table[i];
+        for (irq = MAX_INTR; irq > 0; --irq)
+        {
+            if ((status & (1 << irq)))
+            {
+                status &= ~(1 << irq);
 
-				/* do interrupt */
-				(*irq_func)(i);
+                irq_func = irq_handle_table[irq].handler;
+                param = irq_handle_table[irq].param;
 
-				/* ack interrupt */
-				INT_CLR = (1 << i);
-			}
-		}
-	}
+                /* do interrupt */
+                (*irq_func)(irq, param);
+
+#ifdef RT_USING_INTERRUPT_INFO
+                irq_handle_table[irq].counter++;
+#endif /* RT_USING_INTERRUPT_INFO */
+
+                /* ack interrupt */
+                INT_CLR = (1 << irq);
+            }
+        }
+    }
 }
 
 /*@}*/
