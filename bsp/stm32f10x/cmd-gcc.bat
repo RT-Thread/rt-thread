a0f27d78e12398165aba046e05e1ba3a121849f5
Bright Pan <loststriker@gmail.com>
2014-03-03 17:28:19 +0800

    Port uart driver for new serial driver framework

---
+++ bsp/stm32f10x/cmd-gcc.bat
@@ -0,0 +1,5 @@
+set path=C:\Program Files (x86)\GNU Tools ARM Embedded\4.8 2013q4\bin;d:\Python27\Scripts;%PATH%
+set RTT_ROOT=e:\rt-thread-1
+set RTT_EXEC_PATH=C:\Program Files (x86)\GNU Tools ARM Embedded\4.8 2013q4\bin
+set RTT_CC=gcc
+cmd
