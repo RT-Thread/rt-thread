17ef8ff63e30a4c1a89cbde3d5535fd223496fea
Bernard Xiong <bernard.xiong@gmail.com>
2014-03-29 14:55:36 +0800

    [Kernel] change version number to 1.2.1.

--- include/rtdef.h
+++ include/rtdef.h
@@ -49,7 +49,7 @@ extern "C" {
 /* RT-Thread version information */
 #define RT_VERSION                      1L              /**< major version number */
 #define RT_SUBVERSION                   2L              /**< minor version number */
-#define RT_REVISION                     0L              /**< revise version number */
+#define RT_REVISION                     1L              /**< revise version number */
 
 /* RT-Thread version */
 #define RTTHREAD_VERSION                ((RT_VERSION * 10000) + \
