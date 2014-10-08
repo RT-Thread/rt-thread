4d2972a85f654a7d699a758c26793a029fb5f960
Grissiom <chaos.proton@gmail.com>
2014-10-08 17:53:26 +0800

    tools: fix SrcRemove on empty src
    If the src is empty list, it will crash at:
    
        IndexError: list index out of range:
          File "/home/xxx/src/SConstruct", line 39:
            objs = PrepareBuilding(env, RTT_ROOT, has_libcpu=True)
          ...
          File "/home/xxx/src/drivers/SConscript", line 12:
            SrcRemove(src, src_need_remove)
          File "/home/rt-thread-stable/tools/building.py", line 496:
            if type(src[0]) == type('str'):
    
    Commit d33df46f1921f241199e7520e384e851840571ef in master.

--- tools/building.py
+++ tools/building.py
@@ -493,6 +493,9 @@ def EndBuilding(target, program = None):
         CscopeDatabase(Projects)
 
 def SrcRemove(src, remove):
+    if not src:
+        return
+
     if type(src[0]) == type('str'):
         for item in src:
             if os.path.basename(item) in remove:
