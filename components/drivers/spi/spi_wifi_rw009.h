92fac29f708e57bf2cdf0efb69627dd03128db6e
aozima <wuyangyong@gmail.com>
2014-11-15 16:11:24 +0800

    update RW009 driver: add softAP support.

--- components/drivers/spi/spi_wifi_rw009.h
+++ components/drivers/spi/spi_wifi_rw009.h
@@ -83,6 +83,12 @@ struct spi_data_packet
 #define SSID_NAME_LENGTH_MAX        (32)
 #define PASSWORD_LENGTH_MAX         (64)
 
+typedef enum
+{
+    MODE_STATION=0,
+    MODE_SOFTAP=1,
+} wifi_mode_t;
+
 typedef struct _rw009_ap_info
 {
     char        ssid[SSID_NAME_LENGTH_MAX];
@@ -122,6 +128,15 @@ typedef struct _rw009_cmd_rssi
     uint8_t bssid[8]; // 6byte + 2byte PAD.
 } rw009_cmd_rssi;
 
+typedef struct _rw009_cmd_softap
+{
+    char ssid[SSID_NAME_LENGTH_MAX];
+    char passwd[PASSWORD_LENGTH_MAX];
+
+    uint32_t    security;           /* Security type. */
+    uint32_t    channel;            /* Radio channel that the AP beacon was received on   */
+} rw009_cmd_softap;
+
 typedef struct _rw009_resp_join
 {
     rw009_ap_info ap_info;
@@ -139,6 +154,7 @@ struct rw009_cmd
         rw009_cmd_easy_join easy_join;
         rw009_cmd_join join;
         rw009_cmd_rssi rssi;
+        rw009_cmd_softap softap;
     } params;
 };
 
@@ -162,6 +178,39 @@ struct rw009_resp
 #define RW009_CMD_JOIN              130
 #define RW009_CMD_EASY_JOIN         131
 #define RW009_CMD_RSSI              132
+#define RW009_CMD_SOFTAP            133
+
+/** cond !ADDTHIS*/
+#define SHARED_ENABLED  0x00008000
+#define WPA_SECURITY    0x00200000
+#define WPA2_SECURITY   0x00400000
+#define WPS_ENABLED     0x10000000
+#define WEP_ENABLED        0x0001
+#define TKIP_ENABLED        0x0002
+#define AES_ENABLED        0x0004
+#define WSEC_SWFLAG        0x0008
+/** endcond */
+/**
+ * Enumeration of Wi-Fi security modes
+ */
+typedef enum
+{
+    SECURITY_OPEN           = 0,                                                /**< Open security                           */
+    SECURITY_WEP_PSK        = WEP_ENABLED,                                      /**< WEP Security with open authentication   */
+    SECURITY_WEP_SHARED     = ( WEP_ENABLED | SHARED_ENABLED ),                 /**< WEP Security with shared authentication */
+    SECURITY_WPA_TKIP_PSK   = ( WPA_SECURITY  | TKIP_ENABLED ),                 /**< WPA Security with TKIP                  */
+    SECURITY_WPA_AES_PSK    = ( WPA_SECURITY  | AES_ENABLED ),                  /**< WPA Security with AES                   */
+    SECURITY_WPA2_AES_PSK   = ( WPA2_SECURITY | AES_ENABLED ),                  /**< WPA2 Security with AES                  */
+    SECURITY_WPA2_TKIP_PSK  = ( WPA2_SECURITY | TKIP_ENABLED ),                 /**< WPA2 Security with TKIP                 */
+    SECURITY_WPA2_MIXED_PSK = ( WPA2_SECURITY | AES_ENABLED | TKIP_ENABLED ),   /**< WPA2 Security with AES & TKIP           */
+
+    SECURITY_WPS_OPEN       = WPS_ENABLED,                                      /**< WPS with open security                  */
+    SECURITY_WPS_SECURE     = (WPS_ENABLED | AES_ENABLED),                      /**< WPS with AES security                   */
+
+    SECURITY_UNKNOWN        = -1,                                               /**< May be returned by scan function if security is unknown. Do not pass this to the join function! */
+
+    SECURITY_FORCE_32_BIT   = 0x7fffffff                                        /**< Exists only to force wiced_security_t type to 32 bits */
+} security_t;
 
 /* porting */
 extern void spi_wifi_hw_init(void);
@@ -169,7 +218,9 @@ extern void spi_wifi_int_cmd(rt_bool_t cmd);
 extern rt_bool_t spi_wifi_is_busy(void);
 
 /* export API. */
+extern rt_err_t rt_hw_wifi_init(const char *spi_device_name,wifi_mode_t mode);
 extern int32_t rw009_rssi(void);
 extern rt_err_t rw009_join(const char * SSID, const char * passwd);
+extern rt_err_t rw009_softap(const char * SSID, const char * passwd,uint32_t security,uint32_t channel);
 
 #endif // SPI_WIFI_H_INCLUDED
