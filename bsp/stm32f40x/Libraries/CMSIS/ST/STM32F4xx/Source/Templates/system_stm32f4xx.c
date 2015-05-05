7962a5c46c7b8e17429d1ee10d8e8bf8ac092014
Bernard Xiong <bernard.xiong@gmail.com>
2015-05-05 10:13:52 +0800

    [BSP] Update bsp code
    1. Use RT_USING_LIBC instead of RT_USING_NEWLIB in the beagonebone.
    2. Enable LIBC in default in LPC4088.
    3. Remove the unnecessary interrupt clean in STM32 UART;
    4. Use HSE_VALUE to calculate PLL_M in STM32F2/F4;

--- bsp/stm32f40x/Libraries/CMSIS/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ bsp/stm32f40x/Libraries/CMSIS/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -146,7 +146,7 @@
 
 /************************* PLL Parameters *************************************/
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_M      25
+#define PLL_M      (HSE_VALUE / 1000000)
 #define PLL_N      336
 
 /* SYSCLK = PLL_VCO / PLL_P */
