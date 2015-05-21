d3a031c084affb79c37346a208714a1b3427f129
pangwei <271372443@qq.com>
2015-05-21 15:26:31 +0800

    [BSP]add some header File
    Signed-off-by: pangwei <271372443@qq.com>

--- bsp/simulator/applications/application.c
+++ bsp/simulator/applications/application.c
@@ -16,8 +16,60 @@
 #include <stdio.h>
 #include <board.h>
 
-#include <components.h>
+#ifdef RT_USING_FINSH
+#include <finsh.h>
+#include <shell.h>
+#endif
 
+#ifdef RT_USING_LWIP
+#include <lwip/sys.h>
+#include <netif/ethernetif.h>
+extern void lwip_system_init(void);
+#endif
+
+#ifdef RT_USING_DFS
+#include <dfs_init.h>
+#include <dfs_fs.h>
+#ifdef RT_USING_DFS_ELMFAT
+#include <dfs_elm.h>
+#endif
+#if defined(RT_USING_LWIP) && defined(RT_USING_DFS_NFS)
+#include <dfs_nfs.h>
+#endif
+#ifdef RT_USING_DFS_ROMFS
+#include <dfs_romfs.h>
+#endif
+#ifdef RT_USING_DFS_DEVFS
+#include <devfs.h>
+#endif
+#ifdef RT_USING_DFS_UFFS
+#include <dfs_uffs.h>
+#endif
+#ifdef RT_USING_DFS_JFFS2
+#include <dfs_jffs2.h>
+#endif
+#ifdef RT_USING_DFS_YAFFS2
+#include <dfs_yaffs2.h>
+#endif
+#ifdef RT_USING_DFS_ROMFS
+#include <dfs_romfs.h>
+#endif
+#endif
+
+#ifdef RT_USING_NEWLIB
+#include <libc.h>
+#endif
+#ifdef RT_USING_PTHREADS
+#include <pthread.h>
+#endif
+
+#ifdef RT_USING_MODULE
+#include <rtm.h>
+#endif
+
+#ifdef RT_USING_RTGUI
+#include <rtgui/rtgui_system.h>
+#endif
 
 void rt_init_thread_entry(void *parameter)
 {
