d694043cc7ee72d0652bb82cba8f175dbfa54f48
Xue Liu <liuxuenetmail@gmail.com>
2015-11-10 23:47:12 +0100

    Initial bsp support for nRF52832

---
+++ bsp/nrf52832/drivers/board.c
@@ -0,0 +1,56 @@
+/*
+ * File      : board.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2009 RT-Thread Develop Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ */
+
+#include <rthw.h>
+#include <rtthread.h>
+
+#include <nrf52.h>
+#include <nrf52_bitfields.h>
+
+#include "board.h"
+#include "uart.h"
+
+/**
+ * @addtogroup NRF52832
+ */
+
+/** @brief: Function for handling the Systick interrupts.
+ *  Triggered every 1ms.
+ */
+void SysTick_Handler(void)
+{
+    /* enter interrupt */
+    rt_interrupt_enter();
+	
+	rt_tick_increase();  //This function will notify kernel there is one tick passed
+	
+	/* leave interrupt */
+    rt_interrupt_leave();
+}
+
+
+/**
+ * This function will initial NRF51822 board.
+ */
+void rt_hw_board_init()
+{
+    /* Configure the SysTick */
+	SysTick_Config(SystemCoreClock / RT_TICK_PER_SECOND);
+
+    /* Initial usart deriver, and set console device */
+    rt_hw_uart_init();
+
+#ifdef RT_USING_CONSOLE
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
+}
+
+/*@}*/
