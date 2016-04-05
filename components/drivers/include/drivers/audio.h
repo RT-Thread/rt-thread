80431860208d0795c81decb03e6989de2115d97e
Bernard Xiong <bernard.xiong@gmail.com>
2016-04-05 11:10:08 +0800

    [DeviceDrivers] Add audio.h header file.

---
+++ components/drivers/include/drivers/audio.h
@@ -0,0 +1,14 @@
+#ifndef AUDIO_H__
+#define AUDIO_H__
+
+/* Device Control Commands */
+#define CODEC_CMD_RESET             0
+#define CODEC_CMD_SET_VOLUME        1
+#define CODEC_CMD_GET_VOLUME        2
+#define CODEC_CMD_SAMPLERATE        3
+#define CODEC_CMD_EQ                4
+#define CODEC_CMD_3D                5
+
+#define CODEC_VOLUME_MAX            (63)
+
+#endif
