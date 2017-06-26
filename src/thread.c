1f0081e796a9b697ea5f280ea43bc416d53c7d54
Bernard Xiong <bernard.xiong@gmail.com>
2017-06-26 09:16:19 +0800

    [Kernel] Fix typo

--- src/thread.c
+++ src/thread.c
@@ -162,7 +162,7 @@ static rt_err_t _rt_thread_init(struct rt_thread *thread,
     thread->number_mask = 0;
 #if RT_THREAD_PRIORITY_MAX > 32
     thread->number = 0;
-    therad->high_mask = 0;
+    thread->high_mask = 0;
 #endif
 
     /* tick init */
