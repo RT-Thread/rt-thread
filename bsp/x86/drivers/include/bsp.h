08d13e033c8c1533af51ddf653b5ff980adf41f5
parai <parai@foxmail.com>
2017-08-16 22:37:06 +0800

    x86: keyboad input bug fix
    most of the code copy from tinix, just make it run, code quality is not good.
    
    Signed-off-by: parai <parai@foxmail.com>

--- bsp/x86/drivers/include/bsp.h
+++ bsp/x86/drivers/include/bsp.h
@@ -101,6 +101,16 @@ extern "C" {
 #define	KBSTATP				0x64	/* kbd controller status port(I) */
 #define	KBS_DIB				0x01	/* kbd data in buffer */
 #define	KBDATAP				0x60	/* kbd data port(I) */
+/* AT keyboard */
+/* 8042 ports */
+#define	KB_DATA		0x60	/* I/O port for keyboard data
+					Read : Read Output Buffer 
+					Write: Write Input Buffer(8042 Data&8048 Command) */
+#define	KB_CMD		0x64	/* I/O port for keyboard command
+					Read : Read Status Register
+					Write: Write Input Buffer(8042 Command) */
+#define	LED_CODE	0xED
+#define	KB_ACK		0xFA
 
 /*******************************************************************/
 /* Serial Register */
