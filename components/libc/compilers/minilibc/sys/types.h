b3d872740b7f95c9e2803df298665f31b3f8990f
bernard <bernard.xiong@rt-thread.com>
2017-10-31 10:24:11 +0800

    [libc] Fix the typedef in minilibc.

--- components/libc/compilers/minilibc/sys/types.h
+++ components/libc/compilers/minilibc/sys/types.h
@@ -1,27 +1,25 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-#include <rtthread.h>
+typedef long        off_t;
+typedef unsigned long size_t;
+typedef signed long   ssize_t;      /* Used for a count of bytes or an error indication. */
 
-typedef long    	off_t;
-typedef __SIZE_TYPE__ 	size_t;
-typedef signed long ssize_t;		/* Used for a count of bytes or an error indication. */
+typedef unsigned char  u_char;
+typedef unsigned short u_short;
+typedef unsigned int   u_int;
+typedef unsigned long  u_long;
 
-typedef rt_uint8_t 	u_char;
-typedef rt_uint16_t u_short;
-typedef rt_ubase_t 	u_int;
-typedef rt_uint32_t u_long;
-
-typedef rt_time_t time_t;
 typedef int mode_t;
 
 typedef unsigned long clockid_t;
 typedef int pid_t;
 
 #ifndef NULL
-#define NULL RT_NULL
+#define NULL        (0)
 #endif
 
 #define __u_char_defined
 
 #endif
+
