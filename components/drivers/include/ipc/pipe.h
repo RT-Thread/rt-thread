9db0f6a0b92b7bc91c63b8a1ef43f983ec9ec408
bernard <bernard.xiong@rt-thread.com>
2017-11-03 21:09:04 +0800

    [DeviceDrivers] Add RT_USING_POSIX condition for pipe.

--- components/drivers/include/ipc/pipe.h
+++ components/drivers/include/ipc/pipe.h
@@ -7,6 +7,8 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 
+#if defined(RT_USING_POSIX)
+
 #ifndef RT_PIPE_BUFSZ
 #define PIPE_BUFSZ    512
 #else
@@ -15,22 +17,22 @@
 
 struct rt_pipe_device
 {
-	struct rt_device parent;
+    struct rt_device parent;
 
-	/* ring buffer in pipe device */
-	struct rt_ringbuffer *fifo;
+    /* ring buffer in pipe device */
+    struct rt_ringbuffer *fifo;
 
-	rt_uint8_t readers;
-	rt_uint8_t writers;
+    rt_uint8_t readers;
+    rt_uint8_t writers;
 
-	rt_wqueue_t reader_queue;
-	rt_wqueue_t writer_queue;
+    rt_wqueue_t reader_queue;
+    rt_wqueue_t writer_queue;
 
-	struct rt_mutex lock;
+    struct rt_mutex lock;
 };
 typedef struct rt_pipe_device rt_pipe_t;
 
 rt_pipe_t *rt_pipe_create(const char *name);
 
-#endif
-
+#endif /* RT_USING_POSIX */
+#endif /* PIPE_H__ */
