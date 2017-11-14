9475f098ca9428e4ff6463659f416d0ab0864a2d
tanek liang <tanekliang@gmail.com>
2017-11-14 11:09:46 +0800

    [bsp] add gcc support

--- bsp/lpc54608-LPCXpresso/rtconfig.py
+++ bsp/lpc54608-LPCXpresso/rtconfig.py
@@ -55,7 +55,7 @@ if PLATFORM == 'gcc':
 
     CXXFLAGS = CFLAGS
 
-    POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
+    POST_ACTION = OBJCPY + ' -O ihex $TARGET rtthread.hex\n' + SIZE + ' $TARGET \n'
 
 elif PLATFORM == 'armcc':
     # toolchains
