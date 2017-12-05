08e85a452892ef5d0145ba8b2114f03f44178aa5
Bernard Xiong <bernard.xiong@gmail.com>
2017-12-05 20:17:55 +0800

    [BSP] Update qemu nographic script

--- bsp/qemu-vexpress-a9/qemu-nographic.sh
+++ bsp/qemu-vexpress-a9/qemu-nographic.sh
@@ -1,1 +1,6 @@
-qemu-system-arm -M vexpress-a9 -kernel rtthread.elf -nographic
+if [ ! -f "sd.bin" ]; then
+dd if=/dev/zero of=sd.bin bs=64M count=1
+fi
+
+qemu-system-arm -M vexpress-a9 -kernel rtthread.elf -nographic -sd sd.bin
+
