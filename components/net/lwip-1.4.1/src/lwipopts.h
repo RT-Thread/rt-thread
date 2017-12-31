d219f5bfa904100332b7e30380ab83597fbe8b85
BernardXiong <bernard.xiong@gmail.com>
2017-12-31 14:44:58 +0800

    [lwIP] code cleanup for Kconfig.
    1. code cleanup for Kconfig.
    2. fix the compiling warning for BYTE_ORDER.

--- components/net/lwip-1.4.1/src/lwipopts.h
+++ components/net/lwip-1.4.1/src/lwipopts.h
@@ -36,7 +36,10 @@
 #define LWIP_HAVE_LOOPIF            0
 
 #define LWIP_PLATFORM_BYTESWAP      0
+
+#ifndef BYTE_ORDER
 #define BYTE_ORDER                  LITTLE_ENDIAN
+#endif
 
 /* #define RT_LWIP_DEBUG */
 
