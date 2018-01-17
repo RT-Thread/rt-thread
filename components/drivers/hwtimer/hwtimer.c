bd4340a0c80300f78e3f38ff8d2acb058c6b9eb9
tangyuxin <462747508@qq.com>
2018-01-17 09:36:01 +0800

    [components]修复hwtimer单次长时间定时不准确的问题

--- components/drivers/hwtimer/hwtimer.c
+++ components/drivers/hwtimer/hwtimer.c
@@ -192,6 +192,8 @@ static rt_size_t rt_hwtimer_write(struct rt_device *dev, rt_off_t pos, const voi
     if (size != sizeof(rt_hwtimerval_t))
         return 0;
 
+    t = timeout_calc(timer, (rt_hwtimerval_t*)buffer);
+
     if ((timer->cycles <= 1) && (timer->mode == HWTIMER_MODE_ONESHOT))
     {
         opm = HWTIMER_MODE_ONESHOT;
@@ -199,7 +201,6 @@ static rt_size_t rt_hwtimer_write(struct rt_device *dev, rt_off_t pos, const voi
     timer->ops->stop(timer);
     timer->overflow = 0;
 
-    t = timeout_calc(timer, (rt_hwtimerval_t*)buffer);
     if (timer->ops->start(timer, t, opm) != RT_EOK)
         size = 0;
 
