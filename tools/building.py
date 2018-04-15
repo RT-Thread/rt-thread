74d045093bb4bda0d710c32c1a815aa672742c17
Bernard Xiong <bernard.xiong@gmail.com>
2018-04-15 14:39:35 +0800

    [tools] Fix the sys.path issue in IARVersion.

--- tools/building.py
+++ tools/building.py
@@ -124,6 +124,7 @@ def PrepareBuilding(env, root_directory, has_libcpu=False, remove_components = [
 
     Env = env
     Rtt_Root = os.path.abspath(root_directory)
+    sys.path = sys.path + [os.path.join(Rtt_Root, 'tools')]
 
     # add compability with Keil MDK 4.6 which changes the directory of armcc.exe
     if rtconfig.PLATFORM == 'armcc':
