6725dfa59806410dcb60b59d269a92e64503da2d
zhuangwei123 <zhuangwei@nblink-tech.com>
2018-05-10 22:20:37 +0800

    [bsp/ls1cdev] 1、uart驱动重写，采用uart驱动框架 2、添加并修改勤为本ls1c_uart 3、开启main为用户程序入口，删除application.c和startup.c,并对初始化部分做相应调整更新。 4、开启dhcp

--- bsp/ls1cdev/drivers/drv_uart.h
+++ bsp/ls1cdev/drivers/drv_uart.h
@@ -1,20 +1,19 @@
 /*
- * File      : uart.h
+ * File      : drv_uart.h
  * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006-2012, RT-Thread Develop Team
+ * COPYRIGHT (C) 2009, RT-Thread Development Team
  *
  * The license and distribution terms for this file may be
  * found in the file LICENSE in this distribution or at
  * http://www.rt-thread.org/license/LICENSE
  *
  * Change Logs:
- * Date               Author             Notes
- * 2011-08-08     lgnq            first version for LS1B
- * 2015-07-06     chinesebear  modified for loongson 1c
+ * Date           Author       Notes
+ * 2018-05-08     zhuangwei    the first version
  */
 
-#ifndef __UART_H__
-#define __UART_H__
+#ifndef __DRV_UART_H__
+#define __DRV_UART_H__
 
 #include "ls1c.h"
 
@@ -99,5 +98,6 @@
 #define UARTLSR_DR		(1 << 0)
 
 void rt_hw_uart_init(void);
+
 
 #endif
