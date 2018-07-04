0a8c4317788c69c8a9114241fc9a9bd08e255692
whj4674672 <whj4674672@163.com>
2018-07-04 20:17:28 +0800

    [DeviceDriver][SFUD] add at45db321e

--- components/drivers/spi/sfud/inc/sfud_flash_def.h
+++ components/drivers/spi/sfud/inc/sfud_flash_def.h
@@ -110,9 +110,7 @@ typedef struct {
 #define SFUD_FLASH_CHIP_TABLE                                                                                       \
 {                                                                                                                   \
     {"AT45DB161E", SFUD_MF_ID_ATMEL, 0x26, 0x00, 2L*1024L*1024L, SFUD_WM_BYTE|SFUD_WM_DUAL_BUFFER, 512, 0x81},      \
-
     {"AT45DB321E", SFUD_MF_ID_ATMEL, 0x27, 0x01, 4L*1024L*1024L, SFUD_WM_BYTE|SFUD_WM_DUAL_BUFFER, 512, 0x81},      \
-
     {"W25Q40BV", SFUD_MF_ID_WINBOND, 0x40, 0x13, 512L*1024L, SFUD_WM_PAGE_256B, 4096, 0x20},                        \
     {"W25Q16BV", SFUD_MF_ID_WINBOND, 0x40, 0x15, 2L*1024L*1024L, SFUD_WM_PAGE_256B, 4096, 0x20},                    \
     {"SST25VF016B", SFUD_MF_ID_SST, 0x25, 0x41, 2L*1024L*1024L, SFUD_WM_BYTE|SFUD_WM_AAI, 4096, 0x20},              \
