6b07c895d942ef6c15ca06492c356bc655325a43
heyuanjie <xx>
2018-07-12 10:41:56 +0800

    [example]文件测试增加msh接口

--- examples/file/writespeed.c
+++ examples/file/writespeed.c
@@ -13,6 +13,7 @@
  */
 #include <rtthread.h>
 #include <dfs_posix.h>
+#include <stdlib.h>
 
 void writespeed(const char* filename, int total_length, int block_size)
 {
@@ -36,37 +37,69 @@ void writespeed(const char* filename, int total_length, int block_size)
         return;
     }
 
-	/* prepare write data */
-	for (index = 0; index < block_size; index++)
-	{
-		buff_ptr[index] = index;
-	}
-	index = 0;
+    /* prepare write data */
+    for (index = 0; index < block_size; index++)
+    {
+        buff_ptr[index] = index;
+    }
+    index = 0;
 
-	/* get the beginning tick */
+    /* get the beginning tick */
     tick = rt_tick_get();
-	while (index < total_length / block_size)
-	{
-		length = write(fd, buff_ptr, block_size);
-		if (length != block_size)
-		{
-			rt_kprintf("write failed\n");
-			break;
-		}
+    while (index < total_length / block_size)
+    {
+        length = write(fd, buff_ptr, block_size);
+        if (length != block_size)
+        {
+            rt_kprintf("write failed\n");
+            break;
+        }
 
-		index ++;
-	}
+        index ++;
+    }
     tick = rt_tick_get() - tick;
 
-	/* close file and release memory */
+    /* close file and release memory */
     close(fd);
-	rt_free(buff_ptr);
+    rt_free(buff_ptr);
 
     /* calculate write speed */
-    rt_kprintf("File write speed: %d byte/s\n", total_length / tick * RT_TICK_PER_SECOND);
+	if (tick)
+        rt_kprintf("File write speed: %d byte/s\n", total_length / tick * RT_TICK_PER_SECOND);
+    else
+		rt_kprintf("time is not long enough\n");
+}
+
+static void cmd_writespeed(int argc, char **argv)
+{
+    int ts = 1024*1024*10;
+    int bs = 512;
+    char *name = "/_tws.dat";
+
+    while (argc)
+    {
+        switch (argc)
+        {
+        case 2:
+            name = argv[1];
+            break;  
+        case 3:
+            ts = atoi(argv[2]);
+            break;
+        case 4:
+			bs = atoi(argv[3]);
+            break;
+        }
+            
+        argc --;
+    }
+    rt_kprintf("name: %s, size: %d, blksize: %d\n", name, ts, bs);
+
+    writespeed(name, ts, bs);
 }
 
 #ifdef RT_USING_FINSH
 #include <finsh.h>
 FINSH_FUNCTION_EXPORT(writespeed, perform file write test);
+MSH_CMD_EXPORT_ALIAS(cmd_writespeed, writespeed, writespeed [name] [size] [blksize]);
 #endif
