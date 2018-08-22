bc7a43bdaf0b4c3e2dfb77bc5223ff19eac3a2a4
armink <armink.ztl@gmail.com>
2018-08-22 16:41:56 +0800

    [DeviceDriver][SFUD] Replace the vsnprintf to rt_vsnprintf.

--- components/drivers/spi/spi_flash_sfud.c
+++ components/drivers/spi/spi_flash_sfud.c
@@ -189,7 +189,7 @@ void sfud_log_debug(const char *file, const long line, const char *format, ...) 
     va_start(args, format);
     rt_kprintf("[SFUD] (%s:%ld) ", file, line);
     /* must use vprintf to print */
-    vsnprintf(log_buf, sizeof(log_buf), format, args);
+    rt_vsnprintf(log_buf, sizeof(log_buf), format, args);
     rt_kprintf("%s\n", log_buf);
     va_end(args);
 }
@@ -207,7 +207,7 @@ void sfud_log_info(const char *format, ...) {
     va_start(args, format);
     rt_kprintf("[SFUD] ");
     /* must use vprintf to print */
-    vsnprintf(log_buf, sizeof(log_buf), format, args);
+    rt_vsnprintf(log_buf, sizeof(log_buf), format, args);
     rt_kprintf("%s\n", log_buf);
     va_end(args);
 }
