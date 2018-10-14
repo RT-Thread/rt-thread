bcfeaa061399ef640f97c4f400f4495fd8cf3128
Bernard Xiong <bernard.xiong@gmail.com>
2018-10-14 19:37:18 +0800

    [license] Change license to Apache.

--- components/drivers/usb/usbdevice/class/ndis.h
+++ components/drivers/usb/usbdevice/class/ndis.h
@@ -1,4 +1,12 @@
 /*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+/*
  * ndis.h 
  * 
  * Modified by Colin O'Flynn <coflynn@newae.com>
