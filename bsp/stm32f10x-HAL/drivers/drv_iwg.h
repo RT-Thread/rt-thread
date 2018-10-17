785d0daab1289f661f6a504026581cca101503c8
XXXXzzzz000 <119524428@qq.com>
2018-10-17 15:54:00 +0800

    [driver][iwdg]add iwdg driver. | 添加iwdg驱动,参考:stm32f4xx-HAL/driver_wdg

---
+++ bsp/stm32f10x-HAL/drivers/drv_iwg.h
@@ -0,0 +1,18 @@
+/*
+ * File      : drv_iwg.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2015, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2017-11-08     ZYH            the first version
+ */
+
+#ifndef __DRV_IWG_H__
+#define __DRV_IWG_H__
+extern int rt_iwg_init(void);
+#endif
