2afb12575434e2f5e74848901afe380c5499657a
SummerGift <SummerGift@qq.com>
2018-11-29 17:00:22 +0800

    [bsp][STM32] Add bsp based on new framework

---
+++ bsp/stm32/libraries/templates/stm32l4xx/applications/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   change to new framework
+ */
+
+#include <rtthread.h>
+
+int main(void)
+{
+    return 0;
+}
---
+++ bsp/stm32/stm32f407-atk-explorer/board/board.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-5      SummerGift   change to new framework
+ */
+
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <rtthread.h>
+#include <stm32f4xx.h>
+#include "drv_common.h"
+
+#ifdef BSP_USING_GPIO
+#include "drv_gpio.h"
+/* Board Pin definitions */
+#define LED0_PIN                       GET_PIN(F,  9)
+#define LED1_PIN                       GET_PIN(F, 10)
+#endif
+
+#define STM32_SRAM1_SIZE               (128)
+#define STM32_SRAM1_START              (0x20000000)
+#define STM32_SRAM1_END                (STM32_SRAM1_START + STM32_SRAM1_SIZE * 1024)
+
+#define HEAP_BEGIN                     STM32_SRAM1_START
+#define HEAP_END                       STM32_SRAM1_END
+
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+
+#endif
+
