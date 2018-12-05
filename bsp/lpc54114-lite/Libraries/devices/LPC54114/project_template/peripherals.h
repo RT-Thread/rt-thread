e76cfd0b11a7dac8acf6df833a72bd0e3dc170ff
yangjie <yangjiexaut@126.com>
2018-12-05 11:44:53 +0800

    [bsp]add bsp lpc54114-lite

---
+++ bsp/lpc54114-lite/Libraries/devices/LPC54114/project_template/peripherals.h
@@ -0,0 +1,16 @@
+#ifndef _PERIPHERALS_H_
+#define _PERIPHERALS_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /*_cplusplus. */
+/*******************************************************************************
+ * BOARD_InitBootPeripherals function
+ ******************************************************************************/
+void BOARD_InitBootPeripherals(void);
+
+#if defined(__cplusplus)
+}
+#endif /*_cplusplus. */
+
+#endif /* _PERIPHERALS_H_ */
