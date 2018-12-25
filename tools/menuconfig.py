9ffafacd66c5fe4c27f85eaa5fa53ff49e4df0bb
MurphyZhao <d2014zjt@163.com>
2018-12-25 14:18:50 +0800

    [tools] pyconfig 增加 silent 模式，不显示窗口但可以更新 .config 和 rtconfig.h
    Signed-off-by: MurphyZhao <d2014zjt@163.com>

--- tools/menuconfig.py
+++ tools/menuconfig.py
@@ -251,3 +251,31 @@ def pyconfig(RTT_ROOT):
     if mtime != mtime2:
         mk_rtconfig(fn)
 
+
+# pyconfig_silent for windows and linux
+def pyconfig_silent(RTT_ROOT):
+    import pymenuconfig
+    print("In pyconfig silent mode. Don`t display menuconfig window.")
+
+    touch_env()
+    env_dir = get_env_dir()
+
+    os.environ['PKGS_ROOT'] = os.path.join(env_dir, 'packages')
+
+    fn = '.config'
+
+    if os.path.isfile(fn):
+        mtime = os.path.getmtime(fn)
+    else:
+        mtime = -1
+
+    pymenuconfig.main(['--kconfig', 'Kconfig', '--config', '.config', '--silent', 'True'])
+
+    if os.path.isfile(fn):
+        mtime2 = os.path.getmtime(fn)
+    else:
+        mtime2 = -1
+
+    # make rtconfig.h
+    if mtime != mtime2:
+        mk_rtconfig(fn)
