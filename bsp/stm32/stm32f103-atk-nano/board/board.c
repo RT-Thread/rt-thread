2254a47fdd71a0a63838c6478e85affbff1ea78e
SummerGift <SummerGift@qq.com>
2018-12-26 10:36:54 +0800

    [bap][stm32] update f1 series bsp

--- bsp/stm32/stm32f103-atk-nano/board/board.c
+++ bsp/stm32/stm32f103-atk-nano/board/board.c
@@ -53,13 +53,3 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
-
-void MX_GPIO_Init(void)
-{
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-}
-
