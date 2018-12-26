79cb96bc9eb41b19b06555f72dcfb587251722b4
SummerGift <SummerGift@qq.com>
2018-12-26 18:11:35 +0800

    [bsp][stm32] update template

--- bsp/stm32/libraries/templates/stm32f0xx/board/board.h
+++ bsp/stm32/libraries/templates/stm32f0xx/board/board.h
@@ -15,6 +15,10 @@
 #include <stm32f0xx.h>
 #include "drv_common.h"
 
+#define STM32_FLASH_START_ADRESS     ((uint32_t)0x08000000)
+#define STM32_FLASH_SIZE             (256 * 1024)
+#define STM32_FLASH_END_ADDRESS      ((uint32_t)(STM32_FLASH_START_ADRESS + STM32_FLASH_SIZE))
+
 /* Internal SRAM memory size[Kbytes] <8-64>, Default: 64*/
 #define STM32_SRAM_SIZE      32
 #define STM32_SRAM_END       (0x20000000 + STM32_SRAM_SIZE * 1024)
