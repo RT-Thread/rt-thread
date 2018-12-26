02884fba6b555ffb50a3f34d900bf030aa32a271
SummerGift <SummerGift@qq.com>
2018-12-26 18:11:35 +0800

    [bsp][stm32] update template

--- bsp/stm32/libraries/templates/stm32f7xx/board/board.h
+++ bsp/stm32/libraries/templates/stm32f7xx/board/board.h
@@ -15,9 +15,11 @@
 #include <stm32f7xx.h>
 #include "drv_common.h"
 
-#ifdef BSP_USING_GPIO
+#define STM32_FLASH_START_ADRESS     ((uint32_t)0x08000000)
+#define STM32_FLASH_SIZE             (1024 * 1024)
+#define STM32_FLASH_END_ADDRESS      ((uint32_t)(STM32_FLASH_START_ADRESS + STM32_FLASH_SIZE))
 
-#define STM32_SRAM_SIZE           256
+#define STM32_SRAM_SIZE           (512)
 #define STM32_SRAM_END            (0x20000000 + STM32_SRAM_SIZE * 1024)
 
 #if defined(__CC_ARM) || defined(__CLANG_ARM)
@@ -36,4 +38,3 @@ extern int __bss_end;
 void SystemClock_Config(void);
 
 #endif
-
