1bc7b8e61b6967b9443cbe85433af82fd340692e
armink <armink.ztl@gmail.com>
2018-12-28 19:22:33 +0800

    [libcpu] Sync to latest version.

--- libcpu/arm/am335x/interrupt.c
+++ libcpu/arm/am335x/interrupt.c
@@ -158,7 +158,7 @@ void rt_hw_interrupt_ack(int fiq_irq)
  * @param old_handler the old interrupt service routine
  */
 rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler,
-        void *param, char *name)
+        void *param, const char *name)
 {
     rt_isr_handler_t old_handler = RT_NULL;
 
