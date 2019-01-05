1a43a8270c3d79c7312e88486757d8edfc3cc02e
jinsheng <jinsheng20@outlook.com>
2019-01-05 20:02:40 +0800

    复制stm32f767-atk-apollo工程

---
+++ bsp/stm32f746-disco/drivers/drv_mpu.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-05-17     ZYH          first implementation
+ */
+#ifndef __DRV_MPU_H
+#define __DRV_MPU_H
+/* Initialize Cortex M   MPU */
+extern int bsp_mpu_hw_init(void);
+#endif
