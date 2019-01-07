93ce3b74ecafbedbcc4a16532b59ae54e0caa3e4
Noe Xu <xeonxu@gmail.com>
2019-01-07 01:44:36 -0800

    [STM32] Optimize code generation step.
    Change-Id: Ic9f2bf49c8f48093b62b78dc6bc6882de0919b63

--- bsp/stm32/stm32f407-st-discovery/board/CubeMX_Config/Src/main.c
+++ bsp/stm32/stm32f407-st-discovery/board/CubeMX_Config/Src/main.c
@@ -98,46 +98,6 @@ static void MX_USART1_UART_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_SPI1_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 
 /**
   * @brief System Clock Configuration
