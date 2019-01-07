b4f5ffc39f6137e8b10d25e249c134ed597409ae
andeyqi <qihengchuan8888@163.com>
2019-01-07 23:58:57 +0800

    [简介：LED闪烁程序修改]

--- bsp/stm32/stm32f446-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f446-st-nucleo/applications/main.c
@@ -13,19 +13,23 @@
 #include <board.h>
 #include "drv_gpio.h"
 /* defined the LED0 pin: PB1 */
-#define LED0_PIN    GET_PIN(B, 1)
+#define LED2_PIN    GET_PIN(B, 7)
+#define LED3_PIN    GET_PIN(B, 14)
 
 int main(void)
 {
     int count = 1;
     /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED3_PIN, PIN_MODE_OUTPUT);
 
     while (count++)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_pin_write(LED2_PIN, PIN_HIGH);
+        rt_pin_write(LED3_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(LED2_PIN, PIN_LOW);
+        rt_pin_write(LED3_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
 
