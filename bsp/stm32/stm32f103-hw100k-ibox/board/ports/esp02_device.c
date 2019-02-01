11a3645537b240cd5e6eeae4dd6ef7975442df90
Bernard Xiong <bernard.xiong@gmail.com>
2019-02-01 10:07:56 +0800

    code cleanup

--- bsp/stm32/stm32f103-hw100k-ibox/board/ports/esp02_device.c
+++ bsp/stm32/stm32f103-hw100k-ibox/board/ports/esp02_device.c
@@ -12,14 +12,15 @@
 #include "drv_gpio.h"
 
 #define ESP8266_CH_PD_Pin GET_PIN(G,1)
-#define ESP8266_RST      GET_PIN(G,0)
+#define ESP8266_RST       GET_PIN(G,0)
 
 int esp_02_device_init()
 {
     rt_pin_mode(ESP8266_CH_PD_Pin,PIN_MODE_OUTPUT) ;
-		rt_pin_mode(ESP8266_RST,PIN_MODE_OUTPUT) ;
-		rt_pin_write(ESP8266_CH_PD_Pin,PIN_HIGH) ;
-		rt_pin_write(ESP8266_RST,PIN_HIGH) ;
+    rt_pin_mode(ESP8266_RST,PIN_MODE_OUTPUT) ;
+    rt_pin_write(ESP8266_CH_PD_Pin,PIN_HIGH) ;
+    rt_pin_write(ESP8266_RST,PIN_HIGH) ;
+
     return RT_EOK ;	
 }
 INIT_DEVICE_EXPORT(esp_02_device_init);
