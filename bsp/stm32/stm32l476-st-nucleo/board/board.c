c8a0f750c14adf11920d083234065ea3eda21c9d
Vincent-VG <gycgwzw@gmail.com>
2019-02-05 10:16:46 +0800

    修改board.c文件头

--- bsp/stm32/stm32l476-st-nucleo/board/board.c
+++ bsp/stm32/stm32l476-st-nucleo/board/board.c
@@ -1,15 +1,11 @@
 /*
- * File      : board.c
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2009 RT-Thread Develop Team
+ * Copyright (c) 2006-2019, RT-Thread Development Team
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2019-02-05     gw      first implementation
+ * 2019-02-05     gw        first version
  */
 
 #include <board.h>
