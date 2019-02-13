edddeb8c09eb2ee3fb70f96e130ec3495eea7025
wangyq2018 <wyquan2013@163.com>
2019-02-13 11:06:34 +0800

    [bsp] add bsp es32f0654

---
+++ bsp/es32f0654/drivers/drv_usart.h
@@ -0,0 +1,17 @@
+/*
+ * File      : drv_usart.h
+ * Copyright (C) 2018 Shanghai Eastsoft Microelectronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author        Notes
+ * 2019-01-23     wangyq        the first version
+ */
+
+#ifndef DRV_USART_H__
+#define DRV_USART_H__
+
+int rt_hw_usart_init(void);
+
+#endif
