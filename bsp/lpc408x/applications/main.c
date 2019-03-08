01671090a79b598ee2b185ff8661f32edac8bc77
misonyo <misonyo@foxmail.com>
2019-03-08 15:02:46 +0800

    [bsp/lpc408x]更新旧启动机制

---
+++ bsp/lpc408x/applications/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006-2018, Synwit Technology Co.,Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-12-10     armink    first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+
+int main(void)
+{
+    rt_kprintf("Hello RT-Thread!\n");
+    
+    return RT_EOK;
+} 
