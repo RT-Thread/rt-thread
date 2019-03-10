3960863bd07ffd7f28ef0ed86afe1439927dbecf
obito <496420502@qq.com>
2019-03-10 14:36:53 +0800

    修正对齐用空格而不是制表符

--- bsp/stm32/stm32f103-atk-warshipV3/applications/main.c
+++ bsp/stm32/stm32f103-atk-warshipV3/applications/main.c
@@ -19,21 +19,21 @@
 
 int main(void)
 {
-	int count = 1;
-	/* set LED0 pin mode to output */
-	rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-	/* set LED1 pin mode to output */
-	rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+    int count = 1;
+    /* set LED0 pin mode to output */
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    /* set LED1 pin mode to output */
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
 
-	while (count++)
-	{
-		rt_pin_write(LED0_PIN, PIN_HIGH);
-		rt_pin_write(LED1_PIN, PIN_HIGH);
-		rt_thread_mdelay(500);
-		rt_pin_write(LED0_PIN, PIN_LOW);
-		rt_pin_write(LED1_PIN, PIN_LOW);
-		rt_thread_mdelay(500);
-	}
+    while (count++)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
 
     return RT_EOK;
 }
