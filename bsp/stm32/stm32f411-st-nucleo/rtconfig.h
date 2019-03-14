ef810b1fd1b64ef661fe9f02a596d8d7a6b737c4
misonyo <misonyo@foxmail.com>
2019-03-14 11:09:13 +0800

    [bsp/stm32f411]add pwm driver

--- bsp/stm32/stm32f411-st-nucleo/rtconfig.h
+++ bsp/stm32/stm32f411-st-nucleo/rtconfig.h
@@ -39,7 +39,7 @@
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart2"
-#define RT_VER_NUM 0x40000
+#define RT_VER_NUM 0x40001
 #define ARCH_ARM
 #define ARCH_ARM_CORTEX_M
 #define ARCH_ARM_CORTEX_M4
@@ -78,6 +78,7 @@
 #define RT_USING_DEVICE_IPC
 #define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
+#define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
 
 /* Using WiFi */
