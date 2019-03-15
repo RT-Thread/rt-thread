79fd4d33459c0a52165e14d097670d63b0fa6c09
wangyq2018 <wyquan2013@163.com>
2019-03-15 15:33:29 +0800

    [bsp/es32f0654] fixed pwm timing inaccuracy when period is too small.

--- bsp/es32f0654/drivers/drv_spi.h
+++ bsp/es32f0654/drivers/drv_spi.h
@@ -7,7 +7,7 @@
  * Date           Author        Notes
  * 2019-01-24     wangyq        the first version
  */
- 
+
 #ifndef DRV_SPI_H__
 #define DRV_SPI_H__
 
@@ -20,7 +20,7 @@ struct es32f0_hw_spi_cs
     rt_uint32_t pin;
 };
 
-//cannot be used before completion init
+/* cannot be used before completion init */
 rt_err_t es32f0_spi_device_attach(rt_uint32_t pin, const char *bus_name, const char *device_name);
 int rt_hw_spi_init(void);
 
