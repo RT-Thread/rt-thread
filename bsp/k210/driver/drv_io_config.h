212da1d9976d1bb58775b926802419c6d358e0cd
ZYH <lymz@foxmail.com>
2019-03-18 16:58:16 +0800

    Add SPI driver

--- bsp/k210/driver/drv_io_config.h
+++ bsp/k210/driver/drv_io_config.h
@@ -6,9 +6,17 @@ enum HS_GPIO_CONFIG
 #ifdef BSP_USING_LCD
     LCD_DC_PIN = 0,     /* LCD DC PIN */
 #endif
-
-#ifdef BSP_USING_SDCARD
-    SD_CS_PIN,          /* SD_CS_PIN */
+#ifdef BSP_SPI1_USING_SS0
+    SPI1_CS0_PIN,
+#endif
+#ifdef BSP_SPI1_USING_SS1
+    SPI1_CS1_PIN,
+#endif
+#ifdef BSP_SPI1_USING_SS2
+    SPI1_CS2_PIN,
+#endif
+#ifdef BSP_SPI1_USING_SS3
+    SPI1_CS3_PIN,
 #endif
 };
 
