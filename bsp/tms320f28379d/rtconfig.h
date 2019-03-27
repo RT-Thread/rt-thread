47349bb1f36cbbe22f4779998f69c8cb734db805
xuzhuoyi <xzy476386434@vip.qq.com>
2019-03-27 11:51:20 +0800

    [bsp][tms320f28379d] Update for arch data type

--- bsp/tms320f28379d/rtconfig.h
+++ bsp/tms320f28379d/rtconfig.h
@@ -9,6 +9,7 @@
 /* RT-Thread Kernel */
 
 #define RT_NAME_MAX 8
+#define RT_USING_ARCH_DATA_TYPE
 #define RT_ALIGN_SIZE 4
 #define RT_THREAD_PRIORITY_8
 #define RT_THREAD_PRIORITY_MAX 8
@@ -135,13 +136,12 @@
 
 /* peripheral libraries and drivers */
 
-/* sensors drivers */
-
 
 /* miscellaneous packages */
 
 
 /* samples: kernel and components samples */
 
+#include "rtconfig_project.h"
 
 #endif
