476dabbc02adb0cc16f759ba68783cc14d0073c4
Wenlong.Fan <fanwenl_g@126.com>
2019-03-29 10:50:01 +0800

    修改版权信息,完善一些细节,修改IAR不能打包的问题

--- bsp/w60x/drivers/drv_pwm.c
+++ bsp/w60x/drivers/drv_pwm.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2018, RT-Thread Development Team
+ * Copyright (c) 2019 Winner Microelectronics Co., Ltd.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2018-11-22      fanwenl      1st version
+ * 2018-11-22     fanwenl      1st version
  */
 
 #include <rtthread.h>
