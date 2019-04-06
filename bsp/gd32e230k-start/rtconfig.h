938d9fa3d9788b9504a8c23817fb4d007d8b4471
xuzhuoyi <xzy476386434@vip.qq.com>
2019-04-07 00:37:43 +0800

    [bsp][gd32e230k-start] Fix USART bug and change license

--- bsp/gd32e230k-start/rtconfig.h
+++ bsp/gd32e230k-start/rtconfig.h
@@ -53,18 +53,6 @@
 
 /* Command shell */
 
-#define RT_USING_FINSH
-#define FINSH_THREAD_NAME "tshell"
-#define FINSH_USING_HISTORY
-#define FINSH_HISTORY_LINES 5
-#define FINSH_USING_SYMTAB
-#define FINSH_USING_DESCRIPTION
-#define FINSH_THREAD_PRIORITY 20
-#define FINSH_THREAD_STACK_SIZE 4096
-#define FINSH_CMD_SIZE 80
-#define FINSH_USING_MSH
-#define FINSH_USING_MSH_DEFAULT
-#define FINSH_ARG_MAX 10
 
 /* Device virtual file system */
 
