8f7f7f157f3603816338342c33e4d397b519f771
klivelinux <klivelinux@gmail.com>
2019-05-24 22:18:49 +0800

    [bsp][stm32] fix stm431xx GetBank

--- bsp/stm32/libraries/HAL_Drivers/drv_flash/drv_flash_l4.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_flash/drv_flash_l4.c
@@ -53,7 +53,7 @@ static uint32_t GetPage(uint32_t Addr)
 static uint32_t GetBank(uint32_t Addr)
 {
     uint32_t bank = 0;
-#if defined (STM32L432xx)
+#if defined (STM32L432xx) || (STM32L431xx)
 	bank = FLASH_BANK_1;
 #else
     if (READ_BIT(SYSCFG->MEMRMP, SYSCFG_MEMRMP_FB_MODE) == 0)
