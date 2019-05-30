b56fa473007bd9411997ec7523e4c4832547088e
tangweikang <tangweikang@rt-thread.com>
2019-05-30 09:17:14 +0800

    [bsp][stm32] fix drv_gpio.c

--- bsp/stm32/libraries/HAL_Drivers/drv_gpio.h
+++ bsp/stm32/libraries/HAL_Drivers/drv_gpio.h
@@ -14,18 +14,53 @@
 #include <drv_common.h>
 #include <board.h>
 
-#define __STM32_PORT(port)  GPIO##port
+enum
+{
+#ifdef GPIOA
+    GPIOA_PORT,
+#endif
+#ifdef GPIOB
+    GPIOB_PORT,
+#endif
+#ifdef GPIOC
+    GPIOC_PORT,
+#endif
+#ifdef GPIOD
+    GPIOD_PORT,
+#endif
+#ifdef GPIOE
+    GPIOE_PORT,
+#endif
+#ifdef GPIOF
+    GPIOF_PORT,
+#endif
+#ifdef GPIOG
+    GPIOG_PORT,
+#endif
+#ifdef GPIOH
+    GPIOH_PORT,
+#endif
+#ifdef GPIOI
+    GPIOI_PORT,
+#endif
+#ifdef GPIOJ
+    GPIOJ_PORT,
+#endif
+#ifdef GPIOK
+    GPIOK_PORT,
+#endif
+};
 
-#define GET_PIN(PORTx,PIN) (rt_base_t)((16 * ( ((rt_base_t)__STM32_PORT(PORTx) - (rt_base_t)GPIOA)/(0x0400UL) )) + PIN)
+#define GET_PIN(PORTx,PIN) (rt_base_t)(GPIO##PORTx##_PORT*16 + PIN)
 
-#define __STM32_PIN(index, gpio, gpio_index)                                \
-    {                                                                       \
-        index, GPIO##gpio, GPIO_PIN_##gpio_index                            \
+#define __STM32_PIN(gpio, gpio_index)                                         \
+    {                                                                         \
+        GET_PIN(gpio,gpio_index), GPIO##gpio, GPIO_PIN_##gpio_index           \
     }
 
-#define __STM32_PIN_RESERVE                                                 \
-    {                                                                       \
-        -1, 0, 0                                                            \
+#define __STM32_PIN_RESERVE                                                   \
+    {                                                                         \
+        -1, 0, 0                                                              \
     }
 
 /* STM32 GPIO driver */
