bb3ebe85bb61eb18c5ab26699dc9484fd479ef55
SimpleInit <63694297@qq.com>
2019-06-10 17:13:36 +0800

    更新STM32-F0系列dma，spi配置文件 1.dma_config.h F0有12个DMA通道，但只有3个DMA中断。所以仅保留了三个DMA 因drv_spi在映射DMA的中断函数是根据DMA通道的中断请求号来对应的，但是L4和F0的DMA是多个通道对应一个中断请求号，单个中断号下添加多个DMA通道会报错。希望RTT 检测下串口是否也会存在相同BUG。
    2.spi_config.h
    更新头文件适配drv_spi.c，旧版会导致编译出错
    
    Signed-off-by: SimpleInit <63694297@qq.com>

--- bsp/stm32/libraries/HAL_Drivers/config/f0/dma_config.h
+++ bsp/stm32/libraries/HAL_Drivers/config/f0/dma_config.h
@@ -19,22 +19,27 @@ extern "C" {
 #endif
 
 /* DMA1 channel1  */
-
-/* DMA1 channel2-3 DMA2 channel1-2 */
 #if defined(BSP_UART1_RX_USING_DMA) && !defined(UART1_RX_DMA_INSTANCE)
-#define UART1_DMA_RX_IRQHandler          DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler
+#define UART1_DMA_RX_IRQHandler          DMA1_Channel1_IRQHandler
 #define UART1_RX_DMA_RCC                 RCC_AHBENR_DMA1EN
-#define UART1_RX_DMA_INSTANCE            DMA1_Channel3
-#define UART1_RX_DMA_IRQ                 DMA1_Ch2_3_DMA2_Ch1_2_IRQn
+#define UART1_RX_DMA_INSTANCE            DMA1_Channel1
+#define UART1_RX_DMA_IRQ                 DMA1_Channel1_IRQn
+#endif
+/* DMA1 channel2-3 DMA2 channel1-2 */
+#if defined(BSP_SPI1_TX_USING_DMA) && !defined(SPI1_TX_DMA_INSTANCE)
+#define SPI1_DMA_TX_IRQHandler          DMA1_Channel2_3_IRQHandler
+#define SPI1_TX_DMA_RCC                 RCC_AHBENR_DMA1EN
+#define SPI1_TX_DMA_INSTANCE            DMA1_Channel3
+#define SPI1_TX_DMA_IRQ                 DMA1_Ch2_3_DMA2_Ch1_2_IRQn
 #endif
 /* DMA1 channel2-3 DMA2 channel1-2 */
 
 /* DMA1 channel4-7 DMA2 channel3-5 */
-#if defined(BSP_UART2_RX_USING_DMA) && !defined(UART2_RX_DMA_INSTANCE)
-#define UART2_DMA_RX_IRQHandler          DMA1_Ch4_7_DMA2_Ch3_5_IRQHandler
-#define UART2_RX_DMA_RCC                 RCC_AHBENR_DMA1EN
-#define UART2_RX_DMA_INSTANCE            DMA1_Channel5
-#define UART2_RX_DMA_IRQ                 DMA1_Ch4_7_DMA2_Ch3_5_IRQn
+#if defined(BSP_SPI1_RX_USING_DMA) && !defined(SPI1_RX_DMA_INSTANCE)
+#define SPI1_DMA_RX_IRQHandler          DMA1_Ch4_7_DMA2_Ch3_5_IRQHandler
+#define SPI1_RX_DMA_RCC                 RCC_AHBENR_DMA2EN
+#define SPI1_RX_DMA_INSTANCE            DMA2_Channel3
+#define SPI1_RX_DMA_IRQ                 DMA1_Ch4_7_DMA2_Ch3_5_IRQn
 #endif
 /* DMA1 channel4-7 DMA2 channel3-5 */
 
