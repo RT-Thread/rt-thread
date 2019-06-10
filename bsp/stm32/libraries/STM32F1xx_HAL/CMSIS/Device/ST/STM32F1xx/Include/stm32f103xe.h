7312dd693abbef2aad66ad9fd30412a09baa39c2
SummerGift <SummerGift@qq.com>
2019-06-10 15:55:14 +0800

    【更新】HAL CMSIS 到 4.3.0 版本

--- bsp/stm32/libraries/STM32F1xx_HAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+++ bsp/stm32/libraries/STM32F1xx_HAL/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
@@ -11500,6 +11500,8 @@ typedef struct
    ((INSTANCE) == TIM4)    || \
    ((INSTANCE) == TIM5))
 
+#define IS_TIM_SYNCHRO_INSTANCE(INSTANCE)  IS_TIM_MASTER_INSTANCE(INSTANCE)
+
 #define IS_TIM_DMABURST_INSTANCE(INSTANCE)\
   (((INSTANCE) == TIM1)    || \
    ((INSTANCE) == TIM8)    || \
