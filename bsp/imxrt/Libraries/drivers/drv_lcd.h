745c3ca8434892777234656a2727aa3e8e4d4933
misonyo <misonyo@foxmail.com>
2019-06-12 15:01:12 +0800

    [bsp/nxp] add new imxrt series

---
+++ bsp/imxrt/Libraries/drivers/drv_lcd.h
@@ -0,0 +1,23 @@
+/*
+ * File      : drv_lcd2.c
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2006-2013, RT-Thread Development Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-04-05     Liuguang     the first version.
+ */
+
+#ifndef __DRV_LCD_H__
+#define __DRV_LCD_H__
+
+#include <rtthread.h>
+#include <rtdevice.h>
+
+int rt_hw_lcd_init(void);
+
+#endif
