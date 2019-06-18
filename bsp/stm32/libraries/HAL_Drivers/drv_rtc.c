aae6274ef1e52ad28ef12ed37dcb54b3c7818155
misonyo <misonyo@foxmail.com>
2019-06-18 15:54:36 +0800

    [bsp/stm32]add stm32f2xx template

--- bsp/stm32/libraries/HAL_Drivers/drv_rtc.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_rtc.c
@@ -133,7 +133,7 @@ static rt_err_t rt_rtc_config(struct rt_device *dev)
         RTC_Handler.Init.OutPut = RTC_OUTPUT_DISABLE;
         RTC_Handler.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
         RTC_Handler.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-#elif defined(SOC_SERIES_STM32F4) || defined(SOC_SERIES_STM32F7) || defined(SOC_SERIES_STM32L4) || defined(SOC_SERIES_STM32H7)
+#elif defined(SOC_SERIES_STM32F2) || defined(SOC_SERIES_STM32F4) || defined(SOC_SERIES_STM32F7) || defined(SOC_SERIES_STM32L4) || defined(SOC_SERIES_STM32H7)
 
         /* set the frequency division */
 #ifdef BSP_RTC_USING_LSI
