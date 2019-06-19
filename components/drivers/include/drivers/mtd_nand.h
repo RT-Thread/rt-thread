608ead86f459055ab63a2eabfa5ceda4b4a1fc56
guozhanxin <guozhanxin@rt-thread.com>
2019-06-19 17:18:21 +0800

    [driver][nand] Fix hardfault errors.|修复 hardfault 错误。

--- components/drivers/include/drivers/mtd_nand.h
+++ components/drivers/include/drivers/mtd_nand.h
@@ -21,13 +21,13 @@
 struct rt_mtd_nand_driver_ops;
 #define RT_MTD_NAND_DEVICE(device)  ((struct rt_mtd_nand_device*)(device))
 
-#define RT_MTD_EOK          0   /* NO error */
-#define RT_MTD_EECC         1   /* ECC error */
-#define RT_MTD_EBUSY        2   /* hardware busy */
-#define RT_MTD_EIO          3   /* generic IO issue */
-#define RT_MTD_ENOMEM       4   /* out of memory */
-#define RT_MTD_ESRC         5   /* source issue */
-#define RT_MTD_EECC_CORRECT 6   /* ECC error but correct */
+#define RT_MTD_EOK          0     /* NO error */
+#define RT_MTD_EECC         101   /* ECC error */
+#define RT_MTD_EBUSY        102   /* hardware busy */
+#define RT_MTD_EIO          103   /* generic IO issue */
+#define RT_MTD_ENOMEM       104   /* out of memory */
+#define RT_MTD_ESRC         105   /* source issue */
+#define RT_MTD_EECC_CORRECT 106   /* ECC error but correct */
 
 struct rt_mtd_nand_device
 {
@@ -41,6 +41,7 @@ struct rt_mtd_nand_device
     rt_uint32_t pages_per_block;    /* The number of page a block */
     rt_uint16_t block_total;
 
+    /* Only be touched by driver */
     rt_uint32_t block_start;        /* The start of available block*/
     rt_uint32_t block_end;          /* The end of available block */
 
@@ -72,6 +73,7 @@ rt_err_t rt_mtd_nand_register_device(const char *name, struct rt_mtd_nand_device
 
 rt_inline rt_uint32_t rt_mtd_nand_read_id(struct rt_mtd_nand_device *device)
 {
+    RT_ASSERT(device->ops->read_id);
     return device->ops->read_id(device);
 }
 
@@ -81,6 +83,7 @@ rt_inline rt_err_t rt_mtd_nand_read(
     rt_uint8_t *data, rt_uint32_t data_len,
     rt_uint8_t *spare, rt_uint32_t spare_len)
 {
+    RT_ASSERT(device->ops->read_page);
     return device->ops->read_page(device, page, data, data_len, spare, spare_len);
 }
 
@@ -90,28 +93,45 @@ rt_inline rt_err_t rt_mtd_nand_write(
     const rt_uint8_t *data, rt_uint32_t data_len,
     const rt_uint8_t *spare, rt_uint32_t spare_len)
 {
+    RT_ASSERT(device->ops->write_page);
     return device->ops->write_page(device, page, data, data_len, spare, spare_len);
 }
 
 rt_inline rt_err_t rt_mtd_nand_move_page(struct rt_mtd_nand_device *device,
         rt_off_t src_page, rt_off_t dst_page)
 {
+    RT_ASSERT(device->ops->move_page);
     return device->ops->move_page(device, src_page, dst_page);
 }
 
 rt_inline rt_err_t rt_mtd_nand_erase_block(struct rt_mtd_nand_device *device, rt_uint32_t block)
 {
+    RT_ASSERT(device->ops->erase_block);
     return device->ops->erase_block(device, block);
 }
 
 rt_inline rt_err_t rt_mtd_nand_check_block(struct rt_mtd_nand_device *device, rt_uint32_t block)
 {
-    return device->ops->check_block(device, block);
+    if (device->ops->check_block)
+    {
+        return device->ops->check_block(device, block);
+    }
+    else
+    {
+        return -RT_ENOSYS;
+    }
 }
 
 rt_inline rt_err_t rt_mtd_nand_mark_badblock(struct rt_mtd_nand_device *device, rt_uint32_t block)
 {
-    return device->ops->mark_badblock(device, block);
+    if (device->ops->mark_badblock)
+    {
+        return device->ops->mark_badblock(device, block);
+    }
+    else
+    {
+        return -RT_ENOSYS;
+    }
 }
 
 #endif /* MTD_NAND_H_ */
