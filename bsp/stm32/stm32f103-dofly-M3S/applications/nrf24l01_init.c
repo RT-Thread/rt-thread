2c2522f57100ddeea68bc4e7b8202fe3407e2db0
Willian Chan <chentingwei@rt-thread.com>
2019-06-29 18:22:13 +0800

    [bsp][stm32][f103-dofly-m3s]Add support for nRF24L01 extension module
    Signed-off-by: Willian Chan <chentingwei@rt-thread.com>

---
+++ bsp/stm32/stm32f103-dofly-M3S/applications/nrf24l01_init.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author        Notes
+ * 2019-06-29     WillianChan   first version
+ */
+
+#include <rtthread.h>
+
+#ifdef PKG_USING_NRF24L01
+
+#include "drv_spi.h"
+static int rt_hw_nrf24l01_init(void)
+{
+    rt_hw_spi_device_attach("spi2", "spi20", GPIOG, GPIO_PIN_7);
+    return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(rt_hw_nrf24l01_init);
+
+#endif
