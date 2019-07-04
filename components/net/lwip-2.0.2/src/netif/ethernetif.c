72ed37a92b780ac4e2524465f00334cc61728d69
chenyong <1521761801@qq.com>
2019-07-04 16:17:52 +0800

    [net][lwip] Fix set_if() compile error when enable dhcpd fucntion and close finsh. [net][dhcpd]Fix dhcpd fucntion compile error in lwIP 2.1.0.

--- components/net/lwip-2.0.2/src/netif/ethernetif.c
+++ components/net/lwip-2.0.2/src/netif/ethernetif.c
@@ -715,8 +715,6 @@ int eth_system_device_init_private(void)
     return (int)result;
 }
 
-#ifdef RT_USING_FINSH
-#include <finsh.h>
 void set_if(char* netif_name, char* ip_addr, char* gw_addr, char* nm_addr)
 {
     ip4_addr_t *ip;
@@ -762,6 +760,9 @@ void set_if(char* netif_name, char* ip_addr, char* gw_addr, char* nm_addr)
         netif_set_netmask(netif, ip);
     }
 }
+
+#ifdef RT_USING_FINSH
+#include <finsh.h>
 FINSH_FUNCTION_EXPORT(set_if, set network interface address);
 
 #if LWIP_DNS
