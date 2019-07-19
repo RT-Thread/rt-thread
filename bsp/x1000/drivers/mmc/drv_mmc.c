b9e8f7dfdad0cd975b6fea274fdd544d21407123
Zhou Yanjie <zhouyanjie@zoho.com>
2019-07-19 21:29:29 +0800

    X1000: drv_mmc.c: 更新版权信息/Update copyright information.
    更新版权信息。
    
    Update copyright information.
    
    Signed-off-by: Zhou Yanjie <zhouyanjie@zoho.com>

--- bsp/x1000/drivers/mmc/drv_mmc.c
+++ bsp/x1000/drivers/mmc/drv_mmc.c
@@ -1,17 +1,14 @@
 /*
- * File      : drv_mmc.c
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2013 - 2015, RT-Thread Development Team
+ * Copyright (c) 2006-2019, RT-Thread Development Team
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
  * 2013-03-09     aozima       the first version
- * 2013-03-29     aozima       support Jz4770.
- * 2013-04-01     aozima       add interrupt support for Jz4770.
+ * 2013-03-29     aozima       support JZ4770.
+ * 2013-04-01     aozima       add interrupt support for JZ4770.
+ * 2019-04-04     Jean-Luc     fix bug in jzmmc_submit_dma.
  */
 
 #include <rthw.h>
