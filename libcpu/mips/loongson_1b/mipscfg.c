fa20bd03c233d0905a089dd117bb0a9a5a34db6b
Zhou Yanjie <zhouyanjie@zoho.com>
2019-07-21 00:16:45 +0800

    libcpu: MIPS: 更新版权信息/Update copyright information.
    更新版权信息。
    
    Update copyright information.
    
    Signed-off-by: Zhou Yanjie <zhouyanjie@zoho.com>

--- libcpu/mips/loongson_1b/mipscfg.c
+++ libcpu/mips/loongson_1b/mipscfg.c
@@ -1,16 +1,14 @@
 /*
- * File      : mipscfg.c
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006 - 2011, RT-Thread Development Team
+ * Copyright (c) 2006-2019, RT-Thread Development Team
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
  * 2010-05-27     swkyer       first version
+ * 2019-07-21     Zhou Yanjie  clean up code
  */
+
 #include <rtthread.h>
 #include "../common/mipsregs.h"
 #include "../common/mips_cfg.h"
