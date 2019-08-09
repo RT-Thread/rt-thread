493eed938ec7684b81b220e84fc0ab9ddd63491d
tangyuxin <462747508@qq.com>
2019-08-09 14:23:31 +0800

    [components][net] 1.网卡可卸载 2.dhcpd 服务可停止

--- components/net/lwip-2.1.0/src/netif/ethernetif.c
+++ components/net/lwip-2.1.0/src/netif/ethernetif.c
@@ -334,6 +334,19 @@ static int netdev_add(struct netif *lwip_netif)
     return result;
 }
 
+static void netdev_del(struct netif *lwip_netif)
+{
+    char name[LWIP_NETIF_NAME_LEN + 1];
+    struct netdev *netdev;
+
+    RT_ASSERT(lwip_netif);
+
+    rt_strncpy(name, lwip_netif->name, LWIP_NETIF_NAME_LEN);
+    netdev = netdev_get_by_name(name);
+    netdev_unregister(netdev);
+    rt_free(netdev);
+}
+
 /* synchronize lwIP network interface device and network interface device flags */
 static int netdev_flags_sync(struct netif *lwip_netif)
 {
@@ -541,6 +554,25 @@ rt_err_t eth_device_init(struct eth_device * dev, const char *name)
 #endif
 
     return eth_device_init_with_flag(dev, name, flags);
+}
+
+void eth_device_deinit(struct eth_device *dev)
+{
+    struct netif* netif = dev->netif;
+
+#if LWIP_DHCP
+    dhcp_stop(netif);
+    dhcp_cleanup(netif);
+#endif
+    netif_set_down(netif);
+    netif_remove(netif);
+#ifdef RT_USING_NETDEV
+    netdev_del(netif);
+#endif
+    rt_device_close(&(dev->parent));
+    rt_device_unregister(&(dev->parent));
+    rt_sem_detach(&(dev->tx_ack));
+    rt_free(netif);
 }
 
 #ifndef LWIP_NO_RX_THREAD
