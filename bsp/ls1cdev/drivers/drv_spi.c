95f7faae239a742a482ce8a99c5c8d166917ac63
liufeilong <443199460@qq.com>
2019-08-21 11:11:48 +0800

    完善代码风格

--- bsp/ls1cdev/drivers/drv_spi.c
+++ bsp/ls1cdev/drivers/drv_spi.c
@@ -30,13 +30,9 @@
 #include "ls1c_public.h"
 #include "spi_msd.h"
 #include "dfs_fs.h"
-
 #include "../libraries/ls1c_pin.h"
 
 #ifdef RT_USING_SPI
-
-//#define DEBUG
-
 #ifdef DEBUG
 #define DEBUG_PRINTF(...)           rt_kprintf(__VA_ARGS__)
 #else
@@ -270,24 +266,24 @@ int ls1c_hw_spi_init(void)
 #ifdef RT_USING_SPI0
     /* attach cs */
     {
-        static struct rt_spi_device spi_device0;
-        static struct rt_spi_device spi_device1;
-        static struct rt_spi_device spi_device2;
-		static struct rt_spi_device spi_device3;
-        static struct ls1c_spi_cs  spi_cs0;
-        static struct ls1c_spi_cs  spi_cs1;
-        static struct ls1c_spi_cs  spi_cs2;
-        static struct ls1c_spi_cs  spi_cs3;
-        /* spi02: CS2  SD Card*/
-        spi_cs2.cs = LS1C_SPI_CS_2;
-        rt_spi_bus_attach_device(&spi_device2, "spi02", "spi0", (void *)&spi_cs2);
-        spi_cs1.cs = LS1C_SPI_CS_1;
-        rt_spi_bus_attach_device(&spi_device1, "spi01", "spi0", (void *)&spi_cs1);
-        spi_cs0.cs = LS1C_SPI_CS_0;
-        rt_spi_bus_attach_device(&spi_device0, "spi00", "spi0", (void *)&spi_cs0);
-	    spi_cs3.cs = LS1C_SPI_CS_3;
-        rt_spi_bus_attach_device(&spi_device3, "spi03", "spi0", (void*)&spi_cs3);
-        msd_init("sd0", "spi02");
+     static struct rt_spi_device spi_device0;
+     static struct rt_spi_device spi_device1;
+     static struct rt_spi_device spi_device2;
+     static struct rt_spi_device spi_device3;
+     static struct ls1c_spi_cs  spi_cs0;
+     static struct ls1c_spi_cs  spi_cs1;
+     static struct ls1c_spi_cs  spi_cs2;
+     static struct ls1c_spi_cs  spi_cs3;
+     /* spi02: CS2  SD Card*/
+     spi_cs2.cs = LS1C_SPI_CS_2;
+     rt_spi_bus_attach_device(&spi_device2, "spi02", "spi0", (void *)&spi_cs2);
+     spi_cs1.cs = LS1C_SPI_CS_1;
+     rt_spi_bus_attach_device(&spi_device1, "spi01", "spi0", (void *)&spi_cs1);
+     spi_cs0.cs = LS1C_SPI_CS_0;
+     rt_spi_bus_attach_device(&spi_device0, "spi00", "spi0", (void *)&spi_cs0);
+     spi_cs3.cs = LS1C_SPI_CS_3;
+     rt_spi_bus_attach_device(&spi_device3, "spi03", "spi0", (void*)&spi_cs3);
+     msd_init("sd0", "spi02");
     }
 #endif
 #ifdef RT_USING_SPI1
@@ -319,7 +315,6 @@ static int board_sd_init(void)
         }
     #endif /* RT_USING_DFS && RT_USING_DFS_ELMFAT */
 }
-
 INIT_APP_EXPORT(board_sd_init);
 
 #endif
