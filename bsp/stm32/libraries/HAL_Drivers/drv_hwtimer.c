937809565f744bcb2be26c899df641ca6a4e9e39
yexinfu <yexinfu@163.com>
2019-08-23 07:16:09 +0800

    修复stm32 timer_start函数不能设置HWTIMER_MODE_PERIOD模式 修复timer_start函数注释：将TIM2改为TIM更合理

--- bsp/stm32/libraries/HAL_Drivers/drv_hwtimer.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_hwtimer.c
@@ -235,11 +235,14 @@ static rt_err_t timer_start(rt_hwtimer_t *timer, rt_uint32_t t, rt_hwtimer_mode_
         /* set timer to single mode */
         tim->Instance->CR1 |= TIM_OPMODE_SINGLE;
     }
-
+    else
+    {
+        tim->Instance->CR1 &= (~TIM_OPMODE_SINGLE);
+    }
     /* start timer */
     if (HAL_TIM_Base_Start_IT(tim) != HAL_OK)
     {
-        LOG_E("TIM2 start failed");
+        LOG_E("TIM start failed");
         result = -RT_ERROR;
     }
 
