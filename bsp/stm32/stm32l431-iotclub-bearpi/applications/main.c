b81fc49f42f85be723e6c139ead39214367a6892
mculover666 <2412828003@qq.com>
2019-08-25 10:20:52 +0800

    [bsp][stm32]modify led bug and readme document

--- bsp/stm32/stm32l431-iotclub-bearpi/applications/main.c
+++ bsp/stm32/stm32l431-iotclub-bearpi/applications/main.c
@@ -12,20 +12,20 @@
 #include <rtdevice.h>
 #include <board.h>
 
-/* defined the LED0 pin: PB1 */
-#define LED0_PIN    GET_PIN(C, 13)
+/* defined the LED pin: PC13 */
+#define LED_PIN    GET_PIN(C, 13)
 
 int main(void)
 {
     int count = 1;
-    /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    /* set LED pin mode to output */
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
 
     while (count++)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_pin_write(LED_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(LED_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
 
