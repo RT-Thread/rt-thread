d44db666e72786183800c2e1e18bb863e556564b
Willian Chan <chentingwei@rt-thread.com>
2019-08-28 11:59:21 +0800

    [bsp][stm32][stm32l475-atk-pandora]Add lcd driver and fix compilation report error on spi_config.h
    Signed-off-by: Willian Chan <chentingwei@rt-thread.com>

--- bsp/stm32/libraries/HAL_Drivers/config/l4/spi_config.h
+++ bsp/stm32/libraries/HAL_Drivers/config/l4/spi_config.h
@@ -53,10 +53,10 @@ extern "C" {
 
 #ifdef BSP_USING_SPI2
 #ifndef SPI2_BUS_CONFIG
-#define SPI2_BUS_CONFIG                                  \
-    {                                                    \
-        .Instance = SPI2,                                \
-        .bus_name = "spi2",                              \
+#define SPI2_BUS_CONFIG                                     \
+    {                                                       \
+        .Instance = SPI2,                                   \
+        .bus_name = "spi2",                                 \
     }
 #endif /* SPI2_BUS_CONFIG */
 #endif /* BSP_USING_SPI2 */
@@ -86,21 +86,38 @@ extern "C" {
 #endif /* BSP_SPI2_RX_USING_DMA */
 
 #ifdef BSP_USING_SPI3
-#define SPI3_BUS_CONFIG                                  \
-    {                                                    \
-        .Instance = SPI3,                                \
-        .bus_name = "spi3",                              \
-        .dma_rx.dma_rcc = RCC_AHB1ENR_DMA2EN,            \
-        .dma_tx.dma_rcc = RCC_AHB1ENR_DMA2EN,            \
-        .dma_rx.Instance = DMA2_Channel1,                \
-        .dma_rx.request = DMA_REQUEST_3,                 \
-        .dma_rx.dma_irq = DMA2_Channel1_IRQn,            \
-        .dma_tx.Instance = DMA2_Channel2,                \
-        .dma_tx.request = DMA_REQUEST_3,                 \
-        .dma_tx.dma_irq = DMA2_Channel2_IRQn,            \
+#ifndef SPI3_BUS_CONFIG
+#define SPI3_BUS_CONFIG                                     \
+    {                                                       \
+        .Instance = SPI3,                                   \
+        .bus_name = "spi3",                                 \
     }
+#endif /* SPI3_BUS_CONFIG */
+#endif /* BSP_USING_SPI3 */
 
-#endif
+#ifdef BSP_SPI3_TX_USING_DMA
+#ifndef SPI3_TX_DMA_CONFIG
+#define SPI3_TX_DMA_CONFIG                                  \
+    {                                                       \
+        .dma_rcc = SPI3_TX_DMA_RCC,                         \
+        .Instance = SPI3_TX_DMA_INSTANCE,                   \
+        .request = SPI3_TX_DMA_REQUEST,                     \
+        .dma_irq = SPI3_TX_DMA_IRQ,                         \
+    }
+#endif /* SPI3_TX_DMA_CONFIG */
+#endif /* BSP_SPI3_TX_USING_DMA */
+
+#ifdef BSP_SPI3_RX_USING_DMA
+#ifndef SPI3_RX_DMA_CONFIG
+#define SPI3_RX_DMA_CONFIG                                  \
+    {                                                       \
+        .dma_rcc = SPI3_RX_DMA_RCC,                         \
+        .Instance = SPI3_RX_DMA_INSTANCE,                   \
+        .request = SPI3_RX_DMA_REQUEST,                     \
+        .dma_irq = SPI3_RX_DMA_IRQ,                         \
+    }
+#endif /* SPI3_RX_DMA_CONFIG */
+#endif /* BSP_SPI3_RX_USING_DMA */
 
 #ifdef __cplusplus
 }
