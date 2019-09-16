aa714772f020161f967e932a20f605858a05b8a0
tangbinzy <tangbin@cmss.chinamobile.com>
2019-09-16 11:31:01 +0800

    add some prompt and modification in drv_common.c

--- bsp/stm32/libraries/HAL_Drivers/drv_common.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_common.c
@@ -95,17 +95,20 @@ void _Error_Handler(char *s, int num)
  * This function will delay for some us.
  *
  * @param us the delay time of us
+ * 
+ * us:0~999
  */
 void rt_hw_us_delay(rt_uint32_t us)
 {
-    rt_uint32_t start, now, delta, reload, us_tick;
+    rt_uint32_t start, now, delta, reload, us_tick, ticks;
     start = SysTick->VAL;
     reload = SysTick->LOAD;
     us_tick = SystemCoreClock / 1000000UL;
+    ticks = us * us_tick;
     do {
         now = SysTick->VAL;
         delta = start > now ? start - now : reload + start - now;
-    } while(delta < us_tick * us);
+    } while(delta < ticks);
 }
 
 /**
