7644c0b6fe7c08f199be80f6a771a7fa212ad42e
流光 <1004383796@qq.com>
2019-09-20 14:48:01 +0800

    [修复] 修复msh无法配置超过256字节的长度输入 | Fixed MSH unable to configure input length exceeding 256 bytes

--- components/finsh/shell.h
+++ components/finsh/shell.h
@@ -79,8 +79,8 @@ struct finsh_shell
 #endif
 
     char line[FINSH_CMD_SIZE];
-    rt_uint8_t line_position;
-    rt_uint8_t line_curpos;
+    rt_uint16_t line_position;
+    rt_uint16_t line_curpos;
 
 #ifndef RT_USING_POSIX
     rt_device_t device;
