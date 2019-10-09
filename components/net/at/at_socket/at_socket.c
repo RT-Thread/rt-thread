c7382c1da426676e6e8d71b60469e173fbf83a01
杨连钊 <ylz0923@foxmail.com>
2019-10-09 11:43:58 +0800

    Update at_socket.c

--- components/net/at/at_socket/at_socket.c
+++ components/net/at/at_socket/at_socket.c
@@ -855,7 +855,7 @@ int at_recv(int s, void *mem, size_t len, int flags)
 int at_sendto(int socket, const void *data, size_t size, int flags, const struct sockaddr *to, socklen_t tolen)
 {
     struct at_socket *sock = RT_NULL;
-    int len, result = 0;
+    int len = 0, result = 0;
 
     if (data == RT_NULL || size == 0)
     {
