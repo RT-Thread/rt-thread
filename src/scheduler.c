53091976dc5e04a7f797c9d5e1791f7354af13a6
shaojinchun <shaojinchun@rt-thread.com>
2019-10-23 11:12:35 +0800

    fix signal with cortex-M plat

--- src/scheduler.c
+++ src/scheduler.c
@@ -477,7 +477,13 @@ void rt_schedule(void)
 
                     rt_hw_context_switch((rt_ubase_t)&from_thread->sp,
                             (rt_ubase_t)&to_thread->sp);
+
 #ifdef RT_USING_SIGNALS
+#ifdef ARCH_ARM_CORTEX_M
+                    rt_hw_interrupt_enable(level);
+                    level = rt_hw_interrupt_disable();
+#endif
+
                     if (rt_current_thread->stat & RT_THREAD_STAT_SIGNAL_PENDING)
                     {
                         extern void rt_thread_handle_sig(rt_bool_t clean_state);
