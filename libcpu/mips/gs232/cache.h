22046c8fb73b99718be5e667bf12d49c9e214f96
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-12-07 00:55:46 +0800

    [libcpu] mips: Merge loongson_1 into gs232
    Previously Loongson 1B and Loongson 1C have their own libcpu
    implemention, but they're almost identical. So we merge them
    into gs232 and adapt to new common code.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- libcpu/mips/gs232/cache.h
+++ libcpu/mips/gs232/cache.h
@@ -1,20 +1,19 @@
 /*
- * File      : cache.h
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006 - 2011, RT-Thread Development Team
- *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * Cache Ops For Loongson GS232
+ * 
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ * 
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2010-07-09     Bernard      first version
- * 2011-08-08     lgnq         modified for LS1B
+ * 2010-07-09     Bernard        first version
+ * 2011-08-08     lgnq             modified for LS1B
+ * 2015-07-08     chinesebear   modified for loongson 1c
  */
+
 #ifndef	__CACHE_H__
 #define	__CACHE_H__
-
 /*
  * Cache Operations
  */
