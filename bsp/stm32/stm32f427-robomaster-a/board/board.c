f322e79d46d5f564a26929fdacfb85cac9323233
a1012112796 <1012112796@qq.com>
2019-12-10 23:04:25 +0800

    [BSP/stm32f427-robomaster-a] add SD card suport

--- bsp/stm32/stm32f427-robomaster-a/board/board.c
+++ bsp/stm32/stm32f427-robomaster-a/board/board.c
@@ -32,7 +32,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLM = 6;
   RCC_OscInitStruct.PLL.PLLN = 180;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
