9fe267287e8720c7e77e7667aa42f7a0d0fa449b
liuxinaliang <liuxianliang@rt-thread.com>
2020-01-03 12:45:46 +0800

    [bsp][stm32] fix a problem that using gcc compile the chips of stm G4 series  but chip doesn't work
    Signed-off-by: liuxinaliang <liuxianliang@rt-thread.com>

--- bsp/stm32/libraries/STM32G4xx_HAL/CMSIS/Device/ST/STM32G4xx/Source/Templates/gcc/startup_stm32gbk1cb.s
+++ bsp/stm32/libraries/STM32G4xx_HAL/CMSIS/Device/ST/STM32G4xx/Source/Templates/gcc/startup_stm32gbk1cb.s
@@ -93,9 +93,9 @@ LoopFillZerobss:
 /* Call the clock system intitialization function.*/
     bl  SystemInit
 /* Call static constructors */
-    bl __libc_init_array
+/*  bl __libc_init_array */
 /* Call the application's entry point.*/
-	bl	main
+	bl	entry
 
 LoopForever:
     b LoopForever
