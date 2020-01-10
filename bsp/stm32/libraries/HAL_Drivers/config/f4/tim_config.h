b2bf86f0abc10650f17cf9617216e363ca4f5d34
Ihavedone <mostiger@163.com>
2020-01-11 00:24:55 +0800

    添加了 PWM9 和 TIM3 的默认配置

--- bsp/stm32/libraries/HAL_Drivers/config/f4/tim_config.h
+++ bsp/stm32/libraries/HAL_Drivers/config/f4/tim_config.h
@@ -27,6 +27,17 @@ extern "C" {
     }
 #endif /* TIM_DEV_INFO_CONFIG */
 
+#ifdef BSP_USING_TIM3
+#ifndef TIM3_CONFIG
+#define TIM3_CONFIG                                         \
+    {                                                       \
+       .tim_handle.Instance     = TIM3,                     \
+       .tim_irqn                = TIM3_IRQn,                \
+       .name                    = "timer3",                 \
+    }
+#endif /* TIM3_CONFIG */
+#endif /* BSP_USING_TIM3 */
+
 #ifdef BSP_USING_TIM11
 #ifndef TIM11_CONFIG
 #define TIM11_CONFIG                                        \
