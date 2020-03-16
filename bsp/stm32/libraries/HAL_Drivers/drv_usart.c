7fc71fdba807fe44df82b5db9012e6532418baab
SummerGift <summergift2019@gmail.com>
2020-03-16 16:18:05 +0800

    [optimize] code logic

--- bsp/stm32/libraries/HAL_Drivers/drv_usart.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_usart.c
@@ -189,8 +189,7 @@ static rt_err_t stm32_control(struct rt_serial_device *serial, int cmd, void *ar
                 RT_ASSERT(0);
             }
         }
-
-        if (ctrl_arg == RT_DEVICE_FLAG_DMA_TX)
+        else if(ctrl_arg == RT_DEVICE_FLAG_DMA_TX) 
         {
             HAL_NVIC_DisableIRQ(uart->config->dma_tx->dma_irq);
             if (HAL_DMA_DeInit(&uart->dma_tx.handle) != HAL_OK)
