588bda5553773bc40054d98503637d5cc302d332
gyd0317 <gyd0317@126.com>
2020-03-18 21:42:13 +0800

    change gcc statup files

--- bsp/stm32/libraries/STM32L1xx_HAL/CMSIS/Device/ST/STM32L1xx/Source/Templates/gcc/startup_stm32l152xb.s
+++ bsp/stm32/libraries/STM32L1xx_HAL/CMSIS/Device/ST/STM32L1xx/Source/Templates/gcc/startup_stm32l152xb.s
@@ -90,9 +90,9 @@ LoopFillZerobss:
   bcc FillZerobss
 
 /* Call the clock system intitialization function.*/
-    bl  SystemInit
+  bl  SystemInit
 /* Call static constructors */
-/*     bl __libc_init_array */
+/* bl __libc_init_array */
 /* Call the application's entry point.*/
   bl entry
 .size Reset_Handler, .-Reset_Handler
