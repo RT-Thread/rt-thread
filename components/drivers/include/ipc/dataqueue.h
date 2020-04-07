dfcd38b74df749055c9dc8ea5ff4b8c632de1ce9
yangjie <yangjiexaut@126.com>
2020-04-07 13:35:09 +0800

    [components][dataqueue]add rt_data_queue_deinit

--- components/drivers/include/ipc/dataqueue.h
+++ components/drivers/include/ipc/dataqueue.h
@@ -25,6 +25,7 @@ struct rt_data_queue
     rt_uint16_t size;
     rt_uint16_t lwm;
     rt_bool_t   waiting_lwm;
+    rt_bool_t   is_init;
 
     rt_uint16_t get_index;
     rt_uint16_t put_index;
@@ -57,5 +58,6 @@ rt_err_t rt_data_queue_peak(struct rt_data_queue *queue,
                             const void          **data_ptr,
                             rt_size_t            *size);
 void rt_data_queue_reset(struct rt_data_queue *queue);
+rt_err_t rt_data_queue_deinit(struct rt_data_queue *queue);
 
 #endif
