4b035622ce411ce53455eba96ba3d0835b3e2997
Jonne <qzhang1535@gmail.com>
2020-04-15 21:03:43 +0800

    1. Modify the file indention 2. Modify the license head

--- bsp/mini2440/drivers/s3cmci.c
+++ bsp/mini2440/drivers/s3cmci.c
@@ -1,17 +1,12 @@
 /*
- * File      : s3cmci.c
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2010, RT-Thread Develop Team
+ * Copyright (c) 2006-2018, RT-Thread Development Team
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
  * 2020-04-15     Jonne        first version for s3c2440 mmc controller
  */
-
 #include <rthw.h>
 #include <rtthread.h>
 #include <rtdevice.h>
