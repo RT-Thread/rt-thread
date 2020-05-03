8d943130554469bd8af174114fb812b27089680b
JassyL <jassyliu96@gmail.com>
2020-05-03 20:06:57 +0800

    Fix: the clock is not initialized
    In the new version of cubemx, when the internal clock is checked，HAL_TIM_Base_MspInit () contains the initialization of the PWM clock, and HAL_TIM_PWM_Init () is no longer independently defined.

--- bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
@@ -323,7 +323,7 @@ static rt_err_t stm32_hw_pwm_init(struct stm32_pwm *device)
     tim->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 #endif
 
-    if (HAL_TIM_PWM_Init(tim) != HAL_OK)
+    if (HAL_TIM_Base_Init(tim) != HAL_OK)   
     {
         LOG_E("%s pwm init failed", device->name);
         result = -RT_ERROR;
