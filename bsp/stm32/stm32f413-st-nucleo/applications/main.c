5cd8b4b76944f339863dfa913d8230771e0057b5
XYX12306 <2669599387@qq.com>
2020-06-07 15:30:50 +0800

    update README.md and main.c file update template.uvprojx and enable to automatic operate after burning support SPI/I2C/timer/pwm/ADC/RTC/WDG/FLASH/OTGFS/RNG/UDID on-chip peripheral driver take CubeMX_Config.ioc Rename to STM32F413ZH.ioc

--- bsp/stm32/stm32f413-st-nucleo/applications/main.c
+++ bsp/stm32/stm32f413-st-nucleo/applications/main.c
@@ -12,21 +12,50 @@
 #include <rtdevice.h>
 #include <board.h>
 
+/* defined the LED1 pin: PB0 */
+#define LED1_PIN    GET_PIN(B, 0)
 /* defined the LED2 pin: PB7 */
 #define LED2_PIN    GET_PIN(B, 7)
+/* defined the LED3 pin: PB14 */
+#define LED3_PIN    GET_PIN(B, 14)
+/* defined the USER KEY pin: PC13 */
+#define KEY_PIN    GET_PIN(C, 13)
 
 int main(void)
 {
     int count = 1;
-    /* set LED0 pin mode to output */
+	/* set LED1 pin mode to output */
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+    /* set LED2 pin mode to output */
     rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+	/* set LED3 pin mode to output */
+    rt_pin_mode(LED3_PIN, PIN_MODE_OUTPUT);
+	/* set USER KEY pin mode to input */
+    rt_pin_mode(KEY_PIN, PIN_MODE_INPUT_PULLDOWN);
 
     while (count++)
     {
-        rt_pin_write(LED2_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED2_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+		if(rt_pin_read(KEY_PIN))
+		{
+			rt_pin_write(LED1_PIN, PIN_HIGH);
+			rt_pin_write(LED2_PIN, PIN_LOW);
+			rt_pin_write(LED3_PIN, PIN_LOW);
+			rt_thread_mdelay(500);
+			rt_pin_write(LED1_PIN, PIN_LOW);
+			rt_pin_write(LED2_PIN, PIN_HIGH);
+			rt_pin_write(LED3_PIN, PIN_LOW);
+			rt_thread_mdelay(500);
+			rt_pin_write(LED1_PIN, PIN_LOW);
+			rt_pin_write(LED2_PIN, PIN_LOW);
+			rt_pin_write(LED3_PIN, PIN_HIGH);
+			rt_thread_mdelay(500);
+		}
+		else
+		{
+			rt_pin_write(LED1_PIN, PIN_LOW);
+			rt_pin_write(LED2_PIN, PIN_LOW);
+			rt_pin_write(LED3_PIN, PIN_LOW);
+		}
     }
 
     return RT_EOK;
