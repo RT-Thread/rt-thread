0606261b97fa4ffb860bfa8ae9c254d8813d5703
thread-liu <lk9608@outlook.com>
2020-06-20 14:04:27 +0800

    [add] stm32mp1 drivers

--- bsp/stm32/libraries/HAL_Drivers/drv_gpio.h
+++ bsp/stm32/libraries/HAL_Drivers/drv_gpio.h
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author            Notes
  * 2018-11-06     balanceTWK        first version
+ * 2020-06-16     thread-liu        add stm32mp1 
  */
 
 #ifndef __DRV_GPIO_H__
@@ -16,7 +17,11 @@
 
 #define __STM32_PORT(port)  GPIO##port##_BASE
 
+#if defined(SOC_SERIES_STM32MP1)
+#define GET_PIN(PORTx,PIN) (rt_base_t)((16 * ( ((rt_base_t)__STM32_PORT(PORTx) - (rt_base_t)GPIOA_BASE)/(0x1000UL) )) + PIN)
+#else
 #define GET_PIN(PORTx,PIN) (rt_base_t)((16 * ( ((rt_base_t)__STM32_PORT(PORTx) - (rt_base_t)GPIOA_BASE)/(0x0400UL) )) + PIN)
+#endif
 
 #define __STM32_PIN(index, gpio, gpio_index)                                \
     {                                                                       \
