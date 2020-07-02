0c81552866f2330755916fd5e66ea9dde6b40451
LYH-ux <1780328728@qq.com>
2020-07-02 14:43:10 +0800

    modify the incident in config/*.h

--- bsp/tm4c123bsp/libraries/Drivers/config/spi_config.h
+++ bsp/tm4c123bsp/libraries/Drivers/config/spi_config.h
@@ -21,7 +21,7 @@ extern "C" {
 #ifndef SPI0_BUS_CONFIG
 #define SPI0_BUS_CONFIG                             \
     {                                               \
-        .base = SSI0_BASE,                           \
+        .base = SSI0_BASE,                          \
         .bus_name = "spi0",                         \
     }
 #endif /* SPI0_BUS_CONFIG */
@@ -31,32 +31,32 @@ extern "C" {
 #ifndef SPI1_BUS_CONFIG
 #define SPI1_BUS_CONFIG                             \
     {                                               \
-        .base = SSI1_BASE,                           \
+        .base = SSI1_BASE,                          \
         .bus_name = "spi1",                         \
     }
 #endif /* SPI1_BUS_CONFIG */
 #endif /* BSP_USING_SPI1 */
-    
+
 #ifdef BSP_USING_SPI2
 #ifndef SPI2_BUS_CONFIG
 #define SPI2_BUS_CONFIG                             \
     {                                               \
-        .base = SSI2_BASE,                           \
+        .base = SSI2_BASE,                          \
         .bus_name = "spi2",                         \
     }
 #endif /* SPI2_BUS_CONFIG */
 #endif /* BSP_USING_SPI2 */
-    
+
 #ifdef BSP_USING_SPI3
 #ifndef SPI3_BUS_CONFIG
 #define SPI3_BUS_CONFIG                             \
     {                                               \
-        .base = SSI3_BASE,                           \
+        .base = SSI3_BASE,                          \
         .bus_name = "spi3",                         \
     }
 #endif /* SPI3_BUS_CONFIG */
 #endif /* BSP_USING_SPI3 */
-    
+
 #ifdef __cplusplus
 }
 #endif
