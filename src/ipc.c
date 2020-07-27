ec77827442468cf9e789161cf007bc3482863ea2
chenchaoqun <chenchaoqun@rt-thread.com>
2020-07-27 11:36:57 +0800

    [modify] if ... else if constructs to be terminated with an else clause, which other option is invalid and delete surplus space.

--- src/ipc.c
+++ src/ipc.c
@@ -1080,6 +1080,13 @@ rt_err_t rt_event_send(rt_event_t event, rt_uint32_t set)
                     status = RT_EOK;
                 }
             }
+            else
+            {
+                /* enable interrupt */
+                rt_hw_interrupt_enable(level);
+
+                return -RT_EINVAL;
+            }
 
             /* move node to the next */
             n = n->next;
@@ -1186,10 +1193,10 @@ rt_err_t rt_event_recv(rt_event_t   event,
     {
         /* no waiting */
         thread->error = -RT_ETIMEOUT;
-        
+
         /* enable interrupt */
         rt_hw_interrupt_enable(level);
-        
+
         return -RT_ETIMEOUT;
     }
     else
