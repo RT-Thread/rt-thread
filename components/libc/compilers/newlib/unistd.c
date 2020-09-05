3152326983921b2732654806247f8b7ad3c91db0
mysterywolf <920369182@qq.com>
2020-09-05 17:50:54 +0800

    将unistd.c不再放在common中，分散到各个库中

---
+++ components/libc/compilers/newlib/unistd.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-09-01     Meco Man     First Version
+ */
+
+#include <termios.h>
+#include <unistd.h>
+
+#ifdef RT_USING_POSIX
+
+#ifdef RT_USING_POSIX_TERMIOS
+int isatty(int fd)
+{
+    struct termios ts;
+    return(tcgetattr(fd,&ts) != -1);/*true if no error (is a tty)*/
+}
+#endif
+
+char *ttyname(int fd)
+{
+    return "/dev/tty0"; /*TODO: need to add more specific*/
+}
+
+#endif
