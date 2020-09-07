7c6de68e83a9d954a0d359bfacac0b7bbf7107b9
thread-liu <lk9608@outlook.com>
2020-09-07 11:59:40 +0800

    [update] delete rt-thread header file.

--- bsp/stm32/libraries/STM32MPxx_HAL/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal.h
+++ bsp/stm32/libraries/STM32MPxx_HAL/STM32MP1xx_HAL_Driver/Inc/stm32mp1xx_hal.h
@@ -28,7 +28,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32mp1xx_hal_conf.h"
-#include <rtthread.h>
+#include <stddef.h>
 
 /** @addtogroup STM32MP1xx_HAL_Driver
  * @{
