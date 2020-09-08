fc6c4cb4a3a10dae76d601f2ac9277f097b61c97
mysterywolf <920369182@qq.com>
2020-09-08 13:16:49 +0800

    增加 __GNUC__宏定义 和sconscript的平台识别以避免重复定义

--- include/libc/libc_fdset.h
+++ include/libc/libc_fdset.h
@@ -13,7 +13,7 @@
 
 #include <rtconfig.h>
 
-#if defined(RT_USING_NEWLIB) || defined(_WIN32)
+#if defined(RT_USING_NEWLIB) || defined(_WIN32) || defined( __GNUC__ )
 #include <sys/types.h>
 #if defined(HAVE_SYS_SELECT_H)
 #include <sys/select.h>
