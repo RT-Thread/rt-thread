623bf0abc923210c1b990210a421660122b6c296
michael <michael5hzg@gmail.com>
2020-09-17 10:27:04 +0800

    bsp:ls2k:unify code format

--- bsp/ls2kdev/applications/pmcon.c
+++ bsp/ls2kdev/applications/pmcon.c
@@ -1,15 +1,26 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-09-17     maoxiaochuan    first version
+ */
+
+
 #include <rtthread.h>
 
 static void reboot()
 {
-	rt_hw_cpu_reset();
+    rt_hw_cpu_reset();
 }
-MSH_CMD_EXPORT(reboot, reboot sample);
+MSH_CMD_EXPORT(reboot, reboot system);
 
 
 static void poweroff()
 {
-	rt_hw_cpu_shutdown();
+    rt_hw_cpu_shutdown();
 }
-MSH_CMD_EXPORT(poweroff, shutdown power sample);
+MSH_CMD_EXPORT(poweroff, shutdown system);
 
