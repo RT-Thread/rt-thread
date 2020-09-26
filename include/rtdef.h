abf20e5958cf618a52e0f31c424230c0423beafe
tangyuxin <462747508@qq.com>
2020-09-26 12:12:28 +0800

    [add] The calling sequence of functions at the same level can be controlled

--- include/rtdef.h
+++ include/rtdef.h
@@ -213,20 +213,33 @@ typedef int (*init_fn_t)(void);
 #define INIT_EXPORT(fn, level)
 #endif
 
+/* The default initialization level of the system. */
+/* If the characters and numbers are small, initialize first */
+#define INIT_BOARD_LEVEL        "1"
+#define INIT_PREV_LEVEL         "2"
+#define INIT_DEVICE_LEVEL       "3"
+#define INIT_COMPONENT_LEVEL    "4"
+#define INIT_ENV_LEVEL          "5"
+#define INIT_APP_LEVEL          "6"
+
 /* board init routines will be called in board_init() function */
-#define INIT_BOARD_EXPORT(fn)           INIT_EXPORT(fn, "1")
+#define INIT_BOARD_EXPORT(fn)       INIT_EXPORT(fn, INIT_BOARD_LEVEL)
 
 /* pre/device/component/env/app init routines will be called in init_thread */
 /* components pre-initialization (pure software initilization) */
-#define INIT_PREV_EXPORT(fn)            INIT_EXPORT(fn, "2")
+#define INIT_PREV_EXPORT(fn)        INIT_EXPORT(fn, INIT_PREV_LEVEL)
 /* device initialization */
-#define INIT_DEVICE_EXPORT(fn)          INIT_EXPORT(fn, "3")
+#define INIT_DEVICE_EXPORT(fn)      INIT_EXPORT(fn, INIT_DEVICE_LEVEL)
 /* components initialization (dfs, lwip, ...) */
-#define INIT_COMPONENT_EXPORT(fn)       INIT_EXPORT(fn, "4")
+#define INIT_COMPONENT_EXPORT(fn)   INIT_EXPORT(fn, INIT_COMPONENT_LEVEL)
 /* environment initialization (mount disk, ...) */
-#define INIT_ENV_EXPORT(fn)             INIT_EXPORT(fn, "5")
+#define INIT_ENV_EXPORT(fn)         INIT_EXPORT(fn, INIT_ENV_LEVEL)
 /* appliation initialization (rtgui application etc ...) */
-#define INIT_APP_EXPORT(fn)             INIT_EXPORT(fn, "6")
+#define INIT_APP_EXPORT(fn)         INIT_EXPORT(fn, INIT_APP_LEVEL)
+
+/* If you want to control the initialization sequence determined at the same level */
+/* use this macro to define the export initialization function */
+#define INIT_USER_EXPORT(fn, level, n)    INIT_EXPORT(fn, level"."n)
 
 #if !defined(RT_USING_FINSH)
 /* define these to empty, even if not include finsh.h file */
