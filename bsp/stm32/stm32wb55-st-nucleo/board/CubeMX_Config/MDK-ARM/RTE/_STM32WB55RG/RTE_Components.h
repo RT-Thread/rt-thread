662d9cf81df8dcf4bbc1eb89cc56d18bf2739b59
Dozingfiretruck <1473454180@qq.com>
2020-10-14 09:45:46 +0800

    stm32wb

---
+++ bsp/stm32/stm32wb55-st-nucleo/board/CubeMX_Config/MDK-ARM/RTE/_STM32WB55RG/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32WB55RG' 
+ * Target:  'STM32WB55RG' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32wbxx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
