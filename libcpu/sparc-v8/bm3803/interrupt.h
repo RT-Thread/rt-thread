47496a6e37bd89f59d6e0f7ff004c4e9f0b1bba0
GFWisshit <qq979818120@gmail.com>
2020-10-21 15:08:13 +0800

    修改了license header，修改了sconscipt，修改了board.c指针的强制转换类型，interrupt.c添加了新的函数。

--- libcpu/sparc-v8/bm3803/interrupt.h
+++ libcpu/sparc-v8/bm3803/interrupt.h
@@ -1,30 +1,17 @@
 /*
-Copyright 2020 Shenzhen Academy of Aerospace Technology
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Change Logs:
-Date           Author       Notes
-2020-10-16     Dystopia     the first version
-*/
+ * Copyright (c) 2020, Shenzhen Academy of Aerospace Technology
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020-10-16     Dystopia     the first version
+ * 2020-10-21     Dystopia     Add new function
+ */
 
 #ifndef __INTERRUPT_H__
 #define __INTERRUPT_H__
 
-void rt_hw_interrupt_control(int vector, int priority, int route);
-int rt_hw_interrupt_get_active(int fiq_irq);
-void rt_hw_interrupt_ack(int fiq_irq);
-void rt_hw_interrupt_trigger(int vector);
 void rt_hw_interrupt_clear(int vector);
 
 #endif
