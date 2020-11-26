8e6194bf4d76d27a8dbd795441e7d8241a849815
supperthomas <60349489+supperthomas@users.noreply.github.com>
2020-11-26 09:13:42 +0800

    Update main.c

--- bsp/stm32/stm32h750-armfly-h7-tool/applications/main.c
+++ bsp/stm32/stm32h750-armfly-h7-tool/applications/main.c
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
- * Date           Author       Notes
- * 2019-10-25     zylx         first version
+ * Date           Author         Notes
+ * 2020-11-26     supperthomas   first version
  */
 
 #include <rtthread.h>
