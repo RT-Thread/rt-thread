2c6079cecccb1bd0b8e72fa77d98fad9083f288e
greedyhao <hao_kr@163.com>
2020-12-10 17:13:11 +0800

    [bluetrum] modify copyright information

--- bsp/bluetrum/ab32vg1-ab-prougen/applications/main.c
+++ bsp/bluetrum/ab32vg1-ab-prougen/applications/main.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2018, RT-Thread Development Team
+ * Copyright (c) 2020-2020, Bluetrum Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2018/09/30     Bernard      The first version
+ * 2020/12/10     greedyhao    The first version
  */
 
 #include <rtthread.h>
--- bsp/bluetrum/ab32vg1-ab-prougen/board/board.c
+++ bsp/bluetrum/ab32vg1-ab-prougen/board/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2020, RT-Thread Development Team
+ * Copyright (c) 2006-2020, Bluetrum Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
