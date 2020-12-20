4d5584ae9fa1c60364f405af42f5e877f1dfd1b7
Prry <445395670@qq.com>
2020-12-20 22:23:28 +0800

    缩进由“TAB键”改用为“空格”

--- components/drivers/sensors/sensor.c
+++ components/drivers/sensors/sensor.c
@@ -36,7 +36,7 @@ static char *const sensor_name_str[] =
     "dust_",     /* Dust sensor       */
     "eco2_",     /* eCO2 sensor       */
     "gnss_",     /* GPS/GNSS sensor   */
-	"tof_"		 /* TOF sensor        */
+    "tof_"       /* TOF sensor        */
 };
 
 /* Sensor interrupt correlation function */
