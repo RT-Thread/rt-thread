8795a6a91b83a0c6fb1f4346ff790c03f16674cd
bigmagic <zhaofujin@rt-thread.com>
2020-12-30 10:00:30 +0800

    add ls2k utest framework

--- bsp/ls2kdev/rtconfig.h
+++ bsp/ls2kdev/rtconfig.h
@@ -185,6 +185,23 @@
 
 /* Utilities */
 
+#define RT_USING_ULOG
+#define ULOG_OUTPUT_LVL_D
+#define ULOG_OUTPUT_LVL 7
+#define ULOG_USING_ISR_LOG
+#define ULOG_ASSERT_ENABLE
+#define ULOG_LINE_BUF_SIZE 128
+
+/* log format */
+
+#define ULOG_USING_COLOR
+#define ULOG_OUTPUT_TIME
+#define ULOG_OUTPUT_LEVEL
+#define ULOG_OUTPUT_TAG
+#define ULOG_BACKEND_USING_CONSOLE
+#define RT_USING_UTEST
+#define UTEST_THR_STACK_SIZE 4096
+#define UTEST_THR_PRIORITY 20
 
 /* RT-Thread MIPS CPU */
 
@@ -234,6 +251,11 @@
 
 /* games: games run on RT-Thread console */
 
+#define RT_USING_TESTCASE
+
+/* utest demo */
+
+#define TC_USING_UTEST_DEMO_TC
 #define SOC_LS2K1000
 
 #endif
