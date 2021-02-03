d28ccafe660128ebea8df744932c32dc9fbecafa
tangweikang <tangweikang@rt-thread.com>
2021-02-03 18:09:35 +0800

    [BSP][Infineon-TC264] add led demo.

--- bsp/Infineon-TC264/USER/Cpu0_Main.c
+++ bsp/Infineon-TC264/USER/Cpu0_Main.c
@@ -3,6 +3,17 @@
 
 #pragma section all "cpu0_dsram"
 
+void GPIO_Demo_init(void)
+{
+    /* configure P20.8 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 8,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P20.9 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 9,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.4 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 4,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.5 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 5,  IfxPort_Mode_outputPushPullGeneral);
+}
 
 int core0_main(void)
 {
@@ -20,10 +31,15 @@ extern int rtthread_startup(void);
 
 int main(void)
 {
+    GPIO_Demo_init();
     while(1)
     {
-        rt_thread_mdelay(1000);
         rt_kprintf("hello rt-thread!\n");
+        IfxPort_togglePin(&MODULE_P20, 8);
+        IfxPort_togglePin(&MODULE_P20, 9);
+        IfxPort_togglePin(&MODULE_P21, 4);
+        IfxPort_togglePin(&MODULE_P21, 5);
+        rt_thread_mdelay(1000);
     }
 }
 
