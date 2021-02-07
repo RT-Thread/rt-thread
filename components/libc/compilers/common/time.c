4c6962436ae8a89252c397dffac4b865f5cb663f
Meco Man <920369182@qq.com>
2021-02-07 22:33:37 +0800

    update

--- components/libc/compilers/common/time.c
+++ components/libc/compilers/common/time.c
@@ -213,10 +213,9 @@ RT_WEAK clock_t clock(void)
 /* TODO: timezone */
 int gettimeofday(struct timeval *tp, struct timezone *tz)
 {
-    time_t t = time(RT_NULL);
     if (tp != RT_NULL)
     {
-        tp->tv_sec = t;
+        tp->tv_sec = time(RT_NULL);
         tp->tv_usec = 0;
     }
     return 0;
