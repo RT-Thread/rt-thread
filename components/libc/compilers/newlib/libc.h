b4c8cb1445ad2e249473a101133d06478e8c1170
Meco Man <920369182@qq.com>
2021-02-11 03:02:32 +0800

    remove declare

--- components/libc/compilers/newlib/libc.h
+++ components/libc/compilers/newlib/libc.h
@@ -34,10 +34,6 @@ int libc_system_init(void);
 int libc_stdio_set_console(const char* device_name, int mode);
 int libc_stdio_get_console(void);
 
-/* some time related function */
-int libc_set_time(const struct timespec* time);
-int libc_get_time(struct timespec* time);
-int libc_time_to_tick(const struct timespec* time);
 #ifdef __cplusplus
 }
 #endif
