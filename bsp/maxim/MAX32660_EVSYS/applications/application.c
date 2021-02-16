1490d1538052343bb4d7e640d5e9fe8e5bb28dfd
supperthomas <78900636@qq.com>
2021-02-16 08:53:51 +0800

    add README and fix the code

--- bsp/maxim/MAX32660_EVSYS/applications/application.c
+++ bsp/maxim/MAX32660_EVSYS/applications/application.c
@@ -12,11 +12,18 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 
+#define GPIO_LED_PIN  13
+
 int main(void)
 {
     int count = 1;
+    rt_pin_mode(GPIO_LED_PIN, PIN_MODE_OUTPUT);
     while (count++)
     {
+        rt_pin_write(GPIO_LED_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+
+        rt_pin_write(GPIO_LED_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
     return RT_EOK;
