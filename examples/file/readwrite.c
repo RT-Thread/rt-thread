41b12b7e6796b836a69a5be0c49e03b1743fc97b
Meco Man <920369182@qq.com>
2021-03-07 08:34:04 +0800

    [example] 格式化整理

--- examples/file/readwrite.c
+++ examples/file/readwrite.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2020, RT-Thread Development Team
+ * Copyright (c) 2006-2021, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -17,21 +17,21 @@
 /* file read write test */
 void readwrite(const char* filename)
 {
-	int fd;
-	int index, length;
-	char* test_data;
-	char* buffer;
-	int block_size = TEST_DATA_LEN;
+    int fd;
+    int index, length;
+    char* test_data;
+    char* buffer;
+    int block_size = TEST_DATA_LEN;
 
-	/* open with write only & create */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0);
-	if (fd < 0)
-	{
-		rt_kprintf("open file for write failed\n");
-		return;
-	}
+    /* open with write only & create */
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0);
+    if (fd < 0)
+    {
+        rt_kprintf("open file for write failed\n");
+        return;
+    }
 
-	test_data = rt_malloc(block_size);
+    test_data = rt_malloc(block_size);
     if (test_data == RT_NULL)
     {
         rt_kprintf("no memory\n");
@@ -48,94 +48,94 @@ void readwrite(const char* filename)
         return;
     }
 
-	/* prepare some data */
-	for (index = 0; index < block_size; index ++)
-	{
-		test_data[index] = index + 27;
-	}
+    /* prepare some data */
+    for (index = 0; index < block_size; index ++)
+    {
+        test_data[index] = index + 27;
+    }
 
-	/* write to file */
-	length = write(fd, test_data, block_size);
-	if (length != block_size)
-	{
-		rt_kprintf("write data failed\n");
-		close(fd);
-		goto __exit;
-	}
+    /* write to file */
+    length = write(fd, test_data, block_size);
+    if (length != block_size)
+    {
+        rt_kprintf("write data failed\n");
+        close(fd);
+        goto __exit;
+    }
 
-	/* close file */
-	close(fd);
+    /* close file */
+    close(fd);
 
-	/* reopen the file with append to the end */
-	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0);
-	if (fd < 0)
-	{
-		rt_kprintf("open file for append write failed\n");
-		goto __exit;;
-	}
+    /* reopen the file with append to the end */
+    fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0);
+    if (fd < 0)
+    {
+        rt_kprintf("open file for append write failed\n");
+        goto __exit;;
+    }
 
-	length = write(fd, test_data, block_size);
-	if (length != block_size)
-	{
-		rt_kprintf("append write data failed\n");
-		close(fd);
-		goto __exit;
-	}
-	/* close the file */
-	close(fd);
+    length = write(fd, test_data, block_size);
+    if (length != block_size)
+    {
+        rt_kprintf("append write data failed\n");
+        close(fd);
+        goto __exit;
+    }
+    /* close the file */
+    close(fd);
 
-	/* open the file for data validation. */
-	fd = open(filename, O_RDONLY, 0);
-	if (fd < 0)
-	{
-		rt_kprintf("check: open file for read failed\n");
-		goto __exit;
-	}
+    /* open the file for data validation. */
+    fd = open(filename, O_RDONLY, 0);
+    if (fd < 0)
+    {
+        rt_kprintf("check: open file for read failed\n");
+        goto __exit;
+    }
 
-	/* read the data (should be the data written by the first time ) */
-	length = read(fd, buffer, block_size);
-	if (length != block_size)
-	{
-		rt_kprintf("check: read file failed\n");
-		close(fd);
-		goto __exit;
-	}
+    /* read the data (should be the data written by the first time ) */
+    length = read(fd, buffer, block_size);
+    if (length != block_size)
+    {
+        rt_kprintf("check: read file failed\n");
+        close(fd);
+        goto __exit;
+    }
 
-	/* validate */
-	for (index = 0; index < block_size; index ++)
-	{
-		if (test_data[index] != buffer[index])
-		{
-			rt_kprintf("check: check data failed at %d\n", index);
-			close(fd);
-			goto __exit;
-		}
-	}
+    /* validate */
+    for (index = 0; index < block_size; index ++)
+    {
+        if (test_data[index] != buffer[index])
+        {
+            rt_kprintf("check: check data failed at %d\n", index);
+            close(fd);
+            goto __exit;
+        }
+    }
 
-	/* read the data (should be the second time data) */
-	length = read(fd, buffer, block_size);
-	if (length != block_size)
-	{
-		rt_kprintf("check: read file failed\n");
-		close(fd);
-		goto __exit;
-	}
+    /* read the data (should be the second time data) */
+    length = read(fd, buffer, block_size);
+    if (length != block_size)
+    {
+        rt_kprintf("check: read file failed\n");
+        close(fd);
+        goto __exit;
+    }
 
-	/* validate */
-	for (index = 0; index < block_size; index ++)
-	{
-		if (test_data[index] != buffer[index])
-		{
-			rt_kprintf("check: check data failed at %d\n", index);
-			close(fd);
-			goto __exit;
-		}
-	}
+    /* validate */
+    for (index = 0; index < block_size; index ++)
+    {
+        if (test_data[index] != buffer[index])
+        {
+            rt_kprintf("check: check data failed at %d\n", index);
+            close(fd);
+            goto __exit;
+        }
+    }
 
-	/* close the file */
-	close(fd);
-	/* print result */
-	rt_kprintf("read/write test successful!\n");
+    /* close the file */
+    close(fd);
+    /* print result */
+    rt_kprintf("read/write test successful!\n");
 
 __exit:
     rt_free(test_data);
