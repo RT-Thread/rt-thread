40f351586fbe838bb6241dec033f2e33a6c65ed0
Meco Man <920369182@qq.com>
2021-03-26 17:36:43 +0800

    [libc][time] add LOG_W to give a warning when RTC device is not used

--- components/libc/compilers/common/time.c
+++ components/libc/compilers/common/time.c
@@ -21,6 +21,7 @@
 
 #include <sys/time.h>
 #include <rtthread.h>
+#include <rtdbg.h>
 #ifdef RT_USING_DEVICE
 #include <rtdevice.h>
 #endif
@@ -219,6 +220,7 @@ RT_WEAK time_t time(time_t *t)
 
     if(time_now == (time_t)-1)
     {
+        LOG_W("Cannot find a RTC device to provide time!");
         errno = ENOSYS;
     }
 
@@ -246,12 +248,13 @@ int stime(const time_t *t)
     }
     else
     {
+        LOG_W("Cannot find a RTC device to provide time!");
         errno = ENOSYS;
         return -1;
     }
     return 0;
-
 #else
+    LOG_W("Cannot find a RTC device to provide time!");
     errno = ENOSYS;
     return -1;
 #endif /* RT_USING_RTC */
