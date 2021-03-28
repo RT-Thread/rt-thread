8c719b2bf68c46aac0569b3e2663212a39b432d1
Meco Man <920369182@qq.com>
2021-03-28 22:38:17 +0800

    remove return

--- bsp/stm32/stm32l412-st-nucleo/applications/main.c
+++ bsp/stm32/stm32l412-st-nucleo/applications/main.c
@@ -27,6 +27,4 @@ int main(void)
         rt_pin_write(LD4_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
-
-    return RT_EOK;
 }
--- bsp/stm32/stm32l4r5-st-nucleo/applications/main.c
+++ bsp/stm32/stm32l4r5-st-nucleo/applications/main.c
@@ -32,6 +32,4 @@ int main(void)
         rt_pin_write(LED_RUN_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
-
-    return RT_EOK;
 }
