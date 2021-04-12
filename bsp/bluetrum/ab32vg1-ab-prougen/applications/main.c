a7f1f939e626427eafde833d654296fd9fa11396
Meco Man <920369182@qq.com>
2021-04-12 11:51:59 +0800

    优化main函数

--- bsp/bluetrum/ab32vg1-ab-prougen/applications/main.c
+++ bsp/bluetrum/ab32vg1-ab-prougen/applications/main.c
@@ -19,7 +19,6 @@
 
 int main(void)
 {
-    uint32_t cnt = 0;
     uint8_t pin = rt_pin_get("PE.1");
 
     rt_pin_mode(pin, PIN_MODE_OUTPUT);
@@ -27,14 +26,9 @@ int main(void)
 
     while (1)
     {
-        if (cnt % 2 == 0) {
-            rt_pin_write(pin, PIN_LOW);
-        } else {
-            rt_pin_write(pin, PIN_HIGH);
-        }
-        cnt++;
-        rt_thread_mdelay(1000);
+        rt_pin_write(pin, PIN_LOW);
+        rt_thread_mdelay(500);
+        rt_pin_write(pin, PIN_HIGH);
+        rt_thread_mdelay(500);
     }
-
-    return 0;
 }
