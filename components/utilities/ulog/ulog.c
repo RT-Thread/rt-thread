3ae6ca1fb09a1ec254285f8e5522e8d233f1c85c
armink <armink.ztl@gmail.com>
2021-04-12 22:16:17 +0800

    [ulog] Fix thread info show when kernel not startup.

--- components/utilities/ulog/ulog.c
+++ components/utilities/ulog/ulog.c
@@ -318,9 +318,12 @@ RT_WEAK rt_size_t ulog_formater(char *log_buf, rt_uint32_t level, const char *ta
         /* is not in interrupt context */
         if (rt_interrupt_get_nest() == 0)
         {
-            rt_size_t name_len = rt_strnlen(rt_thread_self()->name, RT_NAME_MAX);
-
-            rt_strncpy(log_buf + log_len, rt_thread_self()->name, name_len);
+            rt_size_t name_len = 0;
+            const char *thread_name = "N/A";
+            if (rt_thread_self()->name)
+                thread_name = rt_thread_self()->name;
+            name_len = rt_strnlen(thread_name, RT_NAME_MAX);
+            rt_strncpy(log_buf + log_len, thread_name, name_len);
             log_len += name_len;
         }
         else
