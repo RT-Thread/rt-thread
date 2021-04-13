4840aa98ed82f11714463069763841359b45dbf7
sheltonyu <sheltonyu@163.com>
2021-04-13 10:17:09 +0800

    [bsp/at32] remove notes

--- bsp/at32/Libraries/AT32_Std_Driver/AT32F4xx_StdPeriph_Driver/inc/at32f4xx_comp.h
+++ bsp/at32/Libraries/AT32_Std_Driver/AT32F4xx_StdPeriph_Driver/inc/at32f4xx_comp.h
@@ -1,27 +1,12 @@
 /**
-  ******************************************************************************
-  * @file    at32f4xx_comp.h
-  * @author  Artery
-  * @version V1.0.1
-  * @date    20-April-2012
-  * @brief   This file contains all the functions prototypes for the COMP firmware 
-  *          library.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2012 Artery</center></h2>
-  *
-  * Licensed under Artery Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+ **************************************************************************
+ * File Name    : at32f4xx_comp.h
+ * Description  : at32f4xx COMP header file
+ * Date         : 2018-10-08
+ * Version      : V1.0.5
+ **************************************************************************
+ */
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __AT32F4XX_COMP_H
