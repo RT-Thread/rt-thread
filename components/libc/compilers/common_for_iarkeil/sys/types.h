cf71c0168e5ad8db03b58f3cb906c24445b4cb97
Meco Man <920369182@qq.com>
2021-04-18 00:34:04 +0800

    重新整理libc重复共用文件，创建common_for_all 和 common_for_keiliar
    Signed-off-by: Meco Man <920369182@qq.com>

--- components/libc/compilers/common_for_iarkeil/sys/types.h
+++ components/libc/compilers/common_for_iarkeil/sys/types.h
@@ -5,6 +5,7 @@
  *
  * Change Logs:
  * Date           Author       Notes
+ * 2020-09-05     Meco Man     fix bugs
  * 2020-12-16     Meco Man     add useconds_t
  */
 #ifndef __TYPES_H__
@@ -17,6 +18,8 @@ typedef int32_t          key_t;         /* Used for interprocess communication. 
 typedef int              pid_t;         /* Used for process IDs and process group IDs. */
 typedef unsigned short   uid_t;
 typedef unsigned short   gid_t;
+typedef signed long      off_t;
+typedef int              mode_t;
 #ifndef ARCH_CPU_64BIT
 typedef signed int       ssize_t;       /* Used for a count of bytes or an error indication. */
 #else
