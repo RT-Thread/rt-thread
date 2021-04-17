969dc033f35cbbcc24669f1c55cd0eb9ce542f44
Meco Man <920369182@qq.com>
2021-04-17 23:03:11 +0800

    move off_t and mode_t to libc

--- components/libc/compilers/armlibc/sys/types.h
+++ components/libc/compilers/armlibc/sys/types.h
@@ -18,6 +18,8 @@ typedef int32_t          key_t;         /* Used for interprocess communication. 
 typedef int              pid_t;         /* Used for process IDs and process group IDs. */
 typedef unsigned short   uid_t;
 typedef unsigned short   gid_t;
+typedef signed long      off_t;
+typedef int              mode_t;
 #ifndef ARCH_CPU_64BIT
 typedef signed int       ssize_t;       /* Used for a count of bytes or an error indication. */
 #else
