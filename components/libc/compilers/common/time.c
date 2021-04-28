82f2d8e53d866598b993054990b73339ef457607
Meco Jianting Man <920369182@qq.com>
2021-04-29 00:21:10 +0800

    time.c 删除函数名与括号之间的空格

--- components/libc/compilers/common/time.c
+++ components/libc/compilers/common/time.c
@@ -270,7 +270,7 @@ char* asctime(const struct tm *timeptr)
 }
 RTM_EXPORT(asctime);
 
-char *ctime_r (const time_t * tim_p, char * result)
+char *ctime_r(const time_t * tim_p, char * result)
 {
     struct tm tm;
     return asctime_r(localtime_r(tim_p, &tm), result);
