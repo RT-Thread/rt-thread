787e46c6a3591d133e051f509b7a262a0eee034a
Meco Man <920369182@qq.com>
2021-05-04 13:06:24 +0800

    [rtc]调整rtc驱动框架函数位置，功能无影响，为后续新的RTC框架铺路

--- components/drivers/include/drivers/rtc.h
+++ components/drivers/include/drivers/rtc.h
@@ -11,10 +11,14 @@
 #ifndef __RTC_H__
 #define __RTC_H__
 
+#include <rtconfig.h>
+
 rt_err_t set_date(rt_uint32_t year, rt_uint32_t month, rt_uint32_t day);
 rt_err_t set_time(rt_uint32_t hour, rt_uint32_t minute, rt_uint32_t second);
 
+#ifdef RTC_SYNC_USING_NTP
 int rt_soft_rtc_init(void);
 int rt_rtc_ntp_sync_init(void);
+#endif /* RTC_SYNC_USING_NTP */
 
 #endif /* __RTC_H__ */
