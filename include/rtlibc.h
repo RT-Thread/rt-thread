23e03d3b2ca77e397bb663af75b39ba7779a28a5
Meco Man <920369182@qq.com>
2021-05-23 00:14:22 +0800

    [libc][rtlibc] remove libc_ioctl.h

--- include/rtlibc.h
+++ include/rtlibc.h
@@ -16,7 +16,6 @@
 #include "libc/libc_errno.h"
 
 #include "libc/libc_fcntl.h"
-#include "libc/libc_ioctl.h"
 #include "libc/libc_dirent.h"
 #include "libc/libc_signal.h"
 #include "libc/libc_fdset.h"
