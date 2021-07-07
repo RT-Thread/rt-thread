af60bf52c312c48d1189771085ebbd44e67ae2e5
David Lin <linpeng1577@gmail.com>
2021-07-07 09:05:37 +0800

    Update sensor.h

--- components/drivers/sensors/sensor.h
+++ components/drivers/sensors/sensor.h
@@ -6,26 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2019-01-31     flybreak     first version
- * 2021-06-23     linpeng      added function declaration
  */
 
-#ifndef __SENSOR_H__
-#define __SENSOR_H__
-
 #include <rtthread.h>
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-int rt_hw_sensor_register(rt_sensor_t sensor,
-                          const char *name,
-                          rt_uint32_t flag,
-                          void *data);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /*__SENSOR_H__*/
+#include <rtdevice.h>
