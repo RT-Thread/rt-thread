a403a3ec3bc4125fca940b297d4d2eb0981e183d
YJIE_1998 <1039241323@qq.com>
2021-08-04 18:22:04 +0800

    【修改】移植 littlefs 文件系统

--- bsp/stm32/stm32f407-atk-explorer/board/ports/drv_filesystem.c
+++ bsp/stm32/stm32f407-atk-explorer/board/ports/drv_filesystem.c
@@ -16,6 +16,9 @@
 #include <dfs_romfs.h>
 #include <dfs_fs.h>
 #include <dfs_posix.h>
+#include <fal.h>
+
+#define BSP_USING_NOR_MTD_FS
 
 #if DFS_FILESYSTEMS_MAX < 4
 #error "Please define DFS_FILESYSTEMS_MAX more than 4"
@@ -75,11 +78,73 @@ static int onboard_sdcard_mount(void)
 }
 #endif
 
+#ifdef BSP_USING_NOR_MTD_FS
+
+#define FS_PARTITION_NAME              "filesystem"
+
+static void mtd_mount(void *parameter)
+{
+    struct rt_device *mtd_dev = RT_NULL;
+    fal_init();
+    mtd_dev = fal_mtd_nor_device_create(FS_PARTITION_NAME);
+    if (!mtd_dev)
+    {
+        LOG_E("Can't create a mtd device on '%s' partition.", FS_PARTITION_NAME);
+    }
+    while (1)
+    {
+        rt_thread_mdelay(500);
+        if(rt_device_find(FS_PARTITION_NAME) != RT_NULL)
+        {
+            if (dfs_mount(FS_PARTITION_NAME, "/flash", "lfs", 0, 0) == RT_EOK)
+            {
+                LOG_I("mtd nor flash mount to '/flash'");
+                break;
+            }
+            else
+            {
+                LOG_W("mtd nor flash mount to '/flash' failed!");
+            }
+        }
+    }
+}
+
+static int onboard_mtd_mount(void)
+{
+    rt_thread_t tid;
+
+    if (dfs_mount(FS_PARTITION_NAME, "/flash", "lfs", 0, 0) == RT_EOK)
+    {
+        LOG_I("mtd nor flash mount to '/flash'");
+    }
+    else
+    {
+        tid = rt_thread_create("mtd_mount", mtd_mount, RT_NULL,
+                               1024, RT_THREAD_PRIORITY_MAX - 3, 20);
+        if (tid != RT_NULL)
+        {
+            rt_thread_startup(tid);
+        }
+        else
+        {
+            LOG_E("create mtd_mount thread err!");
+        }
+    }
+
+    return RT_EOK;
+}
+#endif
+
+
 static const struct romfs_dirent _romfs_root[] =
 {
 #ifdef BSP_USING_SDCARD
     {ROMFS_DIRENT_DIR, "sdcard", RT_NULL, 0},
 #endif
+#ifdef BSP_USING_NOR_MTD_FS
+    {ROMFS_DIRENT_DIR, "flash", RT_NULL, 0},
+#endif
+    
 //  {ROMFS_DIRENT_DIR, "flash", RT_NULL, 0},
 };
 
@@ -97,7 +162,11 @@ static int filesystem_mount(void)
 #ifdef BSP_USING_SDCARD
     onboard_sdcard_mount();
 #endif
-
+    
+#ifdef BSP_USING_NOR_MTD_FS
+    onboard_mtd_mount();
+#endif
+    
     return RT_EOK;
 }
 INIT_APP_EXPORT(filesystem_mount);
