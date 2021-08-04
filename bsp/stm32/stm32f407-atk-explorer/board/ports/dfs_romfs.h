8862b11b2f8edba0f63c9cc8a007c7ec3e39440b
YJIE_1998 <1039241323@qq.com>
2021-08-04 19:21:45 +0800

    【添加】添加dfs_romfs.h文件，并修改构建脚本

---
+++ bsp/stm32/stm32f407-atk-explorer/board/ports/dfs_romfs.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019/01/13     Bernard      code cleanup
+ */
+
+#ifndef __DFS_ROMFS_H__
+#define __DFS_ROMFS_H__
+
+#include <rtthread.h>
+
+#define ROMFS_DIRENT_FILE   0x00
+#define ROMFS_DIRENT_DIR    0x01
+
+struct romfs_dirent
+{
+    rt_uint32_t      type;  /* dirent type */
+
+    const char       *name; /* dirent name */
+    const rt_uint8_t *data; /* file date ptr */
+    rt_size_t        size;  /* file size */
+};
+
+int dfs_romfs_init(void);
+extern const struct romfs_dirent romfs_root;
+
+#endif
