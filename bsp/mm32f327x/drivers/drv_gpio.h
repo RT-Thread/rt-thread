984a32f7f1b573c2909b65c9a0efe55315da50f8
马志远 <1468728718@qq.com>
2021-08-05 10:35:45 +0800

    add bsp of mm32f327x

---
+++ bsp/mm32f327x/drivers/drv_gpio.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-08-05     mazhiyuan    first version
+ */
+#ifndef DRV_GPIO_H__
+#define DRV_GPIO_H__
+
+int rt_hw_pin_init(void);
+
+#endif
