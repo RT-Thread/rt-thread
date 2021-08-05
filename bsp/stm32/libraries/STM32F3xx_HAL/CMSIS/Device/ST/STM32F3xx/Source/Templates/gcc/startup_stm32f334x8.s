bfb9fd78a33b1cda6bf55037be2c7deba421165c
YJIE_1998 <1039241323@qq.com>
2021-08-05 11:59:36 +0800

    【修改】库文件，GCC文件entry入口

--- bsp/stm32/libraries/STM32F3xx_HAL/CMSIS/Device/ST/STM32F3xx/Source/Templates/gcc/startup_stm32f334x8.s
+++ bsp/stm32/libraries/STM32F3xx_HAL/CMSIS/Device/ST/STM32F3xx/Source/Templates/gcc/startup_stm32f334x8.s
@@ -98,11 +98,8 @@ LoopFillZerobss:
 /* Call static constructors */
     bl __libc_init_array
 /* Call the application's entry point.*/
-	bl	main
-
-LoopForever:
-    b LoopForever
-    
+  bl  entry
+  bx  lr  
 .size	Reset_Handler, .-Reset_Handler
 
 /**
