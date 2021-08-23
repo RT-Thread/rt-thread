8a15226b9b0f22ff571e8c5ca8ffba276b5f4903
马志远 <1468728718@qq.com>
2021-08-23 17:30:05 +0800

    Remove C Style Command Shell

--- components/drivers/misc/rt_drv_pwm.c
+++ components/drivers/misc/rt_drv_pwm.c
@@ -198,7 +198,7 @@ rt_err_t rt_pwm_get(struct rt_device_pwm *device, struct rt_pwm_configuration *c
 FINSH_FUNCTION_EXPORT_ALIAS(rt_pwm_enable, pwm_enable, enable pwm by channel.);
 FINSH_FUNCTION_EXPORT_ALIAS(rt_pwm_set, pwm_set, set pwm.);
 
-#ifdef FINSH_USING_MSH
+#ifdef RT_USING_FINSH
 static int pwm_enable(int argc, char **argv)
 {
     int result = 0;
@@ -323,5 +323,5 @@ _exit:
 }
 MSH_CMD_EXPORT(pwm_get, pwm_get <pwm_dev> <channel>);
 
-#endif /* FINSH_USING_MSH */
+#endif /* RT_USING_FINSH */
 #endif /* RT_USING_FINSH */
--- include/rtdef.h
+++ include/rtdef.h
@@ -277,7 +277,6 @@ typedef int (*init_fn_t)(void);
 /* define these to empty, even if not include finsh.h file */
 #define FINSH_FUNCTION_EXPORT(name, desc)
 #define FINSH_FUNCTION_EXPORT_ALIAS(name, alias, desc)
-#define FINSH_VAR_EXPORT(name, type, desc)
 
 #define MSH_CMD_EXPORT(command, desc)
 #define MSH_CMD_EXPORT_ALIAS(command, alias, desc)
