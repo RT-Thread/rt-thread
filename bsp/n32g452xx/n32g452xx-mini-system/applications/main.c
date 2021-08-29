5e3b7318186bc3e7f5913205a7898ae0887b8015
linyuanbo_breo_server <linyuanbo@breo.com.cn>
2021-08-29 08:56:54 +0000

    feat : 完成DEMO测试例子
    1. 完成MAIN函数中的LED测试例子
    2. 完善README文档
    3. 更新添加许可文件

--- bsp/n32g452xx/n32g452xx-mini-system/applications/main.c
+++ bsp/n32g452xx/n32g452xx-mini-system/applications/main.c
@@ -8,15 +8,23 @@
  * 2015-07-29     Arda.Fu      first implementation
  */
 #include <rtthread.h>
+#include <rtdevice.h>
+
+/* defined the LED1 pin: PA8 */
+#define LED1_PIN    57
 
 int main(void)
 {
-    /* user app entry */
+    uint32_t Speed = 200;
+    /* set LED1 pin mode to output */
+    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
 
-    return 0;
+    while (1)
+    {
+        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_thread_mdelay(Speed);
+        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_thread_mdelay(Speed);
+    }
 }
-
-
-
-
 
