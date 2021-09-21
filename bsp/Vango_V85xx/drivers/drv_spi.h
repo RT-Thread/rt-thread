e923ba2884525112532959d2b6e9c544fdff7a16
似水流年 <744709580@qq.com>
2021-09-21 14:56:40 +0800

    Add v85xx spi driver first version

---
+++ bsp/Vango_V85xx/drivers/drv_spi.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2012-01-01    aozima       first implementation.
+ * 
+ */
+
+#ifndef __DRV_SPI_H__
+#define __DRV_SPI_H__
+
+
+#endif // __DRV_SPI_H__
