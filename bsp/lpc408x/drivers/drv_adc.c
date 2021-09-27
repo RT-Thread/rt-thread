32b8be88326b78414c4a8367ad19f29d9231233a
fzxhub <fzxhub@fzxmail.com>
2021-09-27 22:29:53 +0800

    修改lpc408x中ADC驱动的版权信息

--- bsp/lpc408x/drivers/drv_adc.c
+++ bsp/lpc408x/drivers/drv_adc.c
@@ -1,5 +1,11 @@
 /*
- * Copyright (c) 2006-2021, fzxhub
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-09-27     fzxhub       the first version
  */
 
 #include <rtthread.h>
