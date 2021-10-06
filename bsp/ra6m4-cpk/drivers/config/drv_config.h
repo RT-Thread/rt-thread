ff27c7cb515a81c057326d4fba08041097de927d
Sherman <shaopengyu@rt-thread.com>
2021-10-06 16:50:57 +0800

    [add] 添加 RA6m4-CPK 的 BSP 支持包

---
+++ bsp/ra6m4-cpk/drivers/config/drv_config.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2021-07-29     KyleChan          first version
+ */
+
+#ifndef __DRV_CONFIG_H__
+#define __DRV_CONFIG_H__
+
+#include "board.h"
+#include <rtthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef SOC_SERIES_R7FA6M4AF
+#include "ra6m4/uart_config.h"
+
+#endif/* SOC_SERIES_R7FA6M4AF */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif/* __DRV_CONFIG_H__ */
