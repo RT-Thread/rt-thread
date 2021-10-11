de3fe009549401c03e740e4312e2455c9e309f60
Sherman <shaopengyu@rt-thread.com>
2021-10-11 21:14:00 +0800

    [add] 添加 README.md 及相关文档

--- bsp/ra6m4-cpk/src/hal_entry.c
+++ bsp/ra6m4-cpk/src/hal_entry.c
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
- * Date           Author            Notes
- * 2021-10-10     Sherman           first version
+ * Date           Author        Notes
+ * 2021-10-10     Sherman       first version
  */
 
 #include <rtthread.h>
@@ -14,6 +14,7 @@
 void hal_entry(void)
 {
     rt_kprintf("\nHello RT-Thread!\n");
+
     while (1)
     {
         rt_thread_mdelay(1000);
