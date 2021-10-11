2ae8a05963b83c5a0deba6bd9b95f81b93d33092
liukangcc <liukangcc@outlook.com>
2021-10-11 14:52:07 +0800

    [update] bl main to bl entry

--- bsp/stm32/libraries/STM32F2xx_HAL/CMSIS/Device/ST/STM32F2xx/Source/Templates/gcc/startup_stm32f205xx.s
+++ bsp/stm32/libraries/STM32F2xx_HAL/CMSIS/Device/ST/STM32F2xx/Source/Templates/gcc/startup_stm32f205xx.s
@@ -96,7 +96,7 @@ LoopFillZerobss:
 /* Call static constructors */
     bl __libc_init_array
 /* Call the application's entry point.*/
-  bl  main
+  bl  entry
   bx  lr    
 .size  Reset_Handler, .-Reset_Handler
 
--- bsp/stm32/libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f412zx.s
+++ bsp/stm32/libraries/STM32F4xx_HAL/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f412zx.s
@@ -97,7 +97,7 @@ LoopFillZerobss:
 /* Call static constructors */
     bl __libc_init_array
 /* Call the application's entry point.*/
-  bl  main
+  bl  entry
   bx  lr    
 .size  Reset_Handler, .-Reset_Handler
 
