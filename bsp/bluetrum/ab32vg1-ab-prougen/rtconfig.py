9fc0c755067bef2ecba90e0c6859ef8f9728e848
greedyhao <hao_kr@163.com>
2021-10-11 15:56:02 +0800

    [bsp][bluetrum] convert uintxx_t to rt_uintxx_t

--- bsp/bluetrum/ab32vg1-ab-prougen/rtconfig.py
+++ bsp/bluetrum/ab32vg1-ab-prougen/rtconfig.py
@@ -38,9 +38,7 @@ if PLATFORM == 'gcc':
     OBJDUMP = PREFIX + 'objdump'
     OBJCPY  = PREFIX + 'objcopy'
 
-    # DEVICE  = ' -mcmodel=medany -march=rv32imc -mabi=ilp32 -fsingle-precision-constant'
     DEVICE  = ' -mcmodel=medany -march=rv32imc -mabi=ilp32 -msave-restore'
-    # CFLAGS  = DEVICE + ' -fno-common -ffunction-sections -fdata-sections -fstrict-volatile-bitfields'
     CFLAGS = DEVICE + ' -D_USE_LONG_TIME_T'
     AFLAGS  = ' -c' + DEVICE + ' -x assembler-with-cpp'
     LFLAGS  = DEVICE + ' -nostartfiles -Wl,--gc-sections,-Map=rtthread.map,-cref,-u,_start -T link.lds'
