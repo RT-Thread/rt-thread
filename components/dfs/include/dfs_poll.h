702b91c3ce927d5aea775d2dbc6c0c53ff986aa6
Jianting (Meco) Man <920369182@qq.com>
2021-10-27 23:36:42 -0400

    [dfs] 保留dfs_poll.h dfs_select.h，加warning提示

---
+++ components/dfs/include/dfs_poll.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#ifndef DFS_POLL_H__
+#define DFS_POLL_H__
+
+#include <poll.h>
+
+#warning "This file will be obsolete in the next version! Please use <poll.h> to instead."
+
+#endif /* DFS_POLL_H__ */
