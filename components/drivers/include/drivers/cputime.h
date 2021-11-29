e7f93a4ddc49fc982724386dfd4fc5dc93077993
aozima <wuyangyong@gmail.com>
2021-11-29 18:49:09 +0800

    cputime support 64bit.

--- components/drivers/include/drivers/cputime.h
+++ components/drivers/include/drivers/cputime.h
@@ -16,11 +16,11 @@
 struct rt_clock_cputime_ops
 {
     float    (*cputime_getres) (void);
-    uint32_t (*cputime_gettime)(void);
+    uint64_t (*cputime_gettime)(void);
 };
 
 float    clock_cpu_getres(void);
-uint32_t clock_cpu_gettime(void);
+uint64_t clock_cpu_gettime(void);
 
 uint32_t clock_cpu_microsecond(uint32_t cpu_tick);
 uint32_t clock_cpu_millisecond(uint32_t cpu_tick);
