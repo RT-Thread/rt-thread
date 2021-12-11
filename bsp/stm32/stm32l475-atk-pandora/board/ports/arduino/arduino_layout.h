6231cca4e92a80927a8428b8f5439d9108be5e0c
Meco Man <920369182@qq.com>
2021-12-10 23:07:39 -0500

    [arduino] 初步实现Arduino生态兼容组件

---
+++ bsp/stm32/stm32l475-atk-pandora/board/ports/arduino/arduino_layout.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-12-10     Meco Man     first version
+ */
+#ifndef __UNO_LAYOUT_H__
+#define __UNO_LAYOUT_H__
+
+#include <rtconfig.h>
+
+#define LED_BUILTIN  13 /* Built-in LED */
+
+#define ARDUINO_PWM_HZ 500 /* Arduino UNO's PWM is around 500Hz */
+#define ARDUINO_LAYOUT_PIN_MAX   20
+
+#define A0   (14)
+#define A1   (15)
+#define A2   (16)
+#define A3   (17)
+#define A4   (18)
+#define A5   (19)
+#define A6   (20)
+#define A7   (21)
+
+#endif
