1c9cc179025daf987bec315d5a8d4ea7c53c89f7
Meco Man <920369182@qq.com>
2021-12-13 19:53:12 -0500

    [l475][arduino] 增加ADC PWM管脚定义

--- bsp/stm32/stm32l475-atk-pandora/board/ports/arduino/arduino_pinout.h
+++ bsp/stm32/stm32l475-atk-pandora/board/ports/arduino/arduino_pinout.h
@@ -7,15 +7,16 @@
  * Date           Author       Notes
  * 2021-12-10     Meco Man     first version
  */
-#ifndef __UNO_LAYOUT_H__
-#define __UNO_LAYOUT_H__
-
-#include <rtconfig.h>
+#ifndef __UNO_PINOUT_H__
+#define __UNO_PINOUT_H__
 
 #define LED_BUILTIN  13 /* Built-in LED */
 
 #define ARDUINO_PWM_HZ 500 /* Arduino UNO's PWM is around 500Hz */
-#define ARDUINO_LAYOUT_PIN_MAX   20
+
+#define ARDUINO_PINOUT_PIN_MAX  20 /* Arduino UNO has 20 pins in total*/
+#define ARDUINO_PINOUT_ADC_MAX  6  /* Arduino UNO has 5 ADC pins */
+#define ARDUINO_PINOUT_PWM_MAX  5  /* Arduino UNO has 5 PWM pins */
 
 #define A0   (14)
 #define A1   (15)
@@ -23,7 +24,5 @@
 #define A3   (17)
 #define A4   (18)
 #define A5   (19)
-#define A6   (20)
-#define A7   (21)
 
-#endif
+#endif /* __UNO_PINOUT_H__ */
