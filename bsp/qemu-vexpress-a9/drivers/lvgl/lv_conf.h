f6ae47fa94018d1ffe4963eb6766a99514524f3c
Meco Man <920369182@qq.com>
2021-12-14 22:05:06 -0500

    [qemu][lvgl] update player demo configuration

--- bsp/qemu-vexpress-a9/drivers/lvgl/lv_conf.h
+++ bsp/qemu-vexpress-a9/drivers/lvgl/lv_conf.h
@@ -14,11 +14,12 @@
 #define LV_USE_PERF_MONITOR     1
 #define LV_COLOR_DEPTH          16
 
-#define LV_HOR_RES_MAX          (640)
-#define LV_VER_RES_MAX          (480)
-
-#define LV_USE_DEMO_MUSIC           1
-#define LV_DEMO_MUSIC_AUTO_PLAY     1
+/* music player demo */
+#include <rtconfig.h>
+#define LV_HOR_RES_MAX          BSP_LCD_WIDTH
+#define LV_VER_RES_MAX          BSP_LCD_HEIGHT
+#define LV_USE_DEMO_RTT_MUSIC       1
+#define LV_DEMO_RTT_MUSIC_AUTO_PLAY 1
 #define LV_FONT_MONTSERRAT_12       1
 #define LV_FONT_MONTSERRAT_16       1
 
