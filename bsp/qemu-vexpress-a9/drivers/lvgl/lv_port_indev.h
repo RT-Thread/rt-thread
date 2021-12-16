32483efa4f04bdb73e30b19d98dfddc636e1538b
Meco Man <920369182@qq.com>
2021-12-15 22:34:41 -0500

    [qemu][lvgl]移除无用的定义和对外声明

--- bsp/qemu-vexpress-a9/drivers/lvgl/lv_port_indev.h
+++ bsp/qemu-vexpress-a9/drivers/lvgl/lv_port_indev.h
@@ -14,12 +14,7 @@
 extern "C" {
 #endif
 
-#include <lv_hal_indev.h>
-
-extern lv_indev_t * button_indev;
-
 void lv_port_indev_init(void);
-void lv_port_indev_input(rt_int16_t x, rt_int16_t y, lv_indev_state_t state);
 
 #ifdef __cplusplus
 } /*extern "C"*/
