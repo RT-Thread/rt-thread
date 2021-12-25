7b3b5d50b0a19948dcc8922e264a2523136d205d
Meco Man <920369182@qq.com>
2021-12-25 17:38:05 -0500

    [libc][nogcc] test include_next as errno.h

---
+++ components/libc/compilers/common/nogcc/errno.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-12-25     Meco Man     The first version.
+ */
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+#include_next <errno.h>
+#include <sys/errno.h>
+
+#endif
