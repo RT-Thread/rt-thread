9d44317fc0949a9fdcbdc9bf6c4732a16a909123
zuozuojia <2674928186@qq.com>
2022-01-07 22:35:58 +0800

    [bsp/stm32] fix the bug of can filter conflict

--- bsp/stm32/libraries/HAL_Drivers/drv_can.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_can.c
@@ -335,10 +335,25 @@ static rt_err_t _can_control(struct rt_can_device *can, int cmd, void *arg)
             {
                 if (filter_cfg->items[i].hdr == -1)
                 {
-                    drv_can->FilterConfig.FilterBank = i;
+                    /* use default filter bank settings */
+                    /**
+                     * because can1 and can2 use the same filter groups, 
+                     * separate the groups of can1 and can2, to prevent being covered.
+                     */
+                    if (drv_can->name == "can1")
+                    {
+                        /* can1 banks 0~13 */
+                        drv_can->FilterConfig.FilterBank = i;
+                    }
+                    else if (drv_can->name == "can2")
+                    {
+                        /* can1 banks 14~27 */
+                        drv_can->FilterConfig.FilterBank = i + 14;
+                    }
                 }
                 else
                 {
+                    /* use user-defined filter bank settings */
                     drv_can->FilterConfig.FilterBank = filter_cfg->items[i].hdr;
                 }
                  /**
