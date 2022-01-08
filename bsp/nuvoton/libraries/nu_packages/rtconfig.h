6837b3b9748b1ab57eac7e7bf9f6827dd7e5ff71
Meco Man <920369182@qq.com>
2022-01-08 07:33:35 -0500

    refresh projects

---
+++ bsp/nuvoton/libraries/nu_packages/rtconfig.h
@@ -0,0 +1,12 @@
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RootMenu */
+
+/* Nuvoton Packages Config */
+
+#define NU_PKG_USING_UTILS
+#define NU_PKG_USING_DEMO
+
+#endif
