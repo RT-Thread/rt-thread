9d06237e06c71bbb070d81159ca9015889a3d7a2
Meco Man <920369182@qq.com>
2022-01-15 10:15:06 -0500

    format sys/ioctl.h

--- components/libc/compilers/common/sys/ioctl.h
+++ components/libc/compilers/common/sys/ioctl.h
@@ -41,23 +41,23 @@ struct winsize
 #endif
 
 #ifndef _IOC
-#define _IOC(a,b,c,d) ( ((a)<<30) | ((b)<<8) | (c) | ((d)<<16) )
+#define _IOC(a,b,c,d) (((a)<<30) | ((b)<<8) | (c) | ((d)<<16))
 #endif
 
 #ifndef _IO
-#define _IO(a,b)    _IOC(_IOC_NONE,(a),(b),0)
+#define _IO(a,b)    _IOC(_IOC_NONE, (a), (b), 0)
 #endif
 
 #ifndef _IOW
-#define _IOW(a,b,c) _IOC(_IOC_WRITE,(a),(b),sizeof(c))
+#define _IOW(a,b,c) _IOC(_IOC_WRITE, (a), (b), sizeof(c))
 #endif
 
 #ifndef _IOR
-#define _IOR(a,b,c) _IOC(_IOC_READ,(a),(b),sizeof(c))
+#define _IOR(a,b,c) _IOC(_IOC_READ, (a), (b), sizeof(c))
 #endif
 
 #ifndef _IOWR
-#define _IOWR(a,b,c) _IOC(_IOC_READ|_IOC_WRITE,(a),(b),sizeof(c))
+#define _IOWR(a,b,c) _IOC(_IOC_READ|_IOC_WRITE, (a), (b), sizeof(c))
 #endif
 
 #ifndef FIONREAD
