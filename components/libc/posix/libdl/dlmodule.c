f00ec340980ec7ec1e7eae695eac9e3ed36a93c8
Meco Man <920369182@qq.com>
2022-01-20 15:52:42 -0500

    [dlmodule] RT_THREAD_PRIORITY_MAX - 1 -> RT_THREAD_PRIORITY_BACKGROUND

--- components/libc/posix/libdl/dlmodule.c
+++ components/libc/posix/libdl/dlmodule.c
@@ -184,7 +184,7 @@ struct rt_dlmodule *dlmodule_create(void)
         module->stat = RT_DLMODULE_STAT_INIT;
 
         /* set initial priority and stack size */
-        module->priority = RT_THREAD_PRIORITY_MAX - 1;
+        module->priority = RT_THREAD_PRIORITY_BACKGROUND;
         module->stack_size = 2048;
 
         rt_list_init(&(module->object_list));
@@ -546,7 +546,7 @@ struct rt_dlmodule* dlmodule_exec(const char* pgname, const char* cmd, int cmd_s
             module->cmd_line = rt_strdup(cmd);
 
             /* check stack size and priority */
-            if (module->priority > RT_THREAD_PRIORITY_MAX) module->priority = RT_THREAD_PRIORITY_MAX - 1;
+            if (module->priority > RT_THREAD_PRIORITY_MAX) module->priority = RT_THREAD_PRIORITY_BACKGROUND;
             if (module->stack_size < 2048 || module->stack_size > (1024 * 32)) module->stack_size = 2048;
 
             tid = rt_thread_create(module->parent.name, _dlmodule_thread_entry, (void*)module,
@@ -723,7 +723,7 @@ struct rt_dlmodule* dlmodule_exec_custom(const char* pgname, const char* cmd, in
             module->cmd_line = rt_strdup(cmd);
 
             /* check stack size and priority */
-            if (module->priority > RT_THREAD_PRIORITY_MAX) module->priority = RT_THREAD_PRIORITY_MAX - 1;
+            if (module->priority > RT_THREAD_PRIORITY_MAX) module->priority = RT_THREAD_PRIORITY_BACKGROUND;
             if (module->stack_size < 2048 || module->stack_size > (1024 * 32)) module->stack_size = 2048;
 
             tid = rt_thread_create(module->parent.name, _dlmodule_thread_entry, (void*)module,
