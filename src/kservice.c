062dd920c1535604875425b7917cabf75315aacb
Man, Jianting (Meco) <920369182@qq.com>
2022-01-24 01:20:09 -0500

    [kservice] rt_show_version更新年份 (#5552)

--- src/kservice.c
+++ src/kservice.c
@@ -613,7 +613,7 @@ void rt_show_version(void)
     rt_kprintf("- RT -     Thread Operating System\n");
     rt_kprintf(" / | \\     %d.%d.%d build %s %s\n",
                RT_VERSION, RT_SUBVERSION, RT_REVISION, __DATE__, __TIME__);
-    rt_kprintf(" 2006 - 2021 Copyright by rt-thread team\n");
+    rt_kprintf(" 2006 - 2022 Copyright by RT-Thread team\n");
 }
 RTM_EXPORT(rt_show_version);
 
