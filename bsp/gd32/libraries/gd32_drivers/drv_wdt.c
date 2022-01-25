00dadc7e3cb8a920ba5229d14de98cc96a001533
iysheng <iysheng@163.com>
2022-01-25 16:37:19 +0800

    [bsp][gd32103c-eval] Port rtc, adc, wdt, hwtimer drivers to new gd32 frame

--- bsp/gd32/libraries/gd32_drivers/drv_wdt.c
+++ bsp/gd32/libraries/gd32_drivers/drv_wdt.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author            Notes
- * 2021-03-03     iysheng           first version
+ * 2022-01-25     iysheng           first version
  */
 
 #include <board.h>
@@ -25,13 +25,13 @@ typedef struct {
 
 static gd32_wdt_device_t g_wdt_dev;
 
-static rt_err_t gd32_iwdt_init(rt_watchdog_t *wdt)
+static rt_err_t gd32_wdt_init(rt_watchdog_t *wdt)
 {
     rcu_osci_on(RCU_IRC40K);
     if (ERROR == rcu_osci_stab_wait(RCU_IRC40K))
     {
         LOG_E("failed init IRC40K clock for free watchdog.");
-        return -EINVAL;
+        return -RT_EINVAL;
     }
 
     g_wdt_dev.min_threshold_s = 1;
@@ -39,22 +39,21 @@ static rt_err_t gd32_iwdt_init(rt_watchdog_t *wdt)
     LOG_I("threshold section [%u, %d]", \
         g_wdt_dev.min_threshold_s, g_wdt_dev.max_threshold_s);
 
-    IWDG_Write_Enable(IWDG_WRITEACCESS_ENABLE);
-    IWDG_SetPrescaler(IWDG_PRESCALER_256);
-    IWDG_SetReloadValue(0xfff);
-    IWDG_Write_Enable(IWDG_WRITEACCESS_DISABLE);
+    fwdgt_write_enable();
+    fwdgt_config(0xfff, FWDGT_PSC_DIV256);
+    fwdgt_enable();
 
     return 0;
 }
 
-static rt_err_t gd32_iwdt_control(rt_watchdog_t *wdt, int cmd, void *arg)
+static rt_err_t gd32_wdt_control(rt_watchdog_t *wdt, int cmd, void *arg)
 {
     rt_uint32_t param;
 
     switch (cmd)
     {
     case RT_DEVICE_CTRL_WDT_KEEPALIVE:
-        IWDG_ReloadCounter();
+        fwdgt_counter_reload();
         break;
     case RT_DEVICE_CTRL_WDT_SET_TIMEOUT:
         param = *(rt_uint32_t *) arg;
@@ -62,21 +61,21 @@ static rt_err_t gd32_iwdt_control(rt_watchdog_t *wdt, int cmd, void *arg)
             (param < g_wdt_dev.min_threshold_s))
         {
             LOG_E("invalid param@%u.", param);
-            return -E2BIG;
+            return -RT_EINVAL;
         }
         else
         {
             g_wdt_dev.current_threshold_s = param;
         }
-        IWDG_Write_Enable(IWDG_WRITEACCESS_ENABLE);
-        IWDG_SetReloadValue(param * 40000 >> 8);
-        IWDG_Write_Enable(IWDG_WRITEACCESS_DISABLE);
+        fwdgt_write_enable();
+        fwdgt_config(param * 40000 >> 8, FWDGT_PSC_DIV256);
+        fwdgt_write_disable();
         break;
     case RT_DEVICE_CTRL_WDT_GET_TIMEOUT:
         *(rt_uint32_t *)arg = g_wdt_dev.current_threshold_s;
         break;
     case RT_DEVICE_CTRL_WDT_START:
-        IWDG_Enable();
+        fwdgt_enable();
         break;
     default:
         LOG_W("This command is not supported.");
@@ -87,17 +86,17 @@ static rt_err_t gd32_iwdt_control(rt_watchdog_t *wdt, int cmd, void *arg)
 }
 
 static struct rt_watchdog_ops g_wdt_ops = {
-    gd32_iwdt_init,
-    gd32_iwdt_control,
+    gd32_wdt_init,
+    gd32_wdt_control,
 };
 
-static int rt_hw_iwdt_init(void)
+static int rt_hw_wdt_init(void)
 {
     rt_err_t ret;
 
     g_wdt_dev.wdt.ops = &g_wdt_ops;
     /* register watchdog device */
-    if (rt_hw_watchdog_register(&g_wdt_dev.wdt, "iwdt", \
+    if (rt_hw_watchdog_register(&g_wdt_dev.wdt, "wdt", \
         RT_DEVICE_FLAG_DEACTIVATE, RT_NULL) != RT_EOK)
     {
         LOG_E("wdt device register failed.");
@@ -107,5 +106,5 @@ static int rt_hw_iwdt_init(void)
 
     return ret;
 }
-INIT_BOARD_EXPORT(rt_hw_iwdt_init);
+INIT_PREV_EXPORT(rt_hw_wdt_init);
 #endif
