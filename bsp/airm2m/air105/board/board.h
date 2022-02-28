28ea8826d9ac9db82969ef93754fe6bfc61e27ed
Dozingfiretruck <1473454180@qq.com>
2022-02-28 09:31:13 +0800

    add:补充license

--- bsp/airm2m/air105/board/board.h
+++ bsp/airm2m/air105/board/board.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-02-22     airm2m       first version
+ */
 
 #ifndef __BOARD_H__
 #define __BOARD_H__
