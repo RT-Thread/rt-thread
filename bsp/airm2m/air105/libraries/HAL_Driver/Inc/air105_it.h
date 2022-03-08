0e254652aaa12f711b8b4e2383a638fd30dfc7fa
打盹的消防车 <1473454180@qq.com>
2022-03-08 12:04:17 +0800

    add:air105 bsp (#5607)
    * add:air105 bsp
    
    * add:去掉注释代码
    
    * add:注释修改
    
    * fix:格式化一遍代码格式
    
    * add:main函数添加闪灯
    
    * add:完善bsp
    
    * add:添加一些信息
    
    * add:使用工作队列喂狗
    
    * add:整理目录
    
    * add:去掉乱码部分
    
    * add:修改readme
    
    * add:更新readme说明
    
    * add:去掉bootloader.bin和soc_download.exe，README.md中添加标注
    
    * fix:去除多余文件
    
    * add:补充license
    
    * add:支持scons --dist
    
    * add:更新soc_download.exe连接地址

---
+++ bsp/airm2m/air105/libraries/HAL_Driver/Inc/air105_it.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 OpenLuat & AirM2M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef __AIR105_IT_H
+#define __AIR105_IT_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "air105.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
