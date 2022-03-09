0348c9e5ab6f8a98483068e7449ec1a7de40c413
Meco Man <920369182@qq.com>
2022-03-09 14:51:17 -0500

    潘多拉增加arduino引脚别名

--- bsp/stm32/stm32l475-atk-pandora/applications/arduino/pins_arduino.h
+++ bsp/stm32/stm32l475-atk-pandora/applications/arduino/pins_arduino.h
@@ -19,6 +19,20 @@
 
 #define ARDUINO_DEFAULT_IIC_BUS_NAME "i2c4"
 
+#define D0   (0)
+#define D1   (1)
+#define D2   (2)
+#define D3   (3)
+#define D4   (4)
+#define D5   (5)
+#define D6   (6)
+#define D7   (7)
+#define D8   (8)
+#define D9   (9)
+#define D10  (10)
+#define D11  (11)
+#define D12  (12)
+#define D13  (13)
 #define A0   (14)
 #define A1   (15)
 #define A2   (16)
