697e99c90ee66003245e6e42b8538717a8b83b81
Meco Man <920369182@qq.com>
2022-03-09 22:06:41 -0500

    add pwm

--- bsp/stm32/stm32f072-st-nucleo/applications/arduino/pins_arduino.c
+++ bsp/stm32/stm32f072-st-nucleo/applications/arduino/pins_arduino.c
@@ -22,23 +22,23 @@ const pin_map_t pin_map_table[]=
     {0, GET_PIN(A,3)},  /* D0, RX */
     {1, GET_PIN(A,2)},  /* D1, TX */
     {2, GET_PIN(A,10)}, /* D2 */
-    {3, GET_PIN(B,3)},  /* D3, PWM */
+    {3, GET_PIN(B,3), "pwm2", 2},   /* D3, PWM */
     {4, GET_PIN(B,5)},  /* D4 */
-    {5, GET_PIN(B,4)},  /* D5 */
-    {6, GET_PIN(B,10)}, /* D6, PWM */
+    {5, GET_PIN(B,4), "pwm3", 1},   /* D5, PWM */
+    {6, GET_PIN(B,10), "pwm2", 3},  /* D6, PWM */
     {7, GET_PIN(A,8)},  /* D7 */
     {8, GET_PIN(A,9)},  /* D8 */
-    {9, GET_PIN(C,7)},  /* D9, PWM */
-    {10, GET_PIN(B,6)}, /* D10, PWM */
-    {11, GET_PIN(A,7)}, /* D11, PWM */
+    {9, GET_PIN(C,7), "pwm3", 2},   /* D9, PWM */
+    {10, GET_PIN(B,6)}, /* D10 */
+    {11, GET_PIN(A,7)}, /* D11 */
     {12, GET_PIN(A,6)}, /* D12 */
     {13, GET_PIN(A,5)}, /* D13 */
     {14, GET_PIN(B,9)}, /* D14 */
     {15, GET_PIN(B,8)}, /* D15 */
-    {16, GET_PIN(A,0), "adc1", 0}, /* A0 */
-    {17, GET_PIN(A,1), "adc1", 1}, /* A1 */
-    {18, GET_PIN(A,4), "adc1", 4}, /* A2 */
-    {19, GET_PIN(B,0), "adc1", 8}, /* A3 */
+    {16, GET_PIN(A,0), "adc1", 0},  /* A0 */
+    {17, GET_PIN(A,1), "adc1", 1},  /* A1 */
+    {18, GET_PIN(A,4), "adc1", 4},  /* A2 */
+    {19, GET_PIN(B,0), "adc1", 8},  /* A3 */
     {20, GET_PIN(C,1), "adc1", 11}, /* A4 */
     {21, GET_PIN(C,0), "adc1", 10}  /* A5 */
 };
--- bsp/stm32/stm32f072-st-nucleo/board/CubeMX_Config/Src/main.c
+++ bsp/stm32/stm32f072-st-nucleo/board/CubeMX_Config/Src/main.c
@@ -64,6 +64,9 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc;
 
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -76,6 +79,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_ADC_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 
@@ -116,6 +121,8 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_ADC_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -256,6 +263,132 @@ static void MX_ADC_Init(void)
   /* USER CODE BEGIN ADC_Init 2 */
 
   /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 0;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 0;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
 
 }
 
