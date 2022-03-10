c29e37390bd3ffe62ad1a5a080bca6a645af9891
kyle <kylepengchn@163.com>
2022-03-10 15:11:16 +0800

    add soft-spi
    * add spi-bit-ops.c/h to components/drivers/spi
    * add a drv_soft_spi example for gd32303e-eval
    
    Signed-off-by: kyle <kylepengchn@163.com>

---
+++ bsp/gd32303e-eval/drivers/drv_soft_spi.h
@@ -0,0 +1,28 @@
+/*
+ * File      : drv_soft_spi.h
+ * This file is part of RT-Thread RTOS
+ * COPYRIGHT (C) 2022 RT-Thread Develop Team
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rt-thread.org/license/LICENSE
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2012-01-01    aozima       first implementation.
+ */
+
+#ifndef __DRV_SOFT_SPI_H__
+#define __DRV_SOFT_SPI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int rt_soft_spi_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __DRV_SPI_H__
