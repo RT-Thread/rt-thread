9ee5940f25f238b9d59c36a35cb887516f9da93d
Man, Jianting (Meco) <920369182@qq.com>
2022-03-20 03:46:33 -0400

    [arduino][stm32l475 pandora] fullly  support analog output(PWM) and analog input(ADC) (#5690)

--- bsp/stm32/stm32l475-atk-pandora/applications/arduino/pins_arduino.c
+++ bsp/stm32/stm32l475-atk-pandora/applications/arduino/pins_arduino.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -11,34 +11,45 @@
 #include <board.h>
 #include "pins_arduino.h"
 
+/*
+    {Arduino Pin, RT-Thread Pin [, Device Name(PWM or ADC), Channel]}
+    [] means optional
+    Digital pins must NOT give the device name and channel.
+    Analog pins MUST give the device name and channel(ADC or PWM).
+    Arduino Pin must keep in sequence.
+*/
 const pin_map_t pin_map_table[]=
 {
-    /*
-        {Arduino Pin, RT-Thread Pin [, Device Name(PWM or ADC), Channel]}
-        [] means optional
-        Digital pins must NOT give the device name and channel.
-        Analog pins MUST give the device name and channel(ADC or PWM).
-    */
-    {0}, /* D0, RX */
-    {1}, /* D1, TX */
-    {2, GET_PIN(D,10)}, /* D2, BSP:KEY0 */
-    {3}, /* D3, PWM */
-    {4, GET_PIN(D,9)}, /* D4, BSP:KEY1 */
-    {5, GET_PIN(D,8)}, /* D5, BSP:KEY2 */
-    {6}, /* D6, PWM */
-    {7}, /* D7 */
-    {8, GET_PIN(E,7)}, /* D8, BSP: RED-LED */
-    {9}, /* D9, PWM */
-    {10, GET_PIN(E,9), "pwm1", 1}, /* D10, PWM, BSP: BLUE-LED */
-    {11, GET_PIN(B,8), "pwm4", 3}, /* D11, PWM */
-    {12, GET_PIN(B,2)}, /* D12, BSP: BEEP */
-    {13, GET_PIN(E,8)}, /* D13, LED_BUILTIN, BSP: GREEN-LED */
-    {14, GET_PIN(C,2), "adc1", 3}, /* D14, A0 */
-    {15}, /* A1 */
-    {16}, /* A2 */
-    {17}, /* A3 */
-    {18}, /* A4 */
-    {19} /*  A5 */
+    {D0}, /* RX */
+    {D1}, /* TX */
+    {D2, GET_PIN(B,9)},
+    {D3, GET_PIN(D,15), "pwm4", 4}, /* PWM */
+    {D4, GET_PIN(A,8)},
+    {D5, GET_PIN(D,14)},
+    {D6, GET_PIN(B,11), "pwm2", 4}, /* PWM */
+    {D7, GET_PIN(B,14)},
+    {D8, GET_PIN(B,12)},
+    {D9, GET_PIN(D,12), "pwm4", 1}, /* PWM */
+    {D10, GET_PIN(B,10), "pwm2", 3}, /* PWM */
+    {D11, GET_PIN(B,8), "pwm4", 3}, /* PWM */
+    {D12, GET_PIN(B,15)},
+    {D13, GET_PIN(B,13)},
+    {D14, GET_PIN(A,1)}, /* BSP: MOTOR-A */
+    {D15, GET_PIN(A,0)}, /* BSP: MOTOR-B */
+    {D16, GET_PIN(B,2)}, /* BSP: BEEP */
+    {D17, GET_PIN(D,10)}, /* BSP: KEY0 */
+    {D18, GET_PIN(D,9)}, /* BSP: KEY1 */
+    {D19, GET_PIN(D,8)}, /* BSP: KEY2 */
+    {D20, GET_PIN(C,13)}, /* BSP: KEY-WKUP */
+    {D21, GET_PIN(E,7)}, /* BSP: RED-LED */
+    {D22, GET_PIN(E,8)}, /* LED_BUILTIN, BSP: GREEN-LED */
+    {D23, GET_PIN(E,9), "pwm1", 1}, /* PWM, BSP: BLUE-LED */
+    {A0, GET_PIN(C,2), "adc1", 3},
+    {A1, GET_PIN(C,4), "adc1", 13},
+    {A2},
+    {A3},
+    {A4},
+    {A5}
 };
 
 /* initialization for BSP; maybe a blank function  */
--- bsp/stm32/stm32l475-atk-pandora/board/CubeMX_Config/Src/stm32l4xx_it.c
+++ bsp/stm32/stm32l475-atk-pandora/board/CubeMX_Config/Src/stm32l4xx_it.c
@@ -229,4 +229,3 @@ void OTG_FS_IRQHandler(void)
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
