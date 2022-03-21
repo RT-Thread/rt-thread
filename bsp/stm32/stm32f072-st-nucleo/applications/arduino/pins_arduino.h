fba060e5501832c67e389e0810693206ba5f7c73
Man, Jianting (Meco) <920369182@qq.com>
2022-03-20 23:53:31 -0400

    [stm32 pandora] fix the bug when using LCD demo (#5692)
    * [stm32 pandora] fix the bug when using LCD demo
    
    * arduino 增加内部温度传感器和参考电压通道
    
    * remove ARDUINO_PINOUT_PWM_MAX

--- bsp/stm32/stm32f072-st-nucleo/applications/arduino/pins_arduino.h
+++ bsp/stm32/stm32f072-st-nucleo/applications/arduino/pins_arduino.h
@@ -36,7 +36,6 @@
 
 #define LED_BUILTIN  D13 /* Built-in LED */
 
-#define ARDUINO_PINOUT_PWM_MAX  5  /* This Arduino variant has 5 PWM pins */
 #define ARDUINO_PWM_HZ 500 /* Arduino UNO's PWM is around 500Hz */
 
 #define ARDUINO_DEFAULT_IIC_BUS_NAME "i2c4"
