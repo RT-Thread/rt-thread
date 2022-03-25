1cbcd2a3b4e47425ec428125c81c1a278f75fb92
kyle <kylepengchn@163.com>
2022-03-25 14:16:42 +0800

    use formatting.py scripts for scanning
    Signed-off-by: kyle <kylepengchn@163.com>

--- components/drivers/spi/spi-bit-ops.h
+++ components/drivers/spi/spi-bit-ops.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2018, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author        Notes
- * 2021-10-11     kyle         first version
+ * 2021-10-11     kyle          first version
  */
 
 #ifndef __SPI_BIT_OPS_H__
@@ -32,7 +32,7 @@ struct rt_spi_bit_ops
     void (*const dir_miso)(void *data, rt_int32_t state);
 
     void (*const udelay)(rt_uint32_t us);
-    rt_uint32_t delay_us;  /* sclk、mosi and miso line delay */
+    rt_uint32_t delay_us;  /* sclk, mosi and miso line delay */
 };
 
 struct rt_spi_bit_obj
@@ -42,7 +42,7 @@ struct rt_spi_bit_obj
     struct rt_spi_configuration config;
 };
 
-rt_err_t rt_spi_bit_add_bus(struct rt_spi_bit_obj *obj, 
+rt_err_t rt_spi_bit_add_bus(struct rt_spi_bit_obj *obj,
                             const char            *bus_name,
                             struct rt_spi_bit_ops *ops);
 
