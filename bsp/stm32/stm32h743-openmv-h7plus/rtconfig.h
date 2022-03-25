7a4936246af43f0e1dad52ccdf87e3ed943c2d95
Miaowulue <52962604+Miaowulue@users.noreply.github.com>
2022-03-25 22:58:14 +0800

    [bsp/stm32] add usbcdc for openmv (#5702)

--- bsp/stm32/stm32h743-openmv-h7plus/rtconfig.h
+++ bsp/stm32/stm32h743-openmv-h7plus/rtconfig.h
@@ -124,6 +124,18 @@
 
 /* Using USB */
 
+#define RT_USING_USB
+#define RT_USING_USB_DEVICE
+#define RT_USBD_THREAD_STACK_SZ 4096
+#define USB_VENDOR_ID 0x0FFE
+#define USB_PRODUCT_ID 0x0001
+#define _RT_USB_DEVICE_CDC
+#define RT_USB_DEVICE_CDC
+#define RT_VCOM_TASK_STK_SIZE 512
+#define RT_CDC_RX_BUFSIZE 128
+#define RT_VCOM_SERNO "32021919830108"
+#define RT_VCOM_SER_LEN 14
+#define RT_VCOM_TX_TIMEOUT 1000
 
 /* POSIX layer and C standard library */
 
@@ -230,7 +242,6 @@
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
-#define BSP_USING_QSPI
 
 /* Onboard Peripheral Drivers */
 
