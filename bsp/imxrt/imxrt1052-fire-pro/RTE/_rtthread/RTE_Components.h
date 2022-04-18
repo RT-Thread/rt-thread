e481b3db7bc22feb9b939153395720526b21c0d7
Rbb666 <zhangbingru@rt-thread.com>
2022-04-18 17:27:52 +0800

    rt1052 添加 RT-Thread Studio 工程

---
+++ bsp/imxrt/imxrt1052-fire-pro/RTE/_rtthread/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project' 
+ * Target:  'rtthread' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "MIMXRT1052.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
