2174ad6700d49740645adcc0a50299a74cdfb313
shinu <shinu_61@163.com>
2022-04-20 14:37:15 +0800

    Decouple the addition of command line arguments

--- tools/building.py
+++ tools/building.py
@@ -388,17 +388,6 @@ def PrepareModuleBuilding(env, root_directory, bsp_directory):
     PreProcessor.process_contents(contents)
     BuildOptions = PreProcessor.cpp_namespace
 
-    # add build/clean library option for library checking
-    AddOption('--buildlib',
-              dest='buildlib',
-              type='string',
-              help='building library of a component')
-    AddOption('--cleanlib',
-              dest='cleanlib',
-              action='store_true',
-              default=False,
-              help='clean up the library by --buildlib')
-
     # add program path
     env.PrependENVPath('PATH', rtconfig.EXEC_PATH)
 
