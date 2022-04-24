8c7ca1f57e66704515855e32c1f0117e4fabe65f
supperthomas <78900636@qq.com>
2022-04-24 08:01:10 +0800

    add the ESP32_C3 first version

---
+++ bsp/ESP32_C3/main/main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#define BLINK_GPIO 8
+#define LOOP_COUNT 100000000
+#include "rtthread.h"
+#include "esp_private/panic_internal.h"
+#include "hal/uart_hal.h"
+#include "driver/timer.h"
+#include "soc/periph_defs.h"
+#include "hal/systimer_hal.h"
+#include "hal/systimer_ll.h"
+#include "esp_intr_alloc.h"
+
+#include <rthw.h>
+uint32_t scount = 0;
+
+#define rt_kprintf printf
+static uart_hal_context_t s_panic_uart = { .dev = CONFIG_ESP_CONSOLE_UART_NUM == 0 ? &UART0 :&UART1 };
+
+void t_panic_print_char(const char c)
+{
+    uint32_t sz = 0;
+    while (!uart_hal_get_txfifo_len(&s_panic_uart));
+    uart_hal_write_txfifo(&s_panic_uart, (uint8_t *) &c, 1, &sz);
+}
+static systimer_hal_context_t systimer_hal;
+IRAM_ATTR void t_SysTickIsrHandler(void *arg)
+{
+           // printf("t_SysTickIsrHandler=====\n");
+systimer_ll_clear_alarm_int(systimer_hal.dev, 1);
+    rt_interrupt_enter();
+
+    rt_tick_increase();
+    scount++;
+  //   printf("t_SysTickIsrHandler=====\n");
+    /* leave interrupt */
+    rt_interrupt_leave();
+    systimer_ll_is_alarm_int_fired(systimer_hal.dev, 1);
+}
+
+void main_thread_entry(void)
+{
+        uint32_t count = 0;
+        uint8_t system_timer_counter=1;
+
+    //rt_hw_interrupt_enable(0);
+    esp_intr_alloc(ETS_SYSTIMER_TARGET1_EDGE_INTR_SOURCE, ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_LEVEL1, t_SysTickIsrHandler, &systimer_hal, NULL);
+    systimer_hal_init(&systimer_hal);
+    systimer_ll_set_counter_value(systimer_hal.dev, system_timer_counter, 0);
+    systimer_ll_apply_counter_value(systimer_hal.dev, system_timer_counter);
+    uint32_t alarm_id = 1 ;
+    systimer_hal_connect_alarm_counter(&systimer_hal, alarm_id, system_timer_counter);
+    systimer_hal_set_alarm_period(&systimer_hal, alarm_id, 1000000UL / 1000);
+    systimer_hal_select_alarm_mode(&systimer_hal, alarm_id, SYSTIMER_ALARM_MODE_PERIOD);
+    systimer_hal_counter_can_stall_by_cpu(&systimer_hal, 1, 0, true);
+    systimer_hal_enable_alarm_int(&systimer_hal, alarm_id);
+
+    systimer_hal_enable_counter(&systimer_hal, SYSTIMER_LL_COUNTER_OS_TICK);
+
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 1);
+        rt_thread_mdelay(1000);
+        printf("Hello world!cout %x\n",scount);
+        gpio_set_level(BLINK_GPIO, 0);
+        rt_thread_mdelay(1000);
+
+    }
+}
+static struct rt_thread _main_thread;
+static rt_uint8_t _main_thread_stack[2048];
+void rt_application_init(void)
+{
+    rt_thread_t tid;
+
+    rt_thread_init(&_main_thread,
+                "main",
+                main_thread_entry,
+                RT_NULL,
+                &_main_thread_stack[0],
+                sizeof(_main_thread_stack),
+                2,
+                10);
+
+    rt_thread_startup(&_main_thread);
+}
+
+int rt_hw_uart_init(void)
+{
+    return 1;
+}
+void rt_hw_board_init(void)
+{
+extern int __heap_start__;
+extern int __heap_end__;
+    printf("%s:%d__heap_start__:%p,__heap_end__:%p\n",__func__,__LINE__,&__heap_start__,&__heap_end__);
+    rt_system_heap_init((void *)&__heap_start__, (void *)&__heap_end__);
+
+}
+void rtthread_startup(void)
+{
+    /* init board */
+    rt_hw_board_init();
+    /* show RT-Thread version */
+    rt_show_version();
+
+    /* timer system initialization */
+    rt_system_timer_init();
+
+    /* scheduler system initialization */
+    rt_system_scheduler_init();
+
+    /* create init_thread */
+    rt_application_init();
+
+    /* timer thread initialization */
+    rt_system_timer_thread_init();
+
+    /* idle thread initialization */
+    rt_thread_idle_init();
+
+    /* start scheduler */
+    rt_system_scheduler_start();
+    /* init scheduler system */
+    
+    /* never reach here */
+    return ;
+}
+
+void app_main(void)
+{
+    /* startup RT-Thread RTOS */        
+    rtthread_startup();                                                                                                                                                                                                                                 ;
+    return;
+ 
+}
---
+++ bsp/ESP32_C3/main/rtconfig.h
@@ -0,0 +1,143 @@
+/* RT-Thread config file */
+
+#ifndef __RTTHREAD_CFG_H__
+#define __RTTHREAD_CFG_H__
+
+// <<< Use Configuration Wizard in Context Menu >>>
+
+// <h>Basic Configuration
+// <o>Maximal level of thread priority <8-256>
+//  <i>Default: 32
+#define RT_THREAD_PRIORITY_MAX  32
+// <o>OS tick per second
+//  <i>Default: 1000   (1ms)
+#define RT_TICK_PER_SECOND  1000
+// <o>Alignment size for CPU architecture data access
+//  <i>Default: 4
+#define RT_ALIGN_SIZE   4
+// <o>the max length of object name<2-16>
+//  <i>Default: 8
+#define RT_NAME_MAX    8
+// <c1>Using RT-Thread components initialization
+//  <i>Using RT-Thread components initialization
+#define RT_USING_COMPONENTS_INIT
+// </c>
+
+#define RT_USING_USER_MAIN
+
+#define RT_USING_MEMPOOL
+#define RT_USING_SMALL_MEM
+#define RT_USING_SMALL_MEM_AS_HEAP
+#define RT_USING_HEAP
+// <o>the stack size of main thread<1-4086>
+//  <i>Default: 512
+#define RT_MAIN_THREAD_STACK_SIZE     256
+
+// </h>
+
+// <h>Debug Configuration
+// <c1>enable kernel debug configuration
+//  <i>Default: enable kernel debug configuration
+//#define RT_DEBUG
+// </c>
+// <o>enable components initialization debug configuration<0-1>
+//  <i>Default: 0
+#define RT_DEBUG_INIT 0
+// <c1>thread stack over flow detect
+//  <i> Diable Thread stack over flow detect
+//#define RT_USING_OVERFLOW_CHECK
+// </c>
+// </h>
+
+// <h>Hook Configuration
+// <c1>using hook
+//  <i>using hook
+//#define RT_USING_HOOK
+// </c>
+// <c1>using idle hook
+//  <i>using idle hook
+//#define RT_USING_IDLE_HOOK
+// </c>
+// </h>
+
+// <e>Software timers Configuration
+// <i> Enables user timers
+#define RT_USING_TIMER_SOFT         0
+#if RT_USING_TIMER_SOFT == 0
+    #undef RT_USING_TIMER_SOFT
+#endif
+// <o>The priority level of timer thread <0-31>
+//  <i>Default: 4
+#define RT_TIMER_THREAD_PRIO        4
+// <o>The stack size of timer thread <0-8192>
+//  <i>Default: 512
+#define RT_TIMER_THREAD_STACK_SIZE  512
+// </e>
+
+// <h>IPC(Inter-process communication) Configuration
+// <c1>Using Semaphore
+//  <i>Using Semaphore
+#define RT_USING_SEMAPHORE
+// </c>
+// <c1>Using Mutex
+//  <i>Using Mutex
+//#define RT_USING_MUTEX
+// </c>
+// <c1>Using Event
+//  <i>Using Event
+//#define RT_USING_EVENT
+// </c>
+// <c1>Using MailBox
+//  <i>Using MailBox
+#define RT_USING_MAILBOX
+// </c>
+// <c1>Using Message Queue
+//  <i>Using Message Queue
+//#define RT_USING_MESSAGEQUEUE
+// </c>
+// </h>
+
+// <h>Memory Management Configuration
+// <c1>Dynamic Heap Management
+//  <i>Dynamic Heap Management
+//#define RT_USING_HEAP
+// </c>
+// <c1>using small memory
+//  <i>using small memory
+#define RT_USING_SMALL_MEM
+// </c>
+// <c1>using tiny size of memory
+//  <i>using tiny size of memory
+//#define RT_USING_TINY_SIZE
+// </c>
+// </h>
+
+// <h>Console Configuration
+// <c1>Using console
+//  <i>Using console
+//#define RT_USING_CONSOLE
+
+// </c>
+// <o>the buffer size of console <1-1024>
+//  <i>the buffer size of console
+//  <i>Default: 128  (128Byte)
+#define RT_CONSOLEBUF_SIZE          256
+// </h>
+
+// <h>FinSH Configuration
+// <c1>include finsh config
+//  <i>Select this choice if you using FinSH 
+//#include "finsh_config.h"
+// </c>
+// </h>
+
+// <h>Device Configuration
+// <c1>using device framework
+//  <i>using device framework
+//#define RT_USING_DEVICE
+// </c>
+// </h>
+
+// <<< end of configuration section >>>
+
+#endif
