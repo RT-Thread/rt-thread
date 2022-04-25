81f04ad55156566e678fdf3aa62ac2a4425d60a4
tyx <462747508@qq.com>
2022-04-25 17:41:38 +0800

    [drivers][serial] ioctl add F_SETFL cmd

--- components/drivers/serial/serial_v2.c
+++ components/drivers/serial/serial_v2.c
@@ -94,6 +94,8 @@ static int serial_fops_close(struct dfs_fd *fd)
 static int serial_fops_ioctl(struct dfs_fd *fd, int cmd, void *args)
 {
     rt_device_t device;
+    int flags = (int)(rt_base_t)args;
+    int mask  = O_NONBLOCK | O_APPEND;
 
     device = (rt_device_t)fd->data;
     switch (cmd)
@@ -101,6 +103,11 @@ static int serial_fops_ioctl(struct dfs_fd *fd, int cmd, void *args)
     case FIONREAD:
         break;
     case FIONWRITE:
+        break;
+    case F_SETFL:
+        flags &= mask;
+        fd->flags &= ~mask;
+        fd->flags |= flags;
         break;
     }
 
