5abfe2085a126b244e2a1354d97d0e498754a6ec
Kevin Liu <50488882+klmchp@users.noreply.github.com>
2022-04-27 09:24:56 +0800

    添加对Microchip SAML10系列支持 (#5868)
    * Add SAML10 Cortex-M23 series support
    
    1. Add SAML10 Cortex-M23 series support; 2. remove STDIO related code of same54 series, no need any more; 3. update rtconfig.py to support output bin & map file containing DEVICE PART info.

---
+++ bsp/microchip/saml10/bsp/include/component/ptc.h
@@ -0,0 +1,53 @@
+/**
+ * \file
+ *
+ * \brief Component description for PTC
+ *
+ * Copyright (c) 2019 Microchip Technology Inc.
+ *
+ * \license_start
+ *
+ * \page License
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * \license_stop
+ *
+ */
+
+/* file generated from device description version 2019-01-31T14:29:25Z */
+#ifndef _SAML10_PTC_COMPONENT_H_
+#define _SAML10_PTC_COMPONENT_H_
+#define _SAML10_PTC_COMPONENT_         /**< \deprecated  Backward compatibility for ASF */
+
+/** \addtogroup SAML_SAML10 Peripheral Touch Controller
+ *  @{
+ */
+/* ========================================================================== */
+/**  SOFTWARE API DEFINITION FOR PTC */
+/* ========================================================================== */
+
+#define PTC_U2215                      /**< (PTC) Module ID */
+#define REV_PTC 0x500                  /**< (PTC) Module revision */
+
+#if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
+/** \brief No hardware registers defined for PTC */
+typedef void Ptc;
+
+
+#endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
+/** @}  end of Peripheral Touch Controller */
+
+#endif /* _SAML10_PTC_COMPONENT_H_ */
