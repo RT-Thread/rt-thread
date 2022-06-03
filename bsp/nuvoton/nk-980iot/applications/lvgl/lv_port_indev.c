1fb3f1fcecc4c799f38af967239c81b74d00d090
Wayne <wosayttn@gmail.com>
2022-06-03 10:01:15 +0800

    [bsp/nuvoton] Update. (#6022)
    1. Sync 4.1.1+.
    2. Add SDIO drivers for M480/M2354 platforms.
    3. Update LVGL porting.
    4. Add N9H30 FMI NAND driver.
    
    Co-authored-by: Wayne Lin <wclin@nuvoton.com>

--- bsp/nuvoton/nk-980iot/applications/lvgl/lv_port_indev.c
+++ bsp/nuvoton/nk-980iot/applications/lvgl/lv_port_indev.c
@@ -31,14 +31,11 @@ void nu_touch_inputevent_cb(rt_int16_t x, rt_int16_t y, rt_uint8_t state)
     case RT_TOUCH_EVENT_UP:
         last_state = LV_INDEV_STATE_RELEASED;
         break;
+    case RT_TOUCH_EVENT_MOVE:
     case RT_TOUCH_EVENT_DOWN:
         last_x = x;
         last_y = y;
         last_state = LV_INDEV_STATE_PRESSED;
-        break;
-    case RT_TOUCH_EVENT_MOVE:
-        last_x = x;
-        last_y = y;
         break;
     }
 }
