8633a9234574f86d9912b47e5374492fbc054c47
Meco Man <920369182@qq.com>
2022-06-07 17:25:31 -0400

    implement extension standard C functions

--- components/libc/compilers/common/stdlib.c
+++ components/libc/compilers/common/stdlib.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -44,3 +44,101 @@ int system(const char *command)
 }
 RTM_EXPORT(system);
 #endif /* RT_USING_MSH */
+
+char *ltoa(long value, char *string, int radix)
+{
+    char tmp[33];
+    char *tp = tmp;
+    long i;
+    unsigned long v;
+    int sign;
+    char *sp;
+
+    if (string == NULL)
+    {
+        return 0 ;
+    }
+
+    if (radix > 36 || radix <= 1)
+    {
+        return 0 ;
+    }
+
+    sign = (radix == 10 && value < 0);
+    if (sign)
+    {
+        v = -value;
+    }
+    else
+    {
+        v = (unsigned long)value;
+    }
+
+    while (v || tp == tmp)
+    {
+        i = v % radix;
+        v = v / radix;
+        if (i < 10)
+            *tp++ = i+'0';
+        else
+            *tp++ = i + 'a' - 10;
+    }
+
+    sp = string;
+
+    if (sign)
+        *sp++ = '-';
+    while (tp > tmp)
+        *sp++ = *--tp;
+    *sp = 0;
+
+    return string;
+}
+
+char *itoa(int value, char *string, int radix)
+{
+    return ltoa(value, string, radix) ;
+}
+
+
+char *ultoa(unsigned long value, char *string, int radix)
+{
+    char tmp[33];
+    char *tp = tmp;
+    long i;
+    unsigned long v = value;
+    char *sp;
+
+    if (string == NULL)
+    {
+        return 0;
+    }
+
+    if (radix > 36 || radix <= 1)
+    {
+        return 0;
+    }
+
+    while (v || tp == tmp)
+    {
+        i = v % radix;
+        v = v / radix;
+        if (i < 10)
+            *tp++ = i+'0';
+        else
+            *tp++ = i + 'a' - 10;
+    }
+
+    sp = string;
+
+    while (tp > tmp)
+        *sp++ = *--tp;
+    *sp = 0;
+
+    return string;
+}
+
+char *utoa(unsigned value, char *string, int radix)
+{
+    return ultoa(value, string, radix) ;
+}
