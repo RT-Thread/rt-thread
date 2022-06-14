5d5a4b8695990c4447fc4e7a8d1dc1661d3ac632
‘solar-li’ <941843540@qq.com>
2022-06-14 16:58:58 +0800

    [stm32][soft spi] implement soft spi

---
+++ bsp/stm32/stm32f407-atk-explorer/board/ports/soft_spi_flash_init.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-27     SummerGift   add spi flash port file
+ */
+
+#include <rtthread.h>
+#include "spi_flash.h"
+#include "spi_flash_sfud.h"
+#include "drv_spi.h"
+#include "drv_soft_spi.h"
+
+#if defined(BSP_USING_SOFT_SPI_FLASH)
+
+static int rt_soft_spi_flash_init(void)
+{
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    rt_hw_soft_spi_device_attach("sspi2", "sspi20", "PB.14");
+
+    if (RT_NULL == rt_sfud_flash_probe("W25Q128", "sspi20"))
+    {
+        return -RT_ERROR;
+    }
+
+    return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(rt_soft_spi_flash_init);
+#endif
+
