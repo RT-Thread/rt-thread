37c5e3534b0de7b60fcb830ac93b6f06fef171ab
a1012112796 <1012112796@qq.com>
2022-06-29 14:21:21 +0800

    [enhancement] Add string information for error (#3186)
    * [enhancement]Add string information for error
    
    * Update src/kservice.c
    
    * Update src/kservice.c
    
    Co-authored-by: Man, Jianting (Meco) <920369182@qq.com>
    
    * remove %m
    
    Signed-off-by: a1012112796 <1012112796@qq.com>
    
    Co-authored-by: Meco Man <920369182@qq.com>

--- components/finsh/cmd.c
+++ components/finsh/cmd.c
@@ -229,15 +229,14 @@ long list_thread(void)
                                thread->error);
 #else
                     ptr = (rt_uint8_t *)thread->stack_addr;
-                    while (*ptr == '#')ptr ++;
-
-                    rt_kprintf(" 0x%08x 0x%08x    %02d%%   0x%08x %03d\n",
+                    while (*ptr == '#') ptr ++;
+                    rt_kprintf(" 0x%08x 0x%08x    %02d%%   0x%08x %s\n",
                                thread->stack_size + ((rt_ubase_t)thread->stack_addr - (rt_ubase_t)thread->sp),
                                thread->stack_size,
                                (thread->stack_size - ((rt_ubase_t) ptr - (rt_ubase_t) thread->stack_addr)) * 100
                                / thread->stack_size,
                                thread->remaining_tick,
-                               thread->error);
+                               rt_strerror(thread->error));
 #endif
                 }
             }
