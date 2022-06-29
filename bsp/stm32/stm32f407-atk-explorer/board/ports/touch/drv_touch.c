09dd01065aee8232add076a198f899813097d012
solar_li <64183082+solar-li@users.noreply.github.com>
2022-06-29 14:11:36 +0800

    Add lvgl adaptation of touch screen xpt2046 for BSP (#6114)
    * lvgl adaptation to resistive touch screen xpt2046
    
    * some bug
    
    * canonical code
    
    * canonical code
    
    * fix
    
    * Modify the calibration section
    
    * Modify the calibration section
    
    * fix

--- bsp/stm32/stm32f407-atk-explorer/board/ports/touch/drv_touch.c
+++ bsp/stm32/stm32f407-atk-explorer/board/ports/touch/drv_touch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -10,7 +10,7 @@
 
 #include <rtconfig.h>
 
-#ifdef BSP_USING_TOUCH
+#ifdef BSP_USING_TOUCH_CAP
 #include "drv_touch.h"
 #include <string.h>
 
