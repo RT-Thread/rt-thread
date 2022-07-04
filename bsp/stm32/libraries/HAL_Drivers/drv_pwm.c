d842d5b7d262daebba076a2d6fb8379db73d65c1
BreederBai <songshu_bai@icloud.com>
2022-07-04 10:11:27 +0800

    Update bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
    Co-authored-by: Man, Jianting (Meco) <920369182@qq.com>

--- bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
@@ -335,7 +335,7 @@ static rt_err_t drv_pwm_set(TIM_HandleTypeDef *htim, struct rt_pwm_configuration
     please uncommon the following code, but it will cause the last PWM cycle not complete. */
     //__HAL_TIM_SET_COUNTER(htim, 0);
 
-    //HAL_TIM_GenerateEvent(htim, TIM_EVENTSOURCE_UPDATE);
+    //HAL_TIM_GenerateEvent(htim, TIM_EVENTSOURCE_UPDATE); /* Update frequency value */
 
     return RT_EOK;
 }
