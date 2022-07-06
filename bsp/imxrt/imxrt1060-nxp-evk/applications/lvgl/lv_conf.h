f99904769d848612ef7f6fce66e54c09f4b46ec6
Meco Man <920369182@qq.com>
2022-07-06 03:35:37 -0400

    [lvgl] adapt lvgl 8.3.0

--- bsp/imxrt/imxrt1060-nxp-evk/applications/lvgl/lv_conf.h
+++ bsp/imxrt/imxrt1060-nxp-evk/applications/lvgl/lv_conf.h
@@ -15,6 +15,8 @@
 
 #define LV_USE_PERF_MONITOR     1
 #define LV_COLOR_DEPTH          16
+#define LV_HOR_RES_MAX          LCD_WIDTH
+#define LV_VER_RES_MAX          LCD_HEIGHT
 
 #define LV_USE_NXP_SOC          1
 //#define LV_USE_GPU_NXP_PXP        1
@@ -23,12 +25,13 @@
 
 #define LV_ATTRIBUTE_MEM_ALIGN_SIZE 64
 
+#ifdef PKG_USING_LV_MUSIC_DEMO
 /* music player demo */
-#define LV_HOR_RES_MAX              LCD_WIDTH
-#define LV_VER_RES_MAX              LCD_HEIGHT
 #define LV_USE_DEMO_RTT_MUSIC       1
 #define LV_DEMO_RTT_MUSIC_AUTO_PLAY 1
 #define LV_FONT_MONTSERRAT_12       1
 #define LV_FONT_MONTSERRAT_16       1
+#define LV_COLOR_SCREEN_TRANSP      1
+#endif /* PKG_USING_LV_MUSIC_DEMO */
 
 #endif
--- bsp/nuvoton/numaker-m032ki/applications/lvgl/lv_conf.h
+++ bsp/nuvoton/numaker-m032ki/applications/lvgl/lv_conf.h
@@ -24,6 +24,7 @@
 
 //#define LV_USE_DEMO_RTT_MUSIC         1
 #if LV_USE_DEMO_RTT_MUSIC
+    #define LV_COLOR_SCREEN_TRANSP        1
     #define LV_DEMO_RTT_MUSIC_AUTO_PLAY   1
 
     #define LV_USE_DEMO_MUSIC             1
