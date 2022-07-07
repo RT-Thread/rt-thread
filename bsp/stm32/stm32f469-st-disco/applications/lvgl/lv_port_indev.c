19ce98b1a1c53f8ff18b6d60ab0ddc67b585cf0a
liyangyang <941843540@qq.com>
2022-07-07 19:11:14 +0800

    formatting code

--- bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
+++ bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
@@ -32,7 +32,7 @@ static void input_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
 
     if (read_data->event == RT_TOUCH_EVENT_NONE)
         return;
-        
+
     /* Since the origin of the LCD screen and the origin of the touch screen are
      * different, the parameters passed in here need to be simply converted. */
 #ifdef BSP_USING_TOUCH_FT6X36
