457d06f889f8d9fe7c6c93b41f9468ce1fa81c30
liyangyang <941843540@qq.com>
2022-07-07 11:30:53 +0800

    Docking lvgl touch driver using package

--- bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
+++ bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
@@ -1,35 +1,25 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2021-10-18     Meco Man     The first version
+ * 2022-07-07     liYony       The first version
  */
 #include <lvgl.h>
 #include <rtdevice.h>
 
 #include <lcd_port.h>
 
-static lv_indev_state_t last_state = LV_INDEV_STATE_REL;
-static rt_int16_t last_x = 0;
-static rt_int16_t last_y = 0;
-
-lv_indev_t * touch_indev;
+lv_indev_t *touch_indev;
 
 static void input_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
 {
-    data->point.x = last_x;
-    data->point.y = last_y;
-    data->state = last_state;
-}
-
-void lv_port_indev_input(rt_int16_t x, rt_int16_t y, lv_indev_state_t state)
-{
-    last_state = state;
-    last_x = x;
-    last_y = LCD_HEIGHT - y;
+    /* You must create the drv_touch_xxx.c file in the board/ports/touch directory
+     * according to your own touch driver, and implement "touch_common_read()" */
+    extern void touch_common_read(lv_indev_data_t * data);
+    touch_common_read(data);
 }
 
 void lv_port_indev_init(void)
--- bsp/stm32/stm32f469-st-disco/board/ports/touch/drv_touch_ft6x36.c
+++ bsp/stm32/stm32f469-st-disco/board/ports/touch/drv_touch_ft6x36.c
@@ -5,7 +5,7 @@
  *
  * Change Logs:
  * Date           Author       Notes
- * 2022-06-29     solar        the first version
+ * 2022-07-07     liYony       the first version
  */
 
 #include <rtthread.h>
@@ -20,6 +20,7 @@
 
 #ifdef BSP_USING_TOUCH_FT6X36
 
+/* Include the package header files you are using */
 #include "ft6236.h"
 
 #define BSP_TOUCH_I2C_BUS_NAME "i2c1"
@@ -27,40 +28,31 @@
 
 #ifdef PKG_USING_LVGL
 #include <lvgl.h>
-extern void lv_port_indev_input(rt_int16_t x, rt_int16_t y, lv_indev_state_t state);
 #endif /* PKG_USING_LVGL */
 
-rt_thread_t ft6236_thread;
 rt_device_t touch;
 
-void ft6236_thread_entry(void *parameter)
+void touch_common_read(lv_indev_data_t *data)
 {
     struct rt_touch_data *read_data;
-
     read_data = (struct rt_touch_data *)rt_calloc(1, sizeof(struct rt_touch_data));
 
-    while (1)
-    {
-        rt_device_read(touch, 0, read_data, 1);
-#ifdef PKG_USING_LVGL
-        /* Since the origin of the LCD screen and the origin of the touch screen are
-         * different, the parameters passed in here need to be simply converted. */
-        if (read_data->event == RT_TOUCH_EVENT_DOWN)
-            lv_port_indev_input(read_data->y_coordinate, read_data->x_coordinate, LV_INDEV_STATE_PR);
+    rt_device_read(touch, 0, read_data, 1);
 
-        if (read_data->event == RT_TOUCH_EVENT_MOVE)
-            lv_port_indev_input(read_data->y_coordinate, read_data->x_coordinate, LV_INDEV_STATE_PR);
+    /* Since the origin of the LCD screen and the origin of the touch screen are
+     * different, the parameters passed in here need to be simply converted. */
+    data->point.x = read_data->y_coordinate;
+    data->point.y = LCD_HEIGHT - read_data->x_coordinate;
 
-        if (read_data->event == RT_TOUCH_EVENT_UP)
-            lv_port_indev_input(read_data->y_coordinate, read_data->x_coordinate, LV_INDEV_STATE_REL);
-#endif /* PKG_USING_LVGL */
-        if (read_data->event != RT_TOUCH_EVENT_NONE)
-            LOG_I("LCD point x: %03d y: %03d", read_data->y_coordinate, LCD_HEIGHT - read_data->x_coordinate);
-        rt_thread_delay(10);
-    }
+    if (read_data->event == RT_TOUCH_EVENT_DOWN)
+        data->state = LV_INDEV_STATE_PR;
+    if (read_data->event == RT_TOUCH_EVENT_MOVE)
+        data->state = LV_INDEV_STATE_PR;
+    if (read_data->event == RT_TOUCH_EVENT_UP)
+        data->state = LV_INDEV_STATE_REL;
 }
 
-int ft6236_for_lvgl(void)
+int touch_ft6x36_init(void)
 {
     struct rt_touch_config cfg;
 
@@ -70,17 +62,8 @@ int ft6236_for_lvgl(void)
     touch = rt_device_find("touch");
     rt_device_open(touch, RT_DEVICE_FLAG_RDONLY);
 
-    ft6236_thread = rt_thread_create("touch", ft6236_thread_entry, RT_NULL, 1024, 10, 20);
-    if (ft6236_thread == RT_NULL)
-    {
-        LOG_D("create ft6236 thread err");
-
-        return -RT_ENOMEM;
-    }
-    rt_thread_startup(ft6236_thread);
-
     return RT_EOK;
 }
-INIT_ENV_EXPORT(ft6236_for_lvgl);
+INIT_ENV_EXPORT(touch_ft6x36_init);
 
 #endif /* BSP_USING_TOUCH_FT6X36 */
