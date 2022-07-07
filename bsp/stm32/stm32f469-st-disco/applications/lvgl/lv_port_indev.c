a11d92457459ae7135eda25ea0e0868e5796f0aa
liyangyang <941843540@qq.com>
2022-07-07 12:37:02 +0800

    Improve the docking process

--- bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
+++ bsp/stm32/stm32f469-st-disco/applications/lvgl/lv_port_indev.c
@@ -12,14 +12,42 @@
 
 #include <lcd_port.h>
 
-lv_indev_t *touch_indev;
+#ifdef BSP_USING_LVGL
+
+/* Include the package header files you are using */
+#ifdef BSP_USING_TOUCH_FT6X36
+#include "ft6236.h"
+#endif /* BSP_USING_TOUCH_FT6X36 */
+
+/* Touch chip connection information */
+#define BSP_TOUCH_I2C_BUS_NAME      "i2c1"
+#define BSP_TOUCH_I2C_RESET_PIN      119 /* PH.7 */
+/* RT-Thread touch device name */
+#define TOUCH_DEV_NAME "touch"
+
+lv_indev_t * touch_indev;
+rt_device_t touch_dev;
 
 static void input_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
 {
-    /* You must create the drv_touch_xxx.c file in the board/ports/touch directory
-     * according to your own touch driver, and implement "touch_common_read()" */
-    extern void touch_common_read(lv_indev_data_t * data);
-    touch_common_read(data);
+    struct rt_touch_data *read_data;
+    read_data = (struct rt_touch_data *)rt_calloc(1, sizeof(struct rt_touch_data));
+
+    rt_device_read(touch_dev, 0, read_data, 1);
+
+    /* Since the origin of the LCD screen and the origin of the touch screen are
+     * different, the parameters passed in here need to be simply converted. */
+#ifdef BSP_USING_TOUCH_FT6X36
+    data->point.x = read_data->y_coordinate;
+    data->point.y = LCD_HEIGHT - read_data->x_coordinate;
+#endif /* BSP_USING_TOUCH_FT6X36 */
+
+    if (read_data->event == RT_TOUCH_EVENT_DOWN)
+        data->state = LV_INDEV_STATE_PR;
+    if (read_data->event == RT_TOUCH_EVENT_MOVE)
+        data->state = LV_INDEV_STATE_PR;
+    if (read_data->event == RT_TOUCH_EVENT_UP)
+        data->state = LV_INDEV_STATE_REL;
 }
 
 void lv_port_indev_init(void)
@@ -33,3 +61,22 @@ void lv_port_indev_init(void)
     /*Register the driver in LVGL and save the created input device object*/
     touch_indev = lv_indev_drv_register(&indev_drv);
 }
+
+static int lv_hw_touch_init(void)
+{
+    struct rt_touch_config cfg;
+
+#ifdef BSP_USING_TOUCH_FT6X36
+    cfg.dev_name = BSP_TOUCH_I2C_BUS_NAME;
+    rt_hw_ft6236_init(TOUCH_DEV_NAME, &cfg, BSP_TOUCH_I2C_RESET_PIN);
+#endif /* BSP_USING_TOUCH_FT6X36 */
+
+    touch_dev = rt_device_find(TOUCH_DEV_NAME);
+    rt_device_open(touch_dev, RT_DEVICE_FLAG_RDONLY);
+
+    return RT_EOK;
+}
+
+INIT_COMPONENT_EXPORT(lv_hw_touch_init);
+
+#endif /* BSP_USING_LVGL */
