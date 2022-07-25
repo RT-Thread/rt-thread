f5e11f2b37279babefb104f3d79b95e7aa2acb90
wudiyidashi <42587056+wudiyidashi@users.noreply.github.com>
2022-07-25 17:36:38 +0800

    Update board.c

--- bsp/fm33lc026/board/board.c
+++ bsp/fm33lc026/board/board.c
@@ -74,25 +74,26 @@ FL_ErrorStatus FL_UART_GPIO_Init(UART_Type *UARTx)
 FL_ErrorStatus FL_SPI_GPIO_Init(SPI_Type *SPIx)
 {
     FL_ErrorStatus status = FL_FAIL;
-    FL_GPIO_InitTypeDef    GPIO_InitStruct;
-    if (SPIx ==  SPI1)
+    FL_GPIO_InitTypeDef GPIO_InitStruct;
+    if (SPIx == SPI1)
     {
-    GPIO_InitStruct.pin =  FL_GPIO_PIN_11 | FL_GPIO_PIN_10 | FL_GPIO_PIN_9;
-    GPIO_InitStruct.mode = FL_GPIO_MODE_DIGITAL;
-    GPIO_InitStruct.outputType = FL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.pull = FL_DISABLE;
-    GPIO_InitStruct.remapPin = FL_DISABLE;
-    status=FL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        GPIO_InitStruct.pin = FL_GPIO_PIN_11 | FL_GPIO_PIN_10 | FL_GPIO_PIN_9;
+        GPIO_InitStruct.mode = FL_GPIO_MODE_DIGITAL;
+        GPIO_InitStruct.outputType = FL_GPIO_OUTPUT_PUSHPULL;
+        GPIO_InitStruct.pull = FL_DISABLE;
+        GPIO_InitStruct.remapPin = FL_DISABLE;
 
+        status=FL_GPIO_Init(GPIOB, &GPIO_InitStruct);
     }
-    else if (SPIx ==  SPI2)
+    else if (SPIx == SPI2)
     {
-    GPIO_InitStruct.pin =  FL_GPIO_PIN_8 | FL_GPIO_PIN_10 | FL_GPIO_PIN_9;
-    GPIO_InitStruct.mode = FL_GPIO_MODE_DIGITAL;
-    GPIO_InitStruct.outputType = FL_GPIO_OUTPUT_PUSHPULL;
-    GPIO_InitStruct.pull = FL_DISABLE;
-    GPIO_InitStruct.remapPin = FL_DISABLE;
-    status=FL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+        GPIO_InitStruct.pin = FL_GPIO_PIN_8 | FL_GPIO_PIN_10 | FL_GPIO_PIN_9;
+        GPIO_InitStruct.mode = FL_GPIO_MODE_DIGITAL;
+        GPIO_InitStruct.outputType = FL_GPIO_OUTPUT_PUSHPULL;
+        GPIO_InitStruct.pull = FL_DISABLE;
+        GPIO_InitStruct.remapPin = FL_DISABLE;
+        
+        status=FL_GPIO_Init(GPIOC, &GPIO_InitStruct);
     }
 
     return status;
