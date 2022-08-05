dd3024698dfdf754bf26cfae739e04538301aebf
liyangyang <941843540@qq.com>
2022-08-05 23:15:54 +0800

    对接RTdunio

---
+++ bsp/stm32/stm32f103-blue-pill/applications/arduino_main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-08-05     liYony       first version
+ */
+
+#include <Arduino.h>
+
+void setup(void)
+{
+    /* put your setup code here, to run once: */
+}
+
+void loop(void)
+{
+    /* put your main code here, to run repeatedly: */
+    Serial.println("Hello Arduino!\n");
+    delay(800);
+}
