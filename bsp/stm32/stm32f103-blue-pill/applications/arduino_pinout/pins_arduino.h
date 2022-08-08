40f5e1d1eae77dbdf63b04cc21427c391b7efece
liYang~ <941843540@qq.com>
2022-08-08 22:00:03 +0800

    [Ardunio] stm32-blue-pill对接RTdunio软件包 (#6241)
    按照pinout布局图重新设计Arduino引脚布局和功能
    - 目前PB0（D12, PWM）和PA15（D5，PWM）测试不通过
    参考：https://github.com/stm32duino/Arduino_Core_STM32/issues/1082#issuecomment-634027716

--- bsp/stm32/stm32f103-blue-pill/applications/arduino_pinout/pins_arduino.h
+++ bsp/stm32/stm32f103-blue-pill/applications/arduino_pinout/pins_arduino.h
@@ -26,34 +26,26 @@
 #define D11  (11)
 #define D12  (12)
 #define D13  (13)
-#define D14  (14)
-#define D15  (15)
-#define D16  (16)
-#define D17  (17)
-#define D18  (18)
-#define D19  (19)
-#define D20  (20)
-#define D21  (21)
-#define D22  (22)
-#define D23  (23)
-#define D24  (24)
-#define D25  (25)
-#define D26  (26)
-#define D27  (27)
-#define D28  (28)
-#define A0   (29)
-#define A1   (30)
-#define A2   (31)
-#define A3   (32)
-#define A4   (33)
-#define A5   (34)
-#define A6   (35)
+#define A0   (14)
+#define A1   (15)
+#define A2   (16)
+#define A3   (17)
+#define A4   (18)
+#define A5   (19)
+#define A6   (20)
 
 #define F_CPU  72000000L /* CPU: 72MHz */
-#define LED_BUILTIN  D19 /* Default Built-in LED */
+#define LED_BUILTIN  D8 /* Default Built-in LED */
 
+/*
+ * i2c1 - PB9-SDA PB8-SCL
+ */
 #define RTDUINO_DEFAULT_IIC_BUS_NAME            "i2c1"
 
+/*
+ * uart2 - PA2-TX  PA3-RX
+ * uart3 - PB10-TX PB11-RX
+ */
 #define RTDUINO_SERIAL2_DEVICE_NAME             "uart2"
 #define RTDUINO_SERIAL3_DEVICE_NAME             "uart3"
 
