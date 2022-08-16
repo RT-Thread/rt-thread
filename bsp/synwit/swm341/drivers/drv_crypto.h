1bc02ab6e891ad6c5d368f59b7106883d2773fb8
woody <whsj215294062@outlook.com>
2022-08-17 00:43:24 +0800

    将swm320和swm341整合进synwit (#6290)
    * 将swm320和swm341整合进synwit

---
+++ bsp/synwit/swm341/drivers/drv_crypto.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2006-2022, Synwit Technology Co.,Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-07-01     lik          first version
+ */
+
+#ifndef __DRV_CRYPTO_H__
+#define __DRV_CRYPTO_H__
+
+#include "board.h"
+
+int swm_crypto_init(void);
+
+#endif /* __DRV_CRYPTO_H__ */
