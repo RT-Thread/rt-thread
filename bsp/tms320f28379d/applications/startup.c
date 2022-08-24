09b17524192aba18fe953df1c5d7e9d77849c68d
YuQi <qiyu_sjtu@163.com>
2022-08-24 21:23:40 +0800

    some polish
    remove "assert_failed" and reset kservice

--- bsp/tms320f28379d/applications/startup.c
+++ bsp/tms320f28379d/applications/startup.c
@@ -15,23 +15,6 @@
 #include <rtthread.h>
 #include "board.h"
 
-/*******************************************************************************
-* Function Name  : assert_failed
-* Description    : Reports the name of the source file and the source line number
-*                  where the assert error has occurred.
-* Input          : - file: pointer to the source file name
-*                  - line: assert error line source number
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void assert_failed(uint16_t* file, uint32_t line)
-{
-    rt_kprintf("\n\r Wrong parameter value detected on\r\n");
-    rt_kprintf("       file  %s\r\n", file);
-    rt_kprintf("       line  %d\r\n", line);
-
-    while (1) ;
-}
 
 int main(void)
 {
--- src/kservice.c
+++ src/kservice.c
@@ -642,8 +642,8 @@ void rt_show_version(void)
 {
     rt_kprintf("\n \\ | /\n");
     rt_kprintf("- RT -     Thread Operating System\n");
-   // rt_kprintf(" / | \\     %d.%d.%d build %s %s\n",
-   //            (rt_int32_t)RT_VERSION, (rt_int32_t)RT_SUBVERSION, (rt_int32_t)RT_REVISION, __DATE__, __TIME__);
+    rt_kprintf(" / | \\     %d.%d.%d build %s %s\n",
+               (rt_int32_t)RT_VERSION, (rt_int32_t)RT_SUBVERSION, (rt_int32_t)RT_REVISION, __DATE__, __TIME__);
     rt_kprintf(" 2006 - 2022 Copyright by RT-Thread team\n");
 }
 RTM_EXPORT(rt_show_version);
