96d1c3d2fd70043e460450a2633fc46c43729bf6
liyangyang <941843540@qq.com>
2022-08-27 10:46:27 +0800

    first

--- bsp/wch/risc-v/Libraries/ch32_drivers/drv_usart.h
+++ bsp/wch/risc-v/Libraries/ch32_drivers/drv_usart.h
@@ -1,16 +1,36 @@
 /*
- * Copyright (c) 2006-2022, RT-Thread Development Team
+ * Copyright (c) 2006-2018, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2009-01-05     Bernard      the first version
+ * 2021-09-09     WCH        the first version
  */
-#ifndef __USART_H__
-#define __USART_H__
-#include "rthw.h"
-#include "rtthread.h"
+#ifndef __DRV_USART_H__
+#define __DRV_USART_H__
+#include <rtthread.h>
+#include "rtdevice.h"
+#include <rthw.h>
+
+
+/* ch32 config class */
+struct ch32_uart_config
+{
+    const char *name;
+    USART_TypeDef *Instance;
+    IRQn_Type irq_type;
+};
+
+
+/* ch32 uart dirver class */
+struct ch32_uart
+{
+    struct ch32_uart_config *config;
+    USART_InitTypeDef Init;		
+    struct rt_serial_device serial;
+};
+
 
 int rt_hw_usart_init(void);
 #endif
