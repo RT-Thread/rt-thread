64ebc7e60bb53f19eeddd1671aa2a2565aa8856c
hg0720 <1723426975@qq.com>
2022-09-17 20:58:22 +0800

    添加了dac驱动

---
+++ bsp/wch/risc-v/Libraries/ch32_drivers/drv_dac.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-09-17     hg0720       add some operation function
+ * 2021-09-09     WCH          the first version
+ */
+#ifndef DRV_DAC_H__
+#define DRV_DAC_H__
+typedef struct
+{
+    DAC_TypeDef                   *Instance;
+    DAC_InitTypeDef                Init;
+    rt_uint32_t                    Channel;
+}DAC_HandleTypeDef;
+
+#endif
