921c5a3f616ba7485b3ebbdea3d59af55645afda
Meco Man <920369182@qq.com>
2022-09-21 17:17:11 -0400

    update

--- bsp/stm32/stm32f412-st-nucleo/applications/arduino_pinout/pins_arduino.c
+++ bsp/stm32/stm32f412-st-nucleo/applications/arduino_pinout/pins_arduino.c
@@ -38,9 +38,9 @@ const pin_map_t pin_map_table[]=
     {D14, GET_PIN(B,9), "i2c1"},        /* I2C-xx (Wire) */
     {D15, GET_PIN(B,8), "i2c1"},        /* I2C-xx (Wire) */
     {A0, GET_PIN(A,3), "adc1", 3},      /* ADC */
-    {A1, GET_PIN(C,0), "adc1", 3},      /* ADC */
-    {A2, GET_PIN(C,3), "adc1", 3},      /* ADC */
-    {A3, GET_PIN(C,1), "adc1", 3},      /* ADC */
-    {A4, GET_PIN(C,4), "adc1", 3},      /* ADC */
-    {A5, GET_PIN(C,5), "adc1", 3},      /* ADC */
+    {A1, GET_PIN(C,0), "adc1", 10},     /* ADC */
+    {A2, GET_PIN(C,3), "adc1", 13},     /* ADC */
+    {A3, GET_PIN(C,1), "adc1", 11},     /* ADC */
+    {A4, GET_PIN(C,4), "adc1", 14},     /* ADC */
+    {A5, GET_PIN(C,5), "adc1", 15},     /* ADC */
 };
--- bsp/stm32/stm32f412-st-nucleo/board/CubeMX_Config/Src/stm32f4xx_hal_msp.c
+++ bsp/stm32/stm32f412-st-nucleo/board/CubeMX_Config/Src/stm32f4xx_hal_msp.c
@@ -98,6 +98,87 @@ void HAL_MspInit(void)
 }
 
 /**
+* @brief ADC MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspInit 0 */
+
+  /* USER CODE END ADC1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**ADC1 GPIO Configuration
+    PC0     ------> ADC1_IN10
+    PC1     ------> ADC1_IN11
+    PC3     ------> ADC1_IN13
+    PA3     ------> ADC1_IN3
+    PC4     ------> ADC1_IN14
+    PC5     ------> ADC1_IN15
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief ADC MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+{
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+  /* USER CODE END ADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC1_CLK_DISABLE();
+
+    /**ADC1 GPIO Configuration
+    PC0     ------> ADC1_IN10
+    PC1     ------> ADC1_IN11
+    PC3     ------> ADC1_IN13
+    PA3     ------> ADC1_IN3
+    PC4     ------> ADC1_IN14
+    PC5     ------> ADC1_IN15
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5);
+
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_3);
+
+  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+  /* USER CODE END ADC1_MspDeInit 1 */
+  }
+
+}
+
+/**
 * @brief UART MSP Initialization
 * This function configures the hardware resources used in this example
 * @param huart: UART handle pointer
