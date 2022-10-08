e21f62506895a0626ce93b29f29942b3d0941cb5
YuQi <qiyu_sjtu@163.com>
2022-10-08 10:56:11 +0800

    remove unnecessary files

--- bsp/tms320f28379d/RAM/libraries/headers/cmd/subdir_rules.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-SHELL = cmd.exe
-
-# Each subdirectory must supply rules for building sources it contributes
-
