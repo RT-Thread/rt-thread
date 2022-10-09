f31a9fa988d55224a8161a6c4cf4a372a742cede
YuQi <qiyu_sjtu@163.com>
2022-10-09 23:00:11 +0800

    add adc porting

--- bsp/c28x/tms320f28379d/rtconfig.h
+++ bsp/c28x/tms320f28379d/rtconfig.h
@@ -77,6 +77,7 @@
 #define RT_USING_SERIAL_V1
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
+#define RT_USING_ADC
 #define RT_USING_PWM
 
 /* Using USB */
@@ -187,6 +188,8 @@
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
+#define BSP_USING_ADC
+#define BSP_USING_ADC1
 
 /* Board extended module Drivers */
 
