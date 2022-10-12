cdef4428705ad8c254620613eafd4c45d32ce873
Yifang <2053731441@qq.com>
2022-10-12 11:04:43 +0800

    Update drv_common.c

--- bsp/renesas/libraries/HAL_Drivers/drv_common.c
+++ bsp/renesas/libraries/HAL_Drivers/drv_common.c
@@ -130,11 +130,7 @@ RT_WEAK void rt_hw_board_init()
 #endif
 
     /* Set the shell console output device */
-#if defined(SEGGER_RTT_ENABLE)
-    extern int rt_hw_jlink_rtt_init(void);
-    rt_hw_jlink_rtt_init();
-    rt_console_set_device("jlinkRtt");
-#elif defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
+#if defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
     rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
 #endif
 
