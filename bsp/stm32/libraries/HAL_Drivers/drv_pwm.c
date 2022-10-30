315c25883cecbbf6014fc1718597093b5d4723d3
linshire <1127384500@qq.com>
2022-10-30 14:14:05 +0800

    [bsp] fix the return of the function(drv_pwm_control) in stm32 drv_pwm

--- bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
+++ bsp/stm32/libraries/HAL_Drivers/drv_pwm.c
@@ -396,7 +396,7 @@ static rt_err_t drv_pwm_control(struct rt_device_pwm *device, int cmd, void *arg
     case PWM_CMD_GET:
         return drv_pwm_get(htim, configuration);
     default:
-        return RT_EINVAL;
+        return -RT_EINVAL;
     }
 }
 
