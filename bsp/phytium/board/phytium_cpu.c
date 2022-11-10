1537544f6aaf30baf658fbe279b80b76c8bef664
zhugengyu <85654684+zhugengyu@users.noreply.github.com>
2022-11-10 22:22:48 +0800

    [bsp/phytium] add phytium bsp to support e2000 bootup with smp (#6566)
    add phytium board (E2000) bsp
    support usart
    support SMP with demo

---
+++ bsp/phytium/board/phytium_cpu.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Email: opensource_embedded@phytium.com.cn
+ *
+ * Change Logs:
+ * Date        Author       Notes
+ * 2022-10-26  huanghe      first commit
+ *
+ */
+
+#include "rtconfig.h"
+#include <rtthread.h>
+#include "gicv3.h"
+
+#include "fparameters.h"
+#include "fcpu_info.h"
+
+#include "phytium_cpu.h"
+
+int phytium_cpu_id_mapping(int cpu_id)
+{
+#if defined(TARGET_E2000Q)
+    switch (cpu_id)
+    {
+    case 0:
+        return 2;
+    case 1:
+        return 3;
+    case 2:
+        return 0;
+    case 3:
+        return 1;
+    default:
+        RT_ASSERT(0);
+        return 0;
+        break;
+    }
+#else
+    return (int)cpu_id;
+#endif
+}
+
+#if defined(TARGET_ARMV8_AARCH64)
+
+int phytium_cpu_id(void)
+{
+    FError ret;
+    u32 cpu_id;
+    ret = GetCpuId(&cpu_id);
+
+    if (ret != ERR_SUCCESS)
+    {
+        RT_ASSERT(0);
+    }
+    return phytium_cpu_id_mapping(cpu_id);
+};
+
+#else
+
+int rt_hw_cpu_id(void)
+{
+    FError ret;
+    u32 cpu_id;
+    ret = GetCpuId(&cpu_id);
+
+    if (ret != ERR_SUCCESS)
+    {
+        RT_ASSERT(0);
+    }
+
+    return phytium_cpu_id_mapping(cpu_id);
+};
+
+
+
+rt_uint64_t get_main_cpu_affval(void)
+{
+#if defined(TARGET_E2000Q)
+    return CORE2_AFF;
+#else
+    return CORE0_AFF;
+#endif
+}
+
+
+extern u32 GetCpuMaskToAffval(u32 *cpu_mask, u32 *cluster_id, u32 *target_list);
+rt_uint32_t arm_gic_cpumask_to_affval(rt_uint32_t *cpu_mask, rt_uint32_t *cluster_id, rt_uint32_t *target_list)
+{
+    return GetCpuMaskToAffval(cpu_mask, cluster_id, target_list);
+}
+
+
+#ifdef RT_USING_SMP
+
+void send_core_isg(void)
+{
+    for (rt_size_t i = 0; i <= 0xf; i++)
+    {
+        /* code */
+        rt_kprintf("i %x \r\n", i);
+        arm_gic_send_affinity_sgi(0, 0, i, 0);
+        rt_thread_mdelay(100);
+    }
+}
+MSH_CMD_EXPORT(send_core_isg, send_core_isg);
+
+#endif
+
+
+#endif
---
+++ bsp/phytium/libraries/standalone/drivers/rtc/frtc/frtc_intr.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright : (C) 2022 Phytium Information Technology, Inc.
+ * All Rights Reserved.
+ *
+ * This program is OPEN SOURCE software: you can redistribute it and/or modify it
+ * under the terms of the Phytium Public License as published by the Phytium Technology Co.,Ltd,
+ * either version 1.0 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Phytium Public License for more details.
+ *
+ *
+ * FilePath: frtc_intr.c
+ * Date: 2022-02-10 14:53:42
+ * LastEditTime: 2022-02-18 09:02:07
+ * Description:  This files is for
+ *
+ * Modify History:
+ *  Ver   Who        Date         Changes
+ * ----- ------     --------    --------------------------------------
+ * 1.0   Wangxiaodong   2021/8/3   init
+ */
+
+#include "fparameters.h"
+#include "fassert.h"
+#include "frtc.h"
+#include "finterrupt.h"
+
+
