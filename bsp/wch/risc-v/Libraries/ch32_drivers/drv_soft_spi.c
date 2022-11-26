94c47891d817e837846dbae6f1c92700c23c2448
linshire <1127384500@qq.com>
2022-11-26 12:01:39 +0800

    修复了软件软件spi文件时钟无法编译的错误

--- bsp/wch/risc-v/Libraries/ch32_drivers/drv_soft_spi.c
+++ bsp/wch/risc-v/Libraries/ch32_drivers/drv_soft_spi.c
@@ -11,7 +11,7 @@
 #include <board.h>
 #include "drv_soft_spi.h"
 
-#ifdef BSP_USING_SOFT_SPI
+#if defined(BSP_USING_SOFT_SPI1) || defined(BSP_USING_SOFT_SPI2)
 
 #define LOG_TAG             "drv.soft_spi"
 #include <drv_log.h>
@@ -225,4 +225,4 @@ int rt_soft_spi_init(void)
 }
 INIT_BOARD_EXPORT(rt_soft_spi_init);
 
-#endif /* BSP_USING_SOFT_SPI */
+#endif /* defined(BSP_USING_SOFT_SPI1) || defined(BSP_USING_SOFT_SPI2) */
