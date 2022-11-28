40cf081a38f192d7df56a995251ca19c291f0064
guozhanxin <guozhanxin@rt-thread.com>
2022-11-28 18:41:18 +0800

    fix userspace signal define diff from kernel

--- components/libc/compilers/common/include/sys/signal.h
+++ components/libc/compilers/common/include/sys/signal.h
@@ -16,6 +16,12 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#ifdef RT_USING_MUSLLIBC
+
+#include_next <sys/signal.h>
+
+#else
+
 #include <stdint.h>
 #include <sys/types.h>
 
@@ -228,6 +234,8 @@ int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
 #ifndef SIG_IGN
 #define SIG_IGN  ((void (*)(int)) 1)
 #endif
+
+#endif /* RT_USING_MUSLLIBC */
 
 #ifdef __cplusplus
 }
