eccedfc0f6961bf5a1b4cc0eaed4c2b89c405c54
guozhanxin <guozhanxin@rt-thread.com>
2022-11-30 14:28:02 +0800

    fix ci build error

---
+++ libcpu/arm/arm926/cpuport.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#ifndef  CPUPORT_H__
+#define  CPUPORT_H__
+
+#endif  /*CPUPORT_H__*/
