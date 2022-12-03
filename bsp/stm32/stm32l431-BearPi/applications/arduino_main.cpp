6f94acd047024400818da71e9927d21c06ce9ea4
liYony <941843540@qq.com>
2022-12-03 21:01:09 +0800

    delete test code

--- bsp/stm32/stm32l431-BearPi/applications/arduino_main.cpp
+++ bsp/stm32/stm32l431-BearPi/applications/arduino_main.cpp
@@ -22,27 +22,3 @@ void loop(void)
     Serial2.println("Hello Arduino!");
     delay(800);
 }
-int val = 0;
-static int rtduino(int argc, char **argv)
-{   
-    if (!strcmp(argv[1], "pwm"))
-    {
-        for(val=0;val<255;val++)
-        {
-            analogWrite(atoi(argv[2]), val); // 设置PWM输出，val越大，PWM占空比越大，led越亮
-            delay(10);//延迟
-        }
-        for(val=255;val>=0;val--)
-        {
-            analogWrite(atoi(argv[2]), val); // 设置PWM输出，val越大，PWM占空比越大，led越亮
-            delay(10);//延迟
-        }
-
-    }
-    if (!strcmp(argv[1], "adc"))
-    {
-        rt_kprintf("%d %d\n",atoi(argv[2]),analogRead(atoi(argv[2])));
-    }
-    return 0;
-}
-MSH_CMD_EXPORT(rtduino, rtduino [option]);
