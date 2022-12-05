4c44047f78deb8c83de13ec9177053971a475fd4
yandld <1453363089@qq.com>
2022-12-05 14:37:49 +0800

    [bsp][lpc55sxx] add LPC55S36 support

---
+++ bsp/lpc55sxx/Libraries/LPC55S36/LPC55S36/drivers/fsl_puf_v3.h
@@ -0,0 +1,286 @@
+/*
+ * Copyright 2017-2018 NXP
+ * All rights reserved.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _PUF_V3_H_
+#define _PUF_V3_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "fsl_common.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*!
+ * @addtogroup puf_v3_driver
+ * @{
+ */
+/*! @name Driver version */
+/*@{*/
+/*! @brief PUFv3 driver version. Version 2.0.0.
+ *
+ * Current version: 2.0.0
+ *
+ * Change log:
+ * - 2.0.0
+ *   - Initial version.
+ */
+#define FSL_PUF_V3_DRIVER_VERSION (MAKE_VERSION(2, 0, 0))
+/*@}*/
+
+#define kPUF_EndianLittle (0x0u)
+#define kPUF_EndianBig    (0x1u)
+typedef uint32_t puf_endianness_t;
+
+#define kPUF_KeyDestRegister (0x1u)
+#define kPUF_KeyDestKeyBus   (0x2u)
+#define kPUF_KeyDestInvalid  (0x3u)
+typedef uint32_t puf_key_dest_t;
+
+#define kPUF_KeyAllowRegister (0x1u)
+#define kPUF_KeyAllowKeyBus   (0x2u)
+#define kPUF_KeyAllowAll      (0x3u)
+typedef uint32_t puf_key_scope_t;
+
+#define kPUF_ResultOK                  (0x0u)
+#define kPUF_AcNotForThisProductPhase1 (0xf0u)
+#define kPUF_AcNotForThisProductPhase2 (0xf1u)
+#define kPUF_AcCorruptedPhase1         (0xf2u)
+#define kPUF_AcCorruptedPhase2         (0xf3u)
+#define kPUF_AcAuthFailedPhase1        (0xf4u)
+#define kPUF_AcAuthFailedPhase2        (0xf5u)
+#define kPUF_QualityVerificationFail   (0xf6u)
+#define kPUF_ContextIncorrect          (0xf7u)
+#define kPUF_DestinationNotAllowed     (0xf8u)
+#define kPUF_Failure                   (0xFFu)
+typedef uint32_t puf_result_code_t;
+
+typedef struct
+{
+    puf_endianness_t dataEndianness;
+    uint8_t CKGATING;
+} puf_config_t;
+
+typedef struct
+{
+    puf_key_scope_t keyScopeStarted;
+    puf_key_scope_t keyScopeEnrolled;
+    uint32_t userCtx0;
+    uint32_t userCtx1;
+} puf_key_ctx_t;
+
+#define PUF_ACTIVATION_CODE_SIZE              996
+#define PUF_GET_KEY_CODE_SIZE_FOR_KEY_SIZE(x) ((0x34u + x) + 0x10u * (x / 0x32u))
+
+enum _puf_status
+{
+    kStatus_PUF_OperationNotAllowed       = MAKE_STATUS(kStatusGroup_PUF, 0xA5),
+    kStatus_PUF_AcNotForThisProductPhase1 = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcNotForThisProductPhase1),
+    kStatus_PUF_AcNotForThisProductPhase2 = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcNotForThisProductPhase2),
+    kStatus_PUF_AcCorruptedPhase1         = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcCorruptedPhase1),
+    kStatus_PUF_AcCorruptedPhase2         = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcCorruptedPhase2),
+    kStatus_PUF_AcAuthFailedPhase1        = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcAuthFailedPhase1),
+    kStatus_PUF_NBOOT_AcAuthFailedPhase2  = MAKE_STATUS(kStatusGroup_PUF, kPUF_AcAuthFailedPhase2),
+    kStatus_PUF_QualityVerificationFail   = MAKE_STATUS(kStatusGroup_PUF, kPUF_QualityVerificationFail),
+    kStatus_PUF_ContextIncorrect          = MAKE_STATUS(kStatusGroup_PUF, kPUF_ContextIncorrect),
+    kStatus_PUF_DestinationNotAllowed     = MAKE_STATUS(kStatusGroup_PUF, kPUF_DestinationNotAllowed),
+    kStatus_PUF_Failure                   = MAKE_STATUS(kStatusGroup_PUF, kPUF_Failure),
+};
+
+/*******************************************************************************
+ * API
+ *******************************************************************************/
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/*!
+ * brief Sets the default configuration of PUF
+ *
+ * This function initialize PUF config structure to default values.
+ *
+ * @param conf PUF configuration structure
+ */
+void PUF_GetDefaultConfig(puf_config_t *conf);
+
+/*!
+ * brief Initialize PUF
+ *
+ * This function enables power to PUF block and waits until the block initializes.
+ *
+ * @param conf PUF configuration structure
+ * @return Status of the init operation
+ */
+status_t PUF_Init(PUF_Type *base, puf_config_t *conf);
+
+/*!
+ * brief Denitialize PUF
+ *
+ * This function disables power to PUF SRAM and peripheral clock.
+ *
+ * @param base PUF peripheral base address
+ * @param conf PUF configuration structure
+ */
+void PUF_Deinit(PUF_Type *base, puf_config_t *conf);
+
+/*!
+ * brief Enroll PUF
+ *
+ * This function derives a digital fingerprint, generates the corresponding Activation Code (AC)
+ * and returns it to be stored in an NVM or a file. This step needs to be
+ * performed only once for each device. This function may be permanently disallowed by a fuse.
+ *
+ * @param base PUF peripheral base address
+ * @param[out] activationCode Word aligned address of the resulting activation code.
+ * @param activationCodeSize Size of the activationCode buffer in bytes. Shall be 996 bytes.
+ * @param score Value of the PUF Score that was obtained during the enroll operation.
+ * @return Status of enroll operation.
+ */
+status_t PUF_Enroll(PUF_Type *base, uint8_t *activationCode, size_t activationCodeSize, uint8_t *score);
+
+/*!
+ * brief Start PUF
+ *
+ * The Activation Code generated during the Enroll operation is used to
+ * reconstruct the digital fingerprint. This needs to be done after every power-up
+ * and reset.
+ *
+ * @param base PUF peripheral base address
+ * @param[in] activationCode Word aligned address of the input activation code.
+ * @param activationCodeSize Size of the activationCode buffer in bytes. Shall be 996 bytes.
+ * @param score Value of the PUF Score that was obtained during the start operation.
+ * return Status of start operation.
+ */
+status_t PUF_Start(PUF_Type *base, const uint8_t *activationCode, size_t activationCodeSize, uint8_t *score);
+
+/*!
+ * brief Stop PUF
+ *
+ * The Stop operation removes all key material from PUF flipflops and PUF SRAM, and sets
+ * PUF to the Stopped state.
+ *
+ * @param base PUF peripheral base address
+ * @return Status of stop operation.
+ */
+status_t PUF_Stop(PUF_Type *base);
+
+/*!
+ * brief PUF Get Key
+ *
+ * The Get Key operation derives a key from the intrinsic PUF key and externally provided context.
+ *
+ * @param base PUF peripheral base address
+ * @param keyCtx PUF key context struct
+ * @param keyDest output destination of the derived PUF key
+ * @param[out] key Word aligned address of output key (only used when kPUF_KeyDestRegister).
+ * @param keySize Size of the derived key in bytes.
+ * @return Status of get key operation.
+ */
+status_t PUF_GetKey(PUF_Type *base, puf_key_ctx_t *keyCtx, puf_key_dest_t keyDest, uint8_t *key, size_t keySize);
+
+/*!
+ * brief PUF Wrap generated random
+ *
+ * The Wrap Generated Random operation wraps a random key into a Key Code (KC).
+ *
+ * @param base PUF peripheral base address
+ * @param keyCtx PUF key context struct
+ * @param keySize Size of the key to be generated in bytes.
+ * @param[out] keyCode Word aligned address of the resulting key code.
+ * @param keyCodeSize Size of the output keycode in bytes.
+ * @return Status of wrap generated random operation.
+ */
+status_t PUF_WrapGeneratedRandom(
+    PUF_Type *base, puf_key_ctx_t *keyCtx, size_t keySize, uint8_t *keyCode, size_t keyCodeSize);
+
+/*!
+ * brief PUF Wrap user key
+ *
+ * The Wrap operation wraps a user defined key into a Key Code (KC).
+ *
+ * @param base PUF peripheral base address
+ * @param keyCtx PUF key context struct.
+ * @param userKey Word aligned address of input user key.
+ * @param userKeySize Size of the key to be wrapped in bytes.
+ * @param[out] keyCode Word aligned address of the resulting key code.
+ * @param keyCodeSize Size of the output keycode in bytes.
+ * @return Status of wrap operation.
+ */
+status_t PUF_Wrap(
+    PUF_Type *base, puf_key_ctx_t *keyCtx, uint8_t *userKey, size_t userKeySize, uint8_t *keyCode, size_t keyCodeSize);
+
+/*!
+ * brief PUF Unwrap user key
+ *
+ * The unwrap operation unwraps the key from a previously created Key Code (KC)
+ *
+ * @param base PUF peripheral base address
+ * @param keyDest output destination of the unwraped PUF key
+ * @param[in] keyCode Word aligned address of the input key code.
+ * @param keyCodeSize Size of the input keycode in bytes.
+ * @param key Word aligned address of output key (only used when kPUF_KeyDestRegister).
+ * @param keySize Size of the key to be generated in bytes.
+ * @return Status of unwrap operation.
+ */
+status_t PUF_Unwrap(
+    PUF_Type *base, puf_key_dest_t keyDest, uint8_t *keyCode, size_t keyCodeSize, uint8_t *key, size_t keySize);
+
+/*!
+ * brief Generate Random
+ *
+ * The Generate Random operation outputs the requested amount of random data as specified in a
+ * provided context.
+ *
+ * @param base PUF peripheral base address
+ * @param size Size of random data to be genarated in bytes.
+ * @return Status of generate random operation.
+ */
+status_t PUF_GenerateRandom(PUF_Type *base, uint8_t *data, size_t size);
+
+/*!
+ * brief Zeroize PUF
+ *
+ * This function clears all PUF internal logic and puts the PUF to zeroized state.
+ *
+ * @param base PUF peripheral base address
+ * @return Status of the zeroize operation.
+ */
+status_t PUF_Zeroize(PUF_Type *base);
+
+/*!
+ * brief Test PUF
+ *
+ * With the Test PUF operation, diagnostics about the PUF quality is collected and presented in a PUF
+ * score.
+ *
+ * @param base PUF peripheral base address
+ * @param score Value of the PUF Score that was obtained during the enroll operation.
+ * @return Status of the test operation.
+ */
+status_t PUF_Test(PUF_Type *base, uint8_t *score);
+
+/*!
+ * @brief Blocks specified PUF commands
+ *
+ * This function blocks PUF commands specified by mask parameter.
+ *
+ * @param base PUF peripheral base address
+ * @param score Value of the PUF Score that was obtained during the enroll operation.
+ * @return Status of the test operation.
+ */
+static inline void PUF_BlockCommand(PUF_Type *base, uint32_t mask)
+{
+    base->CONFIG |= mask;
+}
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* _PUF_H_ */
---
+++ bsp/lpc55sxx/lpc55s36_nxp_evk/README.md
@@ -0,0 +1,124 @@
+# LPC55S36-EVK 板级支持包
+
+## 1. 简介(Introduction)
+
+LPC55S36 是由恩智浦NXP半导体推出的基于Cortex-M33内核的高性能单片机
+包括如下硬件特性：
+
+| 硬件 | 描述 |
+| -- | -- |
+|芯片型号| LPC5536/LPC55S36 全系列 |
+|CPU| Cortex-M33 , with FPU |
+|主频| 150MHz |
+
+## 2. 硬件开发环境(Hardware development system)
+
+开发板（EVK）
+
+![开发板示意图](./figures/board.png)
+
+
+
+## 3. 编译说明
+
+支持IDE:
+
+* MDK5: V5.36
+* IAR: 9.30.1
+
+1) 下载源码
+
+```bash
+    git clone https://github.com/RT-Thread/rt-thread.git
+```
+
+2) 配置工程并准备env
+
+（Linux/Mac）
+
+```bash
+    cd rt-thread/bsp/lpc55S36_evk
+    scons --menuconfig
+    source ~/.env/env.sh
+    pkgs --upgrade
+```
+
+（Windows）
+
+>在[RT-Thread官网][1]下载ENV工具包
+
+3) 配置芯片型号
+
+（Linux/Mac）
+
+```bash
+    scons --menuconfig
+```
+
+（Windows(ENV环境中)）
+
+```bash
+    menuconfig
+```
+
+在menuconfig页面配置并选择对应的芯片型号，若开发环境为MDK/IAR，则需要生成工程
+
+4) 生成工程
+
+IAR:
+
+```bash
+scons --target=iar
+```
+
+MDK5:
+
+```bash
+scons --target=mdk5
+```
+
+## 3. 烧写及执行
+
+烧写可以使用仿真器 ISP等多种方式 此处不再赘述
+
+### 3.1 运行结果
+
+如果编译 & 烧写无误，会在Flexcomm0串口*上看到RT-Thread的启动logo信息：
+
+```bash
+ \ | /
+- RT -     Thread Operating System
+ / | \     4.0.1 build Jul 30 2019
+ 2006 - 2019 Copyright by rt-thread team
+uising armclang, version: 6120001
+msh />
+```
+
+
+## 4. 驱动支持情况及计划
+
+| 驱动       | 支持情况 | 备注                         |
+| ---------- | :------: | :--------------------------: |
+| UART       | 支持     | UART0/2                 |
+| GPIO       | 支持     | 自动根据芯片型号选择引脚布局 |
+| SPI        | 支持     | 支持High Speed SPI     |
+| USB Device | 不支持 | 暂不支持          |
+| USB Host   | 不支持  | 暂不支持      |
+| Windowed WatchDog | 不支持  | 支持                        |
+| ADC | 不支持 | 暂不支持 |
+| I2C       | 支持     | 可配合MMA8562          |
+| RTC        | 支持     | RTC时钟自动配置              |
+
+## 5.进阶使用
+
+此 BSP 默认只开启了串口 0 的功能，如果需使用更多高级外设功能，需要利用 ENV 工具对 BSP 进行配置， 步骤如下：
+
+1. 在 bsp 下打开 env 工具。
+2. 输入 menuconfig 命令配置工程，配置好之后保存退出。
+3. 输入 pkgs --update 命令更新软件包。
+4. 输入 scons --target=mdk5/iar 命令重新生成工程。
+
+## 6. 联系人信息
+
+维护人:
+alex.yang@nxp.com
