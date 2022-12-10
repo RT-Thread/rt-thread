a26d4d2ba4520fd76525068fa119d0aee642e787
Yaochenger <75192526+Yaochenger@users.noreply.github.com>
2022-12-10 23:35:17 +0800

    Update interrupt.c

--- libcpu/risc-v/cv32e40p/interrupt.c
+++ libcpu/risc-v/cv32e40p/interrupt.c
@@ -11,17 +11,16 @@
 #include <rthw.h>
 #include <stdint.h>
 #include <rtthread.h>
-<<<<<<< HEAD
+
 //#include <board.h>
 //#include <RV32M1_ri5cy.h>
-=======
->>>>>>> 1185482745fa76c9094f8cf9227f85d55ed8e2bd
+
 
 typedef void (*irq_handler_t)(void);
 extern const irq_handler_t isr_table[];
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 1185482745fa76c9094f8cf9227f85d55ed8e2bd
+
+
+
 extern void vSystemIrqHandler(uint32_t mcause);
