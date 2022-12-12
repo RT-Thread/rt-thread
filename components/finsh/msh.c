576e1e610b01665fe29eef1da2f140130e82777c
tangzz98 <tangz98@outlook.com>
2022-12-12 17:12:20 -0500

    ESP32_C3: Improve Readme, fix compile errors/warnings

--- components/finsh/msh.c
+++ components/finsh/msh.c
@@ -294,9 +294,9 @@ static int _msh_exec_cmd(char *cmd, rt_size_t length, int *retp)
     return 0;
 }
 
+#if defined(RT_USING_LWP) && defined(DFS_USING_POSIX)
 pid_t exec(char*, int, int, char**);
 
-#if defined(RT_USING_LWP) && defined(DFS_USING_POSIX)
 /* check whether a file of the given path exits */
 static rt_bool_t _msh_lwp_cmd_exists(const char *path)
 {
