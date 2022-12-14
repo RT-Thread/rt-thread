d4c78d9772277fd4426309fa46d56093b74a28e7
guozhanxin <guozhanxin@rt-thread.com>
2022-12-14 14:06:29 +0800

    [dfs] sync cromfs

---
+++ components/dfs/filesystems/cromfs/dfs_cromfs.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2020/08/21     ShaoJinchun  firset version
+ */
+
+#ifndef  __DFS_CROMFS_H__
+#define  __DFS_CROMFS_H__
+
+int dfs_cromfs_init(void);
+
+#endif  /*__DFS_CROMFS_H__*/
