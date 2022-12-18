dfa3ed95f7d0263e410ffa486aa2d5727284fc33
Meco Man <920369182@qq.com>
2022-12-17 23:45:38 -0500

    [stm32l745] 完善sensor对接

--- bsp/stm32/stm32l475-atk-pandora/board/ports/sensor_port.c
+++ bsp/stm32/stm32l475-atk-pandora/board/ports/sensor_port.c
@@ -11,36 +11,36 @@
 #include <board.h>
 
 #ifdef BSP_USING_ICM20608
-#include "sensor_inven_mpu6xxx.h"
+#include <sensor_inven_mpu6xxx.h>
 
-int sensor_init(void)
+static int rt_hw_icm20608_port(void)
 {
     struct rt_sensor_config cfg;
 
-    cfg.intf.type = RT_SENSOR_INTF_I2C;
     cfg.intf.dev_name = "i2c3";
+    cfg.intf.type = RT_SENSOR_INTF_I2C;
     cfg.intf.arg = (void *)MPU6XXX_ADDR_DEFAULT;
     cfg.irq_pin.pin  = RT_PIN_NONE;
 
     rt_hw_mpu6xxx_init("icm", &cfg);
 
-    return 0;
+    return RT_EOK;
 }
-INIT_ENV_EXPORT(sensor_init);
+INIT_ENV_EXPORT(rt_hw_icm20608_port);
 
 #endif
 
 #ifdef BSP_USING_AHT10
-#include "sensor_asair_aht10.h"
+#include <sensor_asair_aht10.h>
 
-#define AHT10_I2C_BUS  "i2c4"
-
-int rt_hw_aht10_port(void)
+static int rt_hw_aht10_port(void)
 {
     struct rt_sensor_config cfg;
 
-    cfg.intf.dev_name  = AHT10_I2C_BUS;
+    cfg.intf.dev_name  = "i2c4";
+    cfg.intf.type = RT_SENSOR_INTF_I2C;
     cfg.intf.arg = (void *)AHT10_I2C_ADDR;
+    cfg.irq_pin.pin  = RT_PIN_NONE;
 
     rt_hw_aht10_init("aht10", &cfg);
 
