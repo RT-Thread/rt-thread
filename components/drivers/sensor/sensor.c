297f03f0581af1f100e9be7aef000c6e289e636a
Meco Man <920369182@qq.com>
2022-12-24 17:37:11 -0500

    [sensor] add sensor list command

--- components/drivers/sensor/sensor.c
+++ components/drivers/sensor/sensor.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2022, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -422,7 +422,6 @@ const static struct rt_device_ops rt_sensor_ops =
     _sensor_control
 };
 #endif
-
 
 /*
  * sensor register
