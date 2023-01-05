97910d8456e500fb0fda191e685bad68add83348
Man, Jianting (Meco) <920369182@qq.com>
2023-01-05 01:25:04 -0500

    Update components/drivers/serial/serial_v2.c

--- components/drivers/serial/serial_v2.c
+++ components/drivers/serial/serial_v2.c
@@ -694,7 +694,7 @@ static rt_err_t rt_serial_tx_enable(struct rt_device        *dev,
 
         tx_fifo->activated = RT_FALSE;
         tx_fifo->put_size = 0;
-        rt_memset(&tx_fifo->rb, 0, sizeof(tx_fifo->rb));
+        rt_memset(&tx_fifo->rb, RT_NULL, sizeof(tx_fifo->rb));
         rt_completion_init(&(tx_fifo->tx_cpt));
         dev->open_flag |= RT_SERIAL_TX_BLOCKING;
 
