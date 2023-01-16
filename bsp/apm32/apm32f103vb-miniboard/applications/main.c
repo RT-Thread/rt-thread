3f8b92c3032bd97968fe805a469d9a7ff66ad251
luobeihai <83497188+luobeihai@users.noreply.github.com>
2023-01-16 10:16:02 +0800

    add some apm32 bsp and fix apm32f0/apm32f1 gcc startup assembly file bug and fix apm32 drv_eth.c bug (#6855)
    * add some apm32 bsp and fix apm32f0/apm32f4 gcc startup assembly file bug and fix drv_eth.c bug
    
    * fix apm32f103xe-minibroard and apm32f407ig-minibroard link.lds file error
    
    * replace apm32f051r8-evalboard picture
    
    * fix apm32f1 gcc assembly startup file suffix name
    
    * renamed startup_apm32f10x_md.s/startup_apm32f10x_hd.s file and modify apm32f103xe-minibroard/board SConscript file

---
+++ bsp/apm32/apm32f103vb-miniboard/applications/main.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-01-11     luobeihai    first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+/* defined the LED2 pin: PE6 */
+#define LED2_PIN    GET_PIN(E, 6)
+
+int main(void)
+{
+    uint32_t sysclock = 0;
+    /* set LED2 pin mode to output */
+    rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+    /* Print system clock */
+    sysclock = RCM_ReadSYSCLKFreq();
+    rt_kprintf("System Clock: %d\n", sysclock);
+
+    while (1)
+    {
+        rt_pin_write(LED2_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED2_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+}
--- bsp/apm32/libraries/Drivers/drv_eth.h
+++ bsp/apm32/libraries/Drivers/drv_eth.h
@@ -16,21 +16,6 @@
 #include <rtdevice.h>
 #include <board.h>
 
-#ifndef LOG_TAG
-#define DBG_TAG               "drv"
-#else
-#define DBG_TAG               LOG_TAG
-#endif /* LOG_TAG */
-
-#ifdef DRV_DEBUG
-#define DBG_LVL               DBG_LOG
-#else
-#define DBG_LVL               DBG_INFO
-#endif /* DRV_DEBUG */
-
-#include <rtdbg.h>
-
-
 /* The PHY ID one register */
 #define PHY_ID1_REG                 0x02U
 
