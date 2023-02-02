83bd8614abaa4872156e42556c4dcb10f83e9e3e
杨熙 <yandld@hipnuc.com>
2023-02-02 16:41:14 +0800

    Bsp lpc55sxx (#6897)
    * [bsp_lpc55sxx] i2c,rtc bsp update
    
    1. add i2c DMA mode
    2. enable rtc driver
    
    * [bsp_lpc55sxx] formmat code
    
    using formmating.py for format code
    
    * [lpc55sxx] remove .gitignore
    
    * [bsp][lpc55sxx] update bsp driver
    
    1. update sdif driver, enable DFS, FAT32
    2. update rtc driver

--- bsp/lpc55sxx/lpc55s69_nxp_evk/rtconfig.h
+++ bsp/lpc55sxx/lpc55s69_nxp_evk/rtconfig.h
@@ -37,6 +37,7 @@
 
 /* Memory Management */
 
+#define RT_PAGE_MAX_ORDER 11
 #define RT_USING_MEMPOOL
 #define RT_USING_SMALL_MEM
 #define RT_USING_SMALL_MEM_AS_HEAP
@@ -67,13 +68,33 @@
 #define FINSH_THREAD_NAME "tshell"
 #define FINSH_THREAD_PRIORITY 20
 #define FINSH_THREAD_STACK_SIZE 4096
-#define FINSH_USING_HISTORY
-#define FINSH_HISTORY_LINES 5
 #define FINSH_USING_SYMTAB
 #define FINSH_CMD_SIZE 80
 #define MSH_USING_BUILT_IN_COMMANDS
 #define FINSH_USING_DESCRIPTION
 #define FINSH_ARG_MAX 10
+#define RT_USING_DFS
+#define DFS_USING_POSIX
+#define DFS_USING_WORKDIR
+#define DFS_FILESYSTEMS_MAX 4
+#define DFS_FILESYSTEM_TYPES_MAX 4
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_LFN_UNICODE_0
+#define RT_DFS_ELM_LFN_UNICODE 0
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+#define RT_DFS_ELM_REENTRANT
+#define RT_DFS_ELM_MUTEX_TIMEOUT 3000
+#define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
 
@@ -85,6 +106,13 @@
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_I2C
 #define RT_USING_PIN
+#define RT_USING_RTC
+#define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 512
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 1024
+#define RT_MMCSD_THREAD_PREORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
 
 /* Using USB */
 
@@ -239,6 +267,8 @@
 #define BSP_USING_I2C
 #define BSP_USING_I2C4
 #define HW_I2C4_BAUDRATE_100kHZ
+#define BSP_USING_SDIO
+#define BSP_USING_RTC
 
 /* Onboard Peripheral Drivers */
 
