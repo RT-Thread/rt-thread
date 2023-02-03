619293d58db66a5b5dee07a827e38a3dfd2df465
Rbb666 <zhangbingru@rt-thread.com>
2023-02-03 11:50:39 +0800

    [IFX]Add psoc6 bsp

---
+++ bsp/Infineon/psoc6-cy8ckit-062s4/libs/TARGET_CY8CKIT-062S4/docs/html/search/groups_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['capsense',['Capsense',['../group__group__bsp__pins__capsense.html',1,'']]],
+  ['communication_20pins',['Communication Pins',['../group__group__bsp__pins__comm.html',1,'']]]
+];
