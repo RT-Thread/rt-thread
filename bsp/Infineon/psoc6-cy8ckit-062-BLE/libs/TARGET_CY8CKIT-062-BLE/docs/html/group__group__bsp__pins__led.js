a2f3d0e73f4d45190fe6b8d01a74aceb5b490d94
Rbb666 <zhangbingru@rt-thread.com>
2023-02-03 11:50:39 +0800

    [IFX]Add psoc6 bsp

---
+++ bsp/Infineon/psoc6-cy8ckit-062-BLE/libs/TARGET_CY8CKIT-062-BLE/docs/html/group__group__bsp__pins__led.js
@@ -0,0 +1,14 @@
+var group__group__bsp__pins__led =
+[
+    [ "CYBSP_LED_RGB_RED", "group__group__bsp__pins__led.html#ga0d77bed8995ff0662adb3f7b2603af88", null ],
+    [ "CYBSP_LED_RGB_GREEN", "group__group__bsp__pins__led.html#ga2291710e025db3914eb6414f3b444cb1", null ],
+    [ "CYBSP_LED_RGB_BLUE", "group__group__bsp__pins__led.html#gaeee5786a7d2dbb36b7a0c7433a76a038", null ],
+    [ "CYBSP_USER_LED", "group__group__bsp__pins__led.html#gacc2bba8588b183ec1d448eda9a039d7c", null ],
+    [ "CYBSP_USER_LED1", "group__group__bsp__pins__led.html#gaabc3ce31f840a85f1063ff3029ab79eb", null ],
+    [ "CYBSP_USER_LED2", "group__group__bsp__pins__led.html#ga5e8df86514516cce06b41a40f749c898", null ],
+    [ "CYBSP_USER_LED3", "group__group__bsp__pins__led.html#ga765a10242952bc96a34adc577fee8a20", null ],
+    [ "CYBSP_USER_LED4", "group__group__bsp__pins__led.html#gacfc93533aad62754acf05cdf9c376125", null ],
+    [ "CYBSP_USER_LED5", "group__group__bsp__pins__led.html#ga7a84bfbd7c6bf0109422638747d2f0a8", null ],
+    [ "CYBSP_LED8", "group__group__bsp__pins__led.html#gae7dcc47d4d611d3511c279de6992647a", null ],
+    [ "CYBSP_LED9", "group__group__bsp__pins__led.html#gad0f804ddeb67eb76f649fcdcb633e9c7", null ]
+];
---
+++ bsp/Infineon/psoc6-cy8ckit-062s4/libs/TARGET_CY8CKIT-062S4/docs/html/search/all_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['pin_20states',['Pin States',['../group__group__bsp__pin__state.html',1,'']]],
+  ['pin_20mappings',['Pin Mappings',['../group__group__bsp__pins.html',1,'']]],
+  ['peripheral_20default_20bsp_20settings',['Peripheral Default BSP Settings',['../md_source_bsps_psoc6_CY8CKIT-062S4_bsp_settings.html',1,'']]]
+];
