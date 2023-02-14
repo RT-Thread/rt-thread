17e8d0a7fe4b1ae3375126df01ef413ca4bc40a0
Yaochenger <1516081466@qq.com>
2023-02-14 17:38:36 +0800

    修改rv32_trap.c 增加异常时寄存器打印

--- libcpu/risc-v/common_trap/rv32_trap.c
+++ libcpu/risc-v/common_trap/rv32_trap.c
@@ -1,9 +1,12 @@
 #include <rthw.h>
 #include <rtthread.h>
 #include "riscv-ops.h" 
+#include "rt_hw_stack_frame.h"
 
 #define ISR_NUMBER    32 
+static volatile rt_hw_stack_frame_t *s_stack_frame;
 static struct rt_irq_desc rv32irq_table[ISR_NUMBER];
+void rt_show_stack_frame(void);
 /**
  * 预设中断入口函数
  * @param mcause 中断异常ID
@@ -59,11 +62,34 @@ rt_weak void rt_rv32_system_irq_handler(rt_uint32_t mcause)
     rt_uint32_t exception = !(mcause & 0x80000000);
     if(exception)
     {
-        rt_kprintf("Trigger exception mcause = %d\r\n",mcause);
+        s_stack_frame = (rt_hw_stack_frame_t *)mscratch;
+        rt_show_stack_frame();
     }
     else
     {
         rv32irq_table[irq_id].handler(irq_id, rv32irq_table[irq_id].param);
     }
 
+}
+
+rt_weak void rt_show_stack_frame(void)
+{
+    rt_kprintf("Stack frame:\r\n----------------------------------------\r\n");
+    rt_kprintf("ra      : 0x%08x\r\n", s_stack_frame->ra);
+    rt_kprintf("mstatus : 0x%08x\r\n", read_csr(0x300));//mstatus
+    rt_kprintf("t0      : 0x%08x\r\n", s_stack_frame->t0);
+    rt_kprintf("t1      : 0x%08x\r\n", s_stack_frame->t1);
+    rt_kprintf("t2      : 0x%08x\r\n", s_stack_frame->t2);
+    rt_kprintf("a0      : 0x%08x\r\n", s_stack_frame->a0);
+    rt_kprintf("a1      : 0x%08x\r\n", s_stack_frame->a1);
+    rt_kprintf("a2      : 0x%08x\r\n", s_stack_frame->a2);
+    rt_kprintf("a3      : 0x%08x\r\n", s_stack_frame->a3);
+    rt_kprintf("a4      : 0x%08x\r\n", s_stack_frame->a4);
+    rt_kprintf("a5      : 0x%08x\r\n", s_stack_frame->a5);
+    rt_kprintf("a6      : 0x%08x\r\n", s_stack_frame->a6);
+    rt_kprintf("a7      : 0x%08x\r\n", s_stack_frame->a7);
+    rt_kprintf("t3      : 0x%08x\r\n", s_stack_frame->t3);
+    rt_kprintf("t4      : 0x%08x\r\n", s_stack_frame->t4);
+    rt_kprintf("t5      : 0x%08x\r\n", s_stack_frame->t5);
+    rt_kprintf("t6      : 0x%08x\r\n", s_stack_frame->t6);
 }
