19929bdd54839c09bfe569d8a9ad68a3841b43bc
Yaochenger <1516081466@qq.com>
2023-02-20 11:02:29 +0800

    修正格式

--- libcpu/risc-v/common/trap_common.c
+++ libcpu/risc-v/common/trap_common.c
@@ -1,9 +1,9 @@
 #include <rthw.h>
 #include <rtthread.h>
-#include "riscv-ops.h" 
+#include "riscv-ops.h"
 #include "rt_hw_stack_frame.h"
 
-#define ISR_NUMBER    32 
+#define ISR_NUMBER    32
 static volatile rt_hw_stack_frame_t *s_stack_frame;
 static struct rt_irq_desc rv32irq_table[ISR_NUMBER];
 void rt_show_stack_frame(void);
@@ -18,32 +18,32 @@ static rt_isr_handler_t rt_hw_interrupt_handle(rt_uint32_t mcause)
 }
 
 /**
- * 中断函数表初始化 
+ * 中断函数表初始化
  */
-void rt_hw_interrupt_init(void) 
+void rt_hw_interrupt_init(void)
 {
-    int idx = 0; 
+    int idx = 0;
 
     for (idx = 0; idx < ISR_NUMBER; idx++)
     {
-        rv32irq_table[idx].handler = (rt_isr_handler_t)rt_hw_interrupt_handle; 
-        rv32irq_table[idx].param = RT_NULL; 
+        rv32irq_table[idx].handler = (rt_isr_handler_t)rt_hw_interrupt_handle;
+        rv32irq_table[idx].param = RT_NULL;
     }
 }
 
 /**
  * 预装载中断入口函数
  * @param vector  中断号
- * @param handler 中断入口函数 
+ * @param handler 中断入口函数
  */
-rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler, 
-        void *param, const char *name) 
+rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler,
+        void *param, const char *name)
 {
     void *user_param = param;
     char *user_name = name;
-    if(vector < ISR_NUMBER) 
+    if(vector < ISR_NUMBER)
     {
-        if (handler != RT_NULL) 
+        if (handler != RT_NULL)
         {
             rv32irq_table[vector].handler = (rt_isr_handler_t)handler;
             rv32irq_table[vector].param = param;
@@ -52,7 +52,7 @@ rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler,
 }
 
 /**
- * 统一中断入口函数 
+ * 统一中断入口函数
  * @param mcause 中断异常ID
  */
 rt_weak void rt_rv32_system_irq_handler(rt_uint32_t mcause)
