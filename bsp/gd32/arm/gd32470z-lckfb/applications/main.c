4fd4b41b3b56299a3871a2d64aadea9f7ee006bb
yuanzihao <y@yzh.email>
2023-03-05 22:36:54 +0800

    -修改led1和change logs

--- bsp/gd32/arm/gd32470z-lckfb/applications/main.c
+++ bsp/gd32/arm/gd32470z-lckfb/applications/main.c
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2021-08-20     BruceOu      first implementation
+ * 2023-03-05     yuanzihao    change the LED pins
  */
 
 #include <stdio.h>
@@ -13,7 +14,7 @@
 #include <rtdevice.h>
 #include <board.h>
 
-/* defined the LED1 pin: PE4 */
+/* defined the LED1 pin: PE3 */
 #define LED1_PIN GET_PIN(E, 3)
 
 int main(void)
