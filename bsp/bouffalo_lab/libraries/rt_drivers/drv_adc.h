11f590d748f69c47c08a2c4f76bbcd5e01330d61
褚仕成 <1468559561@qq.com>
2023-03-29 14:43:01 +0800

    [bsp][bouffalo_lab]add drv_adc

---
+++ bsp/bouffalo_lab/libraries/rt_drivers/drv_adc.h
@@ -0,0 +1,12 @@
+#ifndef __DRV_ADC_H__
+#define __DRV_ADC_H__
+
+#include <rtthread.h>
+#include "rtdevice.h"
+#include <rthw.h>
+#include "bflb_adc.h"
+#include "bflb_core.h"
+
+int rt_hw_adc_init(void);
+
+#endif /* __DRV_ADC_H__ */
