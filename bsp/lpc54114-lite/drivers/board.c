19c7cd23951f9df89f9ec353be9578b9de39f4c0
shandianchengzi <shenshandian@hust.edu.cn>
2023-04-01 11:05:14 +0800

    [bsp] Fix many abnormal symbols in annotations.

--- bsp/lpc54114-lite/drivers/board.c
+++ bsp/lpc54114-lite/drivers/board.c
@@ -53,7 +53,7 @@ void rt_hw_board_init()
 #endif
 
     BOARD_BootClockFROHF48M();
-    /* init systick  1 systick = 1/(100M / 100) 100��systick = 1s*/
+    /* init systick  1 systick = 1/(100M / 100) 100个systick = 1s*/
     SysTick_Config(SystemCoreClock / RT_TICK_PER_SECOND);
     /* set pend exception priority */
     NVIC_SetPriority(PendSV_IRQn, (1 << __NVIC_PRIO_BITS) - 1);
