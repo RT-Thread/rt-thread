85294a8e86db1175159bbc4250f3932ac45463a8
Meco Man <920369182@qq.com>
2023-04-08 04:08:50 -0400

    [RTduino][STM32] 优化内置ADC通道

--- bsp/stm32/stm32l475-atk-pandora/applications/arduino_pinout/pins_arduino.c
+++ bsp/stm32/stm32l475-atk-pandora/applications/arduino_pinout/pins_arduino.c
@@ -10,7 +10,6 @@
  */
 
 #include <Arduino.h>
-#include <board.h>
 #include "pins_arduino.h"
 
 /*
@@ -60,7 +59,7 @@ const pin_map_t pin_map_table[]=
     {D35, GET_PIN(A,3), "uart2"},       /* Serial2-Rx */
     {A0, GET_PIN(C,2), "adc1", 3},      /* ADC */
     {A1, GET_PIN(C,4), "adc1", 13},     /* ADC */
-    {A2, RT_NULL, "adc1", 0},           /* ADC, On-Chip: internal reference voltage, ADC_CHANNEL_VREFINT */
-    {A3, RT_NULL, "adc1", 17},          /* ADC, On-Chip: internal temperature sensor, ADC_CHANNEL_TEMPSENSOR */
+    {A2, RT_NULL, "adc1", RT_ADC_INTERN_CH_VREF},   /* ADC, On-Chip: internal reference voltage */
+    {A3, RT_NULL, "adc1", RT_ADC_INTERN_CH_TEMPER}, /* ADC, On-Chip: internal temperature sensor */
     {DAC0, GET_PIN(A,4), "dac1", 1},    /* DAC */
 };
