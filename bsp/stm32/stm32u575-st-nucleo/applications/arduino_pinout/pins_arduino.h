64d80e536fb77a3e9f934f0698fa85a800fe19b6
songw4232 <songw4232@163.com>
2023-04-10 13:56:56 +0800

    [add]补充arduino_pinout/README文档

--- bsp/stm32/stm32u575-st-nucleo/applications/arduino_pinout/pins_arduino.h
+++ bsp/stm32/stm32u575-st-nucleo/applications/arduino_pinout/pins_arduino.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
- * Date           Author       Notes
- * 2023-04-04     default      first version
+ * Date           Author         Notes
+ * 2023-04-04     songw4232      first version
  */
 
 #ifndef Pins_Arduino_h
