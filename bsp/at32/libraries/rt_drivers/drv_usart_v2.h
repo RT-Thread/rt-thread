9a09099d45d918d1e3ebf63a0724ce16e6a33647
sheltonyu <sheltonyu@163.com>
2023-04-11 13:15:22 +0800

    [bsp/at32] update for perfection of drv_usart_v2

--- bsp/at32/libraries/rt_drivers/drv_usart_v2.h
+++ bsp/at32/libraries/rt_drivers/drv_usart_v2.h
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2023-02-23     Jonas        first version
+ * 2023-04-16     shelton      update for perfection of drv_usart_v2
  */
 
 #ifndef __DRV_USART_V2_H__
@@ -13,16 +14,21 @@
 
 #include <rtthread.h>
 #include <rtdevice.h>
-#include <drv_common.h>
+#include "drv_dma.h"
 
-struct at32_usart
-{
+struct at32_uart {
     char *name;
-    usart_type *usart_x;
+    usart_type *uart_x;
     IRQn_Type irqn;
+    struct dma_config *dma_rx;
+    rt_size_t last_index;
+    struct dma_config *dma_tx;
+    rt_uint16_t uart_dma_flag;
     struct rt_serial_device serial;
 };
 
 int rt_hw_usart_init(void);
 
 #endif /* __DRV_USART_V2_H__ */
+
+/******************* end of file *******************/
