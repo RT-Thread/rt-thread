825cdefe70d0b96c726f748fed28ded45fba8e84
linshire <89723088+linshire@users.noreply.github.com>
2023-04-14 09:41:38 +0800

    [bsp][lpc55s69]add softi2c (#7221)

---
+++ bsp/lpc55sxx/Libraries/drivers/drv_soft_i2c.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-04-11     linshrie     first version
+ */
+
+#ifndef __DRV_I2C__
+#define __DRV_I2C__
+
+#include <rtthread.h>
+#include <rthw.h>
+#include <rtdevice.h>
+
+/* lpc55s69 config class */
+struct lpc55s69_soft_i2c_config
+{
+    rt_uint8_t scl;
+    rt_uint8_t sda;
+    const char *bus_name;
+};
+/* lpc55s69 i2c dirver class */
+struct lpc55s69_i2c
+{
+    struct rt_i2c_bit_ops ops;
+    struct rt_i2c_bus_device i2c2_bus;
+};
+
+#ifdef BSP_USING_SOFT_I2C1
+#define SOFT_I2C1_BUS_CONFIG                             \
+    {                                                    \
+        .scl = BSP_SOFT_I2C1_SCL_PIN,                    \
+        .sda = BSP_SOFT_I2C1_SCL_PIN,                    \
+        .bus_name = "i2c1",                              \
+    }
+#endif
+
+#ifdef BSP_USING_SOFT_I2C2
+#define SOFT_I2C2_BUS_CONFIG                             \
+    {                                                    \
+        .scl = BSP_SOFT_I2C2_SCL_PIN,                    \
+        .sda = BSP_SOFT_I2C2_SCL_PIN,                    \
+        .bus_name = "i2c2",                              \
+    }
+#endif /*BSP_USING_I2C2*/
+
+int rt_hw_i2c_init(void);
+
+#endif
