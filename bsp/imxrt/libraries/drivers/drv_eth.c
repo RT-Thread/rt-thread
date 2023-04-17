eb683e6807f55e61f83b85877972b4aa550c577a
“7YZ7” <u202012123@hust.edu.cn>
2023-04-17 20:37:03 +0800

    Master ## 拉取/合并请求描述：(PR description)
    [
    <!-- 这段方括号里的内容是您**必须填写并替换掉**的，否则PR不可能被合并。**方括号外面的内容不需要修改，但请仔细阅读。**
    The content in this square bracket must be filled in and replaced, otherwise, PR can not be merged. The contents outside square brackets need not be changed, but please read them carefully.
    
    请在这里填写您的PR描述，可以包括以下之一的内容：为什么提交这份PR；解决的问题是什么，你的解决方案是什么；
    Please fill in your PR description here, which can include one of the following items: why to submit this PR; what is the problem solved and what is your solution;
    
    并确认并列出已经在什么情况或板卡上进行了测试。
    And confirm in which case or board has been tested. -->
    
    #### 为什么提交这份PR (why to submit this PR)
    
    注释里存在大量异常编码字符。静态分析的时候遇到异常字符无法解析，
    虽然去掉注释后重新分析即可，但存在大量异常字符会很不规范。
    因此我修正了其中一部分已知的，并提交了PR。
    
    希望该问题能通过开发者重新提交源码得到彻底的解决。
    
    #### 你的解决方案是什么 (what is your solution)
    
    注释里存在大量异常编码字符。静态分析的时候遇到异常字符无法解析，
    虽然去掉注释后重新分析即可，但存在大量异常字符会很不规范。
    因此我修正了其中一部分已知的，并提交了PR。
    
    希望该问题能通过开发者重新提交源码得到彻底的解决。
    
    #### 在什么测试环境下测试通过 (what is the test environment)
    只修改了注释部分，因此，在所有测试环境下都会测试通过。
    ]
    
    <!-- 以下的内容不应该在提交PR时的message修改，修改下述message，PR会被直接关闭。请在提交PR后，浏览器查看PR并对以下检查项逐项check，没问题后逐条在页面上打钩。
    The following content must not be changed in the submitted PR message. Otherwise, the PR will be closed immediately. After submitted PR, please use a web browser to visit PR, and check items one by one, and ticked them if no problem. -->
    
    ### 当前拉取/合并请求的状态 Intent for your PR
    
    必须选择一项 Choose one (Mandatory):
    
    - [ ] 本拉取/合并请求是一个草稿版本 This PR is for a code-review and is intended to get feedback
    - [ ] 本拉取/合并请求是一个成熟版本 This PR is mature, and ready to be integrated into the repo
    
    ### 代码质量 Code Quality：
    
    我在这个拉取/合并请求中已经考虑了 As part of this pull request, I've considered the following:
    
    - [ ] 已经仔细查看过代码改动的对比 Already check the difference between PR and old code
    - [ ] 代码风格正确，包括缩进空格，命名及其他风格 Style guide is adhered to, including spacing, naming and other styles
    - [ ] 没有垃圾代码，代码尽量精简，不包含`#if 0`代码，不包含已经被注释了的代码 All redundant code is removed and cleaned up
    - [ ] 所有变更均有原因及合理的，并且不会影响到其他软件组件代码或BSP All modifications are justified and not affect other components or BSP
    - [ ] 对难懂代码均提供对应的注释 I've commented appropriately where code is tricky
    - [ ] 代码是高质量的 Code in this PR is of high quality
    - [ ] 已经使用[formatting](https://github.com/mysterywolf/formatting)等源码格式化工具确保格式符合[RT-Thread代码规范](https://github.com/RT-Thread/rt-thread/blob/master/documentation/contribution_guide/coding_style_cn.md) This PR complies with [RT-Thread code specification](https://github.com/RT-Thread/rt-thread/blob/master/documentation/contribution_guide/coding_style_en.md)

--- bsp/imxrt/libraries/drivers/drv_eth.c
+++ bsp/imxrt/libraries/drivers/drv_eth.c
@@ -1020,7 +1020,7 @@ static int rt_hw_imxrt_eth_init(void)
     imxrt_eth_device.dev_addr[4] = 0x22;
     imxrt_eth_device.dev_addr[5] = 0x33;
 
-    imxrt_eth_device.speed = kENET_MiiSpeed100M;//Ҫ֧��ǧ�ף�ֱ�ӽ���ֵ��ΪkENET_MiiSpeed1000M
+    imxrt_eth_device.speed = kENET_MiiSpeed100M;//要支持千兆，直接赋值为kENET_MiiSpeed1000M
     imxrt_eth_device.duplex = kENET_MiiFullDuplex;
 
     imxrt_eth_device.enet_base = ENET_1G;
