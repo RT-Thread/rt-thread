bf543aeaebf5e63620dc6648dad9c69fb6b12e1a
Shicheng Chu <1468559561@qq.com>
2023-04-28 19:24:17 +0800

    [bsp][lpc55sxx]update: add missing library (#7408)

--- bsp/lpc55sxx/lpc55s69_nxp_evk/applications/main.c
+++ bsp/lpc55sxx/lpc55s69_nxp_evk/applications/main.c
@@ -15,10 +15,13 @@
 #include <rtdevice.h>
 #include "drv_pin.h"
 
+#ifdef RT_USING_SDIO
+#include "dfs_fs.h"
+#endif
+
 /* defined the LED pin: GPIO1_IO4 */
 /* GPIO1_4 is Blue LED */
 #define LEDB_PIN      GET_PINS(1, 4)
-
 
 int main(void)
 {
