f3eab0126f05f81c94cecf5a0dd451152f5626a8
xym <565000561@qq.com>
2023-05-02 23:31:46 +0800

    Update format and license

--- bsp/imxrt/imxrt1021-fire-evk/applications/main.c
+++ bsp/imxrt/imxrt1021-fire-evk/applications/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2018, RT-Thread Development Team
+ * Copyright (c) 2006-2023, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -20,7 +20,7 @@ int main(void)
 {
     /* set LED0 pin mode to output */
     rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
-    
+
     while (1)
     {
         rt_pin_write(LED_PIN, PIN_HIGH);
