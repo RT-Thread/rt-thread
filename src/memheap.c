b7d580f1b6ac1fe1a4d8e4cac0fbf8ba79221ffa
supperthomas <78900636@qq.com>
2023-05-14 21:57:56 +0800

    Update memheap.c

--- src/memheap.c
+++ src/memheap.c
@@ -19,7 +19,7 @@
  * 2013-07-15     Grissiom     optimize rt_memheap_realloc
  * 2021-06-03     Flybreak     Fix the crash problem after opening Oz optimization on ac6.
  * 2023-03-01     Bernard      Fix the alignment issue for minimal size
- */
+ */    
 
 #include <rthw.h>
 #include <rtthread.h>
