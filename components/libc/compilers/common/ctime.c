a26e911a295d0c4e05c2c76c93bf04712b4aed92
goldengrandpa <74250839+guoguozi123456@users.noreply.github.com>
2023-06-01 09:16:51 +0800

    将判断提前到开中断之前

--- components/libc/compilers/common/ctime.c
+++ components/libc/compilers/common/ctime.c
@@ -678,13 +678,12 @@ int clock_gettime(clockid_t clockid, struct timespec *tp)
             tick = rt_tick_get(); /* get tick */
             tp->tv_sec  = _timevalue.tv_sec + tick / RT_TICK_PER_SECOND;
             tp->tv_nsec = (_timevalue.tv_usec + (tick % RT_TICK_PER_SECOND) * MICROSECOND_PER_TICK) * 1000;
-            rt_hw_interrupt_enable(level);
             if(tp->tv_nsec>1000000000)
             {
                 tp->tv_nsec %= 1000000000;
                 tp->tv_sec += 1;
             }
-
+            rt_hw_interrupt_enable(level);
         }
         break;
 #endif
