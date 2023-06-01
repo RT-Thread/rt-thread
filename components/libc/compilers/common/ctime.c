e88a19467e85116a81e5fff6475610da6cc1acb3
goldengrandpa <74250839+guoguozi123456@users.noreply.github.com>
2023-06-01 10:52:33 +0800

    [ctime]解决clock_gettime计算出来的nsec超过1sec的问题

--- components/libc/compilers/common/ctime.c
+++ components/libc/compilers/common/ctime.c
@@ -677,8 +677,13 @@ int clock_gettime(clockid_t clockid, struct timespec *tp)
             level = rt_hw_interrupt_disable();
             tick = rt_tick_get(); /* get tick */
             tp->tv_sec  = _timevalue.tv_sec + tick / RT_TICK_PER_SECOND;
-            tp->tv_nsec = (_timevalue.tv_usec + (tick % RT_TICK_PER_SECOND) * MICROSECOND_PER_TICK) * 1000;
+            tp->tv_nsec = (_timevalue.tv_usec + (tick % RT_TICK_PER_SECOND) * MICROSECOND_PER_TICK) * 1000U;
             rt_hw_interrupt_enable(level);
+            if (tp->tv_nsec > 1000000000ULL)
+            {
+                tp->tv_nsec %= 1000000000ULL;
+                tp->tv_sec += 1;
+            }
         }
         break;
 #endif
