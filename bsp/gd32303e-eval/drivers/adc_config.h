4e65938ea83ee51bb25f6b3162331d007fa9f6f0
ZengJianwei <583823429@qq.com>
2023-06-04 19:21:59 +0800

    [bsp][gd32] 格式化 adc_config.h

--- bsp/gd32303e-eval/drivers/adc_config.h
+++ bsp/gd32303e-eval/drivers/adc_config.h
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2006-2023
  *
@@ -17,34 +18,32 @@ extern "C"
 {
 #endif
 
-#define 	ADC_PORT_CHANNEL_MAP_TABLE									\
-			{															\
-			{0, GPIOA, 0, 0, 0},										\
-			{0, GPIOA, 1, 1, 1},										\
-			{0, GPIOA, 4, 2, 4},										\
-			{0, GPIOA, 5, 3, 5},										\
-			{0, GPIOC, 4, 4, 14},										\
-			{0, GPIOC, 5, 5, 15},										\
-			{0, GPIOB, 0, 6, 8},										\
-			{0, GPIOB, 1, 7, 9},										\
-			{0, GPIOA, 6, 8, 6},										\
-			{0, GPIOA, 7, 9, 7},										\
-			}
+#define ADC_PORT_CHANNEL_MAP_TABLE \
+    {\
+        {0, GPIOA, 0, 0, 0},\
+        {0, GPIOA, 1, 1, 1},\
+        {0, GPIOA, 4, 2, 4},\
+        {0, GPIOA, 5, 3, 5},\
+        {0, GPIOC, 4, 4, 14},\
+        {0, GPIOC, 5, 5, 15},\
+        {0, GPIOB, 0, 6, 8},\
+        {0, GPIOB, 1, 7, 9},\
+        {0, GPIOA, 6, 8, 6},\
+        {0, GPIOA, 7, 9, 7},\
+    }
 
-	typedef struct {
-		rt_int8_t		ADCindex;					// ADC index:0~2
-		rt_uint32_t 	Port;						// gpio port:GPIOA/GPIOB/GPIOC/...
-		rt_uint32_t 	pin;						// gpio pin:GPIO_PIN_0~GPIO_PIN_15
-		rt_int8_t		rank;						// Order of regular channels
-		rt_int8_t		channel;					// ADC channel
-	} ADC_PORT_CHANNEL_MAP_S;
-
+    typedef struct
+    {
+        rt_int8_t ADCindex; // ADC index:0~2
+        rt_uint32_t Port;   // gpio port:GPIOA/GPIOB/GPIOC/...
+        rt_uint32_t pin;    // gpio pin:GPIO_PIN_0~GPIO_PIN_15
+        rt_int8_t rank;     // Order of regular channels
+        rt_int8_t channel;  // ADC channel
+    } ADC_PORT_CHANNEL_MAP_S;
 
 #ifdef __cplusplus
 }
 
-
 #endif
 
 #endif /* __ADC_CONFIG_H__ */
-
