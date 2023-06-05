6cc6cf71d92b5290e73f715a7a382ad945fff2e1
Rbb666 <64397326+Rbb666@users.noreply.github.com>
2023-06-05 11:03:53 +0800

    [bsp][Renesas]add definitions to prevent compilation errors (#7608)

--- bsp/renesas/ra6m3-hmi-board/board/ra6m3_it.c
+++ bsp/renesas/ra6m3-hmi-board/board/ra6m3_it.c
@@ -1,10 +1,13 @@
 #include <rtthread.h>
 #include "hal_data.h"
 
+#ifdef BSP_USING_CAN
 rt_weak void can0_callback(can_callback_args_t *p_args)
 {
 }
+#endif
 
+#ifdef BSP_USING_SCI_SPI
 rt_weak void sci_spi3_callback(spi_callback_args_t *p_args)
 {
 }
@@ -20,23 +23,28 @@ rt_weak void sci_spi6_callback(spi_callback_args_t *p_args)
 rt_weak void sci_spi7_callback(spi_callback_args_t *p_args)
 {
 }
+#endif
 
-rt_weak void cb_timer2(timer_callback_args_t *p_args)
-{
-}
-
+#ifdef BSP_USING_ETH
 rt_weak void user_ether0_callback(ether_callback_args_t *p_args)
 {
 }
+#endif
 
+#ifdef BSP_USING_I2S
 rt_weak void i2s_callback(i2s_callback_args_t *p_args)
 {
 }
+#endif
 
+#ifdef BSP_USING_JPEG
 rt_weak void decode_callback(jpeg_callback_args_t *p_args)
 {
 }
+#endif
 
+#ifdef BSP_USING_LCD
 rt_weak void _ra_port_display_callback(display_callback_args_t *p_args)
 {
 }
+#endif
