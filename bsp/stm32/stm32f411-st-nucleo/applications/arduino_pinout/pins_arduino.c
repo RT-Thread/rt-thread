6bffb022b2bbc17219e6ee7e9196f4a5867819b6
Meco Man <920369182@qq.com>
2023-06-15 10:45:43 -0400

    [template][rtduino] can switch between spi and pwm

--- bsp/stm32/stm32f411-st-nucleo/applications/arduino_pinout/pins_arduino.c
+++ bsp/stm32/stm32f411-st-nucleo/applications/arduino_pinout/pins_arduino.c
@@ -30,10 +30,17 @@ const pin_map_t pin_map_table[]=
     {D7, GET_PIN(A,8)},
     {D8, GET_PIN(A,9)},
     {D9, GET_PIN(C,7), "pwm3", 2},      /* PWM */
+#ifdef BSP_ARDUINO_CHOICE_D10D13_SPI
+    {D10, GET_PIN(B,6), "spi?"},
+    {D11, GET_PIN(A,7), "spi?"},
+    {D12, GET_PIN(A,6), "spi?"},
+    {D13, GET_PIN(A,5), "spi?"},
+#else
     {D10, GET_PIN(B,6), "pwm4", 1},     /* PWM */
     {D11, GET_PIN(A,7), "pwm1", -1},    /* PWM */
     {D12, GET_PIN(A,6)},
     {D13, GET_PIN(A,5)},                /* LED_BUILTIN */
+#endif /* BSP_ARDUINO_CHOICE_D10D13_SPI */
     {D14, GET_PIN(B,9), "i2c1"},        /* I2C-SDA (Wire) */
     {D15, GET_PIN(B,8), "i2c1"},        /* I2C-SCL (Wire) */
     {D16, GET_PIN(C,13)},
@@ -46,3 +53,13 @@ const pin_map_t pin_map_table[]=
     {A6, RT_NULL, "adc1", RT_ADC_INTERN_CH_VREF},   /* ADC, On-Chip: internal reference voltage */
     {A7, RT_NULL, "adc1", RT_ADC_INTERN_CH_TEMPER}, /* ADC, On-Chip: internal temperature sensor */
 };
+
+void initVariant(void)
+{
+#ifdef BSP_ARDUINO_CHOICE_D10D13_SPI
+    //1 deinit PWM GPIO
+    //2. deinit 对应PWM的timer通道
+    //3.init SPI GPIO
+    //4. init SPI
+#endif /* BSP_ARDUINO_CHOICE_D10D13_SPI */
+}
