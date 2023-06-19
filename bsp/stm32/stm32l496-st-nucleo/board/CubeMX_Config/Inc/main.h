ca497b43e5642d44bdcfa0b1b1fc866c539f778d
dejavudwh <daweihucs@outlook.com>
2023-06-19 14:36:05 +0800

    Add more Kconfig configuration support for stm32l496-st-nucleo

--- bsp/stm32/stm32l496-st-nucleo/board/CubeMX_Config/Inc/main.h
+++ bsp/stm32/stm32l496-st-nucleo/board/CubeMX_Config/Inc/main.h
@@ -50,6 +50,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
