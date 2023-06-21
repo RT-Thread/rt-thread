9f343b8ee7cf8a01cef0ac4a1306741eac9758cc
guo <guozhanxin@rt-thread.com>
2023-06-21 22:42:50 +0800

    [kernel]add api rt_hw_interrupt_is_disabled (#7706)

--- include/rthw.h
+++ include/rthw.h
@@ -126,11 +126,11 @@ void rt_hw_local_irq_enable(rt_base_t level);
 
 #define rt_hw_interrupt_disable rt_cpus_lock
 #define rt_hw_interrupt_enable rt_cpus_unlock
-
 #else
 rt_base_t rt_hw_interrupt_disable(void);
 void rt_hw_interrupt_enable(rt_base_t level);
 #endif /*RT_USING_SMP*/
+rt_bool_t rt_hw_interrupt_is_disabled(void);
 
 /*
  * Context interfaces
