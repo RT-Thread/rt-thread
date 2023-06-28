8419bb6895a8699a3249933fdfcd2ee625ca56ec
Shell <wangxiaoyao@rt-thread.com>
2023-06-28 09:11:17 +0800

    [components/tty] fix bug on foreground app switch (#7726)

--- components/drivers/tty/include/tty.h
+++ components/drivers/tty/include/tty.h
@@ -179,8 +179,8 @@ enum
 #define TTY_DRIVER_TYPE_SYSTEM      0x0001
 #define TTY_DRIVER_TYPE_CONSOLE     0x0002
 #define TTY_DRIVER_TYPE_SERIAL      0x0003
-#define TTY_DRIVER_TYPE_PTY     0x0004
-#define TTY_DRIVER_TYPE_SCC     0x0005  /* scc driver */
+#define TTY_DRIVER_TYPE_PTY         0x0004
+#define TTY_DRIVER_TYPE_SCC         0x0005  /* scc driver */
 #define TTY_DRIVER_TYPE_SYSCONS     0x0006
 
 /* tty magic number */
