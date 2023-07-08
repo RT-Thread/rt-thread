e941a095a132e13f8a9092e5b00387f25cae512a
Supper Thomas <78900636@qq.com>
2023-07-08 14:58:10 +0800

    [bsp][stm32] remove the useless bsp (#7795)

--- bsp/stm32/stm32l496-st-nucleo/rtconfig.py
+++ bsp/stm32/stm32l496-st-nucleo/rtconfig.py
@@ -17,7 +17,7 @@ if os.getenv('RTT_ROOT'):
 # EXEC_PATH is the compiler execute path, for example, CodeSourcery, Keil MDK, IAR
 if  CROSS_TOOL == 'gcc':
     PLATFORM    = 'gcc'
-    EXEC_PATH   = r'C:\Users\XXYYZZ'
+    EXEC_PATH   = r'.'
 elif CROSS_TOOL == 'keil':
     PLATFORM    = 'armcc'
     EXEC_PATH   = r'C:/Keil_v5'
