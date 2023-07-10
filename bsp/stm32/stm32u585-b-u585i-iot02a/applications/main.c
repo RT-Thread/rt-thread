c63e403e541ceacd087dd3f19ea23f46815ae45c
zheng_bicheng <zheng_bicheng@outlook.com>
2023-07-10 22:43:13 +0800

    update format

--- bsp/stm32/stm32u585-b-u585i-iot02a/applications/main.c
+++ bsp/stm32/stm32u585-b-u585i-iot02a/applications/main.c
@@ -20,7 +20,7 @@ int main(void)
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-		
+
     while (count++)
     {
         rt_pin_write(LED0_PIN, PIN_HIGH);
