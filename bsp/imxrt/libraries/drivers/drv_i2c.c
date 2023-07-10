eb2a93c2594b9bda24f4dae37df6c0ecc4a65f16
sp-cai <70000077+sp-cai@users.noreply.github.com>
2023-07-10 21:19:55 +0800

    i2c_bus_control 形参修改

--- bsp/imxrt/libraries/drivers/drv_i2c.c
+++ bsp/imxrt/libraries/drivers/drv_i2c.c
@@ -141,8 +141,8 @@ static rt_ssize_t imxrt_i2c_slv_xfer(struct rt_i2c_bus_device *bus,
                                     struct rt_i2c_msg msgs[],
                                     rt_uint32_t num);
 static rt_err_t imxrt_i2c_bus_control(struct rt_i2c_bus_device *bus,
-                                      rt_uint32_t,
-                                      rt_uint32_t);
+                                      int cmd,
+                                      void *args);
 
 static const struct rt_i2c_bus_device_ops imxrt_i2c_ops =
 {
@@ -372,8 +372,8 @@ static rt_ssize_t imxrt_i2c_slv_xfer(struct rt_i2c_bus_device *bus,
     return 0;
 }
 static rt_err_t imxrt_i2c_bus_control(struct rt_i2c_bus_device *bus,
-                                      rt_uint32_t cmd,
-                                      rt_uint32_t arg)
+                                      int cmd,
+                                      void *args)
 {
     return -RT_ERROR;
 }
