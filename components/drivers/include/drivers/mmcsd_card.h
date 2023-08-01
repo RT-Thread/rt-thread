6273e6e413ec5272fbd2c1ee7d504199aca94719
heyuanjie87 <sc943313837@gmail.com>
2023-08-01 10:27:39 +0800

    [sdio]添加ext_csd命名结构体

--- components/drivers/include/drivers/mmcsd_card.h
+++ components/drivers/include/drivers/mmcsd_card.h
@@ -127,7 +127,10 @@ struct rt_sdio_function {
 
 #define SDIO_MAX_FUNCTIONS      7
 
-
+struct rt_mmc_ext_csd
+{
+    rt_uint32_t cache_size;
+};
 
 struct rt_mmcsd_card {
     struct rt_mmcsd_host *host;
@@ -165,10 +168,7 @@ struct rt_mmcsd_card {
     struct rt_sdio_function *sdio_function[SDIO_MAX_FUNCTIONS + 1]; /* SDIO functions (devices) */
     rt_list_t blk_devices;  /* for block device list */
 
-    struct
-    {
-        rt_uint32_t cache_size;
-    } ext_csd;
+    struct rt_mmc_ext_csd ext_csd;
 };
 
 #ifdef __cplusplus
