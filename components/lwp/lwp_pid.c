d5c2e2ec0773e30b95d74d892fc75c99f59b8423
geniusgogo <2041245+geniusgogo@users.noreply.github.com>
2023-08-12 10:50:45 +0800

    fix lwp_free tty switch to foreground error (#7945)

--- components/lwp/lwp_pid.c
+++ components/lwp/lwp_pid.c
@@ -469,13 +469,18 @@ void lwp_free(struct rt_lwp* lwp)
         if (lwp->tty != RT_NULL)
         {
             rt_mutex_take(&lwp->tty->lock, RT_WAITING_FOREVER);
-            old_lwp = tty_pop(&lwp->tty->head, RT_NULL);
-            rt_mutex_release(&lwp->tty->lock);
             if (lwp->tty->foreground == lwp)
             {
+                old_lwp = tty_pop(&lwp->tty->head, RT_NULL);
                 lwp->tty->foreground = old_lwp;
-                lwp->tty = RT_NULL;
             }
+            else
+            {
+                tty_pop(&lwp->tty->head, lwp);
+            }
+            rt_mutex_release(&lwp->tty->lock);
+
+            lwp->tty = RT_NULL;
         }
     }
     else
