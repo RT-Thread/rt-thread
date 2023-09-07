ef47ffc54a8e77b840f3d579a3974758ab96ecba
Zhao Maosheng <zmshahaha@gmail.com>
2023-09-07 17:17:55 +0800

    finish reserved memory

--- components/mm/mm_memblock.c
+++ components/mm/mm_memblock.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-09-07     ZhaoMaosheng the first version
+ */
 
 #include "mm_memblock.h"
 #include "mm_page.h"
@@ -15,89 +24,84 @@
 
 #define PHYS_ADDR_MAX (~(rt_ubase_t)0)
 
-static struct rt_mmblk_reg memory_init_regions[INIT_MEMORY_REGIONS];
-static struct rt_mmblk_reg reserved_init_regions[INIT_RESERVED_REGIONS];
+static struct rt_mmblk_reg _memory_init_regions[INIT_MEMORY_REGIONS];
+static struct rt_mmblk_reg _reserved_init_regions[INIT_RESERVED_REGIONS];
 
-static struct rt_memblock memory = {
+struct rt_memblock mmblk_memory = {
     .cnt = 0,
     .max = INIT_MEMORY_REGIONS,
-    .regions = memory_init_regions,
+    .regions = _memory_init_regions,
 };
 
-static struct rt_memblock reserved = {
+struct rt_memblock mmblk_reserved = {
     .cnt = 0,
-    .max = INIT_MEMORY_REGIONS,
-    .regions = reserved_init_regions,
+    .max = INIT_RESERVED_REGIONS,
+    .regions = _reserved_init_regions,
 };
 
+/* adjust size to avoid exceed physical addr range */
 rt_inline rt_size_t _adjust_size(rt_ubase_t base, rt_size_t *size)
 {
-    return *size = MIN(*size, PHYS_ADDR_MAX - base);
+    return *size = MIN(*size, PHYS_ADDR_MAX - base );
 }
 
-rt_inline rt_err_t _memblock_insert_region(struct rt_memblock *memblock, 
-                                        int idx, rt_ubase_t base, 
-                                        rt_size_t size, 
+static void _memblock_insert_region(struct rt_memblock *memblock,
+                                        rt_uint32_t idx, rt_ubase_t base,
+                                        rt_size_t size,
                                         enum mmblk_flag flags)
 {
     struct rt_mmblk_reg *rgn = &memblock->regions[idx];
-    rt_err_t ret = RT_EOK;
 
     RT_ASSERT(idx <= memblock->cnt && memblock->cnt <= memblock->max);
-    
+
     if(memblock->cnt == memblock->max)
     {
         LOG_E("No enough space to store region!");
-        ret = -RT_ENOMEM;
+        RT_ASSERT(0);
     }
 
-    if(ret == RT_EOK)
-    {
-        rt_memmove(rgn + 1, rgn, (memblock->cnt - idx)*sizeof(*rgn));
-        rgn->flags = flags;
-        rgn->base = base;
-        rgn->size = size;
-        memblock->cnt++;
-    }
-
-    return ret;
+    rt_memmove(rgn + 1, rgn, (memblock->cnt - idx)*sizeof(*rgn));
+    rgn->flags = flags;
+    rgn->base = base;
+    rgn->size = size;
+    memblock->cnt++;
 }
 
 static void _memblock_merge_regions(struct rt_memblock *memblock)
 {
-	int i = 0;
+    rt_uint32_t i = 0;
 
-	/* cnt never goes below 1 */
-	while (i < memblock->cnt - 1) {
-		struct rt_mmblk_reg *this = &memblock->regions[i];
-		struct rt_mmblk_reg *next = &memblock->regions[i + 1];
-        
+    /* cnt never goes below 1 */
+    while (i < memblock->cnt - 1) {
+        struct rt_mmblk_reg *this = &memblock->regions[i];
+        struct rt_mmblk_reg *next = &memblock->regions[i + 1];
+
         RT_ASSERT(this->base + this->size <= next->base);
-		
-        if (this->base + this->size != next->base ||
-		    this->flags != next->flags) {
-			i++;
-			continue;
-		}
 
-		this->size += next->size;
-		/* move forward from next + 1, index of which is i + 2 */
-		memmove(next, next + 1, (memblock->cnt - (i + 2)) * sizeof(*next));
-		memblock->cnt--;
-	}
+        /* current region can't merge with next */
+        if (this->base + this->size != next->base ||
+            this->flags != next->flags) {
+            i++;
+            continue;
+        }
+
+        this->size += next->size;
+        /* move forward from next + 1, index of which is i + 2 */
+        rt_memmove(next, next + 1, (memblock->cnt - (i + 2)) * sizeof(*next));
+        memblock->cnt--;
+    }
 }
 
-static rt_err_t _memblock_add_range(struct rt_memblock *memblock,
-                                    rt_ubase_t base, rt_size_t size, 
+static void _memblock_add_range(struct rt_memblock *memblock,
+                                    rt_ubase_t base, rt_size_t size,
                                     enum mmblk_flag flags)
-{   
-    rt_err_t ret = RT_EOK;
+{
     rt_ubase_t end = base + _adjust_size(base, &size);
     struct rt_mmblk_reg *reg = RT_NULL;
-    int i = 0;
+    rt_uint32_t i = 0;
 
     if(!size)
-        return 0;
+        return ;
 
     for_each_region(i, memblock, reg)
     {
@@ -113,10 +117,10 @@ static rt_err_t _memblock_add_range(struct rt_memblock *memblock,
         if(rbase > base)
         {
             if(flags != reg->flags)
-                LOG_W("range [%p-%p] with flags %d overlaps region [%p-%p] with flags %d",\
-                            (void*)obase, (void*)(obase+size), flags, \
+                LOG_W("range [%p-%p] with flags %d overlaps region [%p-%p] with different flags %d",\
+                            (void*)base, (void*)(base+size), flags, \
                             (void*)rbase, (void*)(rbase+rend), reg->flags);
-            ret = _memblock_insert_region(memblock, i++, base, rbase - base, flags);
+            _memblock_insert_region(memblock, i++, base, rbase - base, flags);
         }
         base = MIN(rend, end);
     }
@@ -124,48 +128,50 @@ static rt_err_t _memblock_add_range(struct rt_memblock *memblock,
     /* insert the remaining portion */
     if(base < end)
     {
-        ret = _memblock_insert_region(memblock, i, base, end - base, flags);
+        _memblock_insert_region(memblock, i, base, end - base, flags);
     }
-        
+
     _memblock_merge_regions(memblock);
-    return ret;        
 }
 
-static int _memblock_isolate_range(struct rt_memblock *memblock,
-					rt_ubase_t base, rt_ubase_t size,
-					int *start_reg, int *end_reg)
+static void _memblock_isolate_range(struct rt_memblock *memblock,
+                    rt_ubase_t base, rt_size_t size,
+                    rt_uint32_t *start_reg, rt_uint32_t *end_reg)
 {
     rt_ubase_t end = base + _adjust_size(base, &size);
-    int idx = 0, ret = RT_EOK;
+    rt_uint32_t idx = 0;
     struct rt_mmblk_reg *reg;
 
     *start_reg = *end_reg = 0;
 
     if(!size)
-        return 0;
-    
+        return ;
+
     for_each_region(idx, memblock, reg)
     {
         rt_ubase_t rbase = reg->base;
         rt_ubase_t rend = rbase + reg->size;
 
         if (rbase >= end)
-			break;
-		if (rend <= base)
-			continue;
-        
+            break;
+        if (rend <= base)
+            continue;
+
+        /* the beginning of the range separates its respective region */
         if(rbase < base)
         {
             reg->base = base;
             reg->size -= base - rbase;
-            ret = _memblock_insert_region(memblock, idx, rbase, base - rbase, reg->flags);
+            _memblock_insert_region(memblock, idx, rbase, base - rbase, reg->flags);
         }
+        /* the endpoint of the range separates its respective region */
         else if (rend > end)
         {
             reg->base = end;
-			reg->size -= end - rbase;
-			ret = _memblock_insert_region(memblock, idx--, rbase, end - rbase, reg->flags);
+            reg->size -= end - rbase;
+            _memblock_insert_region(memblock, idx--, rbase, end - rbase, reg->flags);
         }
+        /* start and end index of isolated range to return */
         else
         {
             if(!*end_reg)
@@ -173,35 +179,30 @@ static int _memblock_isolate_range(struct rt_memblock *memblock,
             *end_reg = idx + 1;
         }
     }
-
-    return ret;
 }
 
-static int _memblock_setclr_flag(rt_ubase_t base,
-				rt_ubase_t size, rt_bool_t set, int flag)
+static void _memblock_setclr_flag(rt_ubase_t base,
+                rt_size_t size, rt_bool_t set, enum mmblk_flag flag)
 {
-	int i, ret, start_rgn, end_rgn;
+    rt_uint32_t i, start_rgn, end_rgn;
 
-	ret = _memblock_isolate_range(&memory, base, size, &start_rgn, &end_rgn);
-	if (ret != RT_EOK)
-		return ret;
+    _memblock_isolate_range(&mmblk_memory, base, size, &start_rgn, &end_rgn);
 
-	for (i = start_rgn; i < end_rgn; i++) {
-		struct rt_mmblk_reg *reg = &(memory.regions[i]);
+    for (i = start_rgn; i < end_rgn; i++) {
+        struct rt_mmblk_reg *reg = &(mmblk_memory.regions[i]);
 
-		if (set)
-			reg->flags |= flag;
-		else
-			reg->flags &= ~flag;
-	}
+        if (set)
+            reg->flags |= flag;
+        else
+            reg->flags &= ~flag;
+    }
 
-	_memblock_merge_regions(&memory);
-	return 0;
+    _memblock_merge_regions(&mmblk_memory);
 }
 
-static rt_ubase_t _memblock_size(struct rt_memblock *memblock)
+static rt_size_t _memblock_size(struct rt_memblock *memblock)
 {
-    rt_ubase_t mmblk_size = 0;
+    rt_size_t mmblk_size = 0;
 
     for(int i = 0; i < memblock->cnt; i++)
     {
@@ -213,9 +214,9 @@ static rt_ubase_t _memblock_size(struct rt_memblock *memblock)
 
 static void _memblock_dump(struct rt_memblock *memblock)
 {
-    int idx;
+    rt_uint32_t idx = 0;
     struct rt_mmblk_reg *reg;
-    rt_ubase_t reg_end;
+    rt_ubase_t reg_end = 0;
 
     for_each_region(idx, memblock, reg)
     {
@@ -225,11 +226,11 @@ static void _memblock_dump(struct rt_memblock *memblock)
     }
 }
 
-static void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
-				      rt_ubase_t *out_start, rt_ubase_t *out_end)
+void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
+                      rt_ubase_t *out_start, rt_ubase_t *out_end)
 {
-	rt_uint32_t idx_memory = *idx & 0xffffffff;
-	rt_uint32_t idx_reserved = *idx >> 32;
+    rt_uint32_t idx_memory = *idx & 0xffffffff;
+    rt_uint32_t idx_reserved = *idx >> 32;
 
     /* memory related data */
     struct rt_mmblk_reg *m = RT_NULL;
@@ -241,41 +242,37 @@ static void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
     rt_ubase_t r_start = 0;
     rt_ubase_t r_end = 0;
 
-	for (; idx_memory < memory.cnt; idx_memory++) {
-        m = &memory.regions[idx_memory];
-        
+    for (; idx_memory < mmblk_memory.cnt; idx_memory++) {
+        m = &mmblk_memory.regions[idx_memory];
+
         if(m->flags != flags)
-        {
             continue;
-        }
-            
+
         m_start = m->base;
         m_end = m->base + m->size;
 
-        for(; idx_reserved < reserved.cnt+1; idx_reserved++)
+        for(; idx_reserved < mmblk_reserved.cnt+1; idx_reserved++)
         {
             /*
              * Find the complement of reserved memblock.
              * For example, if reserved memblock is following:
-             * 
+             *
              *  0:[8-16), 1:[32-48), 2:[128-130)
-             * 
+             *
              * The upper 32bit indexes the following regions.
              *
-             *	0:[0-8), 1:[16-32), 2:[48-128), 3:[130-MAX)
-             * 
+             *  0:[0-8), 1:[16-32), 2:[48-128), 3:[130-MAX)
+             *
              * So we can find intersecting region other than excluding.
              */
-            r = &reserved.regions[idx_reserved];
+            r = &mmblk_reserved.regions[idx_reserved];
             r_start = idx_reserved ? r[-1].base + r[-1].size : 0;
-            r_end = idx_reserved < reserved.cnt ? r->base : PHYS_ADDR_MAX;
+            r_end = idx_reserved < mmblk_reserved.cnt ? r->base : PHYS_ADDR_MAX;
 
             /* reserved */
             if(r_start >= m_end)
-            {
                 break;
-            }
-                
+
             /* freed memory */
             if(m_start < r_end)
             {
@@ -283,13 +280,10 @@ static void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
                 *out_end = MIN(m_end, r_end);
 
                 if(m_end <= r_end)
-                {
                     idx_memory++;
-                }
                 else
-                {
                     idx_reserved++;
-                }
+
                 *idx = idx_memory | (rt_uint64_t)idx_reserved << 32;
                 return;
             }
@@ -297,45 +291,44 @@ static void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
     }
 
     /* found all regions */
-  	*idx = ~(rt_uint64_t)0;
+    *idx = ~(rt_uint64_t)0;
 }
 
-
-rt_err_t rt_memblock_add(rt_ubase_t base, rt_size_t size)
+void rt_memblock_add(rt_ubase_t base, rt_size_t size)
 {
-	LOG_D("memblock add: [%p-%p]\n", base, base + size - 1);
+    LOG_D("add physical address range [%p-%p] to overall memory regions\n",\
+                                        base, base + size - 1);
 
-	return _memblock_add_range(&memory, base, size, 0);
+    _memblock_add_range(&mmblk_memory, base, size, 0);
 }
 
-rt_err_t rt_memblock_reserve(rt_ubase_t base, rt_size_t size)
+void rt_memblock_add_ext(rt_ubase_t base, rt_size_t size, enum mmblk_flag flags)
 {
-	LOG_D("memblock reverse: [%p-%p]\n", base, base + size - 1);
+    LOG_D("add physical address range [%p-%p] with flag 0x%x" \
+            " to overall memory regions\n", base, base + size - 1, flag);
 
-	return _memblock_add_range(&reserved, base, size, 0);
+    _memblock_add_range(&mmblk_memory, base, size, flags);
 }
 
-rt_ubase_t rt_memblock_mem_size()
+void rt_memblock_reserve(rt_ubase_t base, rt_size_t size)
 {
-    return _memblock_size(&memory);
+    LOG_D("add physical address range [%p-%p] to reserved memory regions\n",\
+                                        base, base + size - 1);
+
+    _memblock_add_range(&mmblk_reserved, base, size, 0);
 }
 
-rt_ubase_t rt_memblock_reserved_size()
-{
-    return _memblock_size(&reserved);
-}
-
-rt_ubase_t rt_memblock_end()
+rt_size_t rt_memblock_end()
 {
     rt_uint64_t i = 0;
     rt_bool_t init = RT_TRUE;
     rt_region_t reg;
-    rt_ubase_t mem = 0;
-    
-    /* make sure region is compact */
-    _memblock_merge_regions(&memory);
+    rt_size_t mem = 0;
 
-    for_each_free_range(i, MEMBLOCK_NONE, &reg.start, &reg.end)
+    /* make sure region is compact */
+    _memblock_merge_regions(&mmblk_memory);
+
+    for_each_free_region(i, MEMBLOCK_NONE, &reg.start, &reg.end)
     {
         reg.start -= PV_OFFSET;
         reg.end -= PV_OFFSET;
@@ -349,78 +342,44 @@ rt_ubase_t rt_memblock_end()
         {
             rt_page_install(reg);
         }
-        LOG_D("region [%p-%p] added to buddy system", reg.start, reg.end);
+        rt_kprintf("region [%p-%p] added to buddy system", reg.start, reg.end);
         mem += reg.end - reg.start;
     }
 
-    LOG_D("%p bytes memory added to buddy system");
+    rt_kprintf("0x%lx(%ld) bytes memory added to buddy system", mem, mem);
     return mem;
 }
 
 void rt_memblock_dump()
 {
+    rt_size_t mem_size = _memblock_size(&mmblk_memory);
+    rt_size_t reserved_size = _memblock_size(&mmblk_reserved);
+
     rt_kprintf("Memory info:\n");
-    _memblock_dump(&memory);
-    rt_kprintf("\n\nReserved memory info:\n");
-    _memblock_dump(&reserved);
+    _memblock_dump(&mmblk_memory);
+    rt_kprintf("Total memory size: 0x%lx(%ld) bytes\n", mem_size, mem_size);
+
+    rt_kprintf("\nReserved memory info:\n");
+    _memblock_dump(&mmblk_reserved);
+    rt_kprintf("Total reserved size: 0x%lx(%ld) bytes\n", reserved_size, reserved_size);
 }
 
-/**
- * memblock_mark_hotplug - Mark hotpluggable memory with flag MEMBLOCK_HOTPLUG.
- * @base: the base phys addr of the region
- * @size: the size of the region
- *
- * Return: 0 on success, -errno on failure.
- */
-int rt_memblock_mark_hotplug(rt_ubase_t base, rt_ubase_t size)
+void rt_memblock_mark_hotplug(rt_ubase_t base, rt_size_t size)
 {
-	return _memblock_setclr_flag(base, size, 1, MEMBLOCK_HOTPLUG);
+    _memblock_setclr_flag(base, size, 1, MEMBLOCK_HOTPLUG);
 }
 
-/**
- * memblock_clear_hotplug - Clear flag MEMBLOCK_HOTPLUG for a specified region.
- * @base: the base phys addr of the region
- * @size: the size of the region
- *
- * Return: 0 on success, -errno on failure.
- */
-int rt_memblock_clear_hotplug(rt_ubase_t base, rt_ubase_t size)
+void rt_memblock_clear_hotplug(rt_ubase_t base, rt_size_t size)
 {
-	return _memblock_setclr_flag(base, size, 0, MEMBLOCK_HOTPLUG);
+    _memblock_setclr_flag(base, size, 0, MEMBLOCK_HOTPLUG);
 }
 
-/**
- * memblock_mark_mirror - Mark mirrored memory with flag MEMBLOCK_MIRROR.
- * @base: the base phys addr of the region
- * @size: the size of the region
- *
- * Return: 0 on success, -errno on failure.
- */
-int rt_memblock_mark_mirror(rt_ubase_t base, rt_ubase_t size)
+void rt_memblock_mark_nomap(rt_ubase_t base, rt_size_t size)
 {
-	return _memblock_setclr_flag(base, size, 1, MEMBLOCK_MIRROR);
+    _memblock_setclr_flag(base, size, 1, MEMBLOCK_NOMAP);
 }
 
-/**
- * memblock_mark_nomap - Mark a memory region with flag MEMBLOCK_NOMAP.
- * @base: the base phys addr of the region
- * @size: the size of the region
- *
- * Return: 0 on success, -errno on failure.
- */
-int rt_memblock_mark_nomap(rt_ubase_t base, rt_ubase_t size)
+void rt_memblock_clear_nomap(rt_ubase_t base, rt_size_t size)
 {
-	return _memblock_setclr_flag(base, size, 1, MEMBLOCK_NOMAP);
-}
-
-/**
- * memblock_clear_nomap - Clear flag MEMBLOCK_NOMAP for a specified region.
- * @base: the base phys addr of the region
- * @size: the size of the region
- *
- * Return: 0 on success, -errno on failure.
- */
-int rt_memblock_clear_nomap(rt_ubase_t base, rt_ubase_t size)
-{
-	return _memblock_setclr_flag(base, size, 0, MEMBLOCK_NOMAP);
+    _memblock_setclr_flag(base, size, 0, MEMBLOCK_NOMAP);
 }
--- components/mm/mm_memblock.h
+++ components/mm/mm_memblock.h
@@ -1,53 +1,119 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-09-07     ZhaoMaosheng the first version
+ */
+
 #ifndef __MM_MEMBLOCK_H__
 #define __MM_MEMBLOCK_H__
 
 #include "mm_page.h"
+#include <rtdef.h>
 
 enum mmblk_flag
 {
-    MEMBLOCK_NONE		= 0x0,
-	MEMBLOCK_HOTPLUG	= 0x1,
-	MEMBLOCK_MIRROR		= 0x2,
-	MEMBLOCK_NOMAP		= 0x4,
+    MEMBLOCK_NONE       = 0x0,
+    MEMBLOCK_HOTPLUG    = 0x1,
+    MEMBLOCK_NOMAP      = 0x2,
 };
 
+/**
+ * @struct rt_mmblk_reg
+ *
+ * @brief A structure representing a region in rt_memblock
+ */
 struct rt_mmblk_reg
 {
-    rt_ubase_t base;
-    rt_size_t size;
-    enum mmblk_flag flags;
+    rt_ubase_t base;                ///< the beginning physical address of the region
+    rt_size_t size;                 ///< the size of the region
+    enum mmblk_flag flags;          ///< the flag of the region
 };
 
+/**
+ * @struct rt_memblock
+ *
+ * @brief A structure representing physical memory block
+ */
 struct rt_memblock
 {
-    rt_uint32_t cnt;
-    rt_uint32_t max;
-    struct rt_mmblk_reg *regions;
+    rt_uint32_t cnt;                ///< the count of current regions
+    rt_uint32_t max;                ///< the max count of regions
+    struct rt_mmblk_reg *regions;   ///< store the regions of the memory block
 };
 
-rt_inline rt_bool_t rt_memblock_is_hotpluggable(struct rt_mmblk_reg *m)
+extern struct rt_memblock mmblk_memory;     ///< overall memory block
+extern struct rt_memblock mmblk_reserved;   ///< reserved memory block
+
+/**
+ * @brief Add a physical address range to the overall memory region
+ *
+ * @note the new range is allowed to overlap with existing ones and don't affect them
+ *
+ * @param base the beginning of the physical address range
+ * @param size the size of the physical address range
+ */
+void rt_memblock_add(rt_ubase_t base, rt_size_t size);
+
+/**
+ * @brief Add a physical address range to the overall memory region with certain flags
+ *
+ * @param base the beginning of the physical address range
+ * @param size the size of the physical address range
+ * @param flags the flags of the region
+ */
+void rt_memblock_add_ext(rt_ubase_t base, rt_size_t size, enum mmblk_flag flags);
+
+/**
+ * @brief Add a physical address range to the reserved memory region
+ *
+ * @param base the beginning of the physical address range
+ * @param size the size of the physical address range
+ */
+void rt_memblock_reserve(rt_ubase_t base, rt_size_t size);
+
+/**
+ * @brief Print the information about overall and reserved memory regions
+ */
+void rt_memblock_dump();
+
+/**
+ * @brief Free the available to buddy system
+ *
+ * @return the freed memory size in bytes
+ */
+rt_size_t rt_memblock_end();
+
+void rt_memblock_mark_hotplug(rt_ubase_t base, rt_size_t size);
+void rt_memblock_clear_hotplug(rt_ubase_t base, rt_size_t size);
+void rt_memblock_mark_nomap(rt_ubase_t base, rt_size_t size);
+void rt_memblock_clear_nomap(rt_ubase_t base, rt_size_t size);
+
+rt_inline rt_bool_t rt_memreg_is_hotpluggable(struct rt_mmblk_reg *m)
 {
-	return m->flags & MEMBLOCK_HOTPLUG;
+    return m->flags & MEMBLOCK_HOTPLUG;
 }
 
-rt_inline rt_bool_t rt_memblock_is_mirror(struct rt_mmblk_reg *m)
+rt_inline rt_bool_t rt_memreg_is_nomap(struct rt_mmblk_reg *m)
 {
-	return m->flags & MEMBLOCK_MIRROR;
+    return m->flags & MEMBLOCK_NOMAP;
 }
 
-rt_inline rt_bool_t rt_memblock_is_nomap(struct rt_mmblk_reg *m)
-{
-	return m->flags & MEMBLOCK_NOMAP;
-}
+#define for_each_region(i, memblock, rgn)           \
+    for (i = 0, rgn = &memblock->regions[0];            \
+         i < memblock->cnt;                 \
+         i++, rgn = &memblock->regions[i])
 
-#define for_each_region(i, memblock, rgn)			\
-	for (i = 0, rgn = &memblock->regions[0];			\
-	     i < memblock->cnt;					\
-	     i++, rgn = &memblock->regions[i])
+/** just for for_each_free_region api */
+void _next_free_region(rt_uint64_t *idx, enum mmblk_flag flags,
+                      rt_ubase_t *out_start, rt_ubase_t *out_end);
 
-#define for_each_free_range(i, flags, p_start, p_end)   \
-	for (i = 0, _next_free_region(&i, flags, p_start, p_end);\
-	     i != ~(rt_uint64_t)0;\
-	     _next_free_region(&i, flags, p_start, p_end))
+#define for_each_free_region(i, flags, p_start, p_end)   \
+    for (i = 0, _next_free_region(&i, flags, p_start, p_end);\
+         i != ~(rt_uint64_t)0;\
+         _next_free_region(&i, flags, p_start, p_end))
 
 #endif
