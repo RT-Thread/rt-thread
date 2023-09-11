af4294c480903c8fb7d164c45b87995c5cc94475
1ridic <i@8f.al>
2023-09-11 23:55:51 +0800

    [menuconfig] Add env file for fish
    Using env.sh in fish shell will cause
    problems. Creating a new env.fish for it.

--- tools/menuconfig.py
+++ tools/menuconfig.py
@@ -219,6 +219,9 @@ def touch_env():
     if sys.platform != 'win32':
         env_sh = open(os.path.join(env_dir, 'env.sh'), 'w')
         env_sh.write('export PATH=~/.env/tools/scripts:$PATH')
+
+        env_fish = open(os.path.join(env_dir, 'env.fish'), 'w')
+        env_fish.write('set -gx PATH ~/.env/tools/scripts $PATH')
     else:
         if os.path.exists(os.path.join(env_dir, 'tools', 'scripts')):
             os.environ["PATH"] = os.path.join(env_dir, 'tools', 'scripts') + ';' + os.environ["PATH"]
