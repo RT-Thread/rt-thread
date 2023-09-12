bb21e0bd2a5aa149071b41fd86e4c1d7bff34709
Zhao Maosheng <zmshahaha@gmail.com>
2023-09-12 11:41:14 +0800

    delete change in bsp

--- bsp/qemu-vexpress-a9/drivers/automac.h
+++ bsp/qemu-vexpress-a9/drivers/automac.h
@@ -8,8 +8,8 @@
 #define AUTOMAC0  0x52
 #define AUTOMAC1  0x54
 #define AUTOMAC2  0x00
-#define AUTOMAC3  0xe2
-#define AUTOMAC4  0xa7
-#define AUTOMAC5  0xe4
+#define AUTOMAC3  0x78
+#define AUTOMAC4  0xe7
+#define AUTOMAC5  0x23
 
 #endif
