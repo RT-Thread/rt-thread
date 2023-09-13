2a33c8fcb25dbcd0d3ee9727890042389e7ff5d0
xiao-mang <ly1874108@126.com>
2023-09-13 11:02:29 +0800

    fix ifconfig

--- components/net/sal/src/sal_socket.c
+++ components/net/sal/src/sal_socket.c
@@ -1098,9 +1098,9 @@ int sal_ioctlsocket(int socket, long cmd, void *arg)
                     {
                         addr_in = (struct sockaddr_in *)&(ifr->ifr_ifru.ifru_addr);
                     #if NETDEV_IPV4 && NETDEV_IPV6
-                        addr_in->sin_addr.s_addr = sock->netdev->ip_addr.u_addr.ip4.addr;
+                        addr_in->sin_addr.s_addr = netdev->ip_addr.u_addr.ip4.addr;
                     #elif NETDEV_IPV4
-                        addr_in->sin_addr.s_addr = sock->netdev->ip_addr.addr;
+                        addr_in->sin_addr.s_addr = netdev->ip_addr.addr;
                     #elif NETDEV_IPV6
                     #error "Do not only support IPV6"
                     #endif /* NETDEV_IPV4 && NETDEV_IPV6 */
