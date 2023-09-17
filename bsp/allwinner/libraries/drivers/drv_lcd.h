5c7e2d4ddde3ed44b52989e88584c887f82601e9
zbtrs <1339623011@qq.com>
2023-09-17 19:40:55 +0800

    feat: support sdl2

--- bsp/allwinner/libraries/drivers/drv_lcd.h
+++ bsp/allwinner/libraries/drivers/drv_lcd.h
@@ -19,7 +19,11 @@
 
 int rt_hw_lcd_init(void);
 
-#define FBIOGET_FSCREENINFO     0x4602
+#define FBIOGET_VSCREENINFO 0x4600
+#define FBIOPUT_VSCREENINFO 0x4601
+#define FBIOGET_FSCREENINFO 0x4602
+#define FBIOGET_PIXELINFO   0x4603
+#define FBIOPAN_DISPLAY     0x4606
 
 struct fb_fix_screeninfo
 {
@@ -29,6 +33,13 @@ struct fb_fix_screeninfo
 
     uint32_t line_length;
 };
+
+struct fb_var_screeninfo
+{
+    uint32_t xres;
+    uint32_t yres;
+};
+
 rt_uint8_t get_lcd_backlight(void);
 void set_lcd_backlight(rt_uint8_t value);
 void turn_down_lcd_backlight(void);
