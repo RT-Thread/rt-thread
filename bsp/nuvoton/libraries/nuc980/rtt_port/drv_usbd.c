19bfdac891dbb70d475a64ae118ecb83459f4a50
Z8MAN8 <1468559561@qq.com>
2023-10-11 21:39:30 +0800

    [fix][usb]return data type as rt_ssize_t rt_ssize_t can give negative error code, which follows the unix style correctly

--- bsp/nuvoton/libraries/nuc980/rtt_port/drv_usbd.c
+++ bsp/nuvoton/libraries/nuc980/rtt_port/drv_usbd.c
@@ -7,6 +7,7 @@
 * Change Logs:
 * Date            Author           Notes
 * 2020-12-25      Wayne            First version
+* 2023-10-11     ChuShicheng       change rt_size_t to rt_ssize_t
 *
 ******************************************************************************/
 
@@ -437,7 +438,7 @@ __STATIC_INLINE void nu_buffer_cpy(rt_uint8_t address, void *buffer, rt_size_t s
     }
 }
 
-static rt_size_t _ep_read(rt_uint8_t address, void *buffer)
+static rt_ssize_t _ep_read(rt_uint8_t address, void *buffer)
 {
     rt_size_t size  = 0;
 
@@ -465,7 +466,7 @@ static rt_size_t _ep_read(rt_uint8_t address, void *buffer)
     return size;
 }
 
-static rt_size_t _ep_read_prepare(rt_uint8_t address, void *buffer, rt_size_t size)
+static rt_ssize_t _ep_read_prepare(rt_uint8_t address, void *buffer, rt_size_t size)
 {
     RT_ASSERT(!(address & USB_DIR_IN));
 
@@ -497,7 +498,7 @@ static rt_size_t _ep_read_prepare(rt_uint8_t address, void *buffer, rt_size_t si
     return size;
 }
 
-static rt_size_t _ep_write(rt_uint8_t address, void *buffer, rt_size_t size)
+static rt_ssize_t _ep_write(rt_uint8_t address, void *buffer, rt_size_t size)
 {
 
     RT_ASSERT((address & USB_DIR_IN));
