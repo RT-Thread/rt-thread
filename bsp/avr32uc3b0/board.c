47de11da9c10c048a9f1089606b2fc9fb18d917b
Raman Gopalan <ramangopalan@gmail.com>
2023-10-16 13:30:11 +0530

    Implement UART driver files for AT32UC3B: Working commit

--- bsp/avr32uc3b0/board.c
+++ bsp/avr32uc3b0/board.c
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2023, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
- * Date           Author       Notes
- * 2010-03-30     Kyle         First version
+ * Date           Author          Notes
+ * 2010-03-30     Kyle            First version
+ * 2023-10-13     Raman Gopalan   Move UART specific code sections into the drv_uart files
  */
 
 #include <rtthread.h>
@@ -14,16 +15,7 @@
 #include "gpio.h"
 #include "usart.h"
 #include "intc.h"
-#include "serial.h"
-
-#define FOSC0   12000000
-#define FCPU    60000000
-#define FHSB    FCPU
-#define FPBA    FCPU
-#define FPBB    FCPU
-
-extern void rt_hw_serial_isr(void);
-extern void rt_hw_usart_init(void);
+#include "drv_uart.h"
 
 /**
  * System tick interrupt handler.
@@ -52,24 +44,7 @@ static void peripherals_init(void)
         .osc0_startup = AVR32_PM_OSCCTRL0_STARTUP_2048_RCOSC
     };
     pm_configure_clocks(&pm_freq_param);
-
-    /*
-     * USART1 initialization
-     */
-    gpio_enable_module_pin(AVR32_USART1_TXD_0_1_PIN, AVR32_USART1_TXD_0_1_FUNCTION);
-    gpio_enable_module_pin(AVR32_USART1_RXD_0_1_PIN, AVR32_USART1_RXD_0_1_FUNCTION);
-    static const usart_options_t usartOptions = {
-        .baudrate = 115200,
-        .charlength = 8,
-        .paritytype = USART_NO_PARITY,
-        .stopbits = USART_1_STOPBIT,
-        .channelmode = USART_NORMAL_CHMODE
-    };
-    usart_init_rs232(&AVR32_USART1, &usartOptions, FCPU);
-
     INTC_init_interrupts();
-    INTC_register_interrupt(&rt_hw_serial_isr, AVR32_USART1_IRQ, AVR32_INTC_INT0);
-    AVR32_USART1.ier = AVR32_USART_IER_RXRDY_MASK;
 }
 
 /**
@@ -84,14 +59,20 @@ static void cpu_counter_init(void)
 
 void rt_hw_board_init(void)
 {
-    extern struct rt_device _rt_usart_device;
-    extern struct avr32_serial_device uart;
-
     Disable_global_interrupt();
 
     peripherals_init();
     cpu_counter_init();
 
-    rt_hw_serial_register(&_rt_usart_device, "uart1", RT_DEVICE_FLAG_RDWR | RT_DEVICE_FLAG_INT_RX | RT_DEVICE_FLAG_STREAM, &uart);
-    rt_console_set_device("uart1");
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+
+#ifdef RT_USING_SERIAL
+    rt_hw_uart_init();
+#endif
+
+#ifdef RT_USING_CONSOLE
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
 }
