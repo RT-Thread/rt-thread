61f7d483e2e96e627e389bc483f29cf2e8a34c64
WCX <timwcx@qq.com>
2023-10-16 14:57:16 +0800

    [bsp][ESP32_C3]增加ble和wifi驱动|add ble & wifi drivers (#8104)

---
+++ bsp/ESP32_C3/drivers/drv_wifi.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 Winner Microelectronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author          Notes
+ * 2023-07-25     WCX1024979076   1st version
+ */
+
+#ifndef __DRV_WIFI_H__
+#define __DRV_WIFI_H__
+
+#ifdef RT_USING_WIFI
+int rt_hw_wifi_init(void);
+#endif /* RT_USING_WIFI */
+
+#endif /* __DRV_WIFI_H__ */
