e7a1276885b1f6475019f5f3e5fbac07b77783b1
miaoshi <miaoshi@rt-thread.com>
2023-10-18 10:51:53 +0800

    fix busybox echo

--- components/dfs/dfs_v2/src/dfs_file.c
+++ components/dfs/dfs_v2/src/dfs_file.c
@@ -30,6 +30,8 @@
 
 #define MAX_RW_COUNT 0xfffc0000
 
+#define F_DUPFD_CLOEXEC 1030
+
 /*
  * rw_verify_area doesn't like huge counts. We limit
  * them to something that fits in "int" so that others
