478132ac7f593d212df276b55b3dad2ce23246ac
Shell <smokewood@qq.com>
2023-10-18 19:18:10 +0800

    [smart] fixup: mmap support
    This patch including following fixups/enhancements:
    
    - rv64 mmap support
    - flag type defined by enum (1,2,3,...)
    
    Signed-off-by: Shell <smokewood@qq.com>

--- libcpu/risc-v/t-head/c906/riscv_mmu.h
+++ libcpu/risc-v/t-head/c906/riscv_mmu.h
@@ -111,14 +111,12 @@ void mmu_set_pagetable(rt_ubase_t addr);
 void mmu_enable_user_page_access();
 void mmu_disable_user_page_access();
 
-enum rt_hw_mmu_prot_t {
-    RT_HW_MMU_PROT_READ,
-    RT_HW_MMU_PROT_WRITE,
-    RT_HW_MMU_PROT_EXECUTE,
-    RT_HW_MMU_PROT_KERNEL,
-    RT_HW_MMU_PROT_USER,
-    RT_HW_MMU_PROT_CACHE,
-};
+#define RT_HW_MMU_PROT_READ 1
+#define RT_HW_MMU_PROT_WRITE 2
+#define RT_HW_MMU_PROT_EXECUTE 4
+#define RT_HW_MMU_PROT_KERNEL 8
+#define RT_HW_MMU_PROT_USER 16
+#define RT_HW_MMU_PROT_CACHE 32
 
 /**
  * @brief Remove permission from attribution
@@ -127,7 +125,7 @@ enum rt_hw_mmu_prot_t {
  * @param prot protect that will be removed
  * @return size_t returned attribution
  */
-rt_inline size_t rt_hw_mmu_attr_rm_perm(size_t attr, enum rt_hw_mmu_prot_t prot)
+rt_inline size_t rt_hw_mmu_attr_rm_perm(size_t attr, rt_base_t prot)
 {
     switch (prot)
     {
@@ -146,7 +144,7 @@ rt_inline size_t rt_hw_mmu_attr_rm_perm(size_t attr, enum rt_hw_mmu_prot_t prot)
  * @param prot protect that will be added
  * @return size_t returned attribution
  */
-rt_inline size_t rt_hw_mmu_attr_add_perm(size_t attr, enum rt_hw_mmu_prot_t prot)
+rt_inline size_t rt_hw_mmu_attr_add_perm(size_t attr, rt_base_t prot)
 {
     switch (prot)
     {
@@ -165,7 +163,7 @@ rt_inline size_t rt_hw_mmu_attr_add_perm(size_t attr, enum rt_hw_mmu_prot_t prot
  * @param prot protect that will be test
  * @return rt_bool_t RT_TRUE if the prot is allowed, otherwise RT_FALSE
  */
-rt_inline rt_bool_t rt_hw_mmu_attr_test_perm(size_t attr, enum rt_hw_mmu_prot_t prot)
+rt_inline rt_bool_t rt_hw_mmu_attr_test_perm(size_t attr, rt_base_t prot)
 {
     rt_bool_t rc = 0;
     switch (prot)
