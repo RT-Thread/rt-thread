c9125bcdaa7d9c2d11253e80ad4e4ac6a8ec46bb
sq <1838545301@qq.com>
2023-10-30 17:07:46 +0800

    Added comments to the files under rt-thread\components\libc\compilers\picolibc

--- components/libc/compilers/picolibc/syscall.c
+++ components/libc/compilers/picolibc/syscall.c
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2006-2023, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -11,16 +11,21 @@
 #include <rtthread.h>
 #include <sys/types.h>
 
-/* global errno */
+/* Global errno variable */
 static volatile int __pico_errno;
 
+/**
+ * @brief   Get a pointer to the global errno variable.
+ *
+ * @return  A pointer to the global errno variable.
+ */
 int *pico_get_errno(void)
 {
     rt_thread_t tid = RT_NULL;
 
     if (rt_interrupt_get_nest() != 0)
     {
-        /* it's in interrupt context */
+        /* It's in an interrupt context */
         return &__pico_errno;
     }
 
@@ -34,24 +39,53 @@ int *pico_get_errno(void)
 }
 
 #ifdef RT_USING_HEAP
+
+/**
+ * @brief   Allocate memory from the heap.
+ *
+ * @param   n   The number of bytes to allocate.
+ *
+ * @return  A pointer to the allocated memory.
+ */
 void *malloc(size_t n)
 {
     return rt_malloc(n);
 }
 RTM_EXPORT(malloc);
 
+/**
+ * @brief   Resize a previously allocated memory block.
+ *
+ * @param   rmem        A pointer to the previously allocated memory block.
+ * @param   newsize     The new size in bytes to resize the block to.
+ *
+ * @return  A pointer to the resized memory block.
+ */
 void *realloc(void *rmem, size_t newsize)
 {
     return rt_realloc(rmem, newsize);
 }
 RTM_EXPORT(realloc);
 
+/**
+ * @brief   Allocate memory for an array with elements.
+ *
+ * @param   nelem   The number of elements to allocate.
+ * @param   elsize  The size of each element in bytes.
+ *
+ * @return  A pointer to the allocated memory.
+ */
 void *calloc(size_t nelem, size_t elsize)
 {
     return rt_calloc(nelem, elsize);
 }
 RTM_EXPORT(calloc);
 
+/**
+ * @brief   Free previously allocated memory.
+ *
+ * @param   rmem    A pointer to the memory block to free.
+ */
 void free(void *rmem)
 {
     rt_free(rmem);
