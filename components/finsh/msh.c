7008202aa3bfb08bf857f6a397fcb231ad15bc52
yangpeng <yangpengya@qq.com>
2023-11-06 15:17:51 +0800

    还原

--- components/finsh/msh.c
+++ components/finsh/msh.c
@@ -53,7 +53,7 @@ int msh_help(int argc, char **argv)
 
     return 0;
 }
-MSH_CMD_EXPORT_ALIAS(msh_help, help, RT-Thread shell help);
+MSH_CMD_EXPORT_ALIAS(msh_help, help, RT-Thread shell help.);
 
 #ifdef MSH_USING_BUILT_IN_COMMANDS
 int cmd_ps(int argc, char **argv)
@@ -69,7 +69,7 @@ int cmd_ps(int argc, char **argv)
     list_thread();
     return 0;
 }
-MSH_CMD_EXPORT_ALIAS(cmd_ps, ps, List threads in the system);
+MSH_CMD_EXPORT_ALIAS(cmd_ps, ps, List threads in the system.);
 
 #ifdef RT_USING_HEAP
 int cmd_free(int argc, char **argv)
@@ -88,7 +88,7 @@ int cmd_free(int argc, char **argv)
 #endif
     return 0;
 }
-MSH_CMD_EXPORT_ALIAS(cmd_free, free, Show the memory usage in the system);
+MSH_CMD_EXPORT_ALIAS(cmd_free, free, Show the memory usage in the system.);
 #endif /* RT_USING_HEAP */
 #endif /* MSH_USING_BUILT_IN_COMMANDS */
 
