547b27742dfd8034ec0ff8bad8a2d1b2de039ea9
fang jianzhou <fangjianzhou@rt-thread.com>
2023-12-01 11:24:15 +0800

    memory init

--- bsp/qemu-virt64-aarch64/drivers/board.h
+++ bsp/qemu-virt64-aarch64/drivers/board.h
@@ -18,15 +18,7 @@ extern unsigned char __bss_start;
 extern unsigned char __bss_end;
 
 #define HEAP_BEGIN  (void *)&__bss_end
-
-#ifdef RT_USING_SMART
-#define HEAP_END    (rt_size_t)(KERNEL_VADDR_START + 64 * 1024 * 1024)
-#define PAGE_START  HEAP_END + 1 * 1024 * 1024
-#define PAGE_END    ((rt_size_t)KERNEL_VADDR_START + 128 * 1024 * 1024)
-#else
 #define HEAP_END    ((void *)HEAP_BEGIN + 64 * 1024 * 1024)
-#define KERNEL_VADDR_START 0x40000000
-#endif
 
 void rt_hw_board_init(void);
 
