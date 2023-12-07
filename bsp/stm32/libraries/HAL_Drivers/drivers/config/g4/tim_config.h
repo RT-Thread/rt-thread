e1046505382a34fea5addd3a4f7add3e54f8fbe1
Supper Thomas <78900636@qq.com>
2023-12-07 22:56:57 +0800

    fix common.h

--- bsp/stm32/libraries/HAL_Drivers/drivers/config/g4/tim_config.h
+++ bsp/stm32/libraries/HAL_Drivers/drivers/config/g4/tim_config.h
@@ -105,6 +105,38 @@ extern "C" {
 #endif /* TIM8_CONFIG */
 #endif /* BSP_USING_TIM8 */
 
+#ifdef BSP_USING_TIM11
+#ifndef TIM11_CONFIG
+#define TIM11_CONFIG                                        \
+    {                                                       \
+       .tim_handle.Instance     = TIM11,                    \
+       .tim_irqn                = TIM1_TRG_COM_TIM11_IRQn,  \
+       .name                    = "timer11",                \
+    }
+#endif /* TIM11_CONFIG */
+#endif /* BSP_USING_TIM11 */
+
+#ifdef BSP_USING_TIM13
+#ifndef TIM13_CONFIG
+#define TIM13_CONFIG                                        \
+    {                                                       \
+       .tim_handle.Instance     = TIM13,                    \
+       .tim_irqn                = TIM8_UP_TIM13_IRQn,       \
+       .name                    = "timer13",                \
+    }
+#endif /* TIM13_CONFIG */
+#endif /* BSP_USING_TIM13 */
+
+#ifdef BSP_USING_TIM14
+#ifndef TIM14_CONFIG
+#define TIM14_CONFIG                                        \
+    {                                                       \
+       .tim_handle.Instance     = TIM14,                    \
+       .tim_irqn                = TIM8_TRG_COM_TIM14_IRQn,  \
+       .name                    = "timer14",                \
+    }
+#endif /* TIM14_CONFIG */
+#endif /* BSP_USING_TIM14 */
 
 #ifdef BSP_USING_TIM15
 #ifndef TIM15_CONFIG
--- bsp/stm32/stm32g491-st-nucleo/board/board.h
+++ bsp/stm32/stm32g491-st-nucleo/board/board.h
@@ -13,7 +13,6 @@
 #define __BOARD_H__
 
 #include <stm32g4xx.h>
-#include <drv_common.h>
 #define STM32_FLASH_START_ADRESS     ((uint32_t)0x08000000)
 #define STM32_FLASH_SIZE             (512 * 1024)
 #define STM32_FLASH_END_ADDRESS      ((uint32_t)(STM32_FLASH_START_ADRESS + STM32_FLASH_SIZE))
