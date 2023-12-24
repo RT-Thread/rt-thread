713cf9b45f274ef5d202cc9fb9c5a5889aa0098a
yangpeng <yangpengya@qq.com>
2023-12-24 21:03:58 +0800

    #8288

--- components/net/sal/impl/af_inet_at.c
+++ components/net/sal/impl/af_inet_at.c
@@ -81,10 +81,13 @@ static const struct sal_socket_ops at_socket_ops =
     NULL,
 #endif
     at_sendto,
+    NULL,
+    NULL,
     at_recvfrom,
     at_getsockopt,
     at_setsockopt,
     at_shutdown,
+    NULL,
     NULL,
     NULL,
     NULL,
