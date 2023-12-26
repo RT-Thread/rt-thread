0d4841539519c999563c3881f11907ce08925428
Meco Man <920369182@qq.com>
2023-12-26 20:49:53 +0800

    Update drv_eth.h

--- bsp/stm32/libraries/HAL_Drivers/drivers/drv_eth.h
+++ bsp/stm32/libraries/HAL_Drivers/drivers/drv_eth.h
@@ -33,7 +33,6 @@
 /* The PHY auto-negotiate advertise register */
 #define PHY_AUTONEG_ADVERTISE_REG   0x04U
 
-
 #if defined(PHY_USING_LAN8720A) || defined(PHY_USING_LAN8742A)
 /*  The PHY interrupt source flag register. */
 #define PHY_INTERRUPT_FLAG_REG      0x1DU
@@ -41,7 +40,6 @@
 #define PHY_INTERRUPT_MASK_REG      0x1EU
 #define PHY_LINK_DOWN_MASK          (1<<4)
 #define PHY_AUTO_NEGO_COMPLETE_MASK (1<<6)
-
 /*  The PHY status register. */
 #define PHY_Status_REG              0x1FU
 #define PHY_10M_MASK                (1<<2)
@@ -91,7 +89,6 @@
 #define PHY_INTERRUPT_MASK_REG      0x12U
 /*  The PHY auto nego and link change mask. */
 #define PHY_INT_MASK                (1<<15)|(3<<10)
-
 /*  The PHY status register. */
 #define PHY_Status_REG              0x11U
 #define PHY_100M_MASK               (1<<14)
@@ -99,7 +96,6 @@
 #define PHY_Status_SPEED_10M(sr)    (!PHY_Status_SPEED_100M(sr))
 #define PHY_Status_SPEED_100M(sr)   ((sr) & PHY_100M_MASK)
 #define PHY_Status_FULL_DUPLEX(sr)  ((sr) & PHY_FULL_DUPLEX_MASK)
-
 #endif /* defined(PHY_USING_LAN8720A) || defined(PHY_USING_LAN8742A) */
 
 #endif /* __DRV_ETH_H__ */
