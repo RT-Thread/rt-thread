b8710166fd2037d278b80c69451013dd9d8e9a1f
yuanzihao <y@yzh.email>
2024-01-13 10:03:56 +0800

    -修改readme，可以正常编译

--- bsp/gd32/arm/gd32407v-lckfb/applications/main.c
+++ bsp/gd32/arm/gd32407v-lckfb/applications/main.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2022, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2021-08-20     BruceOu      first implementation
+ * 2024-01-13     yuanzihao    first implementation
  */
 
 #include <stdio.h>
@@ -14,20 +14,20 @@
 #include <board.h>
 
 /* defined the LED1 pin: PB2 */
-#define LED1_PIN GET_PIN(B, 2)
+#define LED_PIN GET_PIN(B, 2)
 
 int main(void)
 {
     int count = 1;
 
     /* set LED1 pin mode to output */
-    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
 
     while (count++)
     {
-        rt_pin_write(LED1_PIN, PIN_HIGH);
+        rt_pin_write(LED_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED1_PIN, PIN_LOW);
+        rt_pin_write(LED_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
 
