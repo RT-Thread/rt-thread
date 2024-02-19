23b6dcbbc4042d38014ee63dff34972a074eb39e
Yilin Sun <imi415@imi.moe>
2024-02-19 19:18:10 +0800

    [bsp][nxp/mcxn10] Initial support for RTC driver.
    Signed-off-by: Yilin Sun <imi415@imi.moe>

---
+++ bsp/nxp/mcxn/Libraries/drivers/drv_rtc.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006-2024, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024-02-06     Yilin Sun    Initial version.
+ */
+
+#ifndef __DRV_RTC_H__
+#define __DRV_RTC_H__
+
+#include <rtthread.h>
+#include <rtdevice.h>
+
+int rt_hw_rtc_init(void);
+
+#endif
