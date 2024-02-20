fd4e390d1052a48efe245957d51f1c8ada1cacf2
Yilin Sun <imi415@imi.moe>
2024-02-20 18:46:47 +0800

    [bsp][mcxa] FRDM-MCXA153: GNU toolchain: fixed flags.
    Signed-off-by: Yilin Sun <imi415@imi.moe>

--- bsp/nxp/mcxa/frdm-mcxa153/rtconfig.py
+++ bsp/nxp/mcxa/frdm-mcxa153/rtconfig.py
@@ -44,10 +44,10 @@ if PLATFORM == 'gcc':
     OBJCPY = PREFIX + 'objcopy'
     STRIP = PREFIX + 'strip'
 
-    DEVICE = ' -mcpu=' + CPU + ' -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections'
-    CFLAGS = DEVICE + ' -Wall -D__FPU_PRESENT -eentry'
-    AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp -Wa,-mimplicit-it=thumb -D__START=entry'
-    LFLAGS = DEVICE + ' -lm -lgcc -lc' + ' -nostartfiles -Wl,--gc-sections,-Map=rtthread.map,-cref,-u,Reset_Handler -T board/linker_scripts/MCXN947_cm33_core0_flash.ld'
+    DEVICE = ' -mcpu=' + CPU + '+nodsp' + ' -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections'
+    CFLAGS = DEVICE + ' -Wall'
+    AFLAGS = ' -c' + DEVICE + ' -x assembler-with-cpp -D__START=entry -D__STARTUP_CLEAR_BSS'
+    LFLAGS = DEVICE + ' -specs=nano.specs -specs=nosys.specs -Wl,--defsym=__heap_size__=0x2000,--gc-sections,-Map=rtthread.map,--print-memory-usage -Tboard/linker_scripts/MCXA153_flash.ld'
 
     CPATH = ''
     LPATH = ''
