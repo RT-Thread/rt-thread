24f843b81318144484e3bb94221f6c86481aa9d5
Dyyt587 <805207319@qq.com>
2024-03-04 22:31:38 +0800

    Update drv_hard_i2c.h

--- bsp/stm32/libraries/HAL_Drivers/drivers/drv_hard_i2c.h
+++ bsp/stm32/libraries/HAL_Drivers/drivers/drv_hard_i2c.h
@@ -1,12 +1,4 @@
 /*
- * @Author: Dyyt587 805207319@qq.com
- * @Date: 2024-03-04 22:13:39
- * @LastEditors: Dyyt587 805207319@qq.com
- * @LastEditTime: 2024-03-04 22:22:44
- * @FilePath: \stm32\libraries\HAL_Drivers\drivers\drv_hard_i2c.h
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
  * Copyright (c) 2024
  * SPDX-License-Identifier: Apache-2.0
  *
