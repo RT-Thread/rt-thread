10bf617f4355e63b016624224d83bdd21383caab
Rbb666 <zhangbingru@rt-thread.com>
2024-03-05 13:51:03 +0800

    [bsp][renesas]add ra8d1-vision-board support

---
+++ bsp/renesas/ra8d1-vision-board/ra_gen/vector_data.h
@@ -0,0 +1,32 @@
+/* generated vector header file - do not edit */
+        #ifndef VECTOR_DATA_H
+        #define VECTOR_DATA_H
+        #ifdef __cplusplus
+        extern "C" {
+        #endif
+                /* Number of interrupts allocated */
+        #ifndef VECTOR_DATA_IRQ_COUNT
+        #define VECTOR_DATA_IRQ_COUNT    (4)
+        #endif
+        /* ISR prototypes */
+        void sci_b_uart_rxi_isr(void);
+        void sci_b_uart_txi_isr(void);
+        void sci_b_uart_tei_isr(void);
+        void sci_b_uart_eri_isr(void);
+
+        #if __has_include("r_ioport.h")
+        /* Vector table allocations */
+        #define VECTOR_NUMBER_SCI9_RXI ((IRQn_Type) 0) /* SCI9 RXI (Receive data full) */
+        #define SCI9_RXI_IRQn          ((IRQn_Type) 0) /* SCI9 RXI (Receive data full) */
+        #define VECTOR_NUMBER_SCI9_TXI ((IRQn_Type) 1) /* SCI9 TXI (Transmit data empty) */
+        #define SCI9_TXI_IRQn          ((IRQn_Type) 1) /* SCI9 TXI (Transmit data empty) */
+        #define VECTOR_NUMBER_SCI9_TEI ((IRQn_Type) 2) /* SCI9 TEI (Transmit end) */
+        #define SCI9_TEI_IRQn          ((IRQn_Type) 2) /* SCI9 TEI (Transmit end) */
+        #define VECTOR_NUMBER_SCI9_ERI ((IRQn_Type) 3) /* SCI9 ERI (Receive error) */
+        #define SCI9_ERI_IRQn          ((IRQn_Type) 3) /* SCI9 ERI (Receive error) */
+        #endif
+
+        #ifdef __cplusplus
+        }
+        #endif
+        #endif /* VECTOR_DATA_H */
