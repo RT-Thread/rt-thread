9aba7d5c19c90a335791d738566e02ce34c665bb
rbb666 <zhangbingru@rt-thread.com>
2024-03-19 18:32:48 +0800

    [bsp][stm32]Update stm32l5 mdk project

--- bsp/stm32/stm32l552-st-nucleo/rtconfig.h
+++ bsp/stm32/stm32l552-st-nucleo/rtconfig.h
@@ -4,9 +4,12 @@
 /* Automatically generated file; DO NOT EDIT. */
 /* RT-Thread Configuration */
 
+#define BOARD_STM32L552_NUCLEO
+
 /* RT-Thread Kernel */
 
 #define RT_NAME_MAX 8
+#define RT_CPUS_NR 1
 #define RT_ALIGN_SIZE 8
 #define RT_THREAD_PRIORITY_32
 #define RT_THREAD_PRIORITY_MAX 32
@@ -38,14 +41,12 @@
 #define RT_USING_SMALL_MEM
 #define RT_USING_SMALL_MEM_AS_HEAP
 #define RT_USING_HEAP
-
-/* Kernel Device Object */
-
 #define RT_USING_DEVICE
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 256
 #define RT_CONSOLE_DEVICE_NAME "lpuart1"
-#define RT_VER_NUM 0x50002
+#define RT_VER_NUM 0x50100
+#define RT_BACKTRACE_LEVEL_MAX_NR 32
 #define RT_USING_HW_ATOMIC
 #define RT_USING_CPU_FFS
 #define ARCH_ARM
@@ -110,6 +111,9 @@
 
 
 /* Network */
+
+
+/* Memory protection */
 
 
 /* Utilities */
@@ -233,6 +237,7 @@
 
 #define SOC_FAMILY_STM32
 #define SOC_SERIES_STM32L5
+#define BOARD_SERIES_STM32_NUCLEO_144
 
 /* Hardware Drivers Config */
 
