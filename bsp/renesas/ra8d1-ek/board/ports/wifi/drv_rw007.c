1271a3207cfc86b94938e8660b7ec29a0cd190e0
Rbb666 <zhangbingru@rt-thread.com>
2024-04-12 10:14:38 +0800

    [bsp][renesas]Fix ek-ra8d1 spi binding error problem.

--- bsp/renesas/ra8d1-ek/board/ports/wifi/drv_rw007.c
+++ bsp/renesas/ra8d1-ek/board/ports/wifi/drv_rw007.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-2024, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author        Notes
+ * 2024-04-12     Rbb666        first version
+ */
 #include <rtthread.h>
 #include <rtdbg.h>
 #ifdef BSP_USING_RW007
