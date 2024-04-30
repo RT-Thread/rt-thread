11e9014bede1badba1ccdf5f60ddfddd842ddc18
IceBear003 <1294243258@qq.com>
2024-05-01 00:57:33 +0800

    [bsp][ch32v307]补全中断处理函数

--- bsp/wch/risc-v/Libraries/ch32_drivers/drv_usbd.c
+++ bsp/wch/risc-v/Libraries/ch32_drivers/drv_usbd.c
@@ -276,21 +276,21 @@ rt_err_t usbd_wakeup(void)
     return RT_EOK;
 }
 
-static struct udcd_ops udcd_ops =
-        {
-                .set_address = usbd_set_address,
-                .set_config = usbd_set_config,
-                .ep_set_stall = usbd_ep_set_stall,
-                .ep_clear_stall = usbd_ep_clear_stall,
-                .ep_enable = usbd_ep_enable,
-                .ep_disable = usbd_ep_disable,
-                .ep_read_prepare = usbd_ep_read_prepare,
-                .ep_read = usbd_ep_read,
-                .ep_write = usbd_ep_write,
-                .ep0_send_status = usbd_ep0_send_status,
-                .suspend = usbd_suspend,
-                .wakeup = usbd_wakeup,
-        };
+const struct udcd_ops udcd_ops =
+{
+    .set_address = usbd_set_address,
+    .set_config = usbd_set_config,
+    .ep_set_stall = usbd_ep_set_stall,
+    .ep_clear_stall = usbd_ep_clear_stall,
+    .ep_enable = usbd_ep_enable,
+    .ep_disable = usbd_ep_disable,
+    .ep_read_prepare = usbd_ep_read_prepare,
+    .ep_read = usbd_ep_read,
+    .ep_write = usbd_ep_write,
+    .ep0_send_status = usbd_ep0_send_status,
+    .suspend = usbd_suspend,
+    .wakeup = usbd_wakeup,
+};
 
 rt_err_t dcd_init(rt_device_t dev)
 {
@@ -308,14 +308,90 @@ rt_err_t dcd_init(rt_device_t dev)
     USBFSD->BASE_CTRL = USBFS_UC_DEV_PU_EN | USBFS_UC_INT_BUSY | USBFS_UC_DMA_EN;
     USBFSD->UDEV_CTRL = USBFS_UD_PD_DIS | USBFS_UD_PORT_EN;
 
+    NVIC_EnableIRQ(OTG_FS_IRQn);
+
     return RT_EOK;
 }
 
-void OTG_HS_IRQHandler() __attribute__((interrupt("WCH-Interrupt-fast")));
-void OTG_HS_IRQHandler()
+void USBD_IRQHandler(void) __attribute__((interrupt()));
+void USBD_IRQHandler()
 {
     rt_interrupt_enter();
-    //TODO
+    uint8_t int_fg = USBFSD->INT_FG;
+
+    if (int_fg & USBFS_UIF_TRANSFER) {
+        uint8_t ep_idx = USBFS_DEVICE->INT_ST & USBFS_UIS_ENDP_MASK;
+        uint8_t tog;
+        switch (USBFS_DEVICE->INT_ST & USBFS_UIS_TOKEN_MASK) {
+            case USBFS_UIS_TOKEN_SETUP:
+                _set_rx_ctrl(ep_idx, USBFS_UEP_R_RES_NAK);
+                break;
+
+            case USBFS_UIS_TOKEN_IN:
+                if (ep_idx == 0x00)
+                {
+                    tog = _get_tx_ctrl(ep_idx) & USBFS_UEP_T_TOG;
+                    _set_tx_ctrl(ep_idx, (_get_tx_ctrl(ep_idx) & 0b11111000) | ~tog | USBFS_UEP_T_RES_NAK);
+                    if (_get_dma(ep_idx) != 0)
+                    {
+                        rt_usbd_ep0_in_handler(&udcd);
+                    }
+                }
+                else
+                {
+                    _set_tx_ctrl(ep_idx, (_get_tx_ctrl(ep_idx) & ~USBFS_UEP_T_RES_MASK) | USBFS_UEP_T_RES_NAK);
+                    rt_usbd_ep_in_handler(&udcd, ep_idx | USB_DIR_IN, _get_tx_len(ep_idx));
+                }
+                break;
+            case USBFS_UIS_TOKEN_OUT:
+                if (ep_idx == 0x00)
+                {
+                    if(USBFSD->INT_ST & USBFS_UIS_TOG_OK)
+                    {
+                        tog = _get_rx_ctrl(ep_idx) & USBFS_UEP_R_TOG;
+                        _set_rx_ctrl(ep_idx, (_get_rx_ctrl(ep_idx) & 0b11111000) | ~tog | USBFS_UEP_R_RES_NAK);
+                        rt_usbd_ep0_out_handler(&udcd, _get_rx_len(ep_idx));
+                    }
+                    else
+                    {
+                        _set_rx_ctrl(ep_idx, (_get_rx_ctrl(ep_idx) & ~USBFS_UEP_R_RES_MASK) | USBFS_UEP_R_RES_NAK);
+                    }
+                }
+                else
+                {
+                    USBFSD->RX_CTRL = USBFS_UEP_R_RES_NAK;
+                    if (USBFSD->INT_ST & USBFS_UIS_TOG_OK) {
+                        _set_rx_ctrl(ep_idx, (_get_rx_ctrl(ep_idx) & ~USBFS_UEP_R_RES_MASK) | USBFS_UEP_R_RES_NAK);
+                        rt_usbd_ep_out_handler(&udcd, ep_idx | USB_DIR_OUT, 0);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        USBFS_DEVICE->INT_FG = USBFS_UIF_TRANSFER;
+    } else if (int_fg & USBFS_UIF_BUS_RST) {
+        USBFS_DEVICE->UEP0_TX_LEN = 0;
+        USBFS_DEVICE->UEP0_TX_CTRL = USBFS_UEP_T_RES_NAK;
+        USBFS_DEVICE->UEP0_RX_CTRL = USBFS_UEP_R_RES_NAK;
+
+        for (uint8_t i = 1; i < 8; i++) {
+            _set_tx_len(i, 0);
+            _set_tx_ctrl(i, USBFS_UEP_T_RES_NAK | USBFS_UEP_AUTO_TOG);
+            _set_rx_ctrl(i, USBFS_UEP_R_RES_NAK | USBFS_UEP_AUTO_TOG);
+        }
+
+        _set_rx_ctrl(0, USBFS_UEP_R_RES_ACK);
+        rt_usbd_reset_handler(&udcd);
+
+        USBFS_DEVICE->INT_FG |= USBFS_UIF_BUS_RST;
+    } else if (int_fg & USBFS_UIF_SUSPEND) {
+        USBFS_DEVICE->INT_FG = USBFS_UIF_SUSPEND;
+    } else {
+        USBFS_DEVICE->INT_FG = int_fg;
+    }
+
     rt_interrupt_leave();
 }
 
