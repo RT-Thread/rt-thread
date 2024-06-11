c96eda3dc6358a02a69c1a6c7d41a907134f2556
zhujiale <zhujiale@rt-thread.com>
2024-06-11 14:32:47 +0800

    format document

--- examples/utest/testcases/tmpfs/tmpfs.c
+++ examples/utest/testcases/tmpfs/tmpfs.c
@@ -17,39 +17,39 @@
 void run_copy()
 {
     int ret = 0;
-    ret = msh_exec("cd /tmp",7);
-    if(ret != 0)
+    ret     = msh_exec("cd /tmp", 7);
+    if (ret != 0)
     {
-            LOG_E("errno=%d, ret=%d\n", errno, ret);
-            LOG_E("cd /tmp error");
-            uassert_false(1);
+        LOG_E("errno=%d, ret=%d\n", errno, ret);
+        LOG_E("cd /tmp error");
+        uassert_false(1);
     }
     uassert_true(1);
-    ret = msh_exec("touch test",10);
-    if(ret != 0)
+    ret = msh_exec("touch test", 10);
+    if (ret != 0)
     {
-            LOG_E("errno=%d, ret=%d\n", errno, ret);
-            LOG_E("touch test error");
-            uassert_false(1);
+        LOG_E("errno=%d, ret=%d\n", errno, ret);
+        LOG_E("touch test error");
+        uassert_false(1);
     }
     uassert_true(1);
-    ret = msh_exec("echo this_is_a_test_file test",29);
-    if(ret != 0)
+    ret = msh_exec("echo this_is_a_test_file test", 29);
+    if (ret != 0)
     {
-            LOG_E("errno=%d, ret=%d\n", errno, ret);
-            LOG_E("echo this_is_a_test_file test error");
-            uassert_false(1);
+        LOG_E("errno=%d, ret=%d\n", errno, ret);
+        LOG_E("echo this_is_a_test_file test error");
+        uassert_false(1);
     }
     uassert_true(1);
-    ret = msh_exec("cp test test1",13);
-    if(ret != 0)
+    ret = msh_exec("cp test test1", 13);
+    if (ret != 0)
     {
-            LOG_E("errno=%d, ret=%d\n", errno, ret);
-            LOG_E("cp test test1 error");
-            uassert_false(1);
+        LOG_E("errno=%d, ret=%d\n", errno, ret);
+        LOG_E("cp test test1 error");
+        uassert_false(1);
     }
 
-     uassert_true(1);
+    uassert_true(1);
 }
 
 static rt_err_t utest_tc_init(void)
@@ -65,4 +65,4 @@ static void testcase(void)
 {
     UTEST_UNIT_RUN(run_copy);
 }
-UTEST_TC_EXPORT(testcase,"testcase.tfs.tmpfs", utest_tc_init, utest_tc_cleanup, 10);
+UTEST_TC_EXPORT(testcase, "testcase.tfs.tmpfs", utest_tc_init, utest_tc_cleanup, 10);
