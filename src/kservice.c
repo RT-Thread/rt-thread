1e287b2591c1d9d75f80aa932077b867f0b2ac50
zms123456 <85141075+zmshahaha@users.noreply.github.com>
2024-06-13 16:09:56 +0800

    [src][kservice]fix shutdown log (#9061)
    fix shutdown log

--- src/kservice.c
+++ src/kservice.c
@@ -78,15 +78,11 @@ rt_weak void rt_hw_cpu_reset(void)
 
 rt_weak void rt_hw_cpu_shutdown(void)
 {
-    rt_base_t level;
     LOG_I("CPU shutdown...");
     LOG_W("Using default rt_hw_cpu_shutdown()."
-        "Please consider implementing rt_hw_cpu_reset() in another file.");
-    level = rt_hw_interrupt_disable();
-    while (level)
-    {
-        RT_ASSERT(RT_NULL);
-    }
+        "Please consider implementing rt_hw_cpu_shutdown() in another file.");
+    rt_hw_interrupt_disable();
+    RT_ASSERT(0);
     return;
 }
 
