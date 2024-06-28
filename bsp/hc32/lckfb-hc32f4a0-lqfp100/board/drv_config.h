13d35174995162eef3dcfb4d54a447661708aa11
yuanzihao <y@yzh.email>
2024-06-28 23:41:31 +0800

    [BSP][HC32]add lckfb-hc32f4a0-lqfp100 (#9119)
    * -以ev_hc32f4a0_lqfp176为模板进行修改
    
    * -修改对应引脚
    
    * -更新readme
    
    * -update readme
    
    * -添加 lckfb-hc32f4a0-lqfp100

---
+++ bsp/hc32/lckfb-hc32f4a0-lqfp100/board/drv_config.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022-2024, Xiaohua Semiconductor Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-04-28     CDT          first version
+ */
+
+#ifndef __DRV_CONFIG_H__
+#define __DRV_CONFIG_H__
+
+#include <board.h>
+#include <rtthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "dma_config.h"
+#include "uart_config.h"
+#include "spi_config.h"
+#include "adc_config.h"
+#include "dac_config.h"
+#include "gpio_config.h"
+#include "eth_config.h"
+#include "can_config.h"
+#include "sdio_config.h"
+#include "pm_config.h"
+#include "i2c_config.h"
+#include "qspi_config.h"
+#include "pulse_encoder_config.h"
+#include "timer_config.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
