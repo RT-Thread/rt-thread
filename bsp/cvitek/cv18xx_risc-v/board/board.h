148e5774c96f84c326452ebe2ee7690f2224949a
Shell <smokewood@qq.com>
2024-07-26 10:12:13 +0800

    bsp: enable KERNEL_REMAP for cvitek platform
    Changes:
    
    - board.c: add static assert for KERNEL_VADDR_START to check if it's valid
    - board.h: updated deafult KERNEL_VADDR_START for standard version
    - config bsp for v5.2.0 smart requirements
    - kconfig: update bsp Kconfig for remap kernel
    
    Signed-off-by: Shell <smokewood@qq.com>
    Reviewed-on: https://github.com/RT-Thread/rt-thread/pull/9229
    Reviewed-by: Chen Wang <unicorn_wang@outlook.com>

--- bsp/cvitek/cv18xx_risc-v/board/board.h
+++ bsp/cvitek/cv18xx_risc-v/board/board.h
@@ -19,11 +19,11 @@ extern unsigned int __bss_start;
 extern unsigned int __bss_end;
 
 #ifndef RT_USING_SMART
-#define KERNEL_VADDR_START 0x0
+#define KERNEL_VADDR_START 0x80200000
 #endif
 
 #define RT_HW_HEAP_BEGIN ((void *)&__bss_end)
-#define RT_HW_HEAP_END   ((void *)(RT_HW_HEAP_BEGIN + 16 * 1024 * 1024))
+#define RT_HW_HEAP_END   ((void *)(KERNEL_VADDR_START + 16 * 1024 * 1024))
 #define RT_HW_PAGE_START RT_HW_HEAP_END
 #define RT_HW_PAGE_END   ((void *)(KERNEL_VADDR_START + 32 * 1024 * 1024))
 
