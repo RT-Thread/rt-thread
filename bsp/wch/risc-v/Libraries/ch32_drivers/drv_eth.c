8dd179e3b01e62b1e05512d6fe7f234408fd307f
Yaochenger <1516081466@qq.com>
2024-07-29 09:45:29 +0800

    remove useless code

--- bsp/wch/risc-v/Libraries/ch32_drivers/drv_eth.c
+++ bsp/wch/risc-v/Libraries/ch32_drivers/drv_eth.c
@@ -1,15 +1,11 @@
 /*
- * File      : drv_usart.c
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006-2024, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2021-09-09     WCH        the first version
+ * 2021-09-09     WCH          the first version
  */
 
 #include "board.h"
@@ -21,7 +17,6 @@
 #include "lwipopts.h"
 #include <drv_eth.h>
 
-//#define DRV_DEBUG
 #define LOG_TAG              "drv.eth"
 #include <drv_log.h>
 
