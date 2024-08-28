53faa5970d686f7c4ef00324f6f38a9aa2f0bae1
Shell <smokewood@qq.com>
2024-08-28 20:52:12 +0800

    feat: bsp: qemu-riscv64: updates of building & docs
    - using generic command instead of abs path
    - update BSP configurations
    - update README

--- bsp/qemu-virt64-riscv/qemu-rv64ilp32-nographic.sh
+++ bsp/qemu-virt64-riscv/qemu-rv64ilp32-nographic.sh
@@ -1,1 +1,1 @@
-/home/rv/opt-ilp32/bin/qemu-system-riscv64ilp32  -cpu rv64 -M virt -m 256M -nographic -kernel rtthread.elf
+qemu-system-riscv64ilp32  -cpu rv64 -M virt -m 256M -nographic -kernel rtthread.elf
