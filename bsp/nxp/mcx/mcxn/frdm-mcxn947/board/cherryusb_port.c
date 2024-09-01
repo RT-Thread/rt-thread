7306afc07f36cf6ff597e9dd71474f170d28f8b9
sakumisu <1203593632@qq.com>
2024-09-01 11:04:46 +0800

    update(cherryusb): add config for mcxn947 usbdev and usbhost

---
+++ bsp/nxp/mcx/mcxn/frdm-mcxn947/board/cherryusb_port.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2006-2024, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024/04/23     sakumisu    first version
+ */
+#include <rtthread.h>
+#include <board.h>
+
+/* low level init here, this has implemented in cherryusb */
+
+/* low level deinit here, this has implemented in cherryusb */
+
+#ifdef RT_CHERRYUSB_DEVICE_TEMPLATE_MSC
+int cherryusb_devinit(void)
+{
+    extern void msc_ram_init(uint8_t busid, uintptr_t reg_base);
+
+    msc_ram_init(0, USBHS1__USBC_BASE);
+    return 0;
+}
+INIT_COMPONENT_EXPORT(cherryusb_devinit);
+#endif
+
+#ifdef RT_CHERRYUSB_HOST
+#include "usbh_core.h"
+
+int cherryusb_hostinit(void)
+{
+    usbh_initialize(0, USBHS1__USBC_BASE);
+    return 0;
+}
+INIT_COMPONENT_EXPORT(cherryusb_hostinit);
+#endif
