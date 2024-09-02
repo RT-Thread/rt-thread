c63fbca00e8bcac2e1eed49b2d308f7e1b644f71
Shell <smokewood@qq.com>
2024-09-02 18:02:15 +0800

    feat: libcpu/risc-v: unify interrupt & IO on rv64
    This patch aims to unify the two currently separated RISC-V 64-bit
    architecture ports, 'virt64' and 'c906', into a single generic
    'common64' port. The changes include renaming files and updating
    includes to use a unified 'interrupt.h' header, as well as making
    adjustments to IO and trap handling to be more consistent between the
    two architectures.
    
    Changes:
    - Renamed 'rt_interrupt.h' to 'interrupt.h' and updated includes accordingly.
    - Unified IO register access functions in 'riscv_io.h'.
    - Added 'opcode.h' for portable assembly support.
    - Updated 'plic.c' and 'plic.h' to handle interrupts in a unified manner.
    - Modified 'trap.c' to handle exceptions and interrupts consistently for 'rv64'.
    
    Signed-off-by: Shell <smokewood@qq.com>

--- libcpu/risc-v/t-head/c906/riscv.h
+++ libcpu/risc-v/t-head/c906/riscv.h
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2006-2021, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
  * 2021-01-30     lizhirui     first version
+ * 2024-08-28     RT-Thread    Fit into rv64ilp32 ABI
  */
 
 #ifndef __RISCV_H__
@@ -13,13 +14,15 @@
 
 #include <encoding.h>
 
-#define __SIZE(bit) (1UL << (bit))
-#define __MASK(bit) (__SIZE(bit) - 1UL)
+/* using unsigned long long for the case of rv64ilp32 */
+#define __SIZE(bit) (1ULL << (bit))
+#define __MASK(bit) (__SIZE(bit) - 1ULL)
+
 #define __UMASK(bit) (~(__MASK(bit)))
 #define __MASKVALUE(value,maskvalue) ((value) & (maskvalue))
 #define __UMASKVALUE(value,maskvalue) ((value) & (~(maskvalue)))
-#define __CHECKUPBOUND(value,bit_count) (!(((rt_size_t)value) & (~__MASK(bit_count))))
-#define __CHECKALIGN(value,start_bit) (!(((rt_size_t)value) & (__MASK(start_bit))))
+#define __CHECKUPBOUND(value,bit_count) (!(((rt_ubase_t)value) & (~__MASK(bit_count))))
+#define __CHECKALIGN(value,start_bit) (!(((rt_ubase_t)value) & (__MASK(start_bit))))
 
 #define __PARTBIT(value,start_bit,length) (((value) >> (start_bit)) & __MASK(length))
 
