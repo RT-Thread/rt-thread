6a30ec26256c7bb82a764e9ac59d486031df0bd3
sheltonyu <59359365+sheltonyu@users.noreply.github.com>
2024-09-03 00:51:06 +0800

    [bsp/at32] ethernet support phy lan8720 and yt8512

--- bsp/at32/libraries/rt_drivers/drv_emac.h
+++ bsp/at32/libraries/rt_drivers/drv_emac.h
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2022-05-16     shelton      first version
+ * 2024-09-02     shelton      add support phy lan8720 and yt8512
  */
 
 #ifndef __DRV_EMAC_H__
@@ -57,8 +58,6 @@
   #define PHY_INTERRUPT_FLAG_REG        0x15U
   /*  the phy interrupt mask register. */
   #define PHY_INTERRUPT_MASK_REG        0x15U
-  #define PHY_LINK_CHANGE_FLAG          (1<<2)
-  #define PHY_LINK_CHANGE_MASK          (1<<9)
   #define PHY_INT_MASK                  0
 #elif defined (PHY_USING_DP83848)
   #define PHY_CONTROL_REG               (0x00)       /*!< basic mode control register */
@@ -84,6 +83,50 @@
   /*  the phy interrupt mask register. */
   #define PHY_INTERRUPT_MASK_REG        0x12U
   #define PHY_INT_MASK                  (1<<5)
+#elif defined (PHY_USING_LAN8720)
+  #define PHY_CONTROL_REG               (0x00)       /*!< basic mode control register */
+  #define PHY_STATUS_REG                (0x01)       /*!< basic mode status register */
+  #define PHY_SPECIFIED_CS_REG          (0x1F)       /*!< specified configuration and status register */
+  /* phy control register */
+  #define PHY_AUTO_NEGOTIATION_BIT      (0x1000)     /*!< enable auto negotiation */
+  #define PHY_LOOPBACK_BIT              (0x4000)     /*!< enable loopback */
+  #define PHY_RESET_BIT                 (0x8000)     /*!< reset phy */
+  /* phy status register */
+  #define PHY_LINKED_STATUS_BIT         (0x0004)     /*!< link status */
+  #define PHY_NEGO_COMPLETE_BIT         (0x0020)     /*!< auto negotiation complete */
+  /* phy specified control/status register */
+  #define PHY_FULL_DUPLEX_100MBPS_BIT   (0x0018)     /*!< full duplex 100 mbps */
+  #define PHY_HALF_DUPLEX_100MBPS_BIT   (0x0008)     /*!< half duplex 100 mbps */
+  #define PHY_FULL_DUPLEX_10MBPS_BIT    (0x0014)     /*!< full duplex 10 mbps */
+  #define PHY_HALF_DUPLEX_10MBPS_BIT    (0x0004)     /*!< half duplex 10 mbps */
+  #define PHY_DUPLEX_MODE               (0x0100)     /*!< full duplex mode */
+  #define PHY_SPEED_MODE                (0x2000)     /*!< 100 mbps */
+  /*  the phy interrupt source flag register. */
+  #define PHY_INTERRUPT_FLAG_REG        0x1DU
+  /*  the phy interrupt mask register. */
+  #define PHY_INTERRUPT_MASK_REG        0x1EU
+  #define PHY_INT_MASK                  (1<<4)
+#elif defined (PHY_USING_YT8512)
+  #define PHY_CONTROL_REG               (0x00)       /*!< basic mode control register */
+  #define PHY_STATUS_REG                (0x01)       /*!< basic mode status register */
+  #define PHY_SPECIFIED_CS_REG          (0x11)       /*!< phy status register */
+  /* phy control register */
+  #define PHY_AUTO_NEGOTIATION_BIT      (0x1000)     /*!< enable auto negotiation */
+  #define PHY_LOOPBACK_BIT              (0x4000)     /*!< enable loopback */
+  #define PHY_RESET_BIT                 (0x8000)     /*!< reset phy */
+  /* phy status register */
+  #define PHY_LINKED_STATUS_BIT         (0x0004)     /*!< link status */
+  #define PHY_NEGO_COMPLETE_BIT         (0x0020)     /*!< auto negotiation complete */
+
+  #define PHY_DUPLEX_MODE               (0x2000)     /*!< full duplex mode */
+  #define PHY_SPEED_MODE                (0x4000)     /*!< 100 mbps */
+
+  /*  the phy interrupt source flag register. */
+  #define PHY_INTERRUPT_FLAG_REG        0x13U
+  #define PHY_LINK_CHANGE_FLAG          (3<<10)
+  /*  the phy interrupt mask register. */
+  #define PHY_INTERRUPT_MASK_REG        0x12U
+  #define PHY_INT_MASK                  (3<<10)
 #endif
 
 #endif /* __DRV_EMAC_H__ */
