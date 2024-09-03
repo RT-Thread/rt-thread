0d87b028ed9c84f9ac9b83097d38b808cfe79f17
sakumisu <1203593632@qq.com>
2024-09-03 10:27:04 +0800

    update(cherryusb): add config for bl616 usbdev and usbhost

--- bsp/bouffalo_lab/bl61x/board/cherryusb_port.c
+++ bsp/bouffalo_lab/bl61x/board/cherryusb_port.c
@@ -8,7 +8,30 @@
  * 2024/04/23     sakumisu    first version
  */
 #include <rtthread.h>
+#include <board.h>
 
 /* low level init here, this has implemented in cherryusb */
 
 /* low level deinit here, this has implemented in cherryusb */
+
+#ifdef RT_CHERRYUSB_DEVICE_TEMPLATE_MSC
+int cherryusb_devinit(void)
+{
+    extern void msc_ram_init(uint8_t busid, uintptr_t reg_base);
+
+    msc_ram_init(0, 0x20072000);
+    return 0;
+}
+INIT_COMPONENT_EXPORT(cherryusb_devinit);
+#endif
+
+#ifdef RT_CHERRYUSB_HOST
+#include "usbh_core.h"
+
+int cherryusb_hostinit(void)
+{
+    usbh_initialize(0, 0x20072000);
+    return 0;
+}
+INIT_COMPONENT_EXPORT(cherryusb_hostinit);
+#endif
