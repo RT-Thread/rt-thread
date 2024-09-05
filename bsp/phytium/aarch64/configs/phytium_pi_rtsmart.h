6d8e23e2b2b311f3534c5cb562ee95e671ba841a
zhangyan <103986797+messigogogo@users.noreply.github.com>
2024-09-06 06:18:27 +0800

    [bsp/phytium]默认配置修改

--- bsp/phytium/aarch64/configs/phytium_pi_rtsmart.h
+++ bsp/phytium/aarch64/configs/phytium_pi_rtsmart.h
@@ -178,6 +178,7 @@
 #define RT_USING_QSPI
 #define RT_USING_PIN
 #define RT_USING_KTIME
+#define RT_LWIP_PBUF_POOL_BUFSIZE 1700
 /* end of Device Drivers */
 
 /* C/C++ and POSIX layer */
@@ -302,6 +303,7 @@
 #define RT_USING_LDSO
 #define LWP_USING_TERMINAL
 #define LWP_PTY_MAX_PARIS_LIMIT 64
+#define RT_USING_VDSO
 
 /* Memory management */
 
@@ -508,7 +510,6 @@
 #define RT_USING_QSPI0
 #define USING_QSPI_CHANNEL0
 #define BSP_USING_ETH
-#define RT_LWIP_PBUF_POOL_BUFSIZE 1700
 #define BSP_USING_PWM
 #define RT_USING_PWM2
 #define BSP_USING_I2C
