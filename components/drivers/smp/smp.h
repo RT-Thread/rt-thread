90c1052f5b3ddab627ea812d31139e96d40d0e1a
zhujiale <945386260@qq.com>
2024-09-12 18:30:34 +0800

    smp

--- components/drivers/smp/smp.h
+++ components/drivers/smp/smp.h
@@ -1,8 +1,8 @@
 #ifndef __SMP_IPI_H__
 #define __SMP_IPI_H__
 #include <rtthread.h>
-typedef void (*smp_call_func_back)(void *data);
-typedef rt_bool_t (*smp_cond)(int cpu, void *info);
+typedef void (*rt_smp_call_func_back)(void *data);
+typedef rt_bool_t (*rt_smp_cond)(int cpu, void *info);
 
 #define SMP_CALL_EVENT_FUNC 0x1
 
@@ -15,7 +15,7 @@ struct rt_smp_event
     int cpu_mask;
     int event_id;
     void *data;
-    smp_call_func_back func;
+    rt_smp_call_func_back func;
 };
 struct rt_smp_call
 {
@@ -25,10 +25,10 @@ struct rt_smp_call
 
 
 void rt_smp_call_ipi_handler(int vector, void *param);
-void rt_smp_call_each_cpu(smp_call_func_back func, void *data,rt_uint8_t flag);
-void rt_smp_call_each_cpu_cond(smp_call_func_back func, void *data,rt_uint8_t flag,smp_cond cond_func);
-void rt_smp_call_any_cpu(int cpu_mask,smp_call_func_back func, void *data,rt_uint8_t flag);
-void rt_smp_call_any_cpu_cond(int cpu_mask,smp_call_func_back func, void *data,rt_uint8_t flag,smp_cond cond_func);
+void rt_smp_call_each_cpu(rt_smp_call_func_back func, void *data,rt_uint8_t flag);
+void rt_smp_call_each_cpu_cond(rt_smp_call_func_back func, void *data,rt_uint8_t flag,rt_smp_cond cond_func);
+void rt_smp_call_any_cpu(int cpu_mask,rt_smp_call_func_back func, void *data,rt_uint8_t flag);
+void rt_smp_call_any_cpu_cond(int cpu_mask,rt_smp_call_func_back func, void *data,rt_uint8_t flag,rt_smp_cond cond_func);
 void rt_smp_init(void);
 
 #endif
