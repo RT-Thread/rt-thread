7f331129726515983169fd4ff9e1f497cfc41740
zhao maosheng <zmshahaha@gmail.com>
2024-09-14 11:44:02 +0800

    replace RT_LWIP_NETIF_NAMESIZE with RT_NAME_SIZE

--- components/net/lwip/port/lwipopts.h
+++ components/net/lwip/port/lwipopts.h
@@ -578,14 +578,9 @@
 #endif
 
 /**
- * RT_LWIP_NETIF_NAMESIZE support netif name length(in netif.c)
+ * NETIF_NAMESIZE support netif name length(in netif.c)
  */
-#ifdef RT_LWIP_NETIF_NAMESIZE
-#if (RT_LWIP_NETIF_NAMESIZE < 2)
-#warning NETIF_NAMESIZE too small, the value must be greater than or equal to 6.
-#endif
-#define NETIF_NAMESIZE                  RT_LWIP_NETIF_NAMESIZE
-#endif /* RT_LWIP_NETIF_NAMESIZE */
+#define NETIF_NAMESIZE                  RT_NAME_MAX
 
 /**
  * LWIP_NETIF_API==1: Support netif api (in netifapi.c)
