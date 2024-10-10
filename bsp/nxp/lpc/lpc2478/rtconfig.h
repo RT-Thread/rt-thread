a31d3f52e7dea1760ec8069e8929c2f101034b24
kurisaw <2053731441@qq.com>
2024-10-10 11:08:10 +0800

    [bsp][nxp] Repair of nxp series bsp

--- bsp/nxp/lpc/lpc2478/rtconfig.h
+++ bsp/nxp/lpc/lpc2478/rtconfig.h
@@ -1,9 +1,6 @@
 #ifndef RT_CONFIG_H__
 #define RT_CONFIG_H__
 
-/* Automatically generated file; DO NOT EDIT. */
-/* RT-Thread Project Configuration */
-
 /* RT-Thread Kernel */
 
 #define RT_NAME_MAX 8
@@ -12,7 +9,6 @@
 #define RT_THREAD_PRIORITY_32
 #define RT_THREAD_PRIORITY_MAX 32
 #define RT_TICK_PER_SECOND 100
-#define RT_USING_OVERFLOW_CHECK
 #define RT_USING_HOOK
 #define RT_HOOK_USING_FUNC_PTR
 #define RT_USING_IDLE_HOOK
@@ -24,10 +20,16 @@
 
 /* kservice optimization */
 
-#define RT_KSERVICE_USING_STDLIB
+/* end of kservice optimization */
+
+/* klibc optimization */
+
+/* end of klibc optimization */
 #define RT_USING_DEBUG
+#define RT_DEBUGING_ASSERT
 #define RT_DEBUGING_COLOR
 #define RT_DEBUGING_CONTEXT
+#define RT_USING_OVERFLOW_CHECK
 
 /* Inter-Thread communication */
 
@@ -36,6 +38,7 @@
 #define RT_USING_EVENT
 #define RT_USING_MAILBOX
 #define RT_USING_MESSAGEQUEUE
+/* end of Inter-Thread communication */
 
 /* Memory Management */
 
@@ -43,12 +46,14 @@
 #define RT_USING_SMALL_MEM
 #define RT_USING_SMALL_MEM_AS_HEAP
 #define RT_USING_HEAP
+/* end of Memory Management */
 #define RT_USING_DEVICE
 #define RT_USING_CONSOLE
 #define RT_CONSOLEBUF_SIZE 128
 #define RT_CONSOLE_DEVICE_NAME "uart"
-#define RT_VER_NUM 0x50100
+#define RT_VER_NUM 0x50200
 #define RT_BACKTRACE_LEVEL_MAX_NR 32
+/* end of RT-Thread Kernel */
 #define ARCH_ARM
 
 /* RT-Thread Components */
@@ -78,6 +83,7 @@
 #define DFS_FILESYSTEMS_MAX 4
 #define DFS_FILESYSTEM_TYPES_MAX 4
 #define RT_USING_DFS_DEVFS
+/* end of DFS: device virtual file system */
 
 /* Device Drivers */
 
@@ -88,9 +94,7 @@
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
-
-/* Using USB */
-
+/* end of Device Drivers */
 
 /* C/C++ and POSIX layer */
 
@@ -102,6 +106,8 @@
 #define RT_LIBC_TZ_DEFAULT_HOUR 8
 #define RT_LIBC_TZ_DEFAULT_MIN 0
 #define RT_LIBC_TZ_DEFAULT_SEC 0
+/* end of Timezone and Daylight Saving Time */
+/* end of ISO-ANSI C layer */
 
 /* POSIX (Portable Operating System Interface) layer */
 
@@ -111,18 +117,30 @@
 
 /* Socket is in the 'Network' category */
 
+/* end of Interprocess Communication (IPC) */
+/* end of POSIX (Portable Operating System Interface) layer */
+/* end of C/C++ and POSIX layer */
 
 /* Network */
 
+/* end of Network */
 
 /* Memory protection */
 
+/* end of Memory protection */
 
 /* Utilities */
 
+/* end of Utilities */
+
+/* Using USB legacy version */
+
+/* end of Using USB legacy version */
+/* end of RT-Thread Components */
 
 /* RT-Thread Utestcases */
 
+/* end of RT-Thread Utestcases */
 
 /* RT-Thread online packages */
 
@@ -133,57 +151,78 @@
 
 /* Marvell WiFi */
 
+/* end of Marvell WiFi */
 
 /* Wiced WiFi */
 
+/* end of Wiced WiFi */
 
 /* CYW43012 WiFi */
 
+/* end of CYW43012 WiFi */
 
 /* BL808 WiFi */
 
+/* end of BL808 WiFi */
 
 /* CYW43439 WiFi */
 
+/* end of CYW43439 WiFi */
+/* end of Wi-Fi */
 
 /* IoT Cloud */
 
+/* end of IoT Cloud */
+/* end of IoT - internet of things */
 
 /* security packages */
 
+/* end of security packages */
 
 /* language packages */
 
 /* JSON: JavaScript Object Notation, a lightweight data-interchange format */
 
+/* end of JSON: JavaScript Object Notation, a lightweight data-interchange format */
 
 /* XML: Extensible Markup Language */
 
+/* end of XML: Extensible Markup Language */
+/* end of language packages */
 
 /* multimedia packages */
 
 /* LVGL: powerful and easy-to-use embedded GUI library */
 
+/* end of LVGL: powerful and easy-to-use embedded GUI library */
 
 /* u8g2: a monochrome graphic library */
 
+/* end of u8g2: a monochrome graphic library */
+/* end of multimedia packages */
 
 /* tools packages */
 
+/* end of tools packages */
 
 /* system packages */
 
 /* enhanced kernel services */
 
+/* end of enhanced kernel services */
 
 /* acceleration: Assembly language or algorithmic acceleration packages */
 
+/* end of acceleration: Assembly language or algorithmic acceleration packages */
 
 /* CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
 
+/* end of CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
 
 /* Micrium: Micrium software products porting for RT-Thread */
 
+/* end of Micrium: Micrium software products porting for RT-Thread */
+/* end of system packages */
 
 /* peripheral libraries and drivers */
 
@@ -191,66 +230,94 @@
 
 /* STM32 HAL & SDK Drivers */
 
+/* end of STM32 HAL & SDK Drivers */
+
+/* Infineon HAL Packages */
+
+/* end of Infineon HAL Packages */
 
 /* Kendryte SDK */
 
+/* end of Kendryte SDK */
+/* end of HAL & SDK Drivers */
 
 /* sensors drivers */
 
+/* end of sensors drivers */
 
 /* touch drivers */
 
+/* end of touch drivers */
+/* end of peripheral libraries and drivers */
 
 /* AI packages */
 
+/* end of AI packages */
 
 /* Signal Processing and Control Algorithm Packages */
 
+/* end of Signal Processing and Control Algorithm Packages */
 
 /* miscellaneous packages */
 
 /* project laboratory */
 
+/* end of project laboratory */
+
 /* samples: kernel and components samples */
 
+/* end of samples: kernel and components samples */
 
 /* entertainment: terminal games and other interesting software packages */
 
+/* end of entertainment: terminal games and other interesting software packages */
+/* end of miscellaneous packages */
 
 /* Arduino libraries */
 
 
 /* Projects and Demos */
 
+/* end of Projects and Demos */
 
 /* Sensors */
 
+/* end of Sensors */
 
 /* Display */
 
+/* end of Display */
 
 /* Timing */
 
+/* end of Timing */
 
 /* Data Processing */
 
+/* end of Data Processing */
 
 /* Data Storage */
 
 /* Communication */
 
+/* end of Communication */
 
 /* Device Control */
 
+/* end of Device Control */
 
 /* Other */
 
+/* end of Other */
 
 /* Signal IO */
 
+/* end of Signal IO */
 
 /* Uncategorized */
 
+/* end of Arduino libraries */
+/* end of RT-Thread online packages */
 #define SOC_LPC2478
 
 #endif
--- bsp/nxp/lpc/lpc824/rtconfig.h
+++ bsp/nxp/lpc/lpc824/rtconfig.h
@@ -1,187 +1,344 @@
-/* RT-Thread config file */
+#ifndef RT_CONFIG_H__
+#define RT_CONFIG_H__
 
-#ifndef __RTTHREAD_CFG_H__
-#define __RTTHREAD_CFG_H__
+/* RT-Thread Kernel */
 
-// <<< Use Configuration Wizard in Context Menu >>>
-// <h>Basic Configuration
-// <o>Maximal level of thread priority <8-256>
-//  <i>Default: 32
-#define RT_THREAD_PRIORITY_MAX  8
-// <o>OS tick per second
-//  <i>Default: 1000   (1ms)
-#define RT_TICK_PER_SECOND  100
-// <o>Alignment size for CPU architecture data access
-//  <i>Default: 4
-#define RT_ALIGN_SIZE   8
-// <o>the max length of object name<2-16>
-//  <i>Default: 8
-#define RT_NAME_MAX    8
-// <c1>Using RT-Thread components initialization
-//  <i>Using RT-Thread components initialization
-#define RT_USING_COMPONENTS_INIT
-// </c>
-// <c1>Using user main
-//  <i>Using user main
-//#define RT_USING_USER_MAIN
-// </c>
-// <o>the size of main thread<1-4086>
-//  <i>Default: 512
-#define RT_MAIN_THREAD_STACK_SIZE     256
+#define RT_NAME_MAX 8
+#define RT_CPUS_NR 1
+#define RT_ALIGN_SIZE 8
+#define RT_THREAD_PRIORITY_32
+#define RT_THREAD_PRIORITY_MAX 32
+#define RT_TICK_PER_SECOND 1000
+#define RT_USING_HOOK
+#define RT_HOOK_USING_FUNC_PTR
+#define RT_USING_IDLE_HOOK
+#define RT_IDLE_HOOK_LIST_SIZE 4
+#define IDLE_THREAD_STACK_SIZE 256
+#define RT_USING_TIMER_SOFT
+#define RT_TIMER_THREAD_PRIO 4
+#define RT_TIMER_THREAD_STACK_SIZE 512
 
-// </h>
+/* kservice optimization */
 
-// <h>Debug Configuration
-// <c1>enable kernel debug configuration
-//  <i>Default: enable kernel debug configuration
-//#define RT_DEBUG
-// </c>
-// <o>enable components initialization debug configuration<0-1>
-//  <i>Default: 0
-#define RT_DEBUG_INIT 0
-// <c1>thread stack over flow detect
-//  <i> Diable Thread stack over flow detect
-//#define RT_USING_OVERFLOW_CHECK
-// </c>
-// </h>
+/* end of kservice optimization */
 
-// <h>Hook Configuration
-// <c1>using hook
-//  <i>using hook
-//#define RT_USING_HOOK
-// </c>
-// <c1>using idle hook
-//  <i>using idle hook
-//#define RT_USING_IDLE_HOOK
-// </c>
-// </h>
+/* klibc optimization */
 
-// <e>Software timers Configuration
-// <i> Enables user timers
-#define RT_USING_TIMER_SOFT         0
-#if RT_USING_TIMER_SOFT == 0
-#undef RT_USING_TIMER_SOFT
-#endif
-// <o>The priority level of timer thread <0-31>
-//  <i>Default: 4
-#define RT_TIMER_THREAD_PRIO        4
-// <o>The stack size of timer thread <0-8192>
-//  <i>Default: 512
-#define RT_TIMER_THREAD_STACK_SIZE  512
-// <o>The soft-timer tick per second <0-1000>
-//  <i>Default: 100
-#define RT_TIMER_TICK_PER_SECOND    100
-// </e>
+/* end of klibc optimization */
+#define RT_USING_DEBUG
+#define RT_DEBUGING_ASSERT
+#define RT_DEBUGING_COLOR
+#define RT_DEBUGING_CONTEXT
+#define RT_USING_OVERFLOW_CHECK
 
-// <h>IPC(Inter-process communication) Configuration
-// <c1>Using Semaphore
-//  <i>Using Semaphore
+/* Inter-Thread communication */
+
 #define RT_USING_SEMAPHORE
-// </c>
-// <c1>Using Mutex
-//  <i>Using Mutex
 #define RT_USING_MUTEX
-// </c>
-// <c1>Using Event
-//  <i>Using Event
-//#define RT_USING_EVENT
-// </c>
-// <c1>Using MailBox
-//  <i>Using MailBox
+#define RT_USING_EVENT
 #define RT_USING_MAILBOX
-// </c>
-// <c1>Using Message Queue
-//  <i>Using Message Queue
-//#define RT_USING_MESSAGEQUEUE
-// </c>
-// </h>
+#define RT_USING_MESSAGEQUEUE
+/* end of Inter-Thread communication */
 
-// <h>Memory Management Configuration
-// <c1>Using Memory Pool Management
-//  <i>Using Memory Pool Management
-//#define RT_USING_MEMPOOL
-// </c>
-// <c1>Dynamic Heap Management
-//  <i>Dynamic Heap Management
-#define RT_USING_HEAP
-// </c>
-// <c1>using small memory
-//  <i>using small memory
+/* Memory Management */
+
+#define RT_USING_MEMPOOL
 #define RT_USING_SMALL_MEM
-// </c>
-// <c1>using tiny size of memory
-//  <i>using tiny size of memory
-#define RT_USING_TINY_SIZE
-// </c>
-// </h>
-
-
-// <h>Device System Configuration
-// <c1>Using Device System
-//  <i>Using Device System
+#define RT_USING_SMALL_MEM_AS_HEAP
+#define RT_USING_HEAP
+/* end of Memory Management */
 #define RT_USING_DEVICE
-// </c>
-// <c1>Using device communication
-//  <i>Using device communication
-#define RT_USING_DEVICE_IPC
-// </c>
-// <c1>Using Serial
-//  <i>Using Serial
-//#define RT_USING_SERIAL
-#define RT_SERIAL_USING_DMA
-// </c>
-// </h>
-
-// <h>Console Configuration
-// <c1>Using console
-//  <i>Using console
 #define RT_USING_CONSOLE
-// </c>
-// <o>the buffer size of console <1-1024>
-//  <i>the buffer size of console
-//  <i>Default: 128  (128Byte)
-#define RT_CONSOLEBUF_SIZE          128
-// <s>The device name for console
-//  <i>The device name for console
-//  <i>Default: uart1
-#define RT_CONSOLE_DEVICE_NAME      "uart1"
-// </h>
+#define RT_CONSOLEBUF_SIZE 128
+#define RT_CONSOLE_DEVICE_NAME "uart1"
+#define RT_VER_NUM 0x50200
+#define RT_BACKTRACE_LEVEL_MAX_NR 32
+/* end of RT-Thread Kernel */
+#define ARCH_ARM
+#define ARCH_ARM_CORTEX_M
+#define ARCH_ARM_CORTEX_M0
 
+/* RT-Thread Components */
 
-// <h>Finsh Configuration
-// <c1>Using finsh as shell, which is a C-Express shell
-//  <i>Using finsh as shell, which is a C-Express shell
+#define RT_USING_COMPONENTS_INIT
+#define RT_USING_USER_MAIN
+#define RT_MAIN_THREAD_STACK_SIZE 2048
+#define RT_MAIN_THREAD_PRIORITY 10
+#define RT_USING_MSH
 #define RT_USING_FINSH
-// </c>
-// <o>the priority of finsh thread <1-7>
-//  <i>the priority of finsh thread
-//  <i>Default: 6
-#define FINSH_THREAD_PRIORITY       (RT_THREAD_PRIORITY_MAX / 8 * 5 + 1)
-// <o>the stack of finsh thread <1-4096>
-//  <i>the stack of finsh thread
-//  <i>Default: 4096  (4096Byte)
-#define FINSH_THREAD_STACK_SIZE     512
-// <o>the history lines of finsh thread <1-32>
-//  <i>the history lines of finsh thread
-//  <i>Default: 5
-#define FINSH_HISTORY_LINES         1
-// <c1>Using symbol table in finsh shell
-//  <i>Using symbol table in finsh shell
-#define FINSH_USING_SYMTAB
-// </c>
-// <c1>Using module shell in finsh
-//  <i>Using module shell in finsh
 #define FINSH_USING_MSH
-// </c>
-// <c1>Only using module shell in finsh
-//  <i>Only using module shell in finsh
-#define FINSH_USING_MSH_ONLY
-// </c>
-// </h>
+#define FINSH_THREAD_NAME "tshell"
+#define FINSH_THREAD_PRIORITY 20
+#define FINSH_THREAD_STACK_SIZE 4096
+#define FINSH_USING_HISTORY
+#define FINSH_HISTORY_LINES 5
+#define FINSH_USING_SYMTAB
+#define FINSH_CMD_SIZE 80
+#define MSH_USING_BUILT_IN_COMMANDS
+#define FINSH_USING_DESCRIPTION
+#define FINSH_ARG_MAX 10
+#define FINSH_USING_OPTION_COMPLETION
 
-// <<< end of configuration section >>>
+/* DFS: device virtual file system */
 
-#define RT_USING_UART
-#define RT_USING_UART1
+#define RT_USING_DFS
+#define DFS_USING_POSIX
+#define DFS_USING_WORKDIR
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_V1
+#define DFS_FILESYSTEMS_MAX 4
+#define DFS_FILESYSTEM_TYPES_MAX 4
+#define RT_USING_DFS_DEVFS
+/* end of DFS: device virtual file system */
+
+/* Device Drivers */
+
+#define RT_USING_DEVICE_IPC
+#define RT_UNAMED_PIPE_NUMBER 64
+#define RT_USING_SERIAL
+#define RT_USING_SERIAL_V1
+#define RT_SERIAL_USING_DMA
+#define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_PIN
+/* end of Device Drivers */
+
+/* C/C++ and POSIX layer */
+
+/* ISO-ANSI C layer */
+
+/* Timezone and Daylight Saving Time */
+
+#define RT_LIBC_USING_LIGHT_TZ_DST
+#define RT_LIBC_TZ_DEFAULT_HOUR 8
+#define RT_LIBC_TZ_DEFAULT_MIN 0
+#define RT_LIBC_TZ_DEFAULT_SEC 0
+/* end of Timezone and Daylight Saving Time */
+/* end of ISO-ANSI C layer */
+
+/* POSIX (Portable Operating System Interface) layer */
+
+
+/* Interprocess Communication (IPC) */
+
+
+/* Socket is in the 'Network' category */
+
+/* end of Interprocess Communication (IPC) */
+/* end of POSIX (Portable Operating System Interface) layer */
+/* end of C/C++ and POSIX layer */
+
+/* Network */
+
+/* end of Network */
+
+/* Memory protection */
+
+/* end of Memory protection */
+
+/* Utilities */
+
+/* end of Utilities */
+
+/* Using USB legacy version */
+
+/* end of Using USB legacy version */
+/* end of RT-Thread Components */
+
+/* RT-Thread Utestcases */
+
+/* end of RT-Thread Utestcases */
+
+/* RT-Thread online packages */
+
+/* IoT - internet of things */
+
+
+/* Wi-Fi */
+
+/* Marvell WiFi */
+
+/* end of Marvell WiFi */
+
+/* Wiced WiFi */
+
+/* end of Wiced WiFi */
+
+/* CYW43012 WiFi */
+
+/* end of CYW43012 WiFi */
+
+/* BL808 WiFi */
+
+/* end of BL808 WiFi */
+
+/* CYW43439 WiFi */
+
+/* end of CYW43439 WiFi */
+/* end of Wi-Fi */
+
+/* IoT Cloud */
+
+/* end of IoT Cloud */
+/* end of IoT - internet of things */
+
+/* security packages */
+
+/* end of security packages */
+
+/* language packages */
+
+/* JSON: JavaScript Object Notation, a lightweight data-interchange format */
+
+/* end of JSON: JavaScript Object Notation, a lightweight data-interchange format */
+
+/* XML: Extensible Markup Language */
+
+/* end of XML: Extensible Markup Language */
+/* end of language packages */
+
+/* multimedia packages */
+
+/* LVGL: powerful and easy-to-use embedded GUI library */
+
+/* end of LVGL: powerful and easy-to-use embedded GUI library */
+
+/* u8g2: a monochrome graphic library */
+
+/* end of u8g2: a monochrome graphic library */
+/* end of multimedia packages */
+
+/* tools packages */
+
+/* end of tools packages */
+
+/* system packages */
+
+/* enhanced kernel services */
+
+/* end of enhanced kernel services */
+
+/* acceleration: Assembly language or algorithmic acceleration packages */
+
+/* end of acceleration: Assembly language or algorithmic acceleration packages */
+
+/* CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
+
+/* end of CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
+
+/* Micrium: Micrium software products porting for RT-Thread */
+
+/* end of Micrium: Micrium software products porting for RT-Thread */
+/* end of system packages */
+
+/* peripheral libraries and drivers */
+
+/* HAL & SDK Drivers */
+
+/* STM32 HAL & SDK Drivers */
+
+/* end of STM32 HAL & SDK Drivers */
+
+/* Infineon HAL Packages */
+
+/* end of Infineon HAL Packages */
+
+/* Kendryte SDK */
+
+/* end of Kendryte SDK */
+/* end of HAL & SDK Drivers */
+
+/* sensors drivers */
+
+/* end of sensors drivers */
+
+/* touch drivers */
+
+/* end of touch drivers */
+/* end of peripheral libraries and drivers */
+
+/* AI packages */
+
+/* end of AI packages */
+
+/* Signal Processing and Control Algorithm Packages */
+
+/* end of Signal Processing and Control Algorithm Packages */
+
+/* miscellaneous packages */
+
+/* project laboratory */
+
+/* end of project laboratory */
+
+/* samples: kernel and components samples */
+
+/* end of samples: kernel and components samples */
+
+/* entertainment: terminal games and other interesting software packages */
+
+/* end of entertainment: terminal games and other interesting software packages */
+/* end of miscellaneous packages */
+
+/* Arduino libraries */
+
+
+/* Projects and Demos */
+
+/* end of Projects and Demos */
+
+/* Sensors */
+
+/* end of Sensors */
+
+/* Display */
+
+/* end of Display */
+
+/* Timing */
+
+/* end of Timing */
+
+/* Data Processing */
+
+/* end of Data Processing */
+
+/* Data Storage */
+
+/* Communication */
+
+/* end of Communication */
+
+/* Device Control */
+
+/* end of Device Control */
+
+/* Other */
+
+/* end of Other */
+
+/* Signal IO */
+
+/* end of Signal IO */
+
+/* Uncategorized */
+
+/* end of Arduino libraries */
+/* end of RT-Thread online packages */
+#define SOC_LPC824
+
+/* Hardware Drivers Config */
+
+/* On-chip Peripheral Drivers */
+
+#define BSP_USING_PIN
+#define BSP_USING_UART
+#define BSP_USING_UART1
+/* end of On-chip Peripheral Drivers */
+
+/* Onboard Peripheral Drivers */
+
+/* Board extended module Drivers */
+
+/* end of Hardware Drivers Config */
 
 #endif
