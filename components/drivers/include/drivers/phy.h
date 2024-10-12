5d02e8dba396df43dcc4ec769a3c0bfbb8bd550f
zhujiale <945386260@qq.com>
2024-10-12 10:58:26 +0800

    add phy_v2

--- components/drivers/include/drivers/phy.h
+++ components/drivers/include/drivers/phy.h
@@ -5,17 +5,20 @@
  *
  * Change Logs:
  * Date           Author       Notes
- * 2024-10-08     zhujiale    the first version
+ * 2020-10-14     wangqiang    the first version
+ * 2022-08-17     xjy198903    add 1000M definition
+ * 2024-10-08     zhujiale     add phy v2.0
  */
 #ifndef __NET_PHY_H__
 #define __NET_PHY_H__
 #include <rtthread.h>
-#include <mdio.h>
-#include <general_phy.h>
-#include <ofw.h>
 #include <drivers/core/dm.h>
 #include <drivers/core/driver.h>
 
+#ifdef RT_USING_PHY_V2
+#include <ofw.h>
+#include <mdio.h>
+#include <general_phy.h>
 #define RT_PHY_FIXED_ID 0xa5a55a5a
 #define RT_PHY_NCSI_ID  0xbeefcafe
 
@@ -146,5 +149,78 @@ static int rt_##phy_drv##_register(void)     \
     return 0;                                \
 }                                            \
 INIT_PREV_EXPORT(rt_##phy_drv##_register);
+#endif
 
+#ifdef RT_USING_PHY
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct rt_mdio_bus_ops
+{
+    rt_bool_t (*init)(void *bus, rt_uint32_t src_clock_hz);
+    rt_size_t (*read)(void *bus, rt_uint32_t addr, rt_uint32_t reg, void *data, rt_uint32_t size);
+    rt_size_t (*write)(void *bus, rt_uint32_t addr, rt_uint32_t reg, void *data, rt_uint32_t size);
+    rt_bool_t (*uninit)(void *bus);
+};
+
+struct rt_mdio_bus
+{
+    void *hw_obj;
+    char *name;
+    struct rt_mdio_bus_ops *ops;
+};
+
+typedef struct rt_mdio_bus rt_mdio_t;
+
+/* Defines the PHY link speed. This is align with the speed for MAC. */
+#define PHY_SPEED_10M   0U     /* PHY 10M speed. */
+#define PHY_SPEED_100M  1U     /* PHY 100M speed. */
+#define PHY_SPEED_1000M 2U     /* PHY 1000M speed. */
+
+/* Defines the PHY link duplex. */
+#define PHY_HALF_DUPLEX 0U     /* PHY half duplex. */
+#define PHY_FULL_DUPLEX 1U     /* PHY full duplex. */
+
+/*! @brief Defines the PHY loopback mode. */
+#define PHY_LOCAL_LOOP  0U     /* PHY local loopback. */
+#define PHY_REMOTE_LOOP 1U     /* PHY remote loopback. */
+
+#define PHY_STATUS_OK      0U
+#define PHY_STATUS_FAIL    1U
+#define PHY_STATUS_TIMEOUT 2U
+
+typedef struct rt_phy_msg
+{
+    rt_uint32_t reg;
+    rt_uint32_t value;
+}rt_phy_msg_t;
+
+typedef struct rt_phy_device
+{
+    struct rt_device parent;
+    struct rt_mdio_bus *bus;
+    rt_uint32_t addr;
+    struct rt_phy_ops *ops;
+}rt_phy_t;
+
+typedef rt_int32_t rt_phy_status;
+
+struct rt_phy_ops
+{
+    rt_phy_status (*init)(void *object, rt_uint32_t phy_addr, rt_uint32_t src_clock_hz);
+    rt_phy_status (*read)(rt_phy_t *phy, rt_uint32_t reg, rt_uint32_t *data);
+    rt_phy_status (*write)(rt_phy_t *phy, rt_uint32_t reg, rt_uint32_t data);
+    rt_phy_status (*loopback)(rt_phy_t *phy, rt_uint32_t mode, rt_uint32_t speed, rt_bool_t enable);
+    rt_phy_status (*get_link_status)(rt_phy_t *phy, rt_bool_t *status);
+    rt_phy_status (*get_link_speed_duplex)(rt_phy_t *phy, rt_uint32_t *speed, rt_uint32_t *duplex);
+};
+
+rt_err_t rt_hw_phy_register(struct rt_phy_device *phy, const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
 #endif
--- components/drivers/phy/phy.c
+++ components/drivers/phy/phy.c
@@ -6,8 +6,10 @@
  * Change Logs:
  * Date           Author       Notes
  * 2020-09-27     wangqiang   first version
- * 2024-10-08     zhujiale    add phy bus && mii bus support
+ * 2024-10-08     zhujiale    add phy v2.0
  */
+#include <stddef.h>
+#include <rthw.h>
 #include <drivers/phy.h>
 #include <drivers/core/bus.h>
 #define DBG_TAG "rtdm.phy"
@@ -15,6 +17,67 @@
 #include <rtdbg.h>
 #include <net/ethernet.h>
 
+#ifdef RT_USING_PHY
+
+static rt_ssize_t phy_device_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t count)
+{
+    struct rt_phy_device *phy = (struct rt_phy_device *)dev->user_data;
+    struct rt_phy_msg *msg = (struct rt_phy_msg *)buffer;
+    return phy->bus->ops->read(phy->bus, phy->addr, msg->reg, &(msg->value), 4);
+}
+static rt_ssize_t phy_device_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t count)
+{
+    struct rt_phy_device *phy = (struct rt_phy_device *)dev->user_data;
+    struct rt_phy_msg *msg = (struct rt_phy_msg *)buffer;
+    return phy->bus->ops->write(phy->bus, phy->addr, msg->reg, &(msg->value), 4);
+}
+
+#ifdef RT_USING_DEVICE_OPS
+const static struct rt_device_ops phy_ops =
+{
+    RT_NULL,
+    RT_NULL,
+    RT_NULL,
+    phy_device_read,
+    phy_device_write,
+    RT_NULL,
+};
+#endif
+
+/*
+* phy device register
+*/
+rt_err_t rt_hw_phy_register(struct rt_phy_device *phy, const char *name)
+{
+    rt_err_t ret;
+    struct rt_device *device;
+
+    device = &(phy->parent);
+
+    device->type = RT_Device_Class_PHY;
+    device->rx_indicate = RT_NULL;
+    device->tx_complete = RT_NULL;
+
+#ifdef RT_USING_DEVICE_OPS
+    device->ops = &phy_ops;
+#else
+    device->init = NULL;
+    device->open = NULL;
+    device->close = NULL;
+    device->read = phy_device_read;
+    device->write = phy_device_write;
+    device->control = NULL;
+#endif
+    device->user_data = phy;
+
+    /* register a character device */
+    ret = rt_device_register(device, name, RT_DEVICE_FLAG_RDWR);
+
+    return ret;
+}
+#endif
+
+#ifdef RT_USING_PHY_V2
 int rt_phy_set_supported(struct rt_phy_device *phydev, rt_uint32_t max_speed)
 {
     phydev->supported &= RT_PHY_DEFAULT_FEATURES;
@@ -512,3 +575,4 @@ static int rt_phy_bus_init(void)
     return 0;
 }
 INIT_CORE_EXPORT(rt_phy_bus_init);
+#endif
