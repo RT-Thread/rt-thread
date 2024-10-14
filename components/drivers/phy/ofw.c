6b395bba784b79df6edda52e0ad6eab3b6af706e
zhujiale <945386260@qq.com>
2024-10-14 10:34:05 +0800

    cahnge ofw

--- components/drivers/phy/ofw.c
+++ components/drivers/phy/ofw.c
@@ -9,6 +9,7 @@
  */
 #include <rtthread.h>
 #include <drivers/phy.h>
+#include <stdio.h>
 #define DBG_TAG "rtdm.phy"
 #define DBG_LVL DBG_INFO
 #include <rtdbg.h>
@@ -104,48 +105,28 @@ rt_err_t rt_ofw_get_mac_addr(struct rt_ofw_node *np, rt_uint8_t *addr)
     return -RT_ERROR;
 }
 
-rt_err_t rt_ofw_get_phyid(struct rt_ofw_node *np,rt_uint16_t *vendor,rt_uint16_t *device)
+rt_err_t rt_ofw_get_phyid(struct rt_ofw_node *np,rt_uint32_t *id)
 {
-    const char *phy_id,*end;
-    rt_ssize_t len;
+    const char *phy_id;
+    unsigned int upper, lower;
+    int ret;
 
-    phy_id = rt_ofw_prop_read_raw(np,"compatible",&len);
-    if (!phy_id)
+    ret = rt_ofw_prop_read_string(np,"compatible",&phy_id);
+    if (ret)
+        return ret;
+
+    ret = sscanf(phy_id,"ethernet-phy-id%4x.%4x",&upper, &lower);
+    if(ret != 2)
         return -RT_ERROR;
-
-    end = phy_id + len;
-    while (phy_id < end)
-    {
-        len = strlen(phy_id);
-
-        if (len >= strlen("ethernet-phy-idVVVV.DDDD"))
-        {
-            char *s = strstr(phy_id, "ethernet-phy-id");
-
-            /*
-             * check if the string is something like
-             * ethernet-phy-idVVVV.DDDD
-             */
-            if (s && s[19] == '.')
-            {
-                s += strlen("ethernet-phy-id");
-                rt_memcpy(vendor,s,16);
-                s += 5;
-                rt_memcpy(device,s,16);
-
-                return 0;
-            }
-        }
-        phy_id += (len + 1);
-    }
-    return -RT_ERROR;
+    
+    *id = ((upper & 0xffff) << 16) | (lower & 0xffff);
+    return RT_EOK;
 
 }
 struct rt_phy_device *rt_ofw_create_phy(struct mii_bus *bus,struct rt_ofw_node *np,int phyaddr)
 {
     struct rt_phy_device *dev = RT_NULL;
     struct rt_ofw_node *phy_node;
-    rt_uint16_t vendor, device;
     int ret;
     rt_uint32_t id = 0xffff;
 
@@ -156,13 +137,15 @@ struct rt_phy_device *rt_ofw_create_phy(struct mii_bus *bus,struct rt_ofw_node *
         return RT_NULL;
     }
 
-    ret = rt_ofw_get_phyid(np, &vendor, &device);
+    ret = rt_ofw_get_phyid(np, &id);
     if (ret)
     {
         LOG_D("Failed to read eth PHY id, err: %d\n", ret);
         return RT_NULL;
     }
-    id =  vendor << 16 | device;
+
+    LOG_D("Found a PHY id: 0x%x\n", id);
+    
     dev = rt_phy_device_create(bus, phyaddr, id, RT_FALSE);
 
     if(dev)
