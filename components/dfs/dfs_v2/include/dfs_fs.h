172bc0d4128665a9312aaba8cd6b1fe89efcc199
Shell <smokewood@qq.com>
2024-11-13 15:37:23 +0800

    feat: add support for dfs remount functionality
    This patch introduces a remount feature for the DFS, allowing for the
    modification of mount parameters without unmounting the filesystem,
    the remount functionality helps modify certain mount flags (like `MS_RDONLY`) without
    requiring an unmount, providing more control over mounted filesystems in the system.
    
    The updates is essential for user space init proc to cleanup the runtime
    resource, ensuring clean handling of cached data and enhancing system
    robustness during power down processing.
    
    Changes:
    - Defined new constants for remount flags in `dfs_fs.h`.
    - Added the `dfs_remount()` function in `dfs_fs.c` to handle remount operations.
    - Introduced a check for unsupported flags and handle error conditions such as invalid paths
      or non-directory targets.
    - Updated the `dfs_mnt` structure in `dfs_mnt.h` to include a read-only flag (`MNT_RDONLY`).
    - The `dfs_remount()` function allows changing the read-only status of a mounted filesystem.
    - Added `MNT_LAZY_UMNT` and `MNT_RDONLY` flags to `dfs_mnt` structure.
    - Introduced `dfs_mnt_setflags` function for dynamic flag management.
    - Updated `dfs_remount` to utilize `dfs_mnt_setflags` for flag setting.
    - Enhanced unmount operations with `dfs_mnt_umount_iter` and lazy unmounting.
    - Added `dfs_pcache_clean` to handle cache cleanup for read-only mounts.
    - Improved error reporting in `dfs_umount` for better user feedback.
    - Refactored `sys_mount` to streamline parameter handling and support remounts.
    - Introduced `_cp_from_usr_string` helper for user-space string operations.
    - Updated internal APIs to ensure consistency in reference count management.
    
    Signed-off-by: Shell <smokewood@qq.com>

--- components/dfs/dfs_v2/include/dfs_fs.h
+++ components/dfs/dfs_v2/include/dfs_fs.h
@@ -20,6 +20,38 @@ extern "C"
 {
 #endif
 
+#define MS_RDONLY      1
+#define MS_NOSUID      2
+#define MS_NODEV       4
+#define MS_NOEXEC      8
+#define MS_SYNCHRONOUS 16
+#define MS_REMOUNT     32
+#define MS_MANDLOCK    64
+#define MS_DIRSYNC     128
+#define MS_NOATIME     1024
+#define MS_NODIRATIME  2048
+#define MS_BIND        4096
+#define MS_MOVE        8192
+#define MS_REC         16384
+#define MS_SILENT      32768
+#define MS_POSIXACL    (1<<16)
+#define MS_UNBINDABLE  (1<<17)
+#define MS_PRIVATE     (1<<18)
+#define MS_SLAVE       (1<<19)
+#define MS_SHARED      (1<<20)
+#define MS_RELATIME    (1<<21)
+#define MS_KERNMOUNT   (1<<22)
+#define MS_I_VERSION   (1<<23)
+#define MS_STRICTATIME (1<<24)
+#define MS_LAZYTIME    (1<<25)
+#define MS_NOREMOTELOCK (1<<27)
+#define MS_NOSEC       (1<<28)
+#define MS_BORN        (1<<29)
+#define MS_ACTIVE      (1<<30)
+#define MS_NOUSER      (1U<<31)
+
+#define MS_RMT_MASK (MS_RDONLY|MS_SYNCHRONOUS|MS_MANDLOCK|MS_I_VERSION|MS_LAZYTIME)
+
 /* file system partition table */
 struct dfs_partition
 {
@@ -87,6 +119,7 @@ int dfs_unregister(struct dfs_filesystem_type *fs);
 int dfs_register(struct dfs_filesystem_type *fs);
 const char *dfs_filesystem_get_mounted_path(struct rt_device *device);
 
+int dfs_remount(const char *path, rt_ubase_t flags, void *data);
 int dfs_mount(const char *device_name,
             const char *path,
             const char *filesystemtype,
