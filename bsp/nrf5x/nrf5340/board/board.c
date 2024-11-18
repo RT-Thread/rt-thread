52801152565ca39451b55d77917df86cd94c5034
Supper Thomas <78900636@qq.com>
2024-11-18 11:00:51 +0800

    fix board.c format

--- bsp/nrf5x/nrf5340/board/board.c
+++ bsp/nrf5x/nrf5340/board/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2022, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -60,22 +60,22 @@ void rt_hw_board_init(void)
     rt_system_heap_init((void *)HEAP_BEGIN, (void *)HEAP_END);
 #endif
 
-	
+
 #ifdef RT_USING_SERIAL
     rt_hw_uart_init();
 #endif
-	
+
 #ifdef SEGGER_RTT_ENABLE
 extern int rt_hw_jlink_rtt_init(void);
-		rt_hw_jlink_rtt_init();
-		rt_console_set_device("jlinkRtt");
+        rt_hw_jlink_rtt_init();
+        rt_console_set_device("jlinkRtt");
 #else
-	#if defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
-			rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
-	#endif
+    #if defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
+            rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+    #endif
 #endif
 
-	
+
 #ifdef RT_USING_COMPONENTS_INIT
     rt_components_board_init();
 #endif
