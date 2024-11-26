53865d568cf52ef7605b3a34c4406bf3bb04f742
hywing <2112093745@qq.com>
2024-11-26 15:34:16 +0800

    [bsp][nxp][mcxa153] add hardware timer driver

---
+++ bsp/nxp/mcx/mcxa/Libraries/drivers/drv_hwtimer.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2024 RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+* Change Logs:
+* Date           Author       Notes
+* 2024-11-26     hywing       the first version.
+*/
+
+#ifndef DRV_HWTIMER_H__
+#define DRV_HWTIMER_H__
+
+int rt_hw_hwtimer_init(void);
+
+#endif /* __DRV_HWTIMER_H__ */
+
