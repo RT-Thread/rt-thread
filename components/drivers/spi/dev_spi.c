aa2e3060c746bd9eb0e30220b84f88018e9ccfe5
GuEe-GUI <2991707448@qq.com>
2024-11-27 11:08:08 +0800

    [DM/SPI] Support DM mode in SPI
    Signed-off-by: GuEe-GUI <2991707448@qq.com>

--- components/drivers/spi/dev_spi.c
+++ components/drivers/spi/dev_spi.c
@@ -10,6 +10,14 @@
 #include <rtthread.h>
 #include "drivers/dev_spi.h"
 
+#define DBG_TAG     "spi.dev"
+#define DBG_LVL     DBG_INFO
+#include <rtdbg.h>
+
+#ifdef RT_USING_DM
+#include "dev_spi_dm.h"
+#endif
+
 /* SPI bus device interface, compatible with RT-Thread 0.3.x/1.0.x */
 static rt_ssize_t _spi_bus_device_read(rt_device_t dev,
                                       rt_off_t    pos,
@@ -155,3 +163,66 @@ rt_err_t rt_spidev_device_init(struct rt_spi_device *dev, const char *name)
     /* register to device manager */
     return rt_device_register(device, name, RT_DEVICE_FLAG_RDWR);
 }
+
+#ifdef RT_USING_DM
+static rt_err_t spidev_probe(struct rt_spi_device *spi_dev)
+{
+    const char *bus_name;
+    struct rt_device *dev = &spi_dev->parent;
+
+    if (spi_dev->parent.ofw_node)
+    {
+        if (rt_dm_dev_prop_index_of_string(dev, "compatible", "spidev") >= 0)
+        {
+            LOG_E("spidev is not supported in OFW");
+
+            return -RT_EINVAL;
+        }
+    }
+
+    bus_name = rt_dm_dev_get_name(&spi_dev->bus->parent);
+    rt_dm_dev_set_name(dev, "%s_%d", bus_name, spi_dev->chip_select);
+
+    return RT_EOK;
+}
+
+static const struct rt_spi_device_id spidev_ids[] =
+{
+    { .name = "dh2228fv" },
+    { .name = "ltc2488" },
+    { .name = "sx1301" },
+    { .name = "bk4" },
+    { .name = "dhcom-board" },
+    { .name = "m53cpld" },
+    { .name = "spi-petra" },
+    { .name = "spi-authenta" },
+    { .name = "em3581" },
+    { .name = "si3210" },
+    { /* sentinel */ },
+};
+
+static const struct rt_ofw_node_id spidev_ofw_ids[] =
+{
+    { .compatible = "cisco,spi-petra" },
+    { .compatible = "dh,dhcom-board" },
+    { .compatible = "lineartechnology,ltc2488" },
+    { .compatible = "lwn,bk4" },
+    { .compatible = "menlo,m53cpld" },
+    { .compatible = "micron,spi-authenta" },
+    { .compatible = "rohm,dh2228fv" },
+    { .compatible = "semtech,sx1301" },
+    { .compatible = "silabs,em3581" },
+    { .compatible = "silabs,si3210" },
+    { .compatible = "rockchip,spidev" },
+    { /* sentinel */ },
+};
+
+static struct rt_spi_driver spidev_driver =
+{
+    .ids = spidev_ids,
+    .ofw_ids = spidev_ofw_ids,
+
+    .probe = spidev_probe,
+};
+RT_SPI_DRIVER_EXPORT(spidev_driver);
+#endif /* RT_USING_DM */
