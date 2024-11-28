9386411d13d8f9085eebc1ac7515f008685ab447
Shell <smokewood@qq.com>
2024-11-28 17:53:33 +0800

    feat: mm: added affinity pages allocator
    This patch introduces a tagged pages allocator to address the existing problems
    of page aliasing on specific platforms and the requirement of page coloring.
    It implements an affinity-id aware page manager by separating the runtime page
    list into two types: a normal single linked-list and a multi-dimensional affinity-list.
    
    Changes:
    - Introduced tagged pages allocator and managing algorithm for affinity pages list
    - Modified components to support affinity-id list management
    - Updated page allocation and freeing functions to handle tagged pages
    - Added configuration options for page affinity block size and debugging
    - Modified mmap and elf loading to respect affinity settings
    - Enhanced page list management to support multi-dimensional affinity-list
    
    Signed-off-by: Shell <smokewood@qq.com>

--- components/mm/mm_page.h
+++ components/mm/mm_page.h
@@ -33,6 +33,9 @@
 
 #define PAGE_ANY_AVAILABLE 0x1ul
 
+#define RT_PAGE_PICK_AFFID(ptr) \
+    ((((long)ptr) & (RT_PAGE_AFFINITY_BLOCK_SIZE - 1)) / ARCH_PAGE_SIZE)
+
 #ifdef RT_DEBUGING_PAGE_LEAK
 #define DEBUG_FIELD struct {    \
     /* trace list */            \
@@ -57,6 +60,7 @@ DEF_PAGE_T(
 
 #undef GET_FLOOR
 #undef DEF_PAGE_T
+#undef DEBUG_FIELD
 
 typedef struct tag_region
 {
@@ -77,13 +81,17 @@ void *rt_pages_alloc(rt_uint32_t size_bits);
 
 void *rt_pages_alloc_ext(rt_uint32_t size_bits, size_t flags);
 
+void *rt_pages_alloc_tagged(rt_uint32_t size_bits, long tag, size_t flags);
+
+rt_bool_t rt_page_is_member(rt_base_t page_pa);
+
 void rt_page_ref_inc(void *addr, rt_uint32_t size_bits);
 
 int rt_page_ref_get(void *addr, rt_uint32_t size_bits);
 
 int rt_pages_free(void *addr, rt_uint32_t size_bits);
 
-void rt_page_list(void);
+int rt_page_list(void);
 
 rt_size_t rt_page_bits(rt_size_t size);
 
