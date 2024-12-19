1ed725dac77659de98d769a7b5f9c8f25cb494c4
yuqingli <yuqingli05@outlook.com>
2024-12-20 04:27:50 +0800

    fal_rtt.c:53:9: warning: implicit declaration of function 'memcpy'

--- components/fal/src/fal_rtt.c
+++ components/fal/src/fal_rtt.c
@@ -52,7 +52,7 @@ static rt_err_t blk_dev_control(rt_device_t dev, rt_uint8_t cmd, void *args)
             return -RT_ERROR;
         }
 
-        memcpy(geometry, &part->geometry, sizeof(struct rt_device_blk_geometry));
+        rt_memcpy(geometry, &part->geometry, sizeof(struct rt_device_blk_geometry));
     }
     else if (cmd == RT_DEVICE_CTRL_BLK_ERASE)
     {
