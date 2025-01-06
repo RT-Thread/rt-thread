50e8c4da6fb1e889f27bff81942624045c4f97dc
heyuanjie87 <sc943313837@gmail.com>
2025-01-06 14:12:01 +0800

    [bsp][qemu-virt64-riscv]修正内存大小超限制的问题

--- bsp/qemu-virt64-riscv/driver/board.h
+++ bsp/qemu-virt64-riscv/driver/board.h
@@ -23,7 +23,7 @@ extern unsigned int __bss_end;
 #define RT_HW_HEAP_BEGIN ((void *)&__bss_end)
 #define RT_HW_HEAP_END   ((void *)(RT_HW_HEAP_BEGIN + 64 * 1024 * 1024))
 #define RT_HW_PAGE_START RT_HW_HEAP_END
-#define RT_HW_PAGE_END   ((void *)(KERNEL_VADDR_START + 256 * 1024 * 1024))
+#define RT_HW_PAGE_END   ((void *)(KERNEL_VADDR_START + (256 * 1024 * 1024 - 0x200000)))
 
 void rt_hw_board_init(void);
 void rt_init_user_mem(struct rt_thread *thread, const char *name,
