d0fe59d4f829a7d2bbeb2fd2441c8c407a16e15e
hydevcode <zhjich312@126.com>
2025-01-22 02:49:25 +0800

    [scons] ci.attachconfig.yml is used in combination with scons

--- tools/options.py
+++ tools/options.py
@@ -141,3 +141,10 @@ def AddOptions():
                 action = 'store_true',
                 default = False,
                 help = 'make compile_commands.json')
+    AddOption('--attach',
+                dest = 'attach',
+                type = 'string',
+                help = 'View attachconfig or add attach to.config.'+\
+                'e.g. scons --attach=? View all attachconfig for the current bsp.'+\
+                ' or scons --attach=component.cherryusb_cdc Set option component.cherryusb_cdc inside attachconfig to.config.'+\
+                ' or scons --attach=default Restore.config and rtconfig to before attch was set.')
