5b6944f7fa1d60b64a3f1275da94df1c1a15944a
Chen Wang <unicorn_wang@outlook.com>
2025-02-19 21:21:04 +0800

    doxygen: More strict checks on build results (#10007)
    Github CI doc_doxygen is very strict in checking the
    build results, and will output failure even if there
    are warnings.
    
    In order to match CI's checks, local build scripts
    are also checked more strictly to detect potential
    errors and even warnings as early as possible.
    
    Signed-off-by: Chen Wang <unicorn_wang@outlook.com>

--- documentation/run.sh
+++ documentation/run.sh
@@ -3,6 +3,12 @@
 rm -rf ./html
 
 doxygen
+if [ $? -ne 0 ]; then
+    echo ""
+    echo "OOPS: Something error/warning occurred during Doxygen building, please check it out!"
+    echo ""
+    exit 1
+fi
 
 pushd html
 python3 -m http.server
