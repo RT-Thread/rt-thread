3c8b23576fa81576bf72d5ef07ee32859f631c88
Chen Wang <unicorn_wang@outlook.com>
2025-03-06 11:00:09 +0800

    doxygen: group examples in subpages
    Doxygen examples are grouped on different pages so that you can
    easily link to pages of related types in the documentation.
    For example, macro examples have their own page, and function
    examples have their own page, which correspond to their own code
    examples, such as "macro.h" or "function.h".
    
    Signed-off-by: Chen Wang <unicorn_wang@outlook.com>

--- documentation/0.doxygen/example/include/union.h
+++ documentation/0.doxygen/example/include/union.h
@@ -14,14 +14,22 @@
  *
  * See
  * <a href="https://github.com/RT-Thread/rt-thread/blob/master/documentation/0.doxygen/example/include/union.h">documentation/0.doxygen/example/include/union.h</a>
- * for example.
+ * for code example.
+ *
+ * See
+ * <a href="./group__group__doxygen__example__union.html">Doxygen Example of Union</a>
+ * for html output.
  */
 
 /**
- * @addtogroup group_doxygen_example
+ * @defgroup group_doxygen_example_union Doxygen Example of Union
+ *
+ * @ingroup group_doxygen_example
+ *
+ * @brief Doxygen Example of Union.
+ *
+ * @{
  */
-
-/** @{ */
 
 /**
  * @brief Brief description of this union
