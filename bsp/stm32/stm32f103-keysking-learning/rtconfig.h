cc9e18cd2274da72865868151a0cc66fcfb1e8c2
qianjiuyuan <149981328+heyQianJiu@users.noreply.github.com>
2025-05-28 17:04:40 +0800

    [bsp]关闭config的PWM和Encoder（通过select开启的关闭不会默认取消）

--- bsp/stm32/stm32f103-keysking-learning/rtconfig.h
+++ bsp/stm32/stm32f103-keysking-learning/rtconfig.h
@@ -148,8 +148,6 @@
 #define RT_USING_SERIAL_V1
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
-#define RT_USING_PWM
-#define RT_USING_PULSE_ENCODER
 #define RT_USING_PIN
 /* end of Device Drivers */
 
