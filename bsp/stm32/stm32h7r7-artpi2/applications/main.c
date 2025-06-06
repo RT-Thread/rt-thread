53f9986aaa260a7db2ce3cb932e0361390351ff5
stranding <45762837+StrandingNeko@users.noreply.github.com>
2025-06-06 21:38:13 +0800

    bsp: artpi2: remove unused files.
    Signed-off-by: stranding <45762837+StrandingNeko@users.noreply.github.com>

--- bsp/stm32/stm32h7r7-artpi2/applications/main.c
+++ bsp/stm32/stm32h7r7-artpi2/applications/main.c
@@ -8,34 +8,28 @@
  * 2021-03-17     supperthomas first version
  */
 
+#include "drivers/dev_pin.h"
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <board.h>
 
 /* defined the LED0 pin: PO1 */
-#define LED_PIN 	GET_PIN(O, 1)
+#define LED_RED_PIN 	GET_PIN(O, 1)
+#define LED_BLUE_PIN 	GET_PIN(O, 5)
 
 int main(void)
 {
-    rt_uint32_t count = 1;
+    rt_pin_mode(LED_RED_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_BLUE_PIN, PIN_MODE_OUTPUT);
 
-    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+    rt_pin_write(LED_BLUE_PIN, PIN_HIGH);
 
-    while(count++)
+    while(1)
     {
         rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_HIGH);
+        rt_pin_write(LED_RED_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_LOW);
+        rt_pin_write(LED_RED_PIN, PIN_LOW);
     }
     return RT_EOK;
 }
-
-#include "stm32h7rsxx.h"
-static int vtor_config(void)
-{
-    /* Vector Table Relocation in Internal XSPI2_BASE */
-    SCB->VTOR = XSPI2_BASE;
-    return 0;
-}
-INIT_BOARD_EXPORT(vtor_config);
