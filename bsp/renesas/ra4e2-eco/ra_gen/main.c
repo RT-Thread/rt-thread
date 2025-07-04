af2e0417cfb07feecc1d8b1d9cd815de5dc65814
Erik <1043490933@qq.com>
2025-07-04 14:38:21 +0800

    [bsp/renesas]: add ra4e2-eco

---
+++ bsp/renesas/ra4e2-eco/ra_gen/main.c
@@ -0,0 +1,8 @@
+/* generated main source file - do not edit */
+#include "hal_data.h"
+            int main(void)
+            {
+              hal_entry();
+              return 0;
+            }
+
