7ab359481c8aec42f52e90076b549d1e038141f3
wanwandegit <ziruiwan@hust.edu.cn>
2025-08-09 10:17:42 +0800

    [Fix] enable GDB debugging support for VS Code integration

--- bsp/qemu-vexpress-a9/qemu.bat
+++ bsp/qemu-vexpress-a9/qemu.bat
@@ -4,4 +4,4 @@ qemu-img create -f raw sd.bin 64M
 
 :run
 qemu-system-arm --version
-qemu-system-arm -M vexpress-a9 -smp cpus=2 -kernel rtthread.bin -serial stdio -sd sd.bin
+qemu-system-arm -M vexpress-a9 -smp cpus=2 -kernel rtthread.bin -serial stdio -sd sd.bin -S -gdb tcp::1234
