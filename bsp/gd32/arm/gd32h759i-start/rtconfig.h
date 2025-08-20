a00c5e8226c7c11b226c8090af8ccecc17925813
Yaochenger <1516081466@qq.com>
2025-08-20 18:41:20 +0800

    [fix] Update gd32h759i-start config

--- bsp/gd32/arm/gd32h759i-start/rtconfig.h
+++ bsp/gd32/arm/gd32h759i-start/rtconfig.h
@@ -154,7 +154,6 @@
 #define RT_UNAMED_PIPE_NUMBER 64
 #define RT_USING_SERIAL
 #define RT_USING_SERIAL_V1
-#define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
 /* end of Device Drivers */
