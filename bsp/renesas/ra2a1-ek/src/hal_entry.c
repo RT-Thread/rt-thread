ab860f50e8a55a6012517243c5a334323a775607
kurisaw <2053731441@qq.com>
2025-08-25 10:22:49 +0800

    fix pr conflicts.

--- bsp/renesas/ra2a1-ek/src/hal_entry.c
+++ bsp/renesas/ra2a1-ek/src/hal_entry.c
@@ -22,11 +22,10 @@ void hal_entry(void)
 {
     rt_kprintf("\nHello RT-Thread!\n");
     while (1)
-    {   
+    {
         rt_pin_write(LED_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
         rt_pin_write(LED_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
 }
-
