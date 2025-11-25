cf4a5378947f177d3a77d40be7035f3c4f6f0ca5
Dailingxiang1 <3070269033@qq.com>
2025-11-25 21:06:17 +0800

    add spi flash support

--- bsp/stm32/stm32f407-micu/board/CubeMX_Config/Inc/main.h
+++ bsp/stm32/stm32f407-micu/board/CubeMX_Config/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SPI1_CS_Pin GPIO_PIN_4
+#define SPI1_CS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
