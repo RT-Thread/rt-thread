869599139a29c331a3efb3d093f4a7ce45ffadc7
Dailingxiang1 <3070269033@qq.com>
2025-11-26 11:53:25 +0800

    revise spi_flash_init.c RCC

--- bsp/stm32/stm32f407-micu/board/ports/spi_flash_init.c
+++ bsp/stm32/stm32f407-micu/board/ports/spi_flash_init.c
@@ -18,7 +18,7 @@
 
 static int rt_hw_spi_flash_init(void)
 {
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     rt_hw_spi_device_attach("spi1", "spi10", GET_PIN(A, 4));
 
     if (RT_NULL == rt_sfud_flash_probe("ZD25WQ32", "spi10"))
