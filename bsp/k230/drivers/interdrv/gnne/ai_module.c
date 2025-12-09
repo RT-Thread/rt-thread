239f371fcb6f4aaa4189cd6b5691c24853f76ea8
Chuan <fjchuanil@gmail.com>
2025-12-09 18:17:09 +0800

    [bsp][k230]:add gnne driver Requirement: The BSP for the k230 platform in the RT-Thread repository does not yet have a gnne driver. Solution: Provide gnne driver for the k230 platform in the RT-Thread repository. - Implements mutex lock mechanism for AI2D and GNNE modules. - Adds HARDLOCK_AI2D support in hardlock driver for mutual exclusion. - Implements poll operation for device status monitoring. - Updates documentation in bsp/README.md. Signed-off-by: ChuanN-sudo <fjchuanil@gmail.com>

--- bsp/k230/drivers/interdrv/gnne/ai_module.c
+++ bsp/k230/drivers/interdrv/gnne/ai_module.c
@@ -23,37 +23,27 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef DRV_HARDLOCK_H__
-#define DRV_HARDLOCK_H__
-#include <rtdef.h>
+/*
+ * NOTE: Currently untested - nncase cannot run due to missing mmz library.
+ * The mmz library depends on MPP driver, which relies on deprecated RT-Thread
+ * kernel APIs (rt_proc_entry_create, rt_dma_chan_request, etc.) that are no
+ * longer available in the mainline kernel. Awaiting resolution.
+ */
 
-typedef enum k230_hardlock_type
+/*
+ * Copyright (c) 2006-2025 RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <rtthread.h>
+
+extern int gnne_device_init(void);
+extern int ai_2d_device_init(void);
+
+int ai_module_init(void)
 {
-    HARDLOCK_GPIO = 0,
-    HARDLOCK_KPU = 1,
-    HARDLOCK_TS = 2,
-    HARDLOCK_DISP = 3,
-    HARDLOCK_DISP_CPU0 = 4,
-    HARDLOCK_DISP_CPU1 = 5,
-    HARDLOCK_HASH = 6,
-    HARDLOCK_AES = 7,
-    HARDLOCK_SM4 = 8,
-    HARDLOCK_PDMA = 9,
-    HARDLOCK_AI2D = 10,
-    HARDLOCK_MAX = 128
-} hardlock_type;
-
-#ifdef BSP_USING_HARDLOCK
-extern int kd_hardlock_lock(hardlock_type num);
-extern void kd_hardlock_unlock(hardlock_type num);
-extern int kd_request_lock(hardlock_type num);
-#else
-rt_inline int kd_hardlock_lock(hardlock_type num)
-{ return 0; }
-rt_inline void kd_hardlock_unlock(hardlock_type num)
-{}
-rt_inline int kd_request_lock(hardlock_type num)
-{ return num; }
-#endif
-
-#endif
+    gnne_device_init();
+    ai_2d_device_init();
+}
+INIT_COMPONENT_EXPORT(ai_module_init);
