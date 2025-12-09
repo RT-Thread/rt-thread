77683e8c8c1e7d11c5b0d46a15a42683f8557bc3
GuEe-GUI <2991707448@qq.com>
2025-12-09 17:42:32 +0800

    [dm][core] Set default CLK config for platform device probe
    Signed-off-by: GuEe-GUI <2991707448@qq.com>

--- components/drivers/core/platform.c
+++ components/drivers/core/platform.c
@@ -15,6 +15,9 @@
 #define DBG_LVL DBG_INFO
 #include <rtdbg.h>
 
+#ifdef RT_USING_CLK
+#include <drivers/clk.h>
+#endif
 #include <drivers/platform.h>
 #include <drivers/core/bus.h>
 #include <drivers/core/dm.h>
@@ -117,6 +120,13 @@ static rt_err_t platform_probe(rt_device_t dev)
     struct rt_platform_device *pdev = rt_container_of(dev, struct rt_platform_device, parent);
 #ifdef RT_USING_OFW
     struct rt_ofw_node *np = dev->ofw_node;
+#endif
+
+#ifdef RT_USING_CLK
+    if ((err = rt_ofw_clk_set_defaults(dev->ofw_node)))
+    {
+        return err;
+    }
 #endif
 
     err = rt_dm_power_domain_attach(dev, RT_TRUE);
