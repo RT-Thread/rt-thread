053f9bcdda2a11ccdc168e885bf4f04a034d947f
AngryProton <38059101+AngryProton@users.noreply.github.com>
2025-12-29 14:44:01 +0800

    [fix][blk]解决块设备写入不对齐数据失败的问题

--- components/drivers/block/blk_dfs.c
+++ components/drivers/block/blk_dfs.c
@@ -175,13 +175,17 @@ _goon:
     if ((count - wsize) / bytes_per_sector != 0)
     {
         res = rt_device_write(dev, blk_pos, buf + wsize, (count - wsize) / bytes_per_sector);
-        wsize += res * bytes_per_sector;
-        blk_pos += res;
 
         if (res != (count - wsize) / bytes_per_sector)
         {
+            wsize += res * bytes_per_sector;
             *pos += wsize;
             return wsize;
+        }
+        else
+        {
+            wsize += res * bytes_per_sector;
+            blk_pos += res;
         }
     }
 
