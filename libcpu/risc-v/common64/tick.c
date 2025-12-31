774c8adff31c9bda5258f58d5e03fdb60feb9757
bernard <bernard.xiong@gmail.com>
2025-12-31 17:39:23 +0800

    [risc-v] Use runtime clock timer frequency for tick and delays

--- libcpu/risc-v/common64/tick.c
+++ libcpu/risc-v/common64/tick.c
@@ -20,6 +20,12 @@
 #endif
 
 static volatile unsigned long tick_cycles = 0;
+static rt_uint64_t _clock_timer_freq = 0;
+
+rt_weak rt_uint64_t rt_hw_get_clock_timer_freq(void)
+{
+    return CLOCK_TIMER_FREQ;
+}
 
 static rt_uint64_t _riscv_read_time(void)
 {
@@ -36,14 +42,16 @@ int tick_isr(void)
     return 0;
 }
 
-/* BSP should config clockbase frequency */
-RT_STATIC_ASSERT(defined_clockbase_freq, CLOCK_TIMER_FREQ != 0);
-
 /* Sets and enable the timer interrupt */
 int rt_hw_tick_init(void)
 {
+    rt_uint64_t freq = rt_hw_get_clock_timer_freq();
+    RT_ASSERT(freq != 0);
+
+    _clock_timer_freq = freq;
+
     /* calculate the tick cycles */
-    tick_cycles = CLOCK_TIMER_FREQ / RT_TICK_PER_SECOND;
+    tick_cycles = freq / RT_TICK_PER_SECOND;
 
     /* Clear the Supervisor-Timer bit in SIE */
     clear_csr(sie, SIP_STIP);
@@ -70,9 +78,12 @@ void rt_hw_us_delay(rt_uint32_t us)
     unsigned long start_time;
     unsigned long end_time;
     unsigned long run_time;
+    rt_uint64_t freq;
 
     start_time = _riscv_read_time();
-    end_time = start_time + us * (CLOCK_TIMER_FREQ / 1000000);
+    freq = _clock_timer_freq ? _clock_timer_freq : rt_hw_get_clock_timer_freq();
+    RT_ASSERT(freq != 0);
+    end_time = start_time + (rt_uint64_t)us * freq / 1000000ULL;
     do
     {
         run_time = _riscv_read_time();
