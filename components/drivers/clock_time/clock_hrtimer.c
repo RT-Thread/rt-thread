55a9dc503fc1c14cf80a95c2a24084476cc81bbd
bernard <bernard.xiong@gmail.com>
2026-01-30 06:12:03 +0800

    [clock_time] Fix hrtimer wrap handling

--- components/drivers/clock_time/clock_hrtimer.c
+++ components/drivers/clock_time/clock_hrtimer.c
@@ -18,6 +18,8 @@
 #define DBG_LEVEL                      DBG_INFO
 #include <rtdbg.h>
 
+#define CLOCK_TIME_NSEC_PER_SEC (1000000000ULL)
+
 #ifdef ARCH_CPU_64BIT
 #define _HRTIMER_MAX_CNT UINT64_MAX
 #else
@@ -37,6 +39,16 @@ static unsigned long _clock_time_get_cnt(void)
     return rt_clock_time_get_counter();
 }
 
+static rt_inline rt_bool_t _cnt_before(unsigned long a, unsigned long b)
+{
+    return ((rt_base_t)(a - b)) < 0;
+}
+
+static rt_inline rt_bool_t _cnt_after(unsigned long a, unsigned long b)
+{
+    return _cnt_before(b, a);
+}
+
 rt_weak rt_uint64_t rt_clock_hrtimer_getres(void)
 {
     return rt_clock_time_get_event_res_scaled();
@@ -47,10 +59,36 @@ rt_weak unsigned long rt_clock_hrtimer_getfrq(void)
     return (unsigned long)rt_clock_time_get_event_freq();
 }
 
+static rt_tick_t _hrtimer_cnt_to_tick(unsigned long cnt)
+{
+    rt_uint64_t res = rt_clock_hrtimer_getres();
+    rt_uint64_t ns;
+
+    if (res == 0)
+    {
+        return 0;
+    }
+
+    ns = ((rt_uint64_t)cnt * res) / RT_CLOCK_TIME_RESMUL;
+    if (ns == 0)
+    {
+        return 1;
+    }
+
+    ns = (ns * RT_TICK_PER_SECOND + CLOCK_TIME_NSEC_PER_SEC - 1) / CLOCK_TIME_NSEC_PER_SEC;
+    if (ns == 0)
+    {
+        return 1;
+    }
+
+    return (rt_tick_t)ns;
+}
+
 rt_weak rt_err_t rt_clock_hrtimer_settimeout(unsigned long cnt)
 {
     static rt_timer_t timer = RT_NULL;
     static struct rt_timer _sh_rtimer;
+    rt_tick_t tick;
 
     RT_ASSERT(cnt > 0);
 
@@ -59,15 +97,20 @@ rt_weak rt_err_t rt_clock_hrtimer_settimeout(unsigned long cnt)
         return RT_EOK;
     }
 
+    tick = _hrtimer_cnt_to_tick(cnt);
+    if (tick == 0)
+    {
+        return -RT_ERROR;
+    }
+
     if (timer == RT_NULL)
     {
         timer = &_sh_rtimer;
         rt_timer_init(timer, "shrtimer", (void (*)(void *))rt_clock_hrtimer_process, RT_NULL,
-                      cnt, RT_TIMER_FLAG_ONE_SHOT);
+                      tick, RT_TIMER_FLAG_ONE_SHOT);
     }
     else
     {
-        rt_tick_t tick = cnt;
         rt_timer_control(timer, RT_TIMER_CTRL_SET_TIME, &tick);
         rt_timer_control(timer, RT_TIMER_CTRL_SET_PARM, RT_NULL);
     }
@@ -118,7 +161,7 @@ static void _insert_timer_to_list_locked(rt_clock_hrtimer_t timer)
 
     rt_list_for_each_entry(iter, &_timer_list, node)
     {
-        if (iter->timeout_cnt > timer->timeout_cnt)
+        if (_cnt_before(timer->timeout_cnt, iter->timeout_cnt))
         {
             break;
         }
@@ -132,11 +175,14 @@ static void _hrtimer_process_locked(void)
 {
     rt_clock_hrtimer_t timer;
 
-    for (timer = _first_hrtimer();
-         (timer != RT_NULL) && (timer->timeout_cnt <= _clock_time_get_cnt());
-         timer = _first_hrtimer())
+    while ((timer = _first_hrtimer()) != RT_NULL)
     {
         unsigned long now = _clock_time_get_cnt();
+
+        if (_cnt_before(now, timer->timeout_cnt))
+        {
+            break;
+        }
 
         rt_list_remove(&(timer->node));
 
@@ -303,7 +349,14 @@ rt_err_t rt_clock_hrtimer_control(rt_clock_hrtimer_t timer, int cmd, void *arg)
         *(unsigned long *)arg = timer->timeout_cnt;
         break;
     case RT_TIMER_CTRL_GET_FUNC:
-        arg = (void *)timer->timeout_func;
+        if (arg == RT_NULL)
+        {
+            result = -RT_EEMPTY;
+        }
+        else
+        {
+            *(void **)arg = (void *)timer->timeout_func;
+        }
         break;
 
     case RT_TIMER_CTRL_SET_FUNC:
