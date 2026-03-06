e953366d1499add6f42fb3048df29a1c11d21f15
GuEe-GUI <2991707448@qq.com>
2026-03-06 21:10:56 +0800

    [bsp][zynqmp-a53-dfzu2eg] enable full dm
    This is a builtin fdt example BSP
    
    Signed-off-by: GuEe-GUI <2991707448@qq.com>

--- bsp/zynqmp-a53-dfzu2eg/drivers/board.h
+++ bsp/zynqmp-a53-dfzu2eg/drivers/board.h
@@ -11,26 +11,6 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
 
-#include <zynqmp.h>
-
-extern unsigned char __bss_end;
-
-#define HEAP_BEGIN      ((void*)&__bss_end)
-
-#ifdef RT_USING_SMART
-#define HEAP_END        ((size_t)KERNEL_VADDR_START + 64 * 1024 * 1024)
-#define PAGE_START      HEAP_END
-#define PAGE_END        ((size_t)KERNEL_VADDR_START + 128 * 1024 * 1024)
-#else
-#define KERNEL_VADDR_START 0x0
-
-#define HEAP_END        (KERNEL_VADDR_START + 64 * 1024 * 1024)
-#define PAGE_START      HEAP_END
-#define PAGE_END        ((size_t)PAGE_START + 64 * 1024 * 1024)
-#endif
-
 void rt_hw_board_init(void);
-
-int rt_hw_uart_init(void);
 
 #endif /* __BOARD_H__ */
