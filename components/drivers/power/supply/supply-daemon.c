79b364f20feb7361d545d972b261a666306ffebf
Evlers <1425295900@qq.com>
2026-03-27 16:42:12 +0800

    [drivers][power] add CLI helpers and current control support
    - add msh commands to inspect/operate power supplies and regulators
    - expose snapshot/name helpers so shells and daemons can enumerate nodes safely
    - add current support for regulator
    - relax DM-only constraints in power/regulator stacks so basic builds work without DM/OFW
    - solve the problem of enabling counting for regulator

--- components/drivers/power/supply/supply-daemon.c
+++ components/drivers/power/supply/supply-daemon.c
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2023-02-25     GuEe-GUI     the first version
+ * 2026-03-27     Evlers       support builds without DM names and improve logging
  */
 
 #include <rtdevice.h>
@@ -20,6 +21,15 @@ static rt_uint8_t raw_pm_run_mode = PM_RUN_MODE_MAX;
 static rt_uint8_t raw_pm_sleep_mode = PM_RUN_MODE_MAX;
 static rt_uint8_t last_pm_sleep_mode;
 #endif
+
+static rt_inline const char *power_supply_dev_name(struct rt_device *dev)
+{
+#ifdef RT_USING_DM
+    return rt_dm_dev_get_name(dev);
+#else
+    return dev ? dev->parent.name : "<no-dev>";
+#endif
+}
 
 static rt_err_t daemon_power_supply_notify(struct rt_power_supply_notifier *notifier,
         struct rt_power_supply *psy)
@@ -66,11 +76,11 @@ _capacity_check:
         {
             if (full_power)
             {
-                LOG_I("%s: Power is full", rt_dm_dev_get_name(psy->dev));
+                LOG_I("%s: Power is full", power_supply_dev_name(psy->dev));
             }
             else
             {
-                LOG_I("%s: Power is sufficient", rt_dm_dev_get_name(psy->dev));
+                LOG_I("%s: Power is sufficient", power_supply_dev_name(psy->dev));
             }
         }
     }
@@ -109,12 +119,12 @@ _capacity_check:
                 if (!rt_power_supply_get_property(psy, RT_POWER_SUPPLY_PROP_SCOPE, &propval) &&
                     propval.intval == RT_POWER_SUPPLY_SCOPE_SYSTEM)
                 {
-                    LOG_E("%s: Power is critical, poweroff now", rt_dm_dev_get_name(psy->dev));
+                    LOG_E("%s: Power is critical, poweroff now", power_supply_dev_name(psy->dev));
                     rt_hw_cpu_shutdown();
                 }
             } while (0);
 
-            LOG_E("%s: Power is critical", rt_dm_dev_get_name(psy->dev));
+            LOG_E("%s: Power is critical", power_supply_dev_name(psy->dev));
         }
         else if (propval.intval <= 10)
         {
@@ -136,7 +146,7 @@ _capacity_check:
             pm_sleep_mode = PM_SLEEP_MODE_LIGHT;
             rt_pm_run_enter(PM_RUN_MODE_NORMAL_SPEED);
         #endif
-            LOG_W("%s: Power is low", rt_dm_dev_get_name(psy->dev));
+            LOG_W("%s: Power is low", power_supply_dev_name(psy->dev));
         }
 
     #ifdef RT_USING_PM
