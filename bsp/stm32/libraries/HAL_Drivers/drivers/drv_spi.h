415f924c5fb08b03a9b3a5a5ef63a1a7705378b3
wdfk-prog <1425075683@qq.com>
2026-05-06 21:02:37 +0800

    feat[STM32][SPI]: add interrupt transfer scaffolding for spi driver
    introduce interrupt transfer route selection and SPI IRQ visibility macros
    unify DMA and INT completion handling with timeout-triggered abort recovery
    keep short transfers on polling and use DMA or INT only when eligible
    refine STM32 SPI init and IRQ registration paths for mixed transfer backends

--- bsp/stm32/libraries/HAL_Drivers/drivers/drv_spi.h
+++ bsp/stm32/libraries/HAL_Drivers/drivers/drv_spi.h
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2018-11-5      SummerGift   first version
+ * 2026-04-16     wdfk-prog    Add SPI interrupt transfer mode scaffolding
  */
 
 #ifndef __DRV_SPI_H__
@@ -21,6 +22,43 @@
 /* hardware bus */
 #if defined(BSP_USING_SPI1) || defined(BSP_USING_SPI2) || defined(BSP_USING_SPI3) || defined(BSP_USING_SPI4) || defined(BSP_USING_SPI5) || defined(BSP_USING_SPI6)
 #define BSP_HARDWARE_SPI
+#endif
+
+/* POLL configuration visibility */
+#if defined(BSP_SPI1_TX_USING_POLL) || defined(BSP_SPI2_TX_USING_POLL) || defined(BSP_SPI3_TX_USING_POLL) || defined(BSP_SPI4_TX_USING_POLL) || defined(BSP_SPI5_TX_USING_POLL) || defined(BSP_SPI6_TX_USING_POLL)
+#define BSP_SPI_TX_USING_POLL
+#endif
+
+#if defined(BSP_SPI1_RX_USING_POLL) || defined(BSP_SPI2_RX_USING_POLL) || defined(BSP_SPI3_RX_USING_POLL) || defined(BSP_SPI4_RX_USING_POLL) || defined(BSP_SPI5_RX_USING_POLL) || defined(BSP_SPI6_RX_USING_POLL)
+#define BSP_SPI_RX_USING_POLL
+#endif
+
+#if defined(BSP_SPI1_TX_USING_POLL) || defined(BSP_SPI1_RX_USING_POLL)
+#define BSP_SPI1_USING_POLL
+#endif
+
+#if defined(BSP_SPI2_TX_USING_POLL) || defined(BSP_SPI2_RX_USING_POLL)
+#define BSP_SPI2_USING_POLL
+#endif
+
+#if defined(BSP_SPI3_TX_USING_POLL) || defined(BSP_SPI3_RX_USING_POLL)
+#define BSP_SPI3_USING_POLL
+#endif
+
+#if defined(BSP_SPI4_TX_USING_POLL) || defined(BSP_SPI4_RX_USING_POLL)
+#define BSP_SPI4_USING_POLL
+#endif
+
+#if defined(BSP_SPI5_TX_USING_POLL) || defined(BSP_SPI5_RX_USING_POLL)
+#define BSP_SPI5_USING_POLL
+#endif
+
+#if defined(BSP_SPI6_TX_USING_POLL) || defined(BSP_SPI6_RX_USING_POLL)
+#define BSP_SPI6_USING_POLL
+#endif
+
+#if defined(BSP_SPI_TX_USING_POLL) || defined(BSP_SPI_RX_USING_POLL)
+#define BSP_SPI_USING_POLL
 #endif
 
 /* DMA */
@@ -60,7 +98,68 @@
 #define BSP_SPI_USING_DMA
 #endif
 
-#if defined(BSP_SPI_USING_DMA)
+/* INT */
+#if defined(BSP_SPI1_TX_USING_INT) || defined(BSP_SPI2_TX_USING_INT) || defined(BSP_SPI3_TX_USING_INT) || defined(BSP_SPI4_TX_USING_INT) || defined(BSP_SPI5_TX_USING_INT) || defined(BSP_SPI6_TX_USING_INT)
+#define BSP_SPI_TX_USING_INT
+#endif
+
+#if defined(BSP_SPI1_RX_USING_INT) || defined(BSP_SPI2_RX_USING_INT) || defined(BSP_SPI3_RX_USING_INT) || defined(BSP_SPI4_RX_USING_INT) || defined(BSP_SPI5_RX_USING_INT) || defined(BSP_SPI6_RX_USING_INT)
+#define BSP_SPI_RX_USING_INT
+#endif
+
+#if defined(BSP_SPI1_TX_USING_INT) || defined(BSP_SPI1_RX_USING_INT)
+#define BSP_SPI1_USING_INT
+#endif
+
+#if defined(BSP_SPI2_TX_USING_INT) || defined(BSP_SPI2_RX_USING_INT)
+#define BSP_SPI2_USING_INT
+#endif
+
+#if defined(BSP_SPI3_TX_USING_INT) || defined(BSP_SPI3_RX_USING_INT)
+#define BSP_SPI3_USING_INT
+#endif
+
+#if defined(BSP_SPI4_TX_USING_INT) || defined(BSP_SPI4_RX_USING_INT)
+#define BSP_SPI4_USING_INT
+#endif
+
+#if defined(BSP_SPI5_TX_USING_INT) || defined(BSP_SPI5_RX_USING_INT)
+#define BSP_SPI5_USING_INT
+#endif
+
+#if defined(BSP_SPI6_TX_USING_INT) || defined(BSP_SPI6_RX_USING_INT)
+#define BSP_SPI6_USING_INT
+#endif
+
+#if defined(BSP_SPI_TX_USING_INT) || defined(BSP_SPI_RX_USING_INT)
+#define BSP_SPI_USING_INT
+#endif
+
+#if defined(BSP_SPI1_USING_DMA) || defined(BSP_SPI1_USING_INT)
+#define BSP_SPI1_USING_IRQ
+#endif
+
+#if defined(BSP_SPI2_USING_DMA) || defined(BSP_SPI2_USING_INT)
+#define BSP_SPI2_USING_IRQ
+#endif
+
+#if defined(BSP_SPI3_USING_DMA) || defined(BSP_SPI3_USING_INT)
+#define BSP_SPI3_USING_IRQ
+#endif
+
+#if defined(BSP_SPI4_USING_DMA) || defined(BSP_SPI4_USING_INT)
+#define BSP_SPI4_USING_IRQ
+#endif
+
+#if defined(BSP_SPI5_USING_DMA) || defined(BSP_SPI5_USING_INT)
+#define BSP_SPI5_USING_IRQ
+#endif
+
+#if defined(BSP_SPI6_USING_DMA) || defined(BSP_SPI6_USING_INT)
+#define BSP_SPI6_USING_IRQ
+#endif
+
+#if defined(BSP_SPI_USING_DMA) || defined(BSP_SPI_USING_INT)
 #define BSP_SPI_USING_IRQ
 #endif
 
@@ -81,14 +180,12 @@ struct stm32_spi_config
     SPI_TypeDef *Instance;
     char *bus_name;
     IRQn_Type irq_type;
-#ifdef BSP_SPI_USING_DMA
 #ifdef BSP_SPI_RX_USING_DMA
     const struct stm32_dma_config *dma_rx;
 #endif /* BSP_SPI_RX_USING_DMA */
 #ifdef BSP_SPI_TX_USING_DMA
     const struct stm32_dma_config *dma_tx;
 #endif /* BSP_SPI_TX_USING_DMA */
-#endif /* BSP_SPI_USING_DMA */
 };
 
 struct stm32_spi_device
@@ -105,9 +202,11 @@ struct stm32_spi
     struct stm32_spi_config *config;
     struct rt_spi_configuration *cfg;
     struct rt_spi_bus spi_bus;
-#ifdef BSP_SPI_USING_DMA
     rt_uint16_t spi_dma_flag;
+#ifdef BSP_SPI_USING_IRQ
     struct rt_completion cpt;
+#endif /* BSP_SPI_USING_IRQ */
+#ifdef BSP_SPI_USING_DMA
     struct
     {
 #ifdef BSP_SPI_RX_USING_DMA
