a085b82e37279d80f9f33b458775180136efb7af
CYFS <2805686936@qq.com>
2026-06-03 13:19:51 +0800

    [bsp][raspberry-pico]: adjust file structure

--- bsp/raspberry-pico/tools/sdk_dist.py
+++ bsp/raspberry-pico/tools/sdk_dist.py
@@ -6,6 +6,6 @@ def dist_do_building(BSP_ROOT, dist_dir):
 
     print("=> copy raspberry-pico drivers")
     bsp_copy_files(
-        os.path.join(os.path.dirname(BSP_ROOT), 'Drivers'),
-        os.path.join(os.path.dirname(dist_dir), 'Drivers'),
+        os.path.join(os.path.dirname(BSP_ROOT), 'libraries', 'Drivers'),
+        os.path.join(os.path.dirname(dist_dir), 'libraries', 'Drivers'),
     )
