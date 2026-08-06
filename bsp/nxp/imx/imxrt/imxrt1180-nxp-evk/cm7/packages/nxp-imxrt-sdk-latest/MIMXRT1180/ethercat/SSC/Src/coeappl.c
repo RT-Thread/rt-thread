0417321d0d3d9cce85f245f826d42f5c7923c276
nxp-ran <ran.chen_1@nxp.com>
2026-08-06 17:06:59 +0800

    Remove EtherCAT-related sources, keep only SConscript.

--- bsp/nxp/imx/imxrt/imxrt1180-nxp-evk/cm7/packages/nxp-imxrt-sdk-latest/MIMXRT1180/ethercat/SSC/Src/coeappl.c
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2024 NXP
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-#error "Please read readme.txt and generate the code by the SSC tool."
--- bsp/nxp/imx/imxrt/imxrt1180-nxp-evk/cm7/packages/nxp-imxrt-sdk-latest/MIMXRT1180/ethercat/SSC/Src/ecatslv.c
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright 2024 NXP
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
