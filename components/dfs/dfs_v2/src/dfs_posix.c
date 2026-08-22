6841521a776f18a7f6e6a0919adbe01b975d3943
bernard <bernard.xiong@gmail.com>
2026-08-23 07:43:50 +0800

    [dfs][posix] Add record locks, fchmod, and tmpfs metadata
    为 DFS v2 增加 POSIX 记录锁、fchmod，以及 tmpfs 的权限和时间元数据。
    进程退出时通过 dfs_fdtable_drop_fd 释放文件描述符和记录锁。

--- components/dfs/dfs_v2/src/dfs_posix.c
+++ components/dfs/dfs_v2/src/dfs_posix.c
@@ -119,7 +119,7 @@ int openat(int dirfd, const char *path, int flag, ...)
         return -1;
     }
 
-    fullpath = (char*)path;
+    fullpath = (char *)path;
 
     if (path[0] != '/')
     {
@@ -176,7 +176,7 @@ int utimensat(int __fd, const char *__path, const struct timespec __times[2], in
         }
         else
         {
-            fullpath = (char*)__path;
+            fullpath = (char *)__path;
         }
     }
     else
@@ -291,25 +291,14 @@ RTM_EXPORT(creat);
 int close(int fd)
 {
     int result;
-    struct dfs_file *file;
 
-    file = fd_get(fd);
-    if (file == NULL)
-    {
-        rt_set_errno(-EBADF);
-
-        return -1;
-    }
-
-    result = dfs_file_close(file);
+    result = dfs_fdtable_drop_fd(dfs_fdtable_get(), fd);
     if (result < 0)
     {
         rt_set_errno(result);
 
         return -1;
     }
-
-    fd_release(fd);
 
     return 0;
 }
@@ -335,17 +324,22 @@ ssize_t read(int fd, void *buf, size_t len)
     ssize_t result;
     struct dfs_file *file;
 
-    if (buf == NULL)
-    {
-        rt_set_errno(-EBADF);
-        return -1;
-    }
-
     file = fd_get(fd);
     if (file == NULL)
     {
         rt_set_errno(-EBADF);
 
+        return -1;
+    }
+
+    if (len == 0)
+    {
+        return 0;
+    }
+
+    if (buf == NULL)
+    {
+        rt_set_errno(-EBADF);
         return -1;
     }
 
@@ -380,17 +374,22 @@ ssize_t write(int fd, const void *buf, size_t len)
     ssize_t result;
     struct dfs_file *file;
 
-    if (buf == NULL)
-    {
-        rt_set_errno(-EBADF);
-        return -1;
-    }
-
     file = fd_get(fd);
     if (file == NULL)
     {
         rt_set_errno(-EBADF);
 
+        return -1;
+    }
+
+    if (len == 0)
+    {
+        return 0;
+    }
+
+    if (buf == NULL)
+    {
+        rt_set_errno(-EBADF);
         return -1;
     }
 
@@ -581,6 +580,10 @@ int fstat(int fildes, struct stat *buf)
     if (dfs_is_mounted(file->dentry->mnt) == 0)
     {
         ret = file->dentry->mnt->fs_ops->stat(file->dentry, buf);
+        if (ret == RT_EOK && buf->st_nlink == 0 && file->vnode != RT_NULL)
+        {
+            buf->st_nlink = file->vnode->nlink;
+        }
     }
 
     return ret;
@@ -655,10 +658,17 @@ int fcntl(int fildes, int cmd, ...)
         arg = va_arg(ap, void *);
         va_end(ap);
 
-        ret = dfs_file_ioctl(file, cmd, arg);
-        if (ret < 0)
+        if (cmd == F_GETLK || cmd == F_SETLK || cmd == F_SETLKW)
         {
             ret = dfs_file_fcntl(fildes, cmd, (unsigned long)arg);
+        }
+        else
+        {
+            ret = dfs_file_ioctl(file, cmd, arg);
+            if (ret < 0)
+            {
+                ret = dfs_file_fcntl(fildes, cmd, (unsigned long)arg);
+            }
         }
     }
     else
@@ -675,6 +685,32 @@ int fcntl(int fildes, int cmd, ...)
     return ret;
 }
 RTM_EXPORT(fcntl);
+
+int fchmod(int fildes, mode_t mode)
+{
+    int ret;
+    struct dfs_attr attr = { 0 };
+    struct dfs_file *file;
+
+    file = fd_get(fildes);
+    if (file == RT_NULL)
+    {
+        rt_set_errno(-EBADF);
+        return -1;
+    }
+
+    attr.st_mode = mode;
+    attr.ia_valid = ATTR_MODE_SET;
+    ret = dfs_file_fsetattr(file, &attr);
+    if (ret < 0)
+    {
+        rt_set_errno(ret);
+        return -1;
+    }
+
+    return 0;
+}
+RTM_EXPORT(fchmod);
 
 /**
  * this function is a POSIX compliant version, which shall perform a variety of
@@ -892,7 +928,9 @@ int rmdir(const char *pathname)
         {
             dirent = readdir(dir);
             if (dirent == RT_NULL)
+            {
                 break;
+            }
             if (rt_strcmp(".", dirent->d_name) != 0 &&
                 rt_strcmp("..", dirent->d_name) != 0)
             {
@@ -964,7 +1002,7 @@ DIR *opendir(const char *name)
     if (result >= 0)
     {
         /* open successfully */
-        t = (DIR *) rt_malloc(sizeof(DIR));
+        t = (DIR *)rt_malloc(sizeof(DIR));
         if (t == NULL)
         {
             dfs_file_close(file);
@@ -1109,10 +1147,12 @@ void seekdir(DIR *d, long offset)
         {
             /* seek to the offset position of directory */
             if (dfs_file_lseek(fd_get(d->fd), 0, SEEK_SET) >= 0)
+            {
                 d->num = d->cur = 0;
+            }
         }
 
-        while(file->fpos < offset)
+        while (file->fpos < offset)
         {
             if (!readdir(d))
             {
@@ -1135,7 +1175,9 @@ void rewinddir(DIR *d)
     {
         /* seek to the beginning of directory */
         if (dfs_file_lseek(fd_get(d->fd), 0, SEEK_SET) >= 0)
+        {
             d->num = d->cur = 0;
+        }
     }
 }
 RTM_EXPORT(rewinddir);
@@ -1335,7 +1377,7 @@ void setcwd(char *buf)
     rt_kprintf(NO_WORKING_DIR);
 #endif
 
-    return ;
+    return;
 }
 RTM_EXPORT(setcwd);
 
