2bb31e568ad11f0f631406c7c6afba2a2650057b
CYFS <2805686936@qq.com>
2026-09-10 17:25:36 +0800

    [kernel] Bump version to v5.3.1

--- include/rtdef.h
+++ include/rtdef.h
@@ -61,6 +61,7 @@
  *                             Separate the compiler portings to rtcompiler.h
  * 2024-03-30     Meco Man     update version number to v5.2.0
  * 2025-11-10     Rbb666       update version number to v5.3.0
+ * 2026-09-10     CYFS         update version number to v5.3.1
  */
 
 #ifndef __RT_DEF_H__
@@ -84,7 +85,7 @@ extern "C" {
 /* RT-Thread version information */
 #define RT_VERSION_MAJOR                5               /**< Major version number (X.x.x) */
 #define RT_VERSION_MINOR                3               /**< Minor version number (x.X.x) */
-#define RT_VERSION_PATCH                0               /**< Patch version number (x.x.X) */
+#define RT_VERSION_PATCH                1               /**< Patch version number (x.x.X) */
 
 /* e.g. #if (RTTHREAD_VERSION >= RT_VERSION_CHECK(4, 1, 0) */
 #define RT_VERSION_CHECK(major, minor, revise)          ((major * 10000U) + (minor * 100U) + revise)
